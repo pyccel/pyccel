6cd6bc378dc8bce34a79024f853fc7231277fc37
ratnania <ahr@ipp.mpg.de>
2017-08-16 11:26:09 +0200

    start grammar

---
+++ pyccel/parser.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+
+import os
+from sympy import Symbol, sympify
+
+from vale.utilities import grad, d_var, inner, outer, cross, dot
+from pyccel.syntax import (Pyccel, \
+                           Expression, Term, Operand, \
+                           FactorSigned, FactorUnary, FactorBinary, \
+                           Real)
+
+from textx.metamodel import metamodel_from_str
+
+__all__ = ["PyccelParser", "ast_to_dict", "get_by_name"]
+
+# ...
+def get_by_name(ast, name):
+    """
+    Returns an object from the AST by giving its name.
+    """
+    for token in ast.declarations:
+        if token.name == name:
+            return token
+    return None
+# ...
+
+# ...
+def ast_to_dict(ast):
+    """
+    Returns an object from the AST by giving its name.
+    """
+    tokens = {}
+    for token in ast.declarations:
+        tokens[token.name] = token
+    return tokens
+# ...
+
+class Parser(object):
+    """ Class for a Parser using TextX.
+
+    A parser can be created from a grammar (str) or a filename. It is preferable
+    to specify the list classes to have more control over the abstract grammar;
+    for example, to use a namespace, and to do some specific anotation.
+
+    >>> parser = Parser(filename="gammar.tx")
+
+    Once the parser is created, you can parse a given set of instructions by
+    calling
+
+    >>> parser.parse(["Field(V) :: u"])
+
+    or by providing a file to parse
+
+    >>> parser.parse_from_file("tests/inputs/1d/poisson.vl")
+    """
+    def __init__(self, grammar=None, filename=None, \
+                 classes=None):
+        """Parser constructor.
+
+        grammar : str
+            abstract grammar describing the DSL.
+
+        filename: str
+            name of the file containing the abstract grammar.
+
+        classes : list
+            a list of Python classes to be used to describe the grammar. Take a
+            look at TextX documentation for more details.
+        """
+
+        _grammar = grammar
+
+        # ... read the grammar from a file
+        if not (filename is None):
+            dir_path = os.path.dirname(os.path.realpath(__file__))
+            filename = os.path.join(dir_path, filename)
+
+            f = open(filename)
+            _grammar = f.read()
+            _grammar.replace("\n", "")
+            f.close()
+        # ...
+
+        # ...
+        self.grammar = _grammar
+        # ...
+
+        # ...
+        if classes is None:
+            self.model = metamodel_from_str(_grammar)
+        else:
+            self.model = metamodel_from_str(_grammar, classes=classes)
+        # ...
+
+    def parse(self, instructions):
+        """Parse a set of instructions with respect to the grammar.
+
+        instructions: list
+            list of instructions to parse.
+        """
+        # ... parse the DSL code
+        return self.model.model_from_str(instructions)
+        # ...
+
+    def parse_from_file(self, filename):
+        """Parse a set of instructions with respect to the grammar.
+
+        filename: str
+            a file containing the instructions to parse.
+        """
+        # ... read a DSL code
+        f = open(filename)
+        instructions = f.read()
+        instructions.replace("\n", "")
+        f.close()
+        # ...
+
+        # ... parse the DSL code
+        return self.parse(instructions)
+        # ...
+
+# User friendly parser
+
+class PyccelParser(Parser):
+    """A Class for Pyccel parser.
+
+    This is an extension of the Parser class. Additional treatment is done for
+    Linear and Bilinear Forms to define their dependencies: user_fields,
+    user_functions and user_constants.
+
+    """
+    def __init__(self, **kwargs):
+        """Pyccel parser constructor.
+
+        It takes the same arguments as the Parser class.
+        """
+        classes = [Pyccel, \
+                   Expression, Term, Operand, \
+                   FactorSigned, FactorUnary, FactorBinary, \
+                   Real \
+                   ]
+
+        try:
+            filename = kwargs["filename"]
+        except:
+            filename = "grammar.tx"
+
+        super(PyccelParser, self).__init__(filename = filename, \
+                                         classes=classes)
+
+    def parse_from_file(self, filename):
+        """Parse a set of instructions with respect to the grammar and returns
+        the AST.
+
+        filename: str
+            a file containing the instructions to parse.
+        """
+        ast = super(PyccelParser, self).parse_from_file(filename)
+
+        return ast
---
+++ pyccel/syntax.py
@@ -0,0 +1,196 @@
+# coding: utf-8
+
+from sympy import Symbol, sympify
+
+DEBUG = False
+#DEBUG = True
+
+__all__ = ["Pyccel", \
+           "Expression", "Term", "Operand", \
+           "FactorSigned", "FactorUnary", "FactorBinary", \
+           "Real" \
+           ]
+
+
+# Global variable namespace
+namespace = {}
+stack     = {}
+settings  = {}
+
+operators = {}
+
+class Pyccel(object):
+    """Class for Pyccel syntax."""
+    def __init__(self, **kwargs):
+        """
+        Constructor for Pyccel.
+
+        """
+        self.declarations = kwargs.pop('declarations')
+
+class Real(object):
+    """Class representing a Real number."""
+    def __init__(self, **kwargs):
+        """
+        A Real number is defined by
+
+        * name
+
+        .. note::
+            The grammar rule to define a Real is
+
+            Real:
+            "Real" DEF name=ID
+            ;
+        """
+        self.name  = kwargs.pop('name')
+
+        namespace[self.name] = self
+
+    @property
+    def expr(self):
+        return Symbol(self.name)
+
+class ExpressionElement(object):
+    """Class representing an element of an expression."""
+    def __init__(self, **kwargs):
+
+        # textX will pass in parent attribute used for parent-child
+        # relationships. We can use it if we want to.
+        self.parent = kwargs.get('parent', None)
+
+        # We have 'op' attribute in all grammar rules
+        self.op = kwargs['op']
+
+        super(ExpressionElement, self).__init__()
+
+
+class FactorSigned(ExpressionElement):
+    """Class representing a signed factor."""
+    def __init__(self, **kwargs):
+        self.sign = kwargs.pop('sign', '+')
+        super(FactorSigned, self).__init__(**kwargs)
+
+    @property
+    def expr(self):
+        if DEBUG:
+            print "> FactorSigned "
+        expr = self.op.expr
+        return -expr if self.sign == '-' else expr
+
+class FactorUnary(ExpressionElement):
+    """Class representing a unary factor."""
+    def __init__(self, **kwargs):
+        # name of the unary operator
+        self.name = kwargs['name']
+
+        super(FactorUnary, self).__init__(**kwargs)
+
+    @property
+    def expr(self):
+        if DEBUG:
+            print "> FactorUnary "
+        expr = self.op.expr
+        return expr
+
+
+class FactorBinary(ExpressionElement):
+    def __init__(self, **kwargs):
+        # name of the unary operator
+        self.name = kwargs['name']
+
+        super(FactorBinary, self).__init__(**kwargs)
+
+    @property
+    def expr(self):
+        if DEBUG:
+            print "> FactorBinary "
+#        print self.op
+
+        expr_l = self.op[0].expr
+        expr_r = self.op[1].expr
+
+        if self.name == "dot":
+            return dot(expr_l, expr_r)
+        elif self.name == "inner":
+            return inner(expr_l, expr_r)
+        elif self.name == "outer":
+            return outer(expr_l, expr_r)
+        elif self.name == "cross":
+            return cross(expr_l, expr_r)
+        else:
+            raise Exception('Unknown variable "{}" at position {}'
+                            .format(op, self._tx_position))
+
+
+class Term(ExpressionElement):
+    @property
+    def expr(self):
+        if DEBUG:
+            print "> Term "
+        ret = self.op[0].expr
+        for operation, operand in zip(self.op[1::2], self.op[2::2]):
+            if operation == '*':
+                ret *= sympify(operand.expr)
+            else:
+                ret /= sympify(operand.expr)
+        return ret
+
+
+class Expression(ExpressionElement):
+    @property
+    def expr(self):
+        if DEBUG:
+            print "> Expression "
+        ret = self.op[0].expr
+        for operation, operand in zip(self.op[1::2], self.op[2::2]):
+            if operation == '+':
+                ret += sympify(operand.expr)
+            else:
+                ret -= sympify(operand.expr)
+        return ret
+
+
+class Operand(ExpressionElement):
+    @property
+    def expr(self):
+        if DEBUG:
+            print "> Operand "
+            print "> stack : ", stack
+            print self.op
+#        op = self.op[0]
+        op = self.op
+        if type(op) in {int, float}:
+            return op
+        elif type(op) == list:
+            # op is a list
+            for O in op:
+                if O in namespace:
+                    # TODO use isinstance
+                    if type(namespace[O]) in [Real]:
+                        return namespace[O].expr
+                    else:
+                        return namespace[O]
+                elif O in stack:
+                    if DEBUG:
+                        print ">>> found local variables: " + O
+                    return Symbol(O)
+                else:
+                    raise Exception('Unknown variable "{}" at position {}'
+                                    .format(O, self._tx_position))
+        elif isinstance(op, ExpressionElement):
+            return op.expr
+        elif op in stack:
+            if DEBUG:
+                print ">>> found local variables: " + op
+            return Symbol(op)
+        elif op in namespace:
+            # TODO use isinstance
+            if type(namespace[op]) in [Real]:
+                return namespace[op].expr
+            else:
+                return namespace[op]
+        else:
+            raise Exception('Unknown variable "{}" at position {}'
+                            .format(op, self._tx_position))
+
