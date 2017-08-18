8b944c6a2bbf0082d7c6825e63fcc8cac24a8768
ratnania <ahr@ipp.mpg.de>
2017-08-18 14:09:28 +0200

    add test codegen

---
+++ tests/test_codegen.py
@@ -0,0 +1,57 @@
+# coding: utf-8
+
+"""
+"""
+
+# ...
+def test_Assign():
+    from test_parser import test_Assign as test
+    ast = test()
+# ...
+
+# ...
+def test_Declare():
+    from test_parser import test_Declare as test
+    ast = test()
+# ...
+
+# ...
+def test_Del():
+    from test_parser import test_Del as test
+    ast = test()
+# ...
+
+# ...
+def test_Flow():
+    from test_parser import test_Flow as test
+    ast = test()
+# ...
+
+# ...
+def test_For():
+    from test_parser import test_For as test
+    ast = test()
+# ...
+
+# ...
+def test_If():
+    from test_parser import test_If as test
+    ast = test()
+# ...
+
+# ...
+def test_Pass():
+    from test_parser import test_Pass as test
+    ast = test()
+# ...
+
+
+######################################
+if __name__ == "__main__":
+    test_Assign()
+#    test_Declare()
+#    test_Del()
+#    test_Flow()
+#    test_For()
+#    test_If()
+#    test_Pass()
---
+++ tests/test_parser.py
@@ -0,0 +1,182 @@
+# coding: utf-8
+
+"""
+.. todo:
+    - no need to declare a variable, if it is defined by assignment. ex: 'x=1'
+    means that x is of type real. this must be done automatically.
+"""
+
+from pyccel.parser  import PyccelParser, get_by_name
+from pyccel.syntax import ( \
+                           # statements
+                           DeclarationStmt, \
+                           DelStmt, \
+                           PassStmt, \
+                           AssignStmt, \
+                           IfStmt, ForStmt)
+
+# ... creates an instance of Pyccel parser
+pyccel = PyccelParser()
+# ...
+
+# ...
+def test_Assign(verbose=False):
+    # ... parse the Pyccel code
+    stmts  = ""
+    stmts += "real  a,b" + "\n"
+    stmts += "x=1"       + "\n"
+    stmts += "y=2*3+1"   + "\n"
+    stmts += "x=a"       + "\n"
+    stmts += "y=2*a+b"   + "\n"
+
+    ast = pyccel.parse(stmts)
+
+    if verbose:
+        for stmt in ast.statements:
+            if isinstance(stmt, DeclarationStmt):
+                print "declared variable : ", stmt.variables
+            if isinstance(stmt, AssignStmt):
+                print "lhs : ", stmt.lhs, "     rhs: ", stmt.rhs.expr
+    # ...
+
+    return ast
+# ...
+
+# ...
+def test_Declare(verbose=False):
+    # ... parse the Pyccel code
+    stmts  = ""
+    stmts += "real x,y" + "\n"
+    stmts += "real z"   + "\n"
+    stmts += "int  n"   + "\n"
+
+    ast = pyccel.parse(stmts)
+
+    if verbose:
+        for stmt in ast.statements:
+            if isinstance(stmt, DeclarationStmt):
+                print stmt.variables
+    # ...
+
+    return ast
+# ...
+
+# ...
+def test_Del(verbose=False):
+    # ... parse the Pyccel code
+    stmts  = ""
+    stmts += "real b" + "\n"
+    stmts += "del b"   + "\n"
+
+    ast = pyccel.parse(stmts)
+
+    if verbose:
+        for stmt in ast.statements:
+            print stmt.expr
+    # ...
+
+    return ast
+# ...
+
+# ...
+def test_Flow(verbose=False):
+    # ... parse the Pyccel code
+    stmts  = ""
+    stmts += "return"   + "\n"
+    stmts += "raise"    + "\n"
+    stmts += "break"    + "\n"
+    stmts += "continue" + "\n"
+
+    ast = pyccel.parse(stmts)
+
+    if verbose:
+        for stmt in ast.statements:
+            print type(stmt)
+    # ...
+
+    return ast
+# ...
+
+# ...
+def test_For(verbose=False):
+    # ... parse the Pyccel code
+    stmts  = ""
+    stmts += "real  x" + "\n"
+    stmts += "for i in range(0,10):" + "\n"
+    stmts += "x=1;"                  + "\n"
+    stmts += "x=x+1"                 + "\n"
+    stmts += "end"                   + "\n"
+
+#    stmts += "for j in range(a,b):" + "\n"
+#    stmts += "x=1"                + "\n"
+#    stmts += "end"                   + "\n"
+
+    ast = pyccel.parse(stmts)
+
+    if verbose:
+        for stmt in ast.statements:
+            if isinstance(stmt, DeclarationStmt):
+                print "declared variable : ", stmt.variables
+            if isinstance(stmt, ForStmt):
+                print stmt.expr
+    # ...
+
+    return ast
+# ...
+
+# ... TODO for the moment, intructions must be splitted by ";"
+#          => \n is not recognized
+def test_If(verbose=False):
+    # ... parse the Pyccel code
+    stmts  = ""
+    stmts += "real  a,b" + "\n"
+    stmts += "if a==1:"  + "\n"
+    stmts += "b=a+1;"    + "\n"
+#    stmts += "b=a+1; a=b+a"   + "\n"
+#    stmts += ";"   + "\n"
+    stmts += "a=b*a"     + "\n"
+    stmts += "else:"     + "\n"
+    stmts += "a=b*a"     + "\n"
+    stmts += "end"       + "\n"
+
+    ast = pyccel.parse(stmts)
+
+    if verbose:
+        for stmt in ast.statements:
+            print type(stmt)
+            if isinstance(stmt, DeclarationStmt):
+                print "declared variable : ", stmt.variables
+            if isinstance(stmt, AssignStmt):
+                print "lhs : ", stmt.lhs, "     rhs: ", stmt.rhs.expr
+            if isinstance(stmt, IfStmt):
+                print "body_true : ", stmt.body_true, "     body_false: ", stmt.body_false
+    # ...
+
+    return ast
+# ...
+
+# ...
+def test_Pass(verbose=False):
+    # ... parse the Pyccel code
+    stmts  = ""
+    stmts += "pass" + "\n"
+
+    ast = pyccel.parse(stmts)
+
+    if verbose:
+        for stmt in ast.statements:
+            print type(stmt)
+    # ...
+
+    return ast
+# ...
+
+######################################
+if __name__ == "__main__":
+    test_Assign(verbose=True)
+    test_Declare(verbose=True)
+    test_Del(verbose=True)
+    test_Flow(verbose=True)
+    test_For(verbose=True)
+    test_If(verbose=True)
+    test_Pass(verbose=True)
