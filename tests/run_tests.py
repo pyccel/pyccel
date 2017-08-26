9fac996a3799c85c21fccda3e075521d6e1092e6
Ratnani, Ahmed (ahr) <ahmed.ratnani@ipp.mpg.de>
2017-08-26 11:50:23 +0200

    add run_tests to test all working examples

---
+++ tests/run_tests.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+import os
+
+language = "--language=fortran"
+compiler = "--compiler=gfortran"
+execute  = "--execute"
+show     = "--show"
+
+not_working = ["ex7.py"]
+
+examples = []
+for i in range(1, 10 + 1):
+    example = "ex{0}.py".format(str(i))
+    if not(example in not_working):
+        examples.append(example)
+
+for example in examples:
+    print "===== running example {0} =====".format(example)
+    filename = "--filename=examples/{0}".format(example)
+    cmd = "python pyccel_cmd.py {0} {1} {2} {3} {4}".format(filename, language,
+                                                            compiler, execute, show)
+    os.system(cmd)
+
