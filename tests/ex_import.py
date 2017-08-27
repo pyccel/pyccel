ff09e4142c53df2c7b98323a341448faad8a7806
Ratnani, Ahmed (ahr) <ahmed.ratnani@ipp.mpg.de>
2017-08-27 20:28:52 +0200

    import

--- tests/ex_import.py
+++ tests/ex_import.py
@@ -1,2 +1,4 @@
 from numpy import linspace
 from sympy import Symbol
+
+print x
--- tests/test_import.py
+++ tests/test_import.py
@@ -7,22 +7,31 @@ from os.path import join, dirname
 from textx.metamodel import metamodel_from_file
 from textx.export import metamodel_export, model_export
 
+from pyccel.syntax import ImportFromStmt
 
-def main(debug=False):
 
+def find_imports(filename, debug=False):
     this_folder = dirname(__file__)
 
     # Get meta-model from language description
-    meta = metamodel_from_file(join(this_folder, 'import.tx'), debug=debug)
+    grammar = join(this_folder, 'import.tx')
+    meta = metamodel_from_file(grammar, debug=debug, classes=[ImportFromStmt])
 
     # Instantiate model
-    model = meta.model_from_file(join(this_folder, 'ex_import.py'))
+    model = meta.model_from_file(filename)
 
-    print model.statements
+    d = {}
+    for stmt in model.statements:
+        if isinstance(stmt, ImportFromStmt):
+            module = str(stmt.dotted_name.names[0])
+            names  = [str(n) for n in stmt.import_as_names.names]
+            d[module] = names
+
+    return d
 
 if __name__ == '__main__':
-    main()
-
-
-
-
+    f = 'ex_import.py'
+    f = 'ex6.py'
+    this_folder = dirname(__file__)
+    imports = find_imports(filename=join(this_folder, f))
+    print imports
