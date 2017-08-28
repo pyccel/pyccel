283949a38ca1d407cd991b9b49477439d108f945
Ratnani, Ahmed (ahr) <ahmed.ratnani@ipp.mpg.de>
2017-08-28 03:10:43 +0200

    add incr test

---
+++ tests/incr.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+def incr(x):
+    x = int()
+    y = int()
+    y = x + 1
+    print("> incr (", x, ") = ", y)
+
+def decr(x):
+    x = int()
+    z = int()
+    z = x - 1
+    print("> decr (", x, ") = ", z)
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -8,6 +8,14 @@ def test_helloworld():
     module.helloworld()
     # ...
 
+def test_incr():
+    # ...
+    module = load_module(filename="incr.py")
+    module.incr(3)
+    module.decr(4)
+    # ...
+
 ###################################
 if __name__ == '__main__':
-    test_helloworld()
+#    test_helloworld()
+    test_incr()
