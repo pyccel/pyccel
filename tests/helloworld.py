c11ab505b600df9bdc8bbbb4bb5e01ba00abc546
Ratnani, Ahmed (ahr) <ahmed.ratnani@ipp.mpg.de>
2017-08-28 02:54:26 +0200

    load_module

---
+++ tests/helloworld.py
@@ -0,0 +1,4 @@
+# coding: utf-8
+
+def helloworld():
+    print("> Hello World!!")
--- tests/test_modules.py
+++ tests/test_modules.py
@@ -1,29 +1,13 @@
 # coding: utf-8
 
-"""
-.. todo:
-    - no need to declare a variable, if it is defined by assignment. ex: 'x=1'
-    means that x is of type double. this must be done automatically.
-"""
+from pyccel.codegen import load_module
 
-import sys
-import os
+def test_helloworld():
+    # ...
+    module = load_module(filename="helloworld.py")
+    module.helloworld()
+    # ...
 
-from pyccel.codegen import build_file
-
-# ...
-build_file(filename="core.py", language="fortran", compiler="gfortran", \
-           execute=False, accelerator=None, \
-           debug=False, verbose=True, show=False, inline=True, name="core")
-# ...
-
-# ...
-try:
-    import external
-    reload(external)
-except:
-    pass
-import external
-g = external.core.g
-print g(2,3)
-# ...
+###################################
+if __name__ == '__main__':
+    test_helloworld()
