0822db702eccbdbbf87bbca7179f78217af49298
Ratnani, Ahmed (ahr) <ahmed.ratnani@ipp.mpg.de>
2017-08-31 22:38:00 +0200

    add loop constructor

--- pyccel/openmp/test.py
+++ pyccel/openmp/test.py
@@ -3,3 +3,12 @@
 #@ omp parallel default(private)
 #@ omp parallel private(x,y)
 #@ omp parallel proc_bind(master)
+
+#@ omp do private(x)
+#@ omp do firstprivate(y)
+#@ omp do lastprivate(z)
+#@ omp do reduction(+: x,y)
+#@ omp do collapse(4)
+#@ omp do ordered
+#@ omp do ordered(4)
+#@ omp do schedule(dynamic)
