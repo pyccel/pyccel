9ef9a795d95ff471716550aeae9478cb56e73d17
Ratnani, Ahmed (ahr) <ahmed.ratnani@ipp.mpg.de>
2017-08-31 11:22:16 +0200

    cleaning + found bug in check_type. TODO fix it

--- tests/examples/ex6.py
+++ tests/examples/ex6.py
@@ -4,47 +4,37 @@
 #from numpy import linspace
 
 
-#a = zeros(shape=64, dtype=float)
-#b = zeros(shape=8, dtype=int)
-#
-#a[1] = 1.0
-#a[2] = 1.0
-#a[3] = 1.0
-#
+a = zeros(shape=64, dtype=float)
+b = zeros(shape=8, dtype=int)
+
+a[1] = 1.0
+a[2] = 1.0
+a[3] = 1.0
+
 #c = a[1]
-#
+
 #d = c + 5.3 * a[1+1] + 4.0 - a[3]
 #print(d)
 #
-##e = a # not working. e must be declared as an array
+#e = zeros(shape=(2,8), dtype=float)
+#e[1,1] = 1
 #
-#x = zeros(shape=(2,8), dtype=float)
-#x[1,1] = 1
-#
-#y = x[0,2]
-#print(y)
-#
-#n = int()
-#n = 2
-#m = int()
-#m = 3
-#x = zeros(shape=(n,m,2), dtype=float)
-#
-#for i in range(0, n):
-#    for j in range(0, m):
-#        x[i,j,0] = i-j
-#        x[i,j,1] = i+j
-#
-#print(x)
-
+#f = e[0,2]
+#print(f)
 
 n = int()
 n = 2
 m = int()
 m = 3
-x = zeros(shape=(n,m), dtype=float)
-y = zeros(shape=(n),   dtype=float)
+x = zeros(shape=(n,m,2), dtype=float)
 
-y[:2] = x[:2,0] + 1
+for i in range(0, n):
+    for j in range(0, m):
+        x[i,j,0] = i-j
+        x[i,j,1] = i+j
+print(x)
+
+y = zeros(shape=(n), dtype=float)
+y[:2] = x[:2,0,0] + 1
 print(y)
 
