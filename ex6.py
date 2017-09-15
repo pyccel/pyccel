9f46a2113d89e1eb8d19102120798bb0ce6d41c8
ratnania <ahr@ipp.mpg.de>
2017-09-15 14:22:33 +0200

    start new ex

--- ex6.py
+++ ex6.py
@@ -1,24 +1,27 @@
 # coding: utf-8
 
 
-#$ header fd(double [:])
-def fd(x):
-    n = len(x)
-    z = zeros(n, double)
-    z = x+1
-    y = 2 * z
+##$ header fd(double [:])
+#def fd(x):
+#    n = len(x)
+#    z = zeros(n, double)
+#    z = x+1
+#    y = 2 * z
+#    return y
+
+#$ header gd(double [:])
+def gd(x):
+    y = 2 * x + 1
     return y
 
-##$ header gd(double [:])
-#def gd(x):
-#    y = 2 * x + 1
-#    return y
+#xd = ones(6, double)
+#yd = zeros(6, double)
+#yd = 2.0 * fd(xd) + 1.0
+#print(yd)
+#yd = 3.0 * fd(2.0 * fd(xd) + 1.0) + 1.0
+#print(yd)
 
 xd = ones(6, double)
 yd = zeros(6, double)
-yd = 2.0 * fd(xd) + 1.0
+yd = gd(xd)
 print(yd)
-yd = 3.0 * fd(2.0 * fd(xd) + 1.0) + 1.0
-print(yd)
-#yd = gd(xd)
-#print(yd)
