33723c78436991a8efe5c537e61e55fd8d470f14
Ratnani, Ahmed (ahr) <ratnaniahmed@gmail.com>
2017-09-22 12:29:12 +0200

    interpolator 2d in progress

--- tests/examples/projectors/projectors_2d.py
+++ tests/examples/projectors/projectors_2d.py
@@ -52,8 +52,20 @@ def make_greville(knots, n, p):
         greville[i] = s / p
     return greville
 
-#$ header integrate_element_1d(double [:], double [:], double, double, int)
-def integrate_element_1d(us, ws, x_min, x_max, p):
+#$ header func_V_0(double, double)
+def func_V_0(x, y):
+    f = 1.0-x
+    f = x * y * f
+    return f
+
+#$ header func_V_1(double, double)
+def func_V_1(x, y):
+    f1 = 1.0-2.0*x
+    f2 = 1.0-2.0*y
+    return f1, f2
+
+#$ header integrate_edge(int, int, double, double [:], double [:], double, double, int)
+def integrate_edge(component, axis, y, us, ws, x_min, x_max, p):
     r = 0.0
     d = x_max - x_min
     for j in range(0, p+1):
@@ -61,12 +73,29 @@ def integrate_element_1d(us, ws, x_min, x_max, p):
         w = ws[j]
         x = x_min + d * u
         w = 0.5 * d * w
-        f = x * w
-        r = r + f
+        if axis==0:
+            f1, f2 = func_V_1(x, y)
+        else:
+            f1, f2 = func_V_1(y, x)
+        if component == 0:
+            f = f1
+        else:
+            f = f2
+        r = r + f * w
     return r
 
-#$ header integrate_V_1(double [:], double [:], int, int, int, int)
-def integrate_V_1(t_u, t_v, n_u, n_v, p_u, p_v):
+#$ header interpolate_V_0(double [:], double [:], int, int, int, int)
+def interpolate_V_0(t_u, t_v, n_u, n_v, p_u, p_v):
+    n_elements_u = n_u-p_u
+    n_elements_v = n_v-p_v
+    rs = zeros((n_elements_u+1, n_elements_v+1), double)
+    for i_u in range(0, n_elements_u+1):
+        for i_v in range(0, n_elements_v+1):
+            rs[i_u, i_v] = func_V_0(t_u[i_u], t_v[i_v])
+    return rs
+
+#$ header interpolate_V_1(double [:], double [:], int, int, int, int)
+def interpolate_V_1(t_u, t_v, n_u, n_v, p_u, p_v):
     n_elements_u = n_u-p_u
     n_elements_v = n_v-p_v
     us, wus = legendre(p_u)
@@ -75,16 +104,30 @@ def integrate_V_1(t_u, t_v, n_u, n_v, p_u, p_v):
     us = 0.5 * us
     vs = vs + 1.0
     vs = 0.5 * vs
-    rs = zeros((n_elements_u, n_elements_v), double)
+
+    r_0 = zeros((n_elements_u, n_elements_v+1), double)
+    r_1 = zeros((n_elements_u+1, n_elements_v), double)
+
+    component = 0
+    axis      = 0
     for i_u in range(0, n_elements_u):
-        u_min = t_u[i_u]
-        u_max = t_u[i_u+1]
+        x_min = t_u[i_u]
+        x_max = t_u[i_u+1]
+        for i_v in range(0, n_elements_v+1):
+            y = t_v[i_v]
+            r = integrate_edge(component, axis, y, us, wus, x_min, x_max, p_u)
+            r_0[i_u, i_v] = r
+
+    component = 1
+    axis      = 1
+    for i_u in range(0, n_elements_u+1):
+        y = t_u[i_u]
         for i_v in range(0, n_elements_v):
-            v_min = t_v[i_v]
-            v_max = t_v[i_v+1]
-#            r = integrate_element_1d(us, ws, x_min, x_max, p)
-#            rs[i] = r
-    return rs
+            x_min = t_v[i_v]
+            x_max = t_v[i_v+1]
+            r = integrate_edge(component, axis, y, vs, wvs, x_min, x_max, p_v)
+            r_1[i_u, i_v] = r
+    return r_0, r_1
 
 n_elements_u = 4
 n_elements_v = 4
