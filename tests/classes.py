6f9ad8549399df9c66a117d84876e8c73db6c932
ratnania <ahr@ipp.mpg.de>
2017-09-28 10:37:47 +0200

    add example class def

---
+++ tests/classes.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+#$ header class Matrix(public)
+#$ header method __init__(Matrix, int, int)
+#$ header method __del__(Matrix)
+
+class Matrix(object):
+    def __init__(self, n_rows, n_cols):
+        self.n_rows = n_rows
+        self.n_cols = n_cols
+        self.a = zeros((n_rows, n_cols), double)
+
+    def __del__(self):
+        del self.a
+
