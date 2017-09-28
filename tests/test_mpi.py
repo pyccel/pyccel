697de969f0af7b25331b25ad23c9e6bf8c21d891
ratnania <ahr@ipp.mpg.de>
2017-09-28 18:12:34 +0200

    mpi

--- tests/test_mpi.py
+++ tests/test_mpi.py
@@ -6,4 +6,13 @@ mpi_init     = eval('mpi_init')
 mpi_finalize = eval('mpi_finalize')
 
 ierr = mpi_init()
+
+comm = mpi_comm_world
+print("mpi_comm = ", comm)
+
+#$ header procedure mpi_comm_size(int) results(int, int)
+mpi_comm_size = eval('mpi_comm_size')
+size, ierr = mpi_comm_size(comm)
+print("mpi_size = ", size)
+
 ierr = mpi_finalize()
