097d0701ba8f9293d58dc131f707af295d74ac14
ratnania <ahr@ipp.mpg.de>
2017-10-01 10:39:31 +0200

    add test parallel

--- pyccel/__init__.py
+++ pyccel/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: UTF-8 -*-
 __version__ = "0.1"
 
+from .ast        import *
 from .types      import *
+from .parallel   import *
 from .printers   import *
 from .core       import *
 from .imports    import *
