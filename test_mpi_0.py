fea84117e41d04da4434b13850b3633b3cf07f44
Ratnani, Ahmed (ahr) <ratnaniahmed@gmail.com>
2017-10-05 18:13:56 +0200

    mpi send/recv example

--- test_mpi_0.py
+++ test_mpi_0.py
@@ -8,9 +8,11 @@ rank = comm.rank
 
 n = 4
 x = zeros(n, double)
+y = zeros((3,2), double)
 
 if rank == 0:
     x = 1.0
+    y = 1.0
 
 source = 0
 dest   = 1
@@ -32,5 +34,36 @@ if rank == source:
 if rank == dest:
     ierr = comm.recv(x[1], source, tag1)
     print("processor ", rank, " got  x(1) = ", x[1])
+
+
+tagx = 4321
+if rank == source:
+    ierr = comm.send(y, dest, tagx)
+    print("processor ", rank, " sent ", y)
+
+if rank == dest:
+    ierr = comm.recv(y, source, tagx)
+    print("processor ", rank, " got  ", y)
+
+tag1 = 8765
+if rank == source:
+    y[1,1] = 2.0
+    ierr = comm.send(y[1,1], dest, tag1)
+    print("processor ", rank, " sent y(1,1) = ", y[1,1])
+
+if rank == dest:
+    ierr = comm.recv(y[1,1], source, tag1)
+    print("processor ", rank, " got  y(1,1) = ", y[1,1])
+
+tag1 = 6587
+if rank == source:
+    y[1,:] = 2.0
+    ierr = comm.send(y[1,:], dest, tag1)
+    print("processor ", rank, " sent y(1,:) = ", y[1,:])
+
+if rank == dest:
+    ierr = comm.recv(y[1,:], source, tag1)
+    print("processor ", rank, " got  y(1,:) = ", y[1,:])
+
 
 ierr = mpi_finalize()
