565be5b6b83578f9ab3acc0d09e5fb4610770326
ratnania <ahr@ipp.mpg.de>
2017-10-18 13:27:39 +0200

    poisson fd with mpi_tensor in progress

--- ex_mpi.py
+++ ex_mpi.py
@@ -14,19 +14,37 @@ hx = 1.0/(ntx+1)
 hy = 1.0/(nty+1)
 
 
-npts    = (4,32)
+npts    = (32,32)
 periods = (False,False)
 reorder = False
 pads    = (1,1)
 
 mesh = MPI_Tensor(npts, periods, reorder, pads)
 
+starts = mesh.starts
+ends   = mesh.ends
+
+print ("starts = ", starts, " ends   = ", ends)
+
+rx = range(starts[0], ends[0])
+ry = range(starts[1], ends[1])
+
+tensor = tensor(rx, ry)
+
+u       = zeros(tensor, double)
+u_new   = zeros(tensor, double)
+u_exact = zeros(tensor, double)
+f       = zeros(tensor, double)
+
+#Initialization
 x = 0.0
 y = 0.0
 for i,j in mesh:
     x = i*hx
     y = j*hy
 
+    f[i, j] = 2.0*(x*x-x+y*y-y)
+    u_exact[i, j] = x*y*(x-1.0)*(y-1.0)
 del mesh
 
 ierr = mpi_finalize()
