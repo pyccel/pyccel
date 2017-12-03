42a739accfb2e2a0b7eafa23edbbcc0be3daa9e3
ratnania <ratnaniahmed@gmail.com>
2017-12-03 16:49:02 +0100

    add new tests for travis

--- tests/test_pyccel.py
+++ tests/test_pyccel.py
@@ -1,10 +1,39 @@
 # coding: utf-8
+
+import os
+
 from pyccel.commands.console import pyccel
 
-ignored = [11, 15, 18, 20, 21]
+# ...
+def test_1():
+    ignored = [11, 15, 18, 20, 21]
 
-for i in range(1, 21):
-    filename = 'tests/examples/ex{0}.py'.format(str(i))
-    if not(i in ignored):
-        pyccel(files=[filename])
-        print(' testing {0}: done'.format(str(i)))
+    for i in range(1, 21):
+        filename = 'tests/examples/ex{0}.py'.format(str(i))
+        if not(i in ignored):
+            pyccel(files=[filename])
+            print(' testing {0}: done'.format(str(i)))
+# ...
+
+# ...
+def test_2():
+    ignored = ['classes.py', 'eval.py', 'parallel.py', 'mpi.py',
+              'arrays.py', 'modules.py', 'imports.py']
+
+    base_dir = os.getcwd()
+    path_dir = os.path.join(base_dir, 'tests/scripts')
+
+    files = os.listdir(path_dir)
+    files = [f for f in files if not(f in ignored) and (f.endswith(".py"))]
+
+    os.chdir(path_dir)
+    for f in files:
+        pyccel(files=[f])
+        print(' testing {0}: done'.format(str(f)))
+    os.chdir(base_dir)
+# ...
+
+################################
+if __name__ == '__main__':
+    test_1()
+    test_2()
