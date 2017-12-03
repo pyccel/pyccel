62e9b8daf727e300c608824da4b791398df93b53
ratnania <ratnaniahmed@gmail.com>
2017-12-03 16:52:56 +0100

    cleaning

--- tests/test_pyccel.py
+++ tests/test_pyccel.py
@@ -9,7 +9,7 @@ def test_1():
     ignored = [11, 15, 18, 20, 21]
 
     for i in range(1, 21):
-        filename = 'tests/examples/ex{0}.py'.format(str(i))
+        filename = 'tests/scripts/core/ex{0}.py'.format(str(i))
         if not(i in ignored):
             pyccel(files=[filename])
             print(' testing {0}: done'.format(str(i)))
