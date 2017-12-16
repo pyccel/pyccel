990dc7314d98da5ede01c2464e011cf6aa828c7a
ratnania <ratnaniahmed@gmail.com>
2017-12-16 16:18:07 +0100

    add iter example

---
+++ tests/scripts/itertools/ex1.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+#$ header class OpenmpRange(public)
+#$ header method __init__(OpenmpRange, int, int, int, int, int, int)
+#$ header method __del__(OpenmpRange)
+#$ header method __next__(OpenmpRange)
+#$ header method __iter__(OpenmpRange)
+
+class OpenmpRange(object):
+
+    def __init__(self, start, stop, step, schedule, chunksize, num_threads):
+        self.start = start
+        self.stop  = stop
+        self.step  = step
+
+    def __del__(self):
+        print('> free')
+
+    def __next__(self):
+        print('> next')
+
+    def __iter__(self):
+        print('> iter')
+
+p = OpenmpRange(0,3,1,-1,-1,-1)
