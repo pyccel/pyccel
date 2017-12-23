0acc1838b62d78784a67a174104ba6978398e1cd
ratnania <ratnaniahmed@gmail.com>
2017-12-23 19:24:21 +0100

    ignore poisson 2d fd mpi in tests

--- tests/test_pyccel_mpi.py
+++ tests/test_pyccel_mpi.py
@@ -13,7 +13,8 @@ from utils import clean_tests
 
 # ...
 def test_core(n_procs=2):
-    ignored = ['poisson_fd_1d.py']
+    ignored = ['poisson_fd_1d.py',
+               'poisson_fd_2d.py']
 
     base_dir = os.getcwd()
     path_dir = os.path.join(base_dir, 'tests/scripts/mpi/core')
