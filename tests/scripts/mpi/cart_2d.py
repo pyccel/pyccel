14eba2a32ed7dd05ddec36104b8a7e2b2cda8b7d
ratnania <ratnaniahmed@gmail.com>
2017-12-26 11:54:39 +0100

    update example cart_2d

--- tests/scripts/mpi/cart_2d.py
+++ tests/scripts/mpi/cart_2d.py
@@ -9,8 +9,18 @@ ierr = -1
 
 mpi_init(ierr)
 
-p = Cart()
+mesh = Cart()
 
-del p
+#sx = mesh.starts[0]
+#ex = mesh.ends[0]
+#
+#sy = mesh.starts[1]
+#ey = mesh.ends[1]
+
+#r_ext_x = range(sx-1, ex+1+1)
+#r_ext_y = range(sy-1, ey+1+1)
+#mesh_ext = tensor(r_ext_x, r_ext_y)
+
+del mesh
 
 mpi_finalize(ierr)
