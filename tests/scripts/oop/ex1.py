054c71e88a5fb278ffcdac2ce85a59843f5e3ac0
ratnania <ratnaniahmed@gmail.com>
2017-12-26 09:58:24 +0100

    add new tests for oop

---
+++ tests/scripts/oop/ex1.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+#$ header class Point(public)
+#$ header method __init__(Point, double, double)
+#$ header method __del__(Point)
+#$ header method translate(Point, double, double)
+
+class Point(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __del__(self):
+        pass
+
+    def translate(self, a, b):
+        self.x = self.x + a
+        self.y = self.y + b
+
+p = Point (0.0, 0.0)
+
+p.translate(1.0, 2.0)
+
+#print(p.x, p.y)
+#a = p.x
+#print(a)
+
+del p
