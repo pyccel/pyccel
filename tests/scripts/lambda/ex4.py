2fcdc819f977d32b61246554700a0eb0c4cf155a
ratnania <ratnaniahmed@gmail.com>
2017-12-29 19:23:14 +0100

    add new example for lambda

---
+++ tests/scripts/lambda/ex4.py
@@ -0,0 +1,18 @@
+# coding: utf-8
+
+glt_function = load('pyccel.symbolic.gelato', 'glt_function')
+dx           = load('pyccel.symbolic.gelato', 'dx')
+dy           = load('pyccel.symbolic.gelato', 'dy')
+
+laplace = lambda u,v: dx(u)*dx(v) + dy(u)*dy(v)
+a       = lambda u,v: laplace(u,v) + 0.1 * dx(u) * v
+
+ga = glt_function(a)
+g = lambdify(ga)
+
+# glt symbol is supposed to be 'complex' in this example
+# TODO fix it. for the moment the symbol is always 'double'
+y = g(0.1, 0.3)
+
+print('> symbol := ', ga)
+print('> symbol(0.1, 0.3) = ', y)
