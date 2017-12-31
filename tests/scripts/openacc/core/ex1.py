da43f35a2b78838732c031c13ab7902d4b6e95c6
ratnania <ratnaniahmed@gmail.com>
2017-12-31 15:29:13 +0100

    new openacc tests

--- tests/scripts/openacc/core/ex1.py
+++ tests/scripts/openacc/core/ex1.py
@@ -6,11 +6,5 @@ from pyccel.stdlib.parallel.openacc import acc_get_num_devices
 dev_kind = acc_get_device_type()
 dev_num  = acc_get_num_devices(dev_kind)
 
-print('> number of available OpenACC devices :', dev_num)
-print('> type of available OpenACC devices   :', dev_kind)
-
-#$ acc parallel private(x)
-
-x = 1.0
-
-#$ acc end parallel
+print(' number of available OpenACC devices :', dev_num)
+print(' type of available OpenACC devices   :', dev_kind)
