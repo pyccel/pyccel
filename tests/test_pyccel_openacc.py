e9ac9630366a2c898da690ab25cad063071d37f3
ratnania <ratnaniahmed@gmail.com>
2017-12-31 20:38:40 +0100

    update tests

--- tests/test_pyccel_openacc.py
+++ tests/test_pyccel_openacc.py
@@ -26,27 +26,27 @@ def test_core():
         print('> testing {0}: done'.format(str(f)))
 # ...
 
-## ...
-#def test_openacc():
-#    print('============== testing examples ================')
-#    ignored = ['matrix_multiplication.py']
-#
-#    base_dir = os.getcwd()
-#    path_dir = os.path.join(base_dir, 'tests/scripts/openacc')
-#
-#    files = sorted(os.listdir(path_dir))
-#    files = [f for f in files if not(f in ignored) and (f.endswith(".py"))]
-#
-#    for f in files:
-#        f_name = os.path.join(path_dir, f)
-#
-#        pyccel(files=[f_name], openacc=True)
-#        print('> testing {0}: done'.format(str(f)))
-## ...
+# ...
+def test_openacc():
+    print('============== testing examples ================')
+    ignored = []
+
+    base_dir = os.getcwd()
+    path_dir = os.path.join(base_dir, 'tests/scripts/openacc')
+
+    files = sorted(os.listdir(path_dir))
+    files = [f for f in files if not(f in ignored) and (f.endswith(".py"))]
+
+    for f in files:
+        f_name = os.path.join(path_dir, f)
+
+        pyccel(files=[f_name], openacc=True)
+        print('> testing {0}: done'.format(str(f)))
+# ...
 
 ################################
 if __name__ == '__main__':
     clean_tests()
     test_core()
-#    test_openacc()
+    test_openacc()
     clean_tests()
