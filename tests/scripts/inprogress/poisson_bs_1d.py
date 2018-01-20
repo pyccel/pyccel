383c9bc21a875d55b5ebbafc1cdb91eac6dc8bd7
ratnania <ratnaniahmed@gmail.com>
2018-01-20 22:50:32 +0100

    poisson 1d spline

--- tests/scripts/inprogress/poisson_bs_1d.py
+++ tests/scripts/inprogress/poisson_bs_1d.py
@@ -8,34 +8,29 @@
 #    > export LIB_DIR=$PWD/poisson/usr/lib
 
 # Usage:
-#    > pyccel poisson_bs_1d.py --include='$INCLUDE_DIR' --libdir='$LIB_DIR' --libs=poisson --no-modules
+#    > pyccel poisson_bs_1d.py --include='$INCLUDE_DIR' --libdir='$LIB_DIR' --libs=poisson --no-modules --execute
 
 # Cleaning:
 #    > rm -f *.mod *.pyccel *.f90 *.o
 
 
-
-from pyccelext.math.bsplines     import make_knots
-from pyccelext.math.bsplines     import make_greville
 from pyccelext.math.quadratures  import legendre
-from pyccelext.math.external.bsp import spl_eval_splines_ders
+from pyccelext.math.external.bsp import spl_make_open_knots
 from pyccelext.math.external.bsp import spl_compute_spans
+from pyccelext.math.external.bsp import spl_construct_grid_from_knots
+from pyccelext.math.external.bsp import spl_construct_quadrature_grid
+from pyccelext.math.external.bsp import spl_eval_on_grid_splines_ders
 
-# ... TODO import from pyccelext.math.constants
-pi = 3.141592653589793
-# ...
 
 # ...
 n_elements_1 = 4
-
 p1 = 3
-
-n1 = p1 + n_elements_1
-
-k1 = p1+1
 
 # number of derivatives
 d1 = 1
+
+n1 = p1 + n_elements_1
+k1 = p1 + 1
 
 verbose = False
 #verbose = True
@@ -46,57 +41,33 @@ verbose = False
 # ...
 
 # ...
-knots1 = make_knots (n1, p1)
-
-if verbose:
-    print("> knots1 = ", knots1)
+m1 = n1 + p1 + 1
+knots1 = zeros(m1, double)
+# call to spl
+knots1 = spl_make_open_knots (n1, p1)
 # ...
 
 # ... TODO fix args of zeros
 m1 = n_elements_1+1
-
 grid_1 = zeros(m1, double)
 
-for i in range(0, n_elements_1 + 1):
-    grid_1[i] = knots1[i+p1]
-
-
-if verbose:
-    print("> grid_1 = ", grid_1)
-# ...
-
-# ...
-points_1  = zeros((k1, n_elements_1), double)
-weights_1 = zeros((k1, n_elements_1), double)
+# call to spl
+grid_1 = spl_construct_grid_from_knots(p1, n1, n_elements_1, knots1)
 # ...
 
 # ... construct the quadrature points grid
-for i_element in range(0, n_elements_1):
-    a = grid_1[i_element]
-    b = grid_1[i_element+1]
-    half = (b - a)/2.0
+points_1  = zeros((k1, n_elements_1), double)
+weights_1 = zeros((k1, n_elements_1), double)
 
-    for i_point in range(0, k1):
-        points_1 [i_point, i_element] = a + (1.0 + u1[i_point]) * half
-        weights_1[i_point, i_element] = half * w1[i_point]
-# ...
-
-# ...
-if verbose:
-    print("> points_1 = ", points_1)
+# call to spl
+[points_1, weights_1] = spl_construct_quadrature_grid(u1, w1, grid_1)
 # ...
 
 # ...
 basis_1  = zeros((p1+1, d1+1, k1, n_elements_1), double)
-# ...
 
-# ... evaluates B-Splines and their derivatives on the quad grid
-dN1 = zeros((p1+1, d1+1, k1), double)
-for i_element in range(0, n_elements_1):
-    dN1 = 0.0
-    dN1 = spl_eval_splines_ders(p1, n1, d1, p1, knots1, u1)
-    basis_1[:,:,:,i_element] = dN1
-del dN1
+# call to spl
+basis_1 = spl_eval_on_grid_splines_ders(n1, p1, d1, knots1, points_1)
 # ...
 
 # ...
@@ -105,17 +76,9 @@ spans_1 = spl_compute_spans(p1, n1, knots1)
 # ...
 
 # ...
-if verbose:
-    print ('> spans_1 = ', spans_1)
-# ...
-
-# ...
-start_1 = 0
-end_1   = n1-1
-pad_1   = p1
-
-matrix  = stencil(start_1, end_1, pad_1)
-rhs     = vector(start_1-pad_1, end_1+pad_1)
+mass      = zeros((n1,n1), double)
+stiffness = zeros((n1,n1), double)
+rhs       = zeros(n1, double)
 # ...
 
 # ... build matrix
@@ -126,7 +89,8 @@ for ie1 in range(0, n_elements_1):
             i1 = i_span_1 - p1  - 1 + il_1
             j1 = i_span_1 - p1  - 1 + jl_1
 
-            v = 0.0
+            v_m = 0.0
+            v_s = 0.0
             for g1 in range(0, k1):
                 bi_0 = basis_1[il_1, 0, g1, ie1]
                 bi_x = basis_1[il_1, 1, g1, ie1]
@@ -136,138 +100,11 @@ for ie1 in range(0, n_elements_1):
 
                 wvol = weights_1[g1, ie1]
 
-#                v += (bi_0 * bj_0 + bi_x * bj_x) * wvol
-                v += bi_0 * bj_0 * wvol
+                v_m += bi_0 * bj_0 * wvol
+                v_s += bi_x * bj_x * wvol
 
-            matrix[j1 - i1, i1] += v
-# ...
-
-# ... apply dirichlet boundary conditions
-matrix[:,0]    = 0.0
-matrix[:,n1-1] = 0.0
-
-rhs[0]    = 0.0
-rhs[n1-1] = 0.0
-# ...
-
-# ... define matrix-vector product
-#$ header procedure mv(double [:,:], double [:], double [:])
-def mv(mat, x, y):
-    y = 0.0
-    for i in range(start_1, end_1+1):
-        for k in range(-p1, p1+1):
-            j = k+i
-            y[i] = y[i] + mat[k, i] * x[j]
-# ...
-
-# ... build rhs
-for ie1 in range(0, n_elements_1):
-    i_span_1 = spans_1[ie1]
-    for il_1 in range(0, p1+1):
-        i1 = i_span_1 - p1  - 1 + il_1
-
-        v = 0.0
-        for g1 in range(0, k1):
-            bi_0 = basis_1[il_1, 0, g1, ie1]
-            bi_x = basis_1[il_1, 1, g1, ie1]
-
-            x    = points_1[g1, ie1]
-            wvol = weights_1[g1, ie1]
-
-            v += (bi_0 * sin(2*pi*x)) * wvol
-
-        rhs[i1] += v
-# ...
-
-# ...
-x0 = vector(start_1-pad_1, end_1+pad_1)
-y  = vector(start_1-pad_1, end_1+pad_1)
-# ...
-
-# ... CGL performs maxit CG iterations on the linear system Ax = b
-#     starting from x = x0
-# TODO not working yet
-
-#$ header procedure cgl(double [:,:], double [:], double [:], int)
-def cgl(mat, b, x0, maxit):
-    xk = zeros_like(x0)
-    mx = zeros_like(x0)
-    p  = zeros_like(x0)
-    q  = zeros_like(x0)
-    r  = zeros_like(x0)
-
-    xk = x0
-
-    mv(mat, x0, mx)
-    r = b - mx
-    p = r
-    rdr = dot(r,r)
-    for i_iter in range(1, maxit+1):
-        mv(mat, p, q)
-        alpha = rdr / dot (p, q)
-        xk = xk + alpha * p
-        r  = r - alpha * q
-        rdrold = rdr
-        rdr = dot(r, r)
-        beta = rdr / rdrold
-        p = r + beta * p
-
-    x0 = x
-    print ('> cgl-error = ', rdr)
-# ...
-
-# ... CRL performs maxit CG iterations on the linear system Ax = b
-#     where A is a symmetric positive definite matrix, using CG method
-#     starting from x = x0
-
-#$ header procedure crl(double [:,:], double [:], double [:], int)
-def crl(mat, b, x0, maxit):
-    xk = zeros_like(x0)
-    mx = zeros_like(x0)
-    p  = zeros_like(x0)
-    q  = zeros_like(x0)
-    r  = zeros_like(x0)
-    s  = zeros_like(x0)
-
-    xk = x0
-
-    mv(mat, x0, mx)
-    r = b - mx
-    p = r
-    mv(mat, p, q)
-    s = q
-    sdr = dot(s,r)
-    for i_iter in range(1, maxit+1):
-        alpha = sdr / dot (q, q)
-        xk = xk + alpha * p
-        r  = r - alpha * q
-        mv(mat, r, s)
-        sdrold = sdr
-        sdr = dot(s, r)
-        beta = sdr / sdrold
-        p = r + beta * p
-        q = s + beta * q
-
-    x0 = x
-    print ('> crl-error = ', sdr)
-# ...
-
-# ...
-n_maxiter = 100
-
-xn = vector(start_1-pad_1, end_1+pad_1)
-crl(matrix, rhs, xn, n_maxiter)
-
-mv(matrix, xn, x0)
-err = vector(start_1-pad_1, end_1+pad_1)
-err = abs(x0-rhs)
-err_linfty = max(err)
-print ('> error = ', err_linfty)
-# ...
-
-# ...
-if verbose:
-    print("> basis_1 = ", basis_1)
+            mass[i1, j1] += v_m
+            stiffness[i1, j1] += v_s
 # ...
 
 del knots1
@@ -276,3 +113,6 @@ del points_1
 del weights_1
 del basis_1
 del spans_1
+del mass
+del stiffness
+del rhs
