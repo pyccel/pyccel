0d4e26619afb658513d61be0c450ac35cb832b99
saidhadjout <said-ctb@live.fr>
2018-03-01 20:33:48 +0100

    add examples

--- example.py
+++ example.py
@@ -16,19 +16,19 @@ def decr(x):
 x=1
 
 
-#if a==b:
-#    x=y
-#elif a>b:
-#    x=0
-#else:
-#    y=0
-#while a==b:
-#    x=y
-#a==b and (a>e or b<a and a<c )and (b!=y or c>=a)or b<=t
+if a==b:
+    x=y
+elif a>b:
+    x=0
+else:
+    y=0
+while a==b:
+    x=y
+a==b and (a>e or b<a and a<c )and (b!=y or c>=a)or b<=t
 
-#range(0,1)
-#dot(x,y)
-#x.y.dot(x,y)
-#x.r(0,1)
-#for i in range(10):
-
+range(0,1)
+dot(x,y)
+x.y.dot(x,y)
+x.r(0,1)
+for i in range(10):
+    print(i,j)
