d57b73d01e7a56611293af296c6148fd95192fac
ratnania <ratnaniahmed@gmail.com>
2018-03-13 12:57:07 +0100

    [tests] update codegen for functions

--- tests/pyccel/codegen/scripts/functions.py
+++ tests/pyccel/codegen/scripts/functions.py
@@ -19,4 +19,12 @@ def decr_array(x):
     y = x - 1
     return y
 
+#$ header function f1(int, int, int) results(int)
+def f1(x, n=2, m=3):
+    y = x - n*m
+    return y
+
 print('Improve this test')
+y = decr(2)
+y = f1(1)
+
