6f62cb5439ead98bc94f1dc137bcdb5f600705c0
ratnania <ratnaniahmed@gmail.com>
2018-03-14 11:44:00 +0100

    update run_tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -8,6 +8,7 @@ pytest tests/pyccel/parser/test_openacc.py
 pytest tests/pyccel/parser/test_syntax.py
 pytest tests/pyccel/parser/test_semantic.py
 pytest tests/pyccel/codegen/test_codegen.py  
+pytest tests/epyccel/test_epyccel.py  
 
 #python tests/test_pyccel.py --execute
 #python tests/test_pyccel_openmp.py --openmp --execute
