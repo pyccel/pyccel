41d99f9f0891597fa483c457570362374ceae55a
saidctb <said-ctb@live.fr>
2018-03-22 11:42:11 +0100

    fix the test file path

--- tests/semantic/test_semantic.py
+++ tests/semantic/test_semantic.py
@@ -9,7 +9,7 @@ PY3 = sys.version_info[0] == 3
 
 def test_semantic():
     base_dir = os.path.dirname(os.path.realpath(__file__))
-    path_dir = os.path.join(base_dir, 'scripts/semantic')
+    path_dir = os.path.join(base_dir, 'scripts')
 
     files = sorted(os.listdir(path_dir))
     files = [f for f in files if (f.endswith(".py"))]
