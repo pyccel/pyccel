a7c55c67925c64d6ba4f83155a3dd352fa44063c
ratnania <ratnaniahmed@gmail.com>
2018-04-17 22:15:09 +0200

    update tests

--- tests/run_tests_py2.sh
+++ tests/run_tests_py2.sh
@@ -8,5 +8,7 @@ python tests/syntax/test_syntax.py
 python tests/semantic/test_semantic.py
 python tests/codegen/test_codegen.py  
 python tests/blas/test_blas.py  
+python tests/lapack/test_lapack.py  
+python tests/mpi/test_mpi.py  
 
 cd tests/epyccel/ ; python test_epyccel.py ; cd ../.. 
