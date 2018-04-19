cc214a02db18b0310ae79412d61450f185976d86
ratnania <ratnaniahmed@gmail.com>
2018-04-19 09:51:02 +0200

    update tests with openacc runner

--- tests/run_tests_py2.sh
+++ tests/run_tests_py2.sh
@@ -11,5 +11,6 @@ python tests/blas/test_blas.py
 python tests/lapack/test_lapack.py  
 python tests/mpi/test_mpi.py  
 python tests/openmp/test_openmp.py  
+python tests/openacc/test_openacc.py  
 
 cd tests/epyccel/ ; python test_epyccel.py ; cd ../.. 
