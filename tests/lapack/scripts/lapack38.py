8734816d9fa59a49ea0d7a601a100b09be2997fd
ratnania <ratnaniahmed@gmail.com>
2018-04-19 11:03:01 +0200

    fix lapack examples

--- tests/lapack/scripts/lapack38.py
+++ tests/lapack/scripts/lapack38.py
@@ -4,10 +4,7 @@
 #   gfortran test.f90 -lblas -llapack
 #   ./a.out
 
-# TODO: not working yet. needs
-#     - zeros of int
-#     - assert
-#     - fix str in lapack.pyh headers
+# TODO: - assert
 
 from pyccel.stdlib.external.lapack import dgbtrf
 from pyccel.stdlib.external.lapack import dgbtrs
@@ -50,107 +47,107 @@ def test_1():
     dgbtrs('n', n, ml, mu, 1, a, lda, ipiv, b, n, info)
 #    assert(info == 0)
 
-#def test_2():
-#    n = 3
-#    lda = n
-#
-#    a = zeros((lda,n), double)
-#
-#    a[0,0] = 0.0
-#    a[0,1] = 1.0
-#    a[0,2] = 2.0
-#
-#    a[1,0] = 4.0
-#    a[1,1] = 5.0
-#    a[1,2] = 6.0
-#
-#    a[2,0] = 7.0
-#    a[2,1] = 8.0
-#    a[2,2] = 0.0
-#
-#    info = -1
-#    ipiv = zeros(n, int)
-#
-#    dgetrf(n, n, a, lda, ipiv, info)
+def test_2():
+    n = 3
+    lda = n
+
+    a = zeros((lda,n))
+
+    a[0,0] = 0.0
+    a[0,1] = 1.0
+    a[0,2] = 2.0
+
+    a[1,0] = 4.0
+    a[1,1] = 5.0
+    a[1,2] = 6.0
+
+    a[2,0] = 7.0
+    a[2,1] = 8.0
+    a[2,2] = 0.0
+
+    info = -1
+    ipiv = zeros(n, 'int')
+
+    dgetrf(n, n, a, lda, ipiv, info)
 #    assert(info == 0)
-#
-#    iwork = zeros(n, int)
-#    lwork = 4 * n
-#    work  = zeros(lwork, double)
-#
-#    # Get the condition number.
-#    anorm = 1.0
-#    rcond = -1.0
-#    dgecon('I', n, a, lda, anorm, rcond, work, iwork, info)
+
+    iwork = zeros(n, 'int')
+    lwork = 4 * n
+    work  = zeros(lwork)
+
+    # Get the condition number.
+    anorm = 1.0
+    rcond = -1.0
+    dgecon('I', n, a, lda, anorm, rcond, work, iwork, info)
 #    assert(info == 0)
-#
-#def test_3():
-#    n = 3
-#    lda = n
-#
-#    a = zeros((lda,n), double)
-#
-#    a[0,0] = 0.0
-#    a[0,1] = 1.0
-#    a[0,2] = 2.0
-#
-#    a[1,0] = 4.0
-#    a[1,1] = 5.0
-#    a[1,2] = 6.0
-#
-#    a[2,0] = 7.0
-#    a[2,1] = 8.0
-#    a[2,2] = 0.0
-#
-#    info = -1
-#    ipiv = zeros(n, int)
-#
-#    dgetrf(n, n, a, lda, ipiv, info)
+
+def test_3():
+    n = 3
+    lda = n
+
+    a = zeros((lda,n))
+
+    a[0,0] = 0.0
+    a[0,1] = 1.0
+    a[0,2] = 2.0
+
+    a[1,0] = 4.0
+    a[1,1] = 5.0
+    a[1,2] = 6.0
+
+    a[2,0] = 7.0
+    a[2,1] = 8.0
+    a[2,2] = 0.0
+
+    info = -1
+    ipiv = zeros(n, 'int')
+
+    dgetrf(n, n, a, lda, ipiv, info)
 #    assert(info == 0)
-#
-#    iwork = zeros(n, int)
-#    lwork = 4 * n
-#    work  = zeros(lwork, double)
-#
-#    # Compute the inverse matrix.
-#    dgetri(n, a, lda, ipiv, work, lwork, info)
+
+    iwork = zeros(n, 'int')
+    lwork = 4 * n
+    work  = zeros(lwork)
+
+    # Compute the inverse matrix.
+    dgetri(n, a, lda, ipiv, work, lwork, info)
 #    assert(info == 0)
-#
-#def test_4():
-#    n = 3
-#    lda = n
-#
-#    a = zeros((lda,n), double)
-#
-#    a[0,0] = 0.0
-#    a[0,1] = 1.0
-#    a[0,2] = 2.0
-#
-#    a[1,0] = 4.0
-#    a[1,1] = 5.0
-#    a[1,2] = 6.0
-#
-#    a[2,0] = 7.0
-#    a[2,1] = 8.0
-#    a[2,2] = 0.0
-#
-#    info = -1
-#    ipiv = zeros(n, int)
-#
-#    dgetrf(n, n, a, lda, ipiv, info)
+
+def test_4():
+    n = 3
+    lda = n
+
+    a = zeros((lda,n))
+
+    a[0,0] = 0.0
+    a[0,1] = 1.0
+    a[0,2] = 2.0
+
+    a[1,0] = 4.0
+    a[1,1] = 5.0
+    a[1,2] = 6.0
+
+    a[2,0] = 7.0
+    a[2,1] = 8.0
+    a[2,2] = 0.0
+
+    info = -1
+    ipiv = zeros(n, 'int')
+
+    dgetrf(n, n, a, lda, ipiv, info)
 #    assert(info == 0)
-#
-#    # Compute the inverse matrix.
-#    b = zeros(n, double)
-#    b[0] = 14.0
-#    b[1] = 32.0
-#    b[2] = 23.0
-#
-#    # Solve the linear system.
-#    dgetrs('n', n, 1, a, lda, ipiv, b, n, info)
+
+    # Compute the inverse matrix.
+    b = zeros(n)
+    b[0] = 14.0
+    b[1] = 32.0
+    b[2] = 23.0
+
+    # Solve the linear system.
+    dgetrs('n', n, 1, a, lda, ipiv, b, n, info)
 #    assert(info == 0)
-#
-#test_1()
-#test_2()
-#test_3()
-#test_4()
+
+test_1()
+test_2()
+test_3()
+test_4()
