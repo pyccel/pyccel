57d150982abe475ee20d74271f17fd4d64da418f
ratnania <ratnaniahmed@gmail.com>
2018-04-21 14:44:04 +0200

    add tests for preprocess

---
+++ tests/preprocess/test_preprocess.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+# Note that we need to change the directory for tests involving the import
+# statement
+
+from pyccel.parser import Parser
+import os
+
+def test_preprocess():
+    print('*********************************')
+    print('***                           ***')
+    print('***     TESTING preprocess    ***')
+    print('***                           ***')
+    print('*********************************')
+
+    init_dir = os.getcwd()
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    path_dir = os.path.join(base_dir, 'scripts')
+
+    files = sorted(os.listdir(path_dir))
+    files = [f for f in files if (f.endswith(".py"))]
+
+    os.chdir(path_dir)
+    for f in files:
+        print('> testing {0}'.format(str(f)))
+
+        pyccel = Parser(f)
+        print(pyccel.fst)
+
+    os.chdir(init_dir)
+    print('\n')
+
+######################
+if __name__ == '__main__':
+    test_preprocess()
