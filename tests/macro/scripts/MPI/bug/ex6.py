439be9f7278ec00d0de90442602c5ecd9ae5724a
saidhadjout <said-ctb@live.fr>
2018-05-09 10:32:58 +0200

    mpi4py in progress

---
+++ tests/macro/scripts/MPI/bug/ex6.py
@@ -0,0 +1,60 @@
+# coding: utf-8
+
+from pyccel.stdlib.internal.mpi import mpi_init
+from pyccel.stdlib.internal.mpi import mpi_finalize
+from pyccel.stdlib.internal.mpi import mpi_comm_size
+from pyccel.stdlib.internal.mpi import mpi_comm_rank
+from pyccel.stdlib.internal.mpi import mpi_comm_world
+from pyccel.stdlib.internal.mpi import mpi_status_size
+from pyccel.stdlib.internal.mpi import mpi_recv
+from pyccel.stdlib.internal.mpi import mpi_send
+from pyccel.stdlib.internal.mpi import mpi_type_vector
+from pyccel.stdlib.internal.mpi import mpi_type_commit
+from pyccel.stdlib.internal.mpi import mpi_type_free
+from pyccel.stdlib.internal.mpi import MPI_INTEGER
+
+from numpy import zeros
+
+# we need to declare these variables somehow,
+# since we are calling mpi subroutines
+
+size = -1
+rank = -1
+
+
+
+comm = mpi_comm_world
+
+mpi_comm_size (comm, size, ierr)
+mpi_comm_rank (comm, rank, ierr)
+
+nb_lines   = 3
+nb_columns = 4
+tag        = 100
+
+a      = zeros ((nb_lines, nb_columns), 'int')
+
+
+# Initialization of the matrix on each process
+a = 1000 + rank
+
+# Definition of the type_line datatype
+type_line = -1
+type_line = MPI.INT.Create_vector(nb_columns, 1, nb_lines)
+
+# Validation of the type_line datatype
+type_line.Commit()
+
+# Sending of the first column
+if ( rank == 0 ):
+    comm.send(a[1,0],1, tag)
+
+# Reception in the last column
+if ( rank == 1 ):
+    a[nb_lines-1,0]=comm.recv(a[nb_lines-1,0], 1, type_line, 0, tag, comm, status, ierr)
+
+print('I process ', rank, ', has a = ', a)
+
+# Free the datatype
+mpi_type_free (type_line, ierr)
+
--- tests/macro/scripts/MPI/mpi4py.py
+++ tests/macro/scripts/MPI/mpi4py.py
@@ -6,13 +6,58 @@
 from pyccel.stdlib.internal.mpi import mpi_comm_world
 from pyccel.stdlib.internal.mpi import mpi_comm_rank
 from pyccel.stdlib.internal.mpi import mpi_comm_size
+
 from pyccel.stdlib.internal.mpi import mpi_send
+from pyccel.stdlib.internal.mpi import mpi_ssend
+from pyccel.stdlib.internal.mpi import mpi_bsend
+
+from pyccel.stdlib.internal.mpi import mpi_isend
+from pyccel.stdlib.internal.mpi import mpi_issend
+from pyccel.stdlib.internal.mpi import mpi_ibsend
+
 from pyccel.stdlib.internal.mpi import mpi_recv
-from pyccel.stdlib.internal.mpi import mpi_comm_split
+from pyccel.stdlib.internal.mpi import mpi_irecv
+
+from pyccel.stdlib.internal.mpi import mpi_sendrecv
+from pyccel.stdlib.internal.mpi import mpi_sendrecv_replace
+
 from pyccel.stdlib.internal.mpi import mpi_bcast
+from pyccel.stdlib.internal.mpi import mpi_scatter
+
+from pyccel.stdlib.internal.mpi import mpi_barrier
+from pyccel.stdlib.internal.mpi import mpi_gather
+from pyccel.stdlib.internal.mpi import mpi_allgather
+from pyccel.stdlib.internal.mpi import mpi_gatherv
+from pyccel.stdlib.internal.mpi import mpi_alltoall
+
+from pyccel.stdlib.internal.mpi import mpi_reduce
+from pyccel.stdlib.internal.mpi import mpi_allreduce
+
+from pyccel.stdlib.internal.mpi import mpi_wait
+from pyccel.stdlib.internal.mpi import mpi_waitall
+from pyccel.stdlib.internal.mpi import mpi_waitany
+from pyccel.stdlib.internal.mpi import mpi_waitsome
+
+from pyccel.stdlib.internal.mpi import mpi_test
+from pyccel.stdlib.internal.mpi import mpi_testall
+from pyccel.stdlib.internal.mpi import mpi_testany
+from pyccel.stdlib.internal.mpi import mpi_testsome
+
+from pyccel.stdlib.internal.mpi import mpi_cart_create
+from pyccel.stdlib.internal.mpi import mpi_cart_coords
+from pyccel.stdlib.internal.mpi import mpi_cart_shift
+from pyccel.stdlib.internal.mpi import mpi_cart_sub
+
+from pyccel.stdlib.internal.mpi import mpi_comm_split
 from pyccel.stdlib.internal.mpi import mpi_comm_free
+
 from pyccel.stdlib.internal.mpi import mpi_type_vector
 from pyccel.stdlib.internal.mpi import mpi_type_commit
+from pyccel.stdlib.internal.mpi import mpi_type_contiguous
+from pyccel.stdlib.internal.mpi import mpi_type_free
+from pyccel.stdlib.internal.mpi import mpi_type_indexed
+from pyccel.stdlib.internal.mpi import mpi_type_create_subarray
+
 class MPI_:
     def __init__(self):
         self.COMM_WORLD = 0
@@ -21,10 +66,22 @@ class MPI_:
 MPI = MPI_()
 
 #$ header macro x.COMM_WORLD := mpi_comm_world
+
 #$ header macro (x), y.Get_rank() := mpi_comm_rank(y,x,ierr)
 #$ header macro (x), y.Get_size() := mpi_comm_size(y,x,ierr)
+
 #$ header macro  y.send(data, dest, tag=0) := mpi_send(data, data.count, data.dtype, dest ,tag, y, ierr)
+#$ header macro  y.ssend(data, dest, tag=0) := mpi_send(data, data.count, data.dtype, dest ,tag, y, ierr)
+#$ header macro  y.bsend(data, dest, tag=0) := mpi_send(data, data.count, data.dtype, dest ,tag, y, ierr)
+
+#$ header macro  (req),y.isend(data, dest, tag=0) := mpi_send(data, data.count, data.dtype, dest ,tag, y, req, ierr)
+#$ header macro  (req),y.issend(data, dest, tag=0) := mpi_send(data, data.count, data.dtype, dest ,tag, y, req, ierr)
+#$ header macro  (req),y.ibsend(data, dest, tag=0) := mpi_send(data, data.count, data.dtype, dest ,tag, y, req, ierr)
+
+
 #$ header macro (x), y.recv(source=0, tag=0) := mpi_recv(x, x.count, x.dtype, source ,tag, y, status, ierr)
+#$ header macro (req), y.irecv(source=0, tag=0) := mpi_recv(x, x.count, x.dtype, source ,tag, y, status, ierr)
+
 #$ header macro (x), y.Split(color=0,key=0) := mpi_comm_split(y, color, key, x, ierr)
 #$ header macro  y.bcast(data, root=0) := mpi_bcast(data, data.count, data.dtype, root, y, ierr)
 #$ header macro y.Free() := mpi_comm_free(y, ierr)
