dfab19b3a131fb2691e3f7655aafa66d10adf4ea
saidhadjout <said-ctb@live.fr>
2018-07-02 15:57:22 +0200

    decorators in python in progress

--- pyccel/decorators.py
+++ pyccel/decorators.py
@@ -1,11 +1,24 @@
+#TODO improve sympy to handle Sum objects
+
 def sympy(f):
     args = f.__code__.co_varnames
     from sympy import symbols
     args = symbols(args)
     expr = f(*args)
     def wrapper(*vals):
-       return  expr.subs(zip(args,vals))
+       return  expr.subs(zip(args,vals)).doit()
     
     return wrapper
+
+
+def python(f):
+    args = f.__code__.co_varnames
+    def wrapper(*vals):
+       return  f(*vals)
+
+def lambdify(f):
+    return f
+
+
 
 
