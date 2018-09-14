c11148d016812f4209bee8620ff60ea9f2c9eb7a
Yaman Güçlü <yaman.guclu@gmail.com>
2018-09-14 18:04:32 +0200

    [CI] Cleanup unit tests for epyccel and travis configuration.

--- tests/epyccel/test_epyccel.py
+++ tests/epyccel/test_epyccel.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+import pytest
 import numpy as np
 import os
 
@@ -39,7 +40,6 @@ def f6(m1, m2, x):
     for i in range(0, m1):
         for j in range(0, m2):
             x[i,j] = (i+j) * 1.
-# ..............................................
 
 # ..............................................
 #        functions with types decorator
@@ -66,54 +66,64 @@ def g4(x, m=None):
     else:
         y = x - 1
     return y
-# ..............................................
 
 
-def test_f1():
+#==============================================================================
+# TEST FUNCTIONS WITH HEADERS
+#==============================================================================
+
+def test_header_f1():
     f = epyccel(f1, '#$ header procedure f1(int)')
 
     # ...
-    assert(f(3) == f1(3))
+    assert f(3) == f1(3)
     # ...
 
     clean_test()
 
-def test_f2():
+#------------------------------------------------------------------------------
+def test_header_f2():
     f = epyccel(f2, '#$ header procedure f2(int [:])')
 
     # ...
     x = np.array([3, 4, 5, 6], dtype=int)
-    assert(f(x) == f2(x))
+    assert f(x) == f2(x)
     # ...
 
     # ...
     x = [3, 4, 5, 6]
-    assert(f(x) == f2(x))
+    assert f(x) == f2(x)
     # ...
 
     clean_test()
 
-def test_f3():
+#------------------------------------------------------------------------------
+# TODO: functions returning arrays are not supported yet
+@pytest.mark.xfail
+def test_header_f3():
+
     f = epyccel(f3, '#$ header procedure f3(int [:])')
 
     # ...
     x = np.array([3, 4, 5, 6], dtype=int)
-    assert(np.allclose(f(x), f3(x)))
+    assert np.allclose(f(x), f3(x))
     # ...
 
     clean_test()
 
-def test_f4():
+#------------------------------------------------------------------------------
+def test_header_f4():
     f = epyccel(f4, '#$ header procedure f4(double [:,:])')
 
     # ...
     x = np.random.random((2, 3))
-    assert(np.allclose(f(x), f4(x)))
+    assert np.allclose(f(x), f4(x))
     # ...
 
     clean_test()
 
-def test_f5():
+#------------------------------------------------------------------------------
+def test_header_f5():
     f = epyccel(f5, '#$ header procedure f5(int, double [:])')
 
     # ...
@@ -125,12 +135,13 @@ def test_f5():
     x_expected = np.zeros(m1)
     f5(m1, x_expected)
 
-    assert(np.allclose(x, x_expected))
+    assert np.allclose(x, x_expected)
     # ...
 
     clean_test()
 
-def test_f6():
+#------------------------------------------------------------------------------
+def test_header_f6():
     f = epyccel(f6, '#$ header procedure f6(int, int, double [:,:](order = F))')
 
     # ...
@@ -142,19 +153,27 @@ def test_f6():
     x_expected = np.zeros((m1,m2), order='F')
     f6(m1, m2, x_expected)
 
-    assert(np.allclose(x, x_expected))
+    assert np.allclose(x, x_expected)
     # ...
 
     clean_test()
 
-def test_decorators():
+#==============================================================================
+# TEST FUNCTIONS WITH DECORATORS
+#==============================================================================
 
-    # ...
+def test_decorators_g1():
+
     f = epyccel(g1)
-    assert(f(3) == g1(3))
-    # ...
+    assert f(3) == g1(3)
 
-    # ...
+    clean_test()
+
+#------------------------------------------------------------------------------
+# TODO: functions returning arrays are not supported yet
+@pytest.mark.xfail
+def test_decorators_g2():
+
     f = epyccel(g2)
 
     x = np.array([2, 3, 4], dtype=int)
@@ -163,31 +182,46 @@ def test_decorators():
     f(x)
     g2(x_expected)
 
-    assert(np.allclose(x, x_expected))
-    # ...
-
-    # ...
-    f = epyccel(g3)
-    assert(f(3,2,4) == g3(3,2,4))
-    # ...
-
-#    # ... TODO: not working yet. optional arg is placed before out!
-#    f = epyccel(g4)
-#    assert(f(3) == g4(3))
-#
-#    print(f(3,2))
-#    print(g4(3,2))
-#    assert(f(3, 2) == g4(3, 2))
-#    # ...
+    assert np.allclose(x, x_expected)
 
     clean_test()
 
+#------------------------------------------------------------------------------
+def test_decorators_g3():
+
+    f = epyccel(g3)
+    assert f(3,2,4) == g3(3,2,4)
+
+    clean_test()
+
+#------------------------------------------------------------------------------
+# TODO: not working yet. optional arg is placed before out!
+@pytest.mark.xfail
+def test_decorators_g4():
+
+    f = epyccel(g4)
+    assert f(3) == g4(3)
+
+    print(f(3,2))
+    print(g4(3,2))
+    assert f(3, 2) == g4(3, 2)
+    # ...
+
+    clean_test()
+
+#==============================================================================
+# TEST MODULE
+#==============================================================================
+
+# TODO: functions returning arrays are not supported yet
+@pytest.mark.xfail
 def test_module1():
+
     import mod_test1 as mod
 
     # ...
     m = epyccel(mod)
-    assert(m.g1(3) == mod.g1(3))
+    assert m.g1(3) == mod.g1(3)
     # ...
 
     # ...
@@ -197,24 +231,14 @@ def test_module1():
     m.g2(x)
     mod.g2(x_expected)
 
-    assert(np.allclose(x, x_expected))
+    assert np.allclose(x, x_expected)
     # ...
 
+    clean_test()
 
-if __name__ == '__main__':
-    # ... using headers
-    test_f1()
-    test_f2()
-    test_f3()
-    test_f4()
-    test_f5()
-    test_f6()
-    # ...
+#==============================================================================
+# CLEAN UP GENERATED FILES AFTER RUNNING TESTS
+#==============================================================================
 
-    # ... using decorators
-    test_decorators()
-    # ...
-
-    # ... using modules
-    test_module1()
-    # ...
+def teardown_module():
+    clean_test()
