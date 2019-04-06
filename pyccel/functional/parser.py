d5e11815ae0ef3e25a838c4c5a19925a9839b2e6
ratnania <ratnaniahmed@gmail.com>
2019-04-06 14:31:02 +0200

    [functional] convert text to sympy

--- pyccel/functional/parser.py
+++ pyccel/functional/parser.py
@@ -3,13 +3,25 @@
 import os
 from os.path import join, dirname
 
+from sympy import Symbol, Lambda, Function, Dummy
+from sympy import sympify
+
 from textx.metamodel import metamodel_from_str
 
+namespace = {}
+
+#==============================================================================
+# any argument
+class AnyArgument(Symbol):
+    pass
+
+_ = AnyArgument('_')
 
 #==============================================================================
 class NamedAbstraction(object):
     def __init__(self, **kwargs):
         self.name = kwargs.pop('name')
+        self.abstraction = kwargs.pop('abstraction')
 
 class Abstraction(object):
     def __init__(self, **kwargs):
@@ -20,6 +32,42 @@ class Application(object):
     def __init__(self, **kwargs):
         self.name = kwargs.pop('name')
         self.args = kwargs.pop('args')
+
+#==============================================================================
+def to_sympy(stmt):
+
+    if isinstance(stmt, NamedAbstraction):
+        name = stmt.name
+        expr = to_sympy(stmt.abstraction)
+        # TODO add it to the namespace
+#        namespace[name] = expr
+        return expr
+
+    elif isinstance(stmt, Abstraction):
+        args = [to_sympy(i) for i in stmt.args]
+        expr = to_sympy(stmt.expr)
+
+        return Lambda(args, expr)
+
+    elif isinstance(stmt, Application):
+        args = [to_sympy(i) for i in stmt.args]
+        name = stmt.name
+
+        return Function(name)(*args)
+
+    elif isinstance(stmt, (int, float)):
+        return stmt
+
+    elif isinstance(stmt, str):
+        if stmt == '_':
+            return _
+
+        else:
+            return sympify(stmt)
+
+    else:
+        print(stmt)
+        raise TypeError('Not implemented for {}'.format(type(stmt)))
 
 #==============================================================================
 def parse(inputs, debug=False):
@@ -40,4 +88,7 @@ def parse(inputs, debug=False):
     else:
         ast = meta.model_from_str(inputs)
 
-    return ast
+    expr = to_sympy(ast)
+    print(expr)
+
+    return expr
