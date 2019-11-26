14ed71f7212899ab99bf63165703323e33102044
EmilyBourne <louise.bourne@gmail.com>
2019-11-26 11:33:50 +0100

    Fix codacy problem with list as default arg

--- tests/pyccel/test_pyccel.py
+++ tests/pyccel/test_pyccel.py
@@ -29,13 +29,16 @@ def get_fortran_output(path_dir,test_file):
     assert(p.returncode==0)
     return out
 
-def pyccel_test(test_file, dependencies = []):
+def pyccel_test(test_file, dependencies = None):
     base_dir = os.path.dirname(os.path.realpath(__file__))
     path_dir = os.path.join(base_dir, 'scripts')
 
     pyth_out = get_python_output(path_dir,test_file)
-    for d in dependencies:
-        compile_pyccel(path_dir, d)
+    if (type(dependencies) is list):
+        for d in dependencies:
+            compile_pyccel(path_dir, d)
+    elif (type(dependencies) is str):
+        compile_pyccel(path_dir, dependencies)
     compile_pyccel(path_dir,test_file)
     fort_out = get_fortran_output(path_dir,test_file[:-3])
 
@@ -43,7 +46,7 @@ def pyccel_test(test_file, dependencies = []):
 
 @pytest.mark.xfail
 def test_imports():
-    pyccel_test("test_imports.py",["funcs.py"])
+    pyccel_test("test_imports.py","funcs.py")
 
 def test_funcs():
     pyccel_test("test_funcs.py")
