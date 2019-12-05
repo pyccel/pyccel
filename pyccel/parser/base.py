91fe8025ee4c568d5d9a291b36d3af3a4f2bad4f
EmilyBourne <louise.bourne@gmail.com>
2019-12-05 17:03:39 +0100

    Fix tests by providing input folder

--- pyccel/parser/base.py
+++ pyccel/parser/base.py
@@ -195,7 +195,7 @@ def is_ignored_module(name):
     return False
 
 
-def get_filename_from_import(module,output_folder=''):
+def get_filename_from_import(module,input_folder=''):
     """Returns a valid filename with absolute path, that corresponds to the
     definition of module.
     The priority order is:
@@ -211,7 +211,7 @@ def get_filename_from_import(module,output_folder=''):
     if is_valid_filename_py(filename_py):
         return os.path.abspath(filename_py)
 
-    folders = output_folder.split(""".""")
+    folders = input_folder.split(""".""")
     for i in range(len(folders)):
         poss_dirname      = os.path.join( *folders[:i+1] )
         poss_filename_pyh = os.path.join( poss_dirname, filename_pyh )
