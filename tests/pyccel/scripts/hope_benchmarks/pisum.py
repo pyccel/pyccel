b720c1004853322ed62ccd0625ef21c7717f17ae
Emily.Bourne <louise.bourne@gmail.com>
2020-01-17 15:55:59 +0100

    codacy fix

--- tests/pyccel/scripts/hope_benchmarks/pisum.py
+++ tests/pyccel/scripts/hope_benchmarks/pisum.py
@@ -1,9 +1,9 @@
 
 def pisum():
-    for j in range(1, 501):
-        sum = 0.0
+    for _ in range(1, 501):
+        pi_sum = 0.0
         for k in range(1, 10001):
-            sum += 1.0/(k*k)
-    return sum
+            pi_sum += 1.0/(k*k)
+    return pi_sum
 
 print(pisum())
