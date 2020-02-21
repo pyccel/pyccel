196c7a72a9df7ecfbfc7c7e09674e0c953089fb1
Christopher Albert <albert@alumni.tugraz.at>
2020-02-21 12:24:31 +0100

    Change to bools with precision flag

--- tests/epyccel/modules/base.py
+++ tests/epyccel/modules/base.py
@@ -1,73 +1,64 @@
 from pyccel.decorators import types
 
-# This has been done with c as an integer, until it works in the 
-# fully Boolean version e.g.
-#@types('bool', 'bool')
-#def bool_compare_is(a, b):
-#    c = False
-#    if a is b:
-#        c = True
-#    return c
-
 @types('bool')
 def is_false(a):
-    c = 0
+    c = False
     if a is False:
-        c = 1
+        c = True
     return c
 
 @types('bool')
 def is_true(a):
-    c = 0
+    c = False
     if a is True:
-        c = 1
+        c = True
     return c
 
 @types('bool', 'bool')
 def compare_is(a, b):
-    c = 1
+    c = False
     if a is b:
-        c = 0
+        c = True
     return c
 
 @types('bool')
 def not_false(a):
-    c = 0
+    c = False
     if a is not False:
-        c = 1
+        c = True
     return c
 
 @types('bool')
 def not_true(a):
-    c = 0
+    c = False
     if a is not True:
-        c = 1
+        c = True
     return c
 
 @types('bool')
 def eq_false(a):
-    c = 0
+    c = False
     if a == False:
-        c = 1
+        c = True
     return c
 
 @types('bool')
 def eq_true(a):
-    c = 0
+    c = False
     if a == True:
-        c = 1
+        c = True
     return c
 
 @types('bool')
 def neq_false(a):
-    c = 0
+    c = False
     if a != False:
-        c = 1
+        c = True
     return c
 
 @types('bool')
 def neq_true(a):
-    c = 0
+    c = False
     if a != True:
-        c = 1
+        c = True
     return c
