9462b5b4de029217331dca2baeba588e385f948e
Emily.Bourne <louise.bourne@gmail.com>
2020-03-04 19:30:47 +0100

    Pass verbose settings to parser

--- pyccel/codegen/pipeline.py
+++ pyccel/codegen/pipeline.py
@@ -102,7 +102,7 @@ def execute_pyccel(fname, *,
     # Parse Python file
     try:
         parser = Parser(pymod_filepath, output_folder=pyccel_dirpath.replace('/','.'), show_traceback=verbose)
-        ast = parser.parse()
+        ast = parser.parse(verbose=verbose)
     except Exception:
         handle_error('parsing (syntax)')
         raise
@@ -112,7 +112,7 @@ def execute_pyccel(fname, *,
 
     # Annotate abstract syntax Tree
     try:
-        settings = {}
+        settings = {'verbose':verbose}
         ast = parser.annotate(**settings)
     except Exception:
         handle_error('annotation (semantic)')
