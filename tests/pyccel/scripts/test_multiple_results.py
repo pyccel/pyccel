d8585126fcc0eff59c54fc47b08e2a104f5b7bca
Emily.Bourne <louise.bourne@gmail.com>
2020-03-16 10:45:31 +0100

    Add function call tests for functions with multiple arguments
    TODO: Fix function call in print or raise error

---
+++ tests/pyccel/scripts/test_multiple_results.py
@@ -0,0 +1,43 @@
+from pyccel.decorators import pure, types
+
+@pure
+def const_int_float():
+    return 1, 3.4
+
+a,b = const_int_float()
+print(a)
+print(b)
+
+@pure
+def const_complex_bool_int():
+    return 1+2j, False, 8
+
+c, d, e = const_complex_bool_int()
+print(c)
+print(d)
+print(e)
+
+@pure
+@types('int')
+def expr_complex_int_bool(n):
+    return 0.5+n*1j, 2*n, n==3
+
+f, g, h = expr_complex_int_bool(a)
+print(f)
+print(g)
+print(h)
+
+@types('real','real')
+def f3(x = 1.5, y = 2.5):
+    return x+y, x-y
+
+i,j = f3(19.2,6.7)
+print(i,j)
+i,j = f3(4.5)
+print(i,j)
+i,j = f3(y = 8.2)
+print(i,j)
+i,j = f3()
+print(i, j)
+#TODO: Fix Print
+#print(f3())
