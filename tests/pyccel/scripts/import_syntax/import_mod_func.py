3ea5928dbc05cdb4fe955445fa123eb4b8c9c0c9
EmilyBourne <louise.bourne@gmail.com>
2020-03-19 12:14:17 +0100

    Fix import syntax bugs (#276)
    * Test all import statements in different scopes
    
    * Repeat import tests for user defined modules
    
    * Ensure that all import statements are treated
    
    * Handle as names for user modules (fixes #275)
    
    * [CODACY] Ignore reimport warning in import test

---
+++ tests/pyccel/scripts/import_syntax/import_mod_func.py
@@ -0,0 +1,45 @@
+
+#$ header function matmat(double [:,:], double [:,:], double [:,:])
+def matmat(a,b,c):
+    import numpy #pylint: disable=W0404
+    nm = numpy.shape(a)
+    mp = numpy.shape(b)
+
+    n = nm[0]
+    m = nm[1]
+    p = mp[1]
+
+    for i in range(0, n):
+        for j in range(0, p):
+            for k in range(0, m):
+                c[i,j] = c[i,j] + a[i,k]*b[k,j]
+
+import numpy #pylint: disable=W0404
+n = 3
+m = 4
+p = 3
+
+a = numpy.zeros((n,m), 'double')
+b = numpy.zeros((m,p), 'double')
+
+for i in range(0, n):
+    for j in range(0, m):
+        a[i,j] = (i-j)*1.0
+        print(a[i,j])
+    print()
+print()
+
+for i in range(0, m):
+    for j in range(0, p):
+        b[i,j] = (i+j)*1.0
+        print(b[i,j])
+    print()
+print()
+
+c = numpy.zeros((n,p),'double')
+matmat(a,b,c)
+
+for i in range(0, n):
+    for j in range(0, p):
+        print(c[i,j])
+    print()
