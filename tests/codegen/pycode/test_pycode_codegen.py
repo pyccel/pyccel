7c987861d67165ceb324e4565be44de05f4725bc
Said Hadjout <said-ctb@live.fr>
2020-03-23 09:06:12 +0000

    Devel pycode (#274)
    * update python printer
    
    * add pycode printer tests
    
    * reorganize codegen tests to be like semantics tests

---
+++ tests/codegen/pycode/test_pycode_codegen.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+
+# Note that we need to change the directory for tests involving the import
+# statement
+
+from pyccel.parser import Parser
+from pyccel.codegen import Codegen
+from pyccel.parser.errors import Errors
+import pytest
+import os
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+path_dir = os.path.join(base_dir, 'scripts')
+
+files = sorted(os.listdir(path_dir))
+files = [os.path.join(path_dir,f) for f in files if (f.endswith(".py"))]
+    
+@pytest.mark.parametrize( "f", files )
+def test_codegen(f):
+
+    print('> testing {0}'.format(str(f)))
+
+    pyccel = Parser(f)
+    ast = pyccel.parse()
+
+    settings = {}
+    ast = pyccel.annotate(**settings)
+
+    name = os.path.basename(f)
+    name = os.path.splitext(name)[0]
+
+    codegen = Codegen(ast, name)
+    code = codegen.doprint(language='python')
+
+    # reset Errors singleton
+    errors = Errors()
+    errors.reset()
+######################
+if __name__ == '__main__':
+    print('*********************************')
+    print('***                           ***')
+    print('***  TESTING CODEGEN PYCODE   ***')
+    print('***                           ***')
+    print('*********************************')
+
+    for f in files:
+        test_codegen(f)
+
+    print('\n')
