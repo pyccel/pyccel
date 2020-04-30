9c33a0ed1ed1d1deedf314ca4e0e87271654c0d7
Emily.Bourne <louise.bourne@gmail.com>
2020-04-30 10:15:04 +0200

    [CODACY] stdlib/external/mpi4py.py : Remove unused imports

--- pyccel/stdlib/external/mpi4py.py
+++ pyccel/stdlib/external/mpi4py.py
@@ -3,62 +3,6 @@
 #$ header metavar ignore_at_import=True
 #$ header metavar import_all=True
 
-from pyccel.stdlib.internal.mpi import mpi_comm_world
-
-from pyccel.stdlib.internal.mpi import mpi_send
-from pyccel.stdlib.internal.mpi import mpi_ssend
-from pyccel.stdlib.internal.mpi import mpi_bsend
-
-from pyccel.stdlib.internal.mpi import mpi_isend
-from pyccel.stdlib.internal.mpi import mpi_issend
-from pyccel.stdlib.internal.mpi import mpi_ibsend
-
-from pyccel.stdlib.internal.mpi import mpi_recv
-from pyccel.stdlib.internal.mpi import mpi_irecv
-
-from pyccel.stdlib.internal.mpi import mpi_sendrecv
-from pyccel.stdlib.internal.mpi import mpi_sendrecv_replace
-
-from pyccel.stdlib.internal.mpi import mpi_bcast
-from pyccel.stdlib.internal.mpi import mpi_scatter
-
-from pyccel.stdlib.internal.mpi import mpi_barrier
-from pyccel.stdlib.internal.mpi import mpi_gather
-from pyccel.stdlib.internal.mpi import mpi_allgather
-from pyccel.stdlib.internal.mpi import mpi_allgatherv
-from pyccel.stdlib.internal.mpi import mpi_gatherv
-from pyccel.stdlib.internal.mpi import mpi_alltoall
-
-from pyccel.stdlib.internal.mpi import mpi_reduce
-from pyccel.stdlib.internal.mpi import mpi_allreduce
-
-from pyccel.stdlib.internal.mpi import mpi_wait
-from pyccel.stdlib.internal.mpi import mpi_waitall
-from pyccel.stdlib.internal.mpi import mpi_waitany
-from pyccel.stdlib.internal.mpi import mpi_waitsome
-
-from pyccel.stdlib.internal.mpi import mpi_test
-from pyccel.stdlib.internal.mpi import mpi_testall
-from pyccel.stdlib.internal.mpi import mpi_testany
-from pyccel.stdlib.internal.mpi import mpi_testsome
-
-from pyccel.stdlib.internal.mpi import mpi_cart_create
-from pyccel.stdlib.internal.mpi import mpi_cart_coords
-from pyccel.stdlib.internal.mpi import mpi_cart_shift
-from pyccel.stdlib.internal.mpi import mpi_cart_sub
-
-from pyccel.stdlib.internal.mpi import mpi_comm_split
-from pyccel.stdlib.internal.mpi import mpi_comm_free
-
-from pyccel.stdlib.internal.mpi import mpi_type_vector
-from pyccel.stdlib.internal.mpi import mpi_type_commit
-from pyccel.stdlib.internal.mpi import mpi_type_contiguous
-from pyccel.stdlib.internal.mpi import mpi_type_free
-from pyccel.stdlib.internal.mpi import mpi_type_indexed
-from pyccel.stdlib.internal.mpi import mpi_type_create_subarray
-from pyccel.stdlib.internal.mpi import ANY_TAG
-from pyccel.stdlib.internal.mpi import ANY_SOURCE
-
 from pyccel.stdlib.internal.mpi import MPI_SUM
 from pyccel.stdlib.internal.mpi import MPI_PROD
 from pyccel.stdlib.internal.mpi import MPI_MAX
@@ -77,9 +21,6 @@ from pyccel.stdlib.internal.mpi import MPI_REAL8
 from pyccel.stdlib.internal.mpi import MPI_COMPLEX8
 from pyccel.stdlib.internal.mpi import MPI_COMPLEX16
 from pyccel.stdlib.internal.mpi import MPI_CHARACTER
-
-from pyccel.stdlib.internal.mpiext import mpiext_get_rank
-from pyccel.stdlib.internal.mpiext import mpiext_get_size
 
 #===================================================================================
 
