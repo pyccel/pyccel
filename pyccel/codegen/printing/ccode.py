ecfc2ffbccf09d2a27e4d67c66903600203f5766
saidctb <said-ctb@live.fr>
2020-05-08 05:40:11 +0200

    add more examples and functiondef declarations

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -8,7 +8,7 @@ from pyccel.ast.core import Assign, datatype, Variable, Import
 from pyccel.ast.core import SeparatorComment, CommentBlock, Comment
 
 from pyccel.ast.builtins  import Range
-from pyccel.ast.core import Assign, datatype, Import
+from pyccel.ast.core import Assign, datatype, Import, Declare
 from pyccel.ast.core import SeparatorComment
 
 from pyccel.codegen.printing.codeprinter import CodePrinter
@@ -123,15 +123,27 @@ class CCodePrinter(CodePrinter):
 
     def _print_FunctionDef(self, expr):
         if len(expr.results) == 1:
-            ret_type = self._print(expr.results[0].dtype)
+            result = expr.results[0]
+            dtype = self._print(result.dtype)
+            prec  = result.precision
+            rank  = result.rank
+            ret_type = dtype_registry[(dtype, prec)]
         elif len(expr.results) > 1:
             raise ValueError("C doesn't support multiple return values.")
         else:
             ret_type = self._print(datatype('void'))
         name = expr.name
-        arg_code = ', '.join(self._print(i.dtype) + ' ' + self._print(i) for i in expr.arguments)
-        body = '\n'.join(self._print(i) for i in expr.body.body)
-        return '{0} {1}({2}) {{\n{3}\n}}'.format(ret_type, name, arg_code, body)
+
+        decs  = [Declare(i.dtype, i) for i in expr.local_vars]
+        decs += [Declare(i.dtype, i) for i in expr.results]
+        arg_dtypes = [self._print(i.dtype) for i in expr.arguments]
+        arg_dtypes = [dtype_registry[(dtype, arg.precision)] for dtype,arg in zip(arg_dtypes, expr.arguments)]
+        arguments  = [self._print(i) for i in expr.arguments]
+        arg_code   = ', '.join(dtype + ' ' + arg for dtype,arg in zip(arg_dtypes,arguments))
+        decs       = '\n'.join(self._print(i) for i in decs)
+        body       = '\n'.join(self._print(i) for i in expr.body.body)
+
+        return '{0} {1}({2}) {{\n{3}\n{4}\n}}'.format(ret_type, name, arg_code, decs, body)
 
     def _print_Return(self, expr):
         return 'return {0};'.format(self._print(expr.expr[0]))
--- tests/codegen/ccode/scripts/loops.py
+++ tests/codegen/ccode/scripts/loops.py
@@ -40,6 +40,44 @@ def double_loop( n ):
     return z
 
 # ...
+@types( int )
+def sum_natural_numbers_2( n ):
+    x = 0.
+    for i in range( 1, n+1 ):
+        x= x+i
+    return x
+
+# ...
+@types( int )
+def factorial_2( n ):
+    x = 1.
+    for i in range( 2, n+1 ):
+        x = x*i
+    return x
+
+# ...
+@types( int )
+def fibonacci_2( n ):
+    x = 0.
+    y = 1.
+    for i in range( n ):
+        z = x+y
+        x = y
+        y = z
+    return x
+
+# ...
+@types( int )
+def double_loop_2( n ):
+    x = 0.
+    for i in range( 3, 10 ):
+        x = x+1
+        y  = n*x
+        for j in range( 4, 15 ):
+            z = x-y
+    return z
+
+# ...
 #@types( 'int[:,:](order=C)' )
 #def double_loop_on_2d_array_C( z ):
 #
