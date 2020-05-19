be3262f18b72e9eef7d2062774071b2cf69ce411
Emily.Bourne <louise.bourne@gmail.com>
2020-05-19 16:11:03 +0200

    Split error tests into individual tests

--- tests/errors/test_errors.py
+++ tests/errors/test_errors.py
@@ -10,74 +10,64 @@ from pyccel.parser import Parser
 from pyccel.codegen import Codegen
 from pyccel.parser.errors import Errors
 import os
+import pytest
 
-def test_syntax_errors():
+def get_files_from_folder(foldername):
+    base_dir = os.path.dirname(os.path.realpath(__file__))
+    path_dir = os.path.join(base_dir, os.path.join(foldername))
+
+    files = sorted(os.listdir(path_dir))
+    files = [os.path.join(path_dir,f) for f in files if (f.endswith(".py"))]
+    return files
+
+@pytest.mark.parametrize("f",get_files_from_folder("syntax"))
+def test_syntax_errors(f):
+    pyccel = Parser(f)
+
+    try:
+        ast = pyccel.parse()
+    except:
+        pass
+
+    # reset Errors singleton
+    errors = Errors()
+    errors.reset()
+
+@pytest.mark.parametrize("f",get_files_from_folder("semantic"))
+def test_semantic_errors(f):
+    print('> testing {0}'.format(str(f)))
+
+    pyccel = Parser(f, show_traceback=False)
+    ast = pyccel.parse()
+
+    try:
+        settings = {}
+        ast = pyccel.annotate(**settings)
+    except:
+        pass
+
+    # reset Errors singleton
+    errors = Errors()
+    errors.reset()
+
+######################
+if __name__ == '__main__':
     print('*********************************')
     print('***                           ***')
     print('***   TESTING SYNTAX ERRORS   ***')
     print('***                           ***')
     print('*********************************')
-
-    init_dir = os.getcwd()
-    base_dir = os.path.dirname(os.path.realpath(__file__))
-    path_dir = os.path.join(base_dir, 'syntax')
-
-    files = sorted(os.listdir(path_dir))
-    files = [f for f in files if (f.endswith(".py"))]
-
-    os.chdir(path_dir)
-    for f in files:
-        print('> testing {0}'.format(str(f)))
-
-        pyccel = Parser(f)
-
-        try:
-            ast = pyccel.parse()
-        except:
-            pass
-
-        # reset Errors singleton
-        errors = Errors()
-        errors.reset()
-
-    os.chdir(init_dir)
+    for f in get_files_from_folder("syntax"):
+        print('> testing {0}'.format(str(os.path.basename(f))))
+        test_syntax_errors(f)
     print('\n')
 
-def test_semantic_errors():
     print('*********************************')
     print('***                           ***')
     print('***  TESTING SEMANTIC ERRORS  ***')
     print('***                           ***')
     print('*********************************')
-
-    init_dir = os.getcwd()
-    base_dir = os.path.dirname(os.path.realpath(__file__))
-    path_dir = os.path.join(base_dir, 'semantic')
-
-    files = sorted(os.listdir(path_dir))
-    files = [f for f in files if (f.endswith(".py"))]
-
-    os.chdir(path_dir)
-    for f in files:
-        print('> testing {0}'.format(str(f)))
-
-        pyccel = Parser(f, show_traceback=False)
-        ast = pyccel.parse()
-
-        try:
-            settings = {}
-            ast = pyccel.annotate(**settings)
-        except:
-            pass
-
-        # reset Errors singleton
-        errors = Errors()
-        errors.reset()
-
-    os.chdir(init_dir)
+    for f in get_files_from_folder("semantic"):
+        print('> testing {0}'.format(str(os.path.basename(f))))
+        test_semantic_errors(f)
     print('\n')
-
-######################
-if __name__ == '__main__':
-    test_syntax_errors()
-    test_semantic_errors()
