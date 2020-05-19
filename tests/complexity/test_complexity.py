64c7236fcb70dc7b8781494ecec24de34eccefbf
Emily.Bourne <louise.bourne@gmail.com>
2020-05-19 16:37:49 +0200

    Remove chdir in complexity tests by providing complete filename

--- tests/complexity/test_complexity.py
+++ tests/complexity/test_complexity.py
@@ -8,21 +8,16 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 path_dir = os.path.join(base_dir, 'scripts')
 
 files = sorted(os.listdir(path_dir))
-files = [f for f in files if (f.endswith(".py"))]
+files = [os.path.join(path_dir,f) for f in files if (f.endswith(".py"))]
 
 
 @pytest.mark.parametrize( "f", files )
 def test_complexity(f):
 
-    os.chdir(path_dir)
-
-    print('> testing {0}'.format(str(f)))
     mem_complexity = MemComplexity(f)
     arithmetic_complexity = OpComplexity(f)
     print(mem_complexity.cost())
     print(arithmetic_complexity.cost())
-
-    os.chdir(os.getcwd())
 
 ######################
 if __name__ == '__main__':
@@ -34,5 +29,6 @@ if __name__ == '__main__':
     print('*********************************')
 
     for f in files:
+        print('> testing {0}'.format(str(os.path.basename(f))))
         test_complexity(f)
         print("\n")
