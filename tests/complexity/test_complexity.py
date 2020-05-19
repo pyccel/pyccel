45db2f060f265b4d1abbd47ba4549117f61be94d
Emily.Bourne <louise.bourne@gmail.com>
2020-05-19 14:19:37 +0200

    Change directory is necessary for complexity tests

--- tests/complexity/test_complexity.py
+++ tests/complexity/test_complexity.py
@@ -3,6 +3,7 @@ from pyccel.complexity.arithmetic import OpComplexity
 import os
 import pytest
 
+
 base_dir = os.path.dirname(os.path.realpath(__file__))
 path_dir = os.path.join(base_dir, 'scripts')
 
@@ -13,12 +14,15 @@ files = [f for f in files if (f.endswith(".py"))]
 @pytest.mark.parametrize( "f", files )
 def test_complexity(f):
 
+    os.chdir(path_dir)
+
     print('> testing {0}'.format(str(f)))
     mem_complexity = MemComplexity(f)
     arithmetic_complexity = OpComplexity(f)
     print(mem_complexity.cost())
     print(arithmetic_complexity.cost())
 
+    os.chdir(os.getcwd())
 
 ######################
 if __name__ == '__main__':
