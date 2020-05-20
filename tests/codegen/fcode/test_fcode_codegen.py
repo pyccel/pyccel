0f9d63cceb827bf714e2c54d5896e81e46c693e1
Emily.Bourne <louise.bourne@gmail.com>
2020-05-20 12:18:21 +0200

    Remove xpass mark on cross.py as issue 326 has now been created

--- tests/codegen/fcode/test_fcode_codegen.py
+++ tests/codegen/fcode/test_fcode_codegen.py
@@ -13,12 +13,13 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 path_dir = os.path.join(base_dir, 'scripts')
 
 files = sorted(os.listdir(path_dir))
-files = [os.path.join(path_dir,f) for f in files if (f.endswith(".py"))]
-
-failing_files = [os.path.join(path_dir,'cross.py')]
-passing_files = list(set(files).difference(set(failing_files)))
-    
-def call_test_codegen(f):
+files = [os.path.join(path_dir,f) \
+         #if f not in failing_files \
+         #else pytest.param(os.path.join(path_dir,f), marks = pytest.mark.xfail(reason=failing_files[f])) \
+         for f in files]
+@pytest.mark.fortran
+@pytest.mark.parametrize("f", files)
+def test_codegen(f):
 
     pyccel = Parser(f)
     ast = pyccel.parse()
@@ -35,17 +36,6 @@ def call_test_codegen(f):
     # reset Errors singleton
     errors = Errors()
     errors.reset()
-
-@pytest.mark.fortran
-@pytest.mark.parametrize( "f", passing_files )
-def test_passing_codegen(f):
-    call_test_codegen(f)
-
-@pytest.mark.fortran
-@pytest.mark.parametrize( "f", failing_files )
-@pytest.mark.xfail
-def test_failing_codegen(f):
-    call_test_codegen(f)
 
 ######################
 if __name__ == '__main__':
