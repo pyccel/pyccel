4a9387edf582cff8de4d0cd3c3e5503b1645bdf5
Emily.Bourne <louise.bourne@gmail.com>
2020-06-10 16:10:12 +0200

    Add test of pointer to pointer

--- tests/epyccel/modules/pointers.py
+++ tests/epyccel/modules/pointers.py
@@ -1,6 +1,7 @@
 __all__ = [
         'slice_is_pointer_idx_0',
-        'array_copy_is_pointer'
+        'array_copy_is_pointer',
+        'pointer_to_pointer_is_pointer'
         ]
 
 def slice_is_pointer_idx_0():
@@ -17,3 +18,11 @@ def array_copy_is_pointer():
     b = a
     a[2] = 9
     return b[0], b[1], b[2], b[3], shape(b)[0]
+
+def pointer_to_pointer_is_pointer():
+    from numpy import array, shape
+    a = array([1, 2, 3, 4])
+    b = a[1:]
+    c = b
+    a[2] = 9
+    return c[0], c[1], c[2], shape(c)[0]
