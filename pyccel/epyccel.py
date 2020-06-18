f8d51b635de471d3f0a485cf37cb81c2901dc18f
Emily.Bourne <louise.bourne@gmail.com>
2020-06-18 16:19:20 +0200

    Use Random instead of SystemRandom which is not available on all operating systems (https://docs.python.org/3/library/random.html#random.SystemRandom)

--- pyccel/epyccel.py
+++ pyccel/epyccel.py
@@ -19,7 +19,7 @@ __all__ = ['random_string', 'get_source_function', 'epyccel_seq', 'epyccel']
 def random_string( n ):
     # we remove uppercase letters because of f2py
     chars    = string.ascii_lowercase + string.digits
-    selector = random.SystemRandom()
+    selector = random.Random()
     return ''.join( selector.choice( chars ) for _ in range( n ) )
 
 #==============================================================================
