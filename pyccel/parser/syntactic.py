1e2e758c6a1913de80eba8859452ebb307c72248
Emily.Bourne <louise.bourne@gmail.com>
2020-07-10 10:46:02 +0200

    [Codacy] Don't use list comprehension with function returning nothing

--- pyccel/parser/syntactic.py
+++ pyccel/parser/syntactic.py
@@ -851,7 +851,8 @@ class SyntaxParser(BasicParser):
 
         name = stmt.name
         methods = [self._visit(i) for i in stmt.body if isinstance(i, ast.FunctionDef)]
-        [i.set_cls_name(name) for i in methods]
+        for i in methods:
+            i.set_cls_name(name)
         attributes = methods[0].arguments
         parent = [self._visit(i) for i in stmt.bases]
         expr = ClassDef(name=name, attributes=attributes,
