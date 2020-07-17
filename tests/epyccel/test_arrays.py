9011ed642eb4375c3d9009b7cbf2b8cf5eb7551e
Emily.Bourne <louise.bourne@gmail.com>
2020-07-17 14:38:29 +0200

    Add tests that would have been failing before corrections

--- tests/epyccel/test_arrays.py
+++ tests/epyccel/test_arrays.py
@@ -1145,6 +1145,142 @@ def test_array_real_2d_F_initialization():
 
     assert np.array_equal(x1, x2)
 
+
+
+#==============================================================================
+# TEST: COMPLEX EXPRESSIONS IN 3D : TEST CONSTANT AND UNKNOWN SHAPES
+#==============================================================================
+
+
+def test_array_int32_1d_complex_3d_expr():
+
+    f1 = arrays.array_int32_1d_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x1 = np.array( [1,2,3], dtype=np.int32 )
+    x2 = np.copy(x1)
+    a  = np.array( [-1,-2,-3], dtype=np.int32 )
+
+    f1(x1, a)
+    f2(x2, a)
+
+    assert np.array_equal( x1, x2 )
+
+def test_array_int32_2d_C_complex_3d_expr():
+
+    f1 = arrays.array_int32_2d_C_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x1 = np.array( [[1,2,3], [4,5,6]], dtype=np.int32 )
+    x2 = np.copy(x1)
+    a  = np.array( [[-1,-2,-3], [-4,-5,-6]], dtype=np.int32 )
+
+    f1(x1, a)
+    f2(x2, a)
+
+    assert np.array_equal( x1, x2 )
+
+def test_array_int32_2d_F_complex_3d_expr():
+
+    f1 = arrays.array_int32_2d_F_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x1 = np.array( [[1,2,3], [4,5,6]], dtype=np.int32, order='F' )
+    x2 = np.copy(x1)
+    a  = np.array( [[-1,-2,-3], [-4,-5,-6]], dtype=np.int32, order='F' )
+
+    f1(x1, a)
+    f2(x2, a)
+
+    assert np.array_equal( x1, x2 )
+
+def test_array_int32_in_bool_out_1d_complex_3d_expr():
+
+    f1 = arrays.array_int32_in_bool_out_1d_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x  = np.array( [1,2,3], dtype=np.int32 )
+    a  = np.array( [-1,-2,-3], dtype=np.int32 )
+    r1 = np.empty( 3 , dtype=np.bool )
+    r2 = np.copy(r1)
+
+    f1(x, a, r1)
+    f2(x, a, r2)
+
+    assert np.array_equal( r1, r2 )
+
+def test_array_int32_in_bool_out_2d_C_complex_3d_expr():
+
+    f1 = arrays.array_int32_in_bool_out_2d_C_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x  = np.array( [[1,2,3], [4,5,6]], dtype=np.int32 )
+    a  = np.array( [[-1,-2,-3], [-4,-5,-6]], dtype=np.int32 )
+    r1 = np.empty( (2,3) , dtype=np.bool )
+    r2 = np.copy(r1)
+
+    f1(x, a, r1)
+    f2(x, a, r2)
+
+    assert np.array_equal( r1, r2 )
+
+def test_array_int32_in_bool_out_2d_F_complex_3d_expr():
+
+    f1 = arrays.array_int32_in_bool_out_2d_F_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x  = np.array( [[1,2,3], [4,5,6]], dtype=np.int32, order='F' )
+    a  = np.array( [[-1,-2,-3], [-4,-5,-6]], dtype=np.int32, order='F' )
+    r1 = np.empty( (2,3) , dtype=np.bool, order='F' )
+    r2 = np.copy(r1)
+
+    f1(x, a, r1)
+    f2(x, a, r2)
+
+    assert np.array_equal( r1, r2 )
+
+def test_array_real_1d_complex_3d_expr():
+
+    f1 = arrays.array_real_1d_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x1 = np.array( [1.,2.,3.] )
+    x2 = np.copy(x1)
+    a  = np.array( [-1.,-2.,-3.] )
+
+    f1(x1, a)
+    f2(x2, a)
+
+    assert np.array_equal( x1, x2 )
+
+def test_array_real_2d_C_complex_3d_expr():
+
+    f1 = arrays.array_real_2d_C_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x1 = np.array( [[1.,2.,3.], [4.,5.,6.]] )
+    x2 = np.copy(x1)
+    a  = np.array( [[-1.,-2.,-3.], [-4.,-5.,-6.]] )
+
+    f1(x1, a)
+    f2(x2, a)
+
+    assert np.array_equal( x1, x2 )
+
+def test_array_real_2d_F_complex_3d_expr():
+
+    f1 = arrays.array_real_2d_F_complex_3d_expr
+    f2 = epyccel( f1 )
+
+    x1 = np.array( [[ 1., 2., 3.], [4.,5.,6.]], order='F' )
+    x2 = np.copy(x1)
+    a  = np.array( [[-1.,-2.,-3.], [-4.,-5.,-6.]], order='F' )
+
+    f1(x1, a)
+    f2(x2, a)
+
+    assert np.array_equal( x1, x2 )
+
 #==============================================================================
 # TEST: 1D Stack ARRAYS OF REAL
 #==============================================================================
