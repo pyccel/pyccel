5b4d742b41d2c14b21f4749d7dc8b075623ed902
EmilyBourne <louise.bourne@gmail.com>
2020-09-16 18:18:43 +0200

    Cwrapper rewrite (#428)
    Rewrite of the cwrapper to increase modularity. This allows more complicated datatypes to be handled. Bool is implemented as argument/return value (fixes #390). Complex is implemented as argument/return value (fixes #386). Default arguments are implemented (fixes #413)
    
    - Create `pyccel/codegen/printer/cwrappercode.py` file which has the same layout as the other printers and inherits from `CCodePrinter`
    - Create `ast/cwrapper.py` file which contains any nodes or objects which are created for the `CWrapperCodePrinter`
    - Make Module into a mutable class to allow the name to be modified (it must match the name of the generated shared library file)
    - Create `VariableAddress` class to handle passing the address of pointers/variables. Add necessary prints
    - Create `ImaginaryUnit` class. Previously sympy was used however complex initialisation in C requires mathematical interaction with the `ImaginaryUnit`
    - Remove dead code from codegen/cwrapper.py
    - Add printing of `Complex`, `PythonComplex`, `ImaginaryUnit`, `AliasAssign`, `Nil`, `Bool`, `Real`, `Imag` in `ccode.py`
    - Add printing of imports in `_print_Module`
    - Add additional_imports property to collect necessary additional imports (e.g. stdio.h, complex.h)
    - Add `find_in_dtype_registry` and `get_declare` type functions to reduce code duplication
    - Change `_print_Symbol` to `_print_Variable`
    - Ensure parser argument is provided to printer class. Update docstring
    - Remove unused `_print_ValuedArgument` function in `fcode.py` and add `_print_ValuedVariable`. Print valued variable if this was used in python
    - Allow us to know if an argument is keyword only (no way to enforce this in the c wrapper currently)
    - Activate tests with bool arguments/return values
    - Activate tests with complex arguments/return values
    - Activate tests with default arguments
    - Add tests for bool/complex default arguments
    - Mark tests for bool/complex default arguments as xfail in fortran
    
    
    Co-authored-by: menoIy <karim.daou.1993@gmail.com>

--- tests/epyccel/test_base.py
+++ tests/epyccel/test_base.py
@@ -52,25 +52,25 @@ def test_not_true():
     compare_epyccel(base.not_true, True)
     compare_epyccel(base.not_true, False)
 
-def test_eq_false():
-    compare_epyccel(base.eq_false, True)
-    compare_epyccel(base.eq_false, False)
+def test_eq_false(language):
+    compare_epyccel(base.eq_false, True, language=language)
+    compare_epyccel(base.eq_false, False, language=language)
 
-def test_eq_true():
-    compare_epyccel(base.eq_true, True)
-    compare_epyccel(base.eq_true, False)
+def test_eq_true(language):
+    compare_epyccel(base.eq_true, True, language=language)
+    compare_epyccel(base.eq_true, False, language=language)
 
-def test_neq_false():
-    compare_epyccel(base.eq_false, True)
-    compare_epyccel(base.eq_false, False)
+def test_neq_false(language):
+    compare_epyccel(base.eq_false, True, language=language)
+    compare_epyccel(base.eq_false, False, language=language)
 
-def test_neq_true():
-    compare_epyccel(base.eq_true, True)
-    compare_epyccel(base.eq_true, False)
+def test_neq_true(language):
+    compare_epyccel(base.eq_true, True, language=language)
+    compare_epyccel(base.eq_true, False, language=language)
 
-def test_not():
-    compare_epyccel(base.not_val, True)
-    compare_epyccel(base.not_val, False)
+def test_not(language):
+    compare_epyccel(base.not_val, True, language=language)
+    compare_epyccel(base.not_val, False, language=language)
 
 @pytest.mark.xfail(reason="f2py does not support optional arguments https://github.com/numpy/numpy/issues/4013")
 def test_compare_is_nil():
@@ -84,17 +84,17 @@ def test_cast_int(language):
     compare_epyccel(base.cast_int, 4, language=language)
     compare_epyccel(base.cast_float_to_int, 4.5, language=language)
 
-def test_cast_bool():
-    compare_epyccel(base.cast_bool, True)
+def test_cast_bool(language):
+    compare_epyccel(base.cast_bool, True, language=language)
 
 def test_cast_float(language):
     compare_epyccel(base.cast_float, 4.5, language=language)
     compare_epyccel(base.cast_int_to_float, 4, language=language)
 
-def test_if_0_int():
-    compare_epyccel(base.if_0_int, 22)
-    compare_epyccel(base.if_0_int, 0)
+def test_if_0_int(language):
+    compare_epyccel(base.if_0_int, 22, language=language)
+    compare_epyccel(base.if_0_int, 0, language=language)
 
-def test_if_0_real():
-    compare_epyccel(base.if_0_real, 22.3)
-    compare_epyccel(base.if_0_real, 0.0)
+def test_if_0_real(language):
+    compare_epyccel(base.if_0_real, 22.3, language=language)
+    compare_epyccel(base.if_0_real, 0.0, language=language)
