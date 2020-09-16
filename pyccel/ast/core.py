677aaf8f6d29ab2a1789326fa533d55fbab93bb8
Emily.Bourne <louise.bourne@gmail.com>
2020-09-16 17:09:34 +0200

    Make kwonly a keyword only argument

--- pyccel/ast/core.py
+++ pyccel/ast/core.py
@@ -2927,10 +2927,10 @@ class Argument(Symbol, PyccelAstNode):
     n
     """
 
-    def __new__(cls, *args, kwonly=False, **assumptions):
+    def __new__(cls, *args, *, kwonly=False, **assumptions):
         return Symbol.__new__(cls, *args, **assumptions)
 
-    def __init__(self, *args, kwonly=False, **assumptions):
+    def __init__(self, *args, *, kwonly=False, **assumptions):
         self._kwonly = kwonly
 
     @property
