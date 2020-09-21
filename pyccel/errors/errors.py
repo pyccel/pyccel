7771896e76706e8eb8cb03ee43d5f42775ed55be
Emily.Bourne <louise.bourne@gmail.com>
2020-09-21 09:51:46 +0200

    Use ast object to collect line information in errors class

--- pyccel/errors/errors.py
+++ pyccel/errors/errors.py
@@ -1,4 +1,5 @@
 from collections import OrderedDict
+from ast import stmt as ast_stmt, dump as ast_dump
 
 # ...
 #ERROR = 'error'
@@ -220,6 +221,17 @@ class Errors:
         if bounding_box:
             line   = bounding_box[0]
             column = bounding_box[1]
+
+        if symbol is not None:
+            if isinstance(symbol, ast_stmt):
+                line   = fst.lineno
+                column = fst.col_offset
+                symbol = ast_dump(symbol)
+            else:
+                fst = getattr(symbol, 'fst', None)
+                if fst is not None:
+                    line   = fst.lineno
+                    column = fst.col_offset
 
         info = ErrorInfo(filename,
                          line=line,
