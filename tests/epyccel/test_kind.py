276502851f68de25afbf9e13103fa2040f88d8b0
Emily.Bourne <louise.bourne@gmail.com>
2020-09-24 16:46:45 +0200

    Reorder flags

--- tests/epyccel/test_kind.py
+++ tests/epyccel/test_kind.py
@@ -43,10 +43,10 @@ def test_input_output_matching_types(language):
         return c
 
     fflags="-Werror -Wconversion"
-    if platform.system() == 'Darwin': # If macosx
-        fflags=fflags+" -Wno-error=unused-command-line-argument"
     if language=="fortran":
         fflags=fflags+"-extra"
+    if platform.system() == 'Darwin': # If macosx
+        fflags=fflags+" -Wno-error=unused-command-line-argument"
     epyc_add_real = epyccel(add_real, fflags=fflags, language=language)
 
     assert(add_real(1.0,2.0)==epyc_add_real(1.0,2.0))
