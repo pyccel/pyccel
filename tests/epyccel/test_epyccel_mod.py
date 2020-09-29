adaca8de1e40e47a18f42bcb774823d70e2141d6
rakati <ouhaddounouredine@gmail.com>
2020-09-29 10:06:58 +0100

    remove: whitespaces

--- tests/epyccel/test_epyccel_mod.py
+++ tests/epyccel/test_epyccel_mod.py
@@ -10,7 +10,7 @@ def test_modulo_int_int(language):
     @types(int, int)
     def modulo_i_i(x, y):
         return x % y
-    
+
     f = epyccel(modulo_i_i, language=language)
     x = randint(1e6)
     y = randint(1e6) + 1
@@ -22,7 +22,7 @@ def test_modulo_real_real(language):
     @types('real', 'real')
     def modulo_r_r(x, y):
         return x % y
-    
+
     f = epyccel(modulo_r_r, language=language)
     x = uniform(high=1e6)
     y = uniform(high=1e6) + 1
@@ -34,7 +34,7 @@ def test_modulo_real_int(language):
     @types('real', 'int')
     def modulo_r_i(x, y):
         return x % y
-    
+
     f = epyccel(modulo_r_i, language=language)
     x = uniform(high=1e6)
     y = randint(1e6) + 1
@@ -46,7 +46,7 @@ def test_modulo_int_real(language):
     @types('int', 'real')
     def modulo_i_r(x, y):
         return x % y
-    
+
     f = epyccel(modulo_i_r, language=language)
     x = randint(1e6)
     y = uniform(high=1e6) + 1
@@ -58,7 +58,7 @@ def test_modulo_multiple(language):
     @types('int', 'real', 'int')
     def modulo_multiple(x, y, z):
         return x % y % z
-    
+
     f = epyccel(modulo_multiple, language=language)
     x = randint(1e6)
     y = uniform(high=1e6) + 1
