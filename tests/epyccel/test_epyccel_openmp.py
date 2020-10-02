e6610c8855a66d0024ee87641fb696434fec5153
mostafa <mostafa-1470@live.fr>
2020-10-02 12:28:11 +0100

    make the code readable

--- tests/epyccel/test_epyccel_openmp.py
+++ tests/epyccel/test_epyccel_openmp.py
@@ -17,7 +17,9 @@ def test_module_1():
     assert mod.f1(2) == 2
     assert mod.f1(3) == 3
     assert mod.f1(5) == -1
+
     assert mod.test_omp_number_of_procs() == multiprocessing.cpu_count()
+
     omp_cancel = os.environ.get('OMP_CANCELLATION')
     if omp_cancel is not None:
         if omp_cancel.lower() == 'true':
@@ -26,10 +28,16 @@ def test_module_1():
             assert mod.test_omp_get_cancellation() == 0
     else:
         assert mod.test_omp_get_cancellation() == 0
+
     assert mod.test_omp_in_parallel1() == 0
     assert mod.test_omp_in_parallel2() == 1
+
     assert mod.test_omp_set_get_dynamic(1) == 1
     assert mod.test_omp_set_get_dynamic(0) == 0
+
     mod.set_num_threads(8)
     assert mod.get_num_threads() == 8
+
+    assert mod.test_omp_get_max_task_priority() > 0
+
     assert mod.f1(5) == 5
