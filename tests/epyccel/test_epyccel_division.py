9dc97cd2841b20db636073c273dbce8398a3dd9a
rakati <ouhaddounouredine@gmail.com>
2020-10-05 16:49:16 +0100

    Add: complex number division tests

--- tests/epyccel/test_epyccel_division.py
+++ tests/epyccel/test_epyccel_division.py
@@ -61,6 +61,73 @@ def test_call_div_r_r(language):
     assert (f(x, -y) == div_r_r(x, -y))
     assert (f(-x, -y) == div_r_r(-x, -y))
 
+# -------------------- Complex division ---------------------- #
+
+def test_call_div_c_c(language):
+    @types('complex', 'complex')
+    def div_c_c(x, y):
+        return x / y
+
+    f = epyccel(div_c_c, language=language)
+    x = complex(uniform(high= 1e5), uniform(high= 1e5))
+    y = complex(uniform(low=1, high= 1e2), uniform(low=1, high= 1e2))
+    assert (f(x, y) == div_c_c(x, y))
+    assert (f(-x, y) == div_c_c(-x, y))
+    assert (f(x, -y) == div_c_c(x, -y))
+    assert (f(-x, -y) == div_c_c(-x, -y))
+
+def test_call_div_i_c(language):
+    @types(int, 'complex')
+    def div_i_c(x, y):
+        return x / y
+
+    f = epyccel(div_i_c, language=language)
+    x = randint(1e5)
+    y = complex(uniform(low=1, high= 1e2), uniform(low=1, high= 1e2))
+    assert (f(x, y) == div_i_c(x, y))
+    assert (f(-x, y) == div_i_c(-x, y))
+    assert (f(x, -y) == div_i_c(x, -y))
+    assert (f(-x, -y) == div_i_c(-x, -y))
+
+def test_call_div_c_i(language):
+    @types('complex', int)
+    def div_c_i(x, y):
+        return x / y
+
+    f = epyccel(div_c_i, language=language)
+    x = complex(uniform(high= 1e5), uniform(high= 1e5))
+    y = randint(low=1, high= 1e2)
+    assert (f(x, y) == div_c_i(x, y))
+    assert (f(-x, y) == div_c_i(-x, y))
+    assert (f(x, -y) == div_c_i(x, -y))
+    assert (f(-x, -y) == div_c_i(-x, -y))
+
+def test_call_div_r_c(language):
+    @types('real', 'complex')
+    def div_r_c(x, y):
+        return x / y
+
+    f = epyccel(div_r_c, language=language)
+    x = uniform(high=1e9)
+    y = complex(uniform(low=1, high= 1e2), uniform(low=1, high= 1e2))
+    assert (f(x, y) == div_r_c(x, y))
+    assert (f(-x, y) == div_r_c(-x, y))
+    assert (f(x, -y) == div_r_c(x, -y))
+    assert (f(-x, -y) == div_r_c(-x, -y))
+
+def test_call_div_c_r(language):
+    @types('complex', 'real')
+    def div_c_r(x, y):
+        return x / y
+
+    f = epyccel(div_c_r, language=language)
+    x = complex(uniform(high= 1e5), uniform(high= 1e5))
+    y = uniform(low=1e-14, high= 1e3)
+    assert (f(x, y) == div_c_r(x, y))
+    assert (f(-x, y) == div_c_r(-x, y))
+    assert (f(x, -y) == div_c_r(x, -y))
+    assert (f(-x, -y) == div_c_r(-x, -y))
+
 # -------------------- floor division ---------------------- #
 
 def test_call_fdiv_i_i(language):
