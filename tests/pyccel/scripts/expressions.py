1820ab800147ea0e508beea7ec139fc8947db12e
rakati <ouhaddounouredine@gmail.com>
2020-10-09 17:45:01 +0100

    remove: commented expressions

--- tests/pyccel/scripts/expressions.py
+++ tests/pyccel/scripts/expressions.py
@@ -1,8 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
 a1 = 2/2
 
-# pyccel div with need parentheses
-# a2 = 2/2j
 
 a3 = 2//2
 a4 = 2.//2
@@ -16,15 +14,14 @@ a9  = 1.*10
 a10 = 1.*10j
 
 b1 = 1-1-2-4-5
-# b2 = 1-1-2j-4.-5
-# b3 = 1-1-2j-5
+
 b4 = 1-(1-1)
 
 c1 = 2**2
 c2 = 2**2.
 c3 = 2**2j
 c4 = 2j**3
-# c5 = 2j**4.
+
 c6 = 2.**-4
 c7 = -2.**4
 c8 = -.2**-.4
@@ -34,8 +31,7 @@ d2 = 1-1-2**2.-4*3+7-5
 d3 = 3*(1+4//2*3.-3.)
 d4 = (2*2)**(3*4)
 
-# f1  = 100/10/10/2
-# f2  = 100/-10/10/-2
+
 f3  = 100//4//5//2
 f4  = 100//-4//5//-2
 f5  = 100.//4//5//2
@@ -51,8 +47,6 @@ e1 = -(a1 + a3)
 e2 = 23*4%16.032/16.4//2./2%3 + 34 + 23*23*4%16.0+32/16.4//2./2%3
 e3 = (1%1**4/23**2/6//2.)
 e4 = 2 * 4 + 3**4 - 3*5**3//2
-# e5 = (1%16.032/16.032//25.9948+25.9948*25.99-(25.9-33.74//33.746134%33.746139653844324-33.7464%36.13899562242578)-36.13899562242578*\
-     # (36.1389-36.138*6.133%6.1332**6.1332%6.1332)%(11.57607+11.5747*11.5760-11.57609-22.9+ 100//6//2.//3)**(0.5))**(3/4 + 3//2)
 
 x = 10
 y = 4
@@ -67,7 +61,7 @@ g8 = True and True or False and False
 g9 = False or True and False or True
 
 print(a1)
-# print(a2)
+
 print(a3)
 print(a4)
 print(a5)
@@ -78,15 +72,13 @@ print(a9)
 print(a10)
 
 print(b1)
-# print(b2)
-# print(b3)
+
 print(b4)
 
 print(c1)
 print(c2)
 print(c3)
 print(c4)
-# print(c5)
 print(c6)
 print(c7)
 print(c8)
@@ -100,10 +92,7 @@ print(e1)
 print(e2)
 print(e3)
 print(e4)
-# print(e5)
 
-# print(f1)
-# print(f2)
 print(f3)
 print(f4)
 print(f5)
@@ -113,7 +102,6 @@ print(f8)
 print(f9)
 print(f10)
 print(f11)
-# print(f12)
 
 print(g1)
 print(g2)
