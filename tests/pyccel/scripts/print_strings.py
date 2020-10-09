53ec90b2afb0ba4db037eaca773eda2e52fe70a1
rakati <ouhaddounouredine@gmail.com>
2020-10-09 03:40:02 +0100

    Add: print string tests

---
+++ tests/pyccel/scripts/print_strings.py
@@ -0,0 +1,20 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring/
+# ------------------------------- Strings ------------------------------------
+
+print("mcha bba lsbata")
+print('voilaaa')
+print("3...2...1...Go!")
+print('line1\nline2\nline3')
+
+print('The last sentence.')
+print('Preventing Line Breaks')
+print('"walo" + "walo" == \'hiya walo\'.')
+print('//\\//\/\/\\/\/\/\\/\/\\/\/\/\\/\/')
+print("[^_^]")
+print("[-_-]")
+print("[+__+]")
+print('[*-*]')
+print("(0_0)")
+print(1, 2, 3, "mcha bba lsbata")
+print("yan", "sin", "krad")
+print('Azul', 'fellawn')
