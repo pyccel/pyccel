207de2c51039b26d9c2449b4c3e8b911ae8c97f2
Emily.Bourne <louise.bourne@gmail.com>
2020-10-15 19:02:41 +0200

    Only include imports once for the module

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -285,8 +285,7 @@ class CCodePrinter(CodePrinter):
         body    = '\n\n'.join(self._print(i) for i in expr.body)
 
         # Print imports last to be sure that all additional_imports have been collected
-        imports = [*expr.imports, *map(Import, self._additional_imports)]
-        imports = '\n'.join(self._print(i) for i in imports)
+        imports = self._print(Import(expr.name))
         return ('{imports}\n\n'
                 '{body}').format(
                         imports = imports,
