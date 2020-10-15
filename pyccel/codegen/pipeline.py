38d0cfedfada99d6d988360511cbd49c438521c6
nhamidn <mac@MacBook-Pro-de-mac.local>
2020-10-15 13:13:06 +0100

    remove -lomp when creating a shared library

--- pyccel/codegen/pipeline.py
+++ pyccel/codegen/pipeline.py
@@ -235,6 +235,9 @@ def execute_pyccel(fname, *,
                                 includes=includes,
                                 libdirs=libdirs)
 
+        if not codegen.is_program and language == "c":
+            if sys.platform == "darwin":
+                flags = flags.replace("-lomp", "")
         # Compile Fortran code
         #
         # TODO: stop at object files, do not compile executable
