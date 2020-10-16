58428612fab0029b72916551ab36806fb28b455a
melalj <mouadelalj@gmail.com>
2020-10-16 19:50:35 +0100

    trying to create a map function

--- samples/c_equivalent/ndarray_dump.c
+++ samples/c_equivalent/ndarray_dump.c
@@ -2,48 +2,53 @@
 
 int array_value_dump(t_ndarray *arr)
 {
-    switch (arr->nd)
-    {
-        case 1:
-            for (int i = 0; i < arr->lenght; i++)
-            {
-                printf(" %f,", arr->data->double_nd[get_index(arr, i)]);
-            }
-            putchar('\n');
-            break;
-        case 2:
-            for (int i = 0; i < arr->shape[0]; i++)
-            {
-                for (int j = 0; j < arr->shape[1]; j++)
-                    printf(" %f,", arr->data->double_nd[get_index(arr, i, j)]);
-                putchar('\n');  
-            }
-            putchar('\n');  
-            break;
-        case 3:
-            for (int i = 0; i < arr->shape[0]; i++)
-            {
-                for (int j = 0; j < arr->shape[1]; j++)
-                {
-                    for (int k = 0; k < arr->shape[2]; k++)
-                        printf(" %f,", arr->data->double_nd[get_index(arr, i, j, k)]);
-                    putchar('\n');  
-                }
-                putchar('\n');  
-                putchar('\n');  
-            }
-            putchar('\n');  
-            break;
-        default:
-            break;
-    }
-    return (1);
+	switch (arr->nd)
+	{
+		case 1:
+			for (int i = 0; i < arr->lenght; i++)
+			{
+				printf(" %f,", arr->data->double_nd[get_index(arr, i)]);
+			}
+			putchar('\n');
+			break;
+		case 2:
+			for (int i = 0; i < arr->shape[0]; i++)
+			{
+				for (int j = 0; j < arr->shape[1]; j++)
+					printf(" %f,", arr->data->double_nd[get_index(arr, i, j)]);
+				putchar('\n');  
+			}
+			putchar('\n');  
+			break;
+		case 3:
+			for (int i = 0; i < arr->shape[0]; i++)
+			{
+				for (int j = 0; j < arr->shape[1]; j++)
+				{
+					for (int k = 0; k < arr->shape[2]; k++)
+						printf(" %f,", arr->data->double_nd[get_index(arr, i, j, k)]);
+					putchar('\n');
+				}
+				putchar('\n');
+				putchar('\n');
+			}
+			putchar('\n');
+			break;
+		default:
+			break;
+	}
+	return (1);
 }
+
 int array_data_dump(t_ndarray *arr)
 {
-    int a;
-    printf("array : \n\tndim %d\n\ttype %d\n\tlenght %d\n", arr->nd, arr->type, arr->lenght);
-    printf(" %d - %d - %d\n", arr->shape[0], arr->shape[1], arr->shape[2]);
-    array_value_dump(arr);
-    return (1);
+	int a;
+	printf("array : \n\tndim %d\n\ttype %d\n\tlenght %d\n", arr->nd, arr->type, arr->lenght);
+	printf(" %d - %d - %d\n", arr->shape[0], arr->shape[1], arr->shape[2]);
+	// for (int i = 0; i < arr->lenght; i++)
+	// {
+	// 	printf(" %f,", arr->data->double_nd[get_index_o(arr, i)]);
+	// }
+	array_value_dump(arr);
+	return (1);
 }
