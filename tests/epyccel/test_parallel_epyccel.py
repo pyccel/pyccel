3350a3b84237e22b4822d51b9f7cc46e45ddd132
Yaman Güçlü <yaman.guclu@gmail.com>
2020-10-26 18:52:50 +0100

    Test parallel epyccel on functions

--- tests/epyccel/test_parallel_epyccel.py
+++ tests/epyccel/test_parallel_epyccel.py
@@ -43,3 +43,33 @@ def test_module_2():
 
     assert np.allclose( x, x_expected, rtol=1e-15, atol=1e-15 )
     # ...
+
+#==============================================================================
+@pytest.mark.parallel
+def test_function():
+    from modules.Module_1 import f, g, h
+
+    comm = MPI.COMM_WORLD
+
+    f_fast = epyccel(f, comm=comm)
+    g_fast = epyccel(g, comm=comm)
+    h_fast = epyccel(h, comm=comm)
+
+    assert f_fast is not f
+    assert g_fast is not g
+    assert h_fast is not h
+
+    x_expected = np.zeros(3)
+    f(x_expected)
+    g(x_expected)
+    h(x_expected)
+
+    x = np.zeros(3)
+    f_fast(x)
+    g_fast(x)
+    h_fast(x)
+
+    print(x_expected)
+    print(x)
+
+    assert np.array_equal(x, x_expected)
