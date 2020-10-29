c4312d5e55368362df51b40a758a3f756b943fa0
Emily.Bourne <louise.bourne@gmail.com>
2020-10-29 10:46:41 +0100

    Mark c tests with c marker

--- tests/epyccel/test_higherorder_functions.py
+++ tests/epyccel/test_higherorder_functions.py
@@ -2,6 +2,7 @@
 import modules.highorder_functions as mod
 from pyccel.epyccel import epyccel
 
+@pytest.mark.c
 def test_int_1():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -9,6 +10,7 @@ def test_int_1():
     x = modnew.test_int_1()
     assert x == x_expected
 
+@pytest.mark.c
 def test_int_int_1():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -16,6 +18,7 @@ def test_int_int_1():
     x = modnew.test_int_int_1()
     assert x == x_expected
 
+@pytest.mark.c
 def test_real_1():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -23,6 +26,7 @@ def test_real_1():
     x = modnew.test_real_1()
     assert x == x_expected
 
+@pytest.mark.c
 def test_real_2():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -30,6 +34,7 @@ def test_real_2():
     x = modnew.test_real_2()
     assert x == x_expected
 
+@pytest.mark.c
 def test_real_3():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -37,6 +42,7 @@ def test_real_3():
     x = modnew.test_real_3()
     assert x == x_expected
 
+@pytest.mark.c
 def test_valuedarg_1():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -44,6 +50,7 @@ def test_valuedarg_1():
     x = modnew.test_valuedarg_1()
     assert x == x_expected
 
+@pytest.mark.c
 def test_real_real_int_1():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -51,6 +58,7 @@ def test_real_real_int_1():
     x = modnew.test_real_real_int_1()
     assert x == x_expected
 
+@pytest.mark.c
 def test_real_4():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -58,6 +66,7 @@ def test_real_4():
     x = modnew.test_real_4()
     assert x == x_expected
 
+@pytest.mark.c
 def test_valuedarg_2():
     modnew = epyccel(mod, language = "c", verbose = True)
 
@@ -65,6 +74,7 @@ def test_valuedarg_2():
     x = modnew.test_valuedarg_2()
     assert x == x_expected
 
+@pytest.mark.c
 def test_real_real_int_2():
     modnew = epyccel(mod, language = "c", verbose = True)
 
