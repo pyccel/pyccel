89247691227efad319e718b311b7458ff87e46f1
Emily.Bourne <louise.bourne@gmail.com>
2020-10-29 15:59:43 +0100

    Make language a parameter and skip fortran tests. Remove verbosity

--- tests/epyccel/test_higherorder_functions.py
+++ tests/epyccel/test_higherorder_functions.py
@@ -3,81 +3,83 @@ import pytest
 import modules.highorder_functions as mod
 from pyccel.epyccel import epyccel
 
-@pytest.mark.c
-def test_int_1():
-    modnew = epyccel(mod, language = "c", verbose = True)
+@pytest.fixture( params=[
+        pytest.param("fortran", marks = [
+            pytest.mark.fortran,
+            pytest.mark.skip]),
+        pytest.param("c", marks = [
+            pytest.mark.c]
+        )
+    ]
+)
+def language(request):
+    return request.param
+
+def test_int_1(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_int_1()
     x = modnew.test_int_1()
     assert x == x_expected
 
-@pytest.mark.c
-def test_int_int_1():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_int_int_1(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_int_int_1()
     x = modnew.test_int_int_1()
     assert x == x_expected
 
-@pytest.mark.c
-def test_real_1():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_real_1(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_real_1()
     x = modnew.test_real_1()
     assert x == x_expected
 
-@pytest.mark.c
-def test_real_2():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_real_2(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_real_2()
     x = modnew.test_real_2()
     assert x == x_expected
 
-@pytest.mark.c
-def test_real_3():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_real_3(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_real_3()
     x = modnew.test_real_3()
     assert x == x_expected
 
-@pytest.mark.c
-def test_valuedarg_1():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_valuedarg_1(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_valuedarg_1()
     x = modnew.test_valuedarg_1()
     assert x == x_expected
 
-@pytest.mark.c
-def test_real_real_int_1():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_real_real_int_1(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_real_real_int_1()
     x = modnew.test_real_real_int_1()
     assert x == x_expected
 
-@pytest.mark.c
-def test_real_4():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_real_4(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_real_4()
     x = modnew.test_real_4()
     assert x == x_expected
 
-@pytest.mark.c
-def test_valuedarg_2():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_valuedarg_2(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_valuedarg_2()
     x = modnew.test_valuedarg_2()
     assert x == x_expected
 
-@pytest.mark.c
-def test_real_real_int_2():
-    modnew = epyccel(mod, language = "c", verbose = True)
+def test_real_real_int_2(language):
+    modnew = epyccel(mod, language = language)
 
     x_expected = mod.test_real_real_int_2()
     x = modnew.test_real_real_int_2()
