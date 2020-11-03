9cda93d7a861bbd4b30dc4ebbc7c533cb124b86f
rmahjoubi <redouanemahjoubii@gmail.com>
2020-11-03 03:46:23 +0100

    add tests for templates and multiple headers and many combinations of the two

--- tests/epyccel/test_generic_functions.py
+++ tests/epyccel/test_generic_functions.py
@@ -15,8 +15,80 @@ from pyccel.epyccel import epyccel
 def language(request):
     return request.param
 
-@pytest.mark.parametrize("a",[(5),(5.5)])
-def test_f1(language, a):
-    f1 = mod.f1
-    f2 = epyccel(f1, language = language)
-    assert f1(a) == f2(a)
+def test_gen_1(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_gen_1()
+    x = modnew.tst_gen_1()
+    assert x == x_expected
+
+def test_gen_2(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_gen_2()
+    x = modnew.tst_gen_2()
+    assert x == x_expected
+
+def test_gen_3(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_gen_3()
+    x = modnew.tst_gen_3()
+    assert x == x_expected
+
+def test_gen_4(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_gen_4()
+    x = modnew.tst_gen_4()
+    assert x == x_expected
+
+def test_gen_5(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_gen_5()
+    x = modnew.tst_gen_5()
+    assert x == x_expected
+
+def test_gen_6(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_gen_6()
+    x = modnew.tst_gen_6()
+    assert x == x_expected
+
+def test_gen_7(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_gen_7()
+    x = modnew.tst_gen_7()
+    assert x == x_expected
+
+def test_multi_heads_1(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_multi_heads_1()
+    x = modnew.tst_multi_heads_1()
+    assert x == x_expected
+
+def test_tmplt_1(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_tmplt_1()
+    x = modnew.tst_tmplt_1()
+    assert x == x_expected
+
+def test_multi_tmplt_1(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_multi_tmplt_1()
+    x = modnew.tst_multi_tmplt_1()
+    assert x == x_expected
+
+def test_tmplt_head_1(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_tmplt_head_1()
+    x = modnew.tst_tmplt_head_1()
+    assert x == x_expected
+
+def test_local_overide_1(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_local_overide_1()
+    x = modnew.tst_local_overide_1()
+    assert x == x_expected
+
+def test_tmplt_tmplt_1(language):
+    modnew = epyccel(mod, language = language)
+    x_expected = mod.tst_tmplt_tmplt_1()
+    x = modnew.tst_tmplt_tmplt_1()
+    assert x == x_expected
