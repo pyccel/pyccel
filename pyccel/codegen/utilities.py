d46494bad52e9c80db6730e63fd0d4bc3b8a2d82
Emily.Bourne <louise.bourne@gmail.com>
2020-11-06 10:48:02 +0100

    Use -Xpreprocessor for gcc and gfortran

--- pyccel/codegen/utilities.py
+++ pyccel/codegen/utilities.py
@@ -70,7 +70,7 @@ def construct_flags(compiler,
 
     if accelerator is not None:
         if accelerator == "openmp":
-            if sys.platform == "darwin" and compiler == "gcc":
+            if sys.platform == "darwin" and compiler in ["gcc", "gfortran"]:
                 flags += " -Xpreprocessor"
 
             if compiler == 'ifort':
