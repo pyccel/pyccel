97c45afb5128fd238f8cc379d70e64e721149df1
rakati <ouhaddounouredine.com>
2020-11-16 13:28:49 +0100

    restore additional imports in module header

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -273,8 +273,8 @@ class CCodePrinter(CodePrinter):
         # TODO: Add classes and interfaces
         funcs = '\n\n'.join('{};'.format(self.function_signature(f)) for f in expr.module.funcs)
 
-        # Print imports
-        imports = [*expr.module.imports]
+        # Print imports last to be sure that all additional_imports have been collected
+        imports = [*expr.module.imports,*map(Import, self._additional_imports)]
         imports = '\n'.join(self._print(i) for i in imports)
 
         return ('#ifndef {name}_H\n'
