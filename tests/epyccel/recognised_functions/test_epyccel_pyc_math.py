0e519509bf34ec38873e4b8b8492d82f994cac15
rakati <ouhaddounouredine@gmail.com>
2020-11-25 18:35:46 +0100

    remove: conftest import and import pytest

--- tests/epyccel/recognised_functions/test_epyccel_pyc_math.py
+++ tests/epyccel/recognised_functions/test_epyccel_pyc_math.py
@@ -1,9 +1,9 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
 # pylint: disable=wildcard-import
 import sys
+import pytest
 from numpy.random import randint, uniform
 from numpy import isclose
-from conftest       import *
 
 from pyccel.epyccel import epyccel
 from pyccel.decorators import types
