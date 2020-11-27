1aa8b35aacf7c33980abcb78c4f2c0eacb76dd09
menoIy <karim.daou.1993@gmail.com>
2020-11-27 16:34:34 +0100

    codacy warning

--- pyccel/ast/cwrapper.py
+++ pyccel/ast/cwrapper.py
@@ -138,6 +138,9 @@ class PyArg_ParseTupleNode(Basic):
         Default value False and True when working with interface functions
     """
 
+    def __new__(cls, *args, **kwargs):
+        return Basic.__new__(cls, *args, **kwargs)
+
     def __init__(self, python_func_args,
                         python_func_kwargs,
                         c_func_args, parse_args,
