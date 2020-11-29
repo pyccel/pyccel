52910f3328160388ec9e3e314efb05e8e23b7082
rakati <ouhaddounouredine@gmail.com>
2020-11-29 18:18:52 +0100

    combine fortran pyf_math and pyc_math into one lib called math

---
+++ pyccel/stdlib/math/math.f90
@@ -0,0 +1,19 @@
+module pyf_math
+    implicit none
+
+        real, parameter,private :: pi = 3.1415926536
+        real, parameter, private :: e = 2.7182818285
+
+    contains
+        function factorial(x)result(fx)
+        implicit none
+            integer::x
+            integer::fx
+            fx = 1
+            do while (x > 0)
+                fx = fx * x
+                x = x - 1
+            enddo
+        end function factorial
+
+end module pyf_math
