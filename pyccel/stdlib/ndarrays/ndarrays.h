f894d9b53394c9ea4c597377a4942511b371791a
saniQQ <mouadelalj@gmail.com>
2020-11-30 14:17:49 +0100

    change C src files to precise types

--- pyccel/stdlib/ndarrays/ndarrays.h
+++ pyccel/stdlib/ndarrays/ndarrays.h
@@ -21,9 +21,9 @@
                                         double complex : _array_fill_cdouble)(c, arr)
 typedef struct  s_slice
 {
-    int start;
-    int end;
-    int step;
+    int32_t start;
+    int32_t end;
+    int32_t step;
 }               t_slice;
 
 enum e_types
@@ -55,26 +55,26 @@ typedef struct  s_ndarray
             float  complex  *nd_cfloat;
             };
     /* number of dimensions */
-    int                 nd;
+    int32_t                 nd;
     /* shape 'size of each dimension' */
-    int                 *shape;
+    int32_t                 *shape;
     /* strides 'number of bytes to skip to get the next element' */
-    int                 *strides;
+    int32_t                 *strides;
     /* type of the array elements */
     enum e_types        type;
     /* type size of the array elements */
-    int                 type_size;
+    int32_t                 type_size;
     /* number of element in the array */
-    int                 length;
+    int32_t                 length;
     /* size of the array */
-    int                 buffer_size;
+    int32_t                 buffer_size;
     bool                is_slice;
 }               t_ndarray;
 
 /* functions prototypes */
 
 /* allocations */
-t_ndarray   array_create(int nd, int *shape, enum e_types type);
+t_ndarray   array_create(int32_t nd, int32_t *shape, enum e_types type);
 void        _array_fill_int8(int8_t c, t_ndarray arr);
 void        _array_fill_int16(int16_t c, t_ndarray arr);
 void        _array_fill_int32(int32_t c, t_ndarray arr);
@@ -87,14 +87,14 @@ void        _array_fill_cdouble(double complex c, t_ndarray arr);
 
 /* slicing */
                 /* creating a Slice object */
-t_slice     new_slice(int start, int end, int step);
+t_slice     new_slice(int32_t start, int32_t end, int32_t step);
                 /* creating an array view */
 t_ndarray   array_slicing(t_ndarray p, ...);
 
 /* free */
-int         free_array(t_ndarray dump);
+int32_t         free_array(t_ndarray dump);
 
 /* indexing */
-int         get_index(t_ndarray arr, ...);
+int32_t         get_index(t_ndarray arr, ...);
 
 #endif
