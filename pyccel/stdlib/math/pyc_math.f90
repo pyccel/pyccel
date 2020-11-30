0d1197a083d8a3e5e4f7b38ec420c31c2c32aac1
rakati <ouhaddounouredine@gmail.com>
2020-12-01 00:18:37 +0100

    Improve: pyc_math lib for fortran lang
    - Add new functions (gcd, lcm, degrees, radians).
    - Fix errors and precision in factorial functions.
    - Fix PI value for better precision result.

--- pyccel/stdlib/math/pyc_math.f90
+++ pyccel/stdlib/math/pyc_math.f90
@@ -1,19 +1,81 @@
-module math
+module pyc_math
+
     implicit none
 
-        real, parameter,private :: pi = 3.1415926536
-        real, parameter, private :: e = 2.7182818285
+        real(kind=8), parameter, private :: pi = 4.D0 * DATAN(1.D0)
 
     contains
-        function factorial(x)result(fx)
-        implicit none
-            integer::x
-            integer::fx
+
+        ! Implementation of math factorial function
+        function pyc_factorial(x) result(fx)
+
+            implicit none
+            integer(kind=8), value  :: x
+            integer(kind=8)         :: fx
+
             fx = 1
             do while (x > 0)
                 fx = fx * x
                 x = x - 1
             enddo
-        end function factorial
+            return
 
-end module math
+        end function pyc_factorial
+
+        ! Implementation of math gcd function
+        function pyc_gcd(a, b) result(gcd)
+
+            implicit none
+            integer(kind=8), value  :: a
+            integer(kind=8), value  :: b
+            integer(kind=8)         :: gcd
+
+            do while (b > 0)
+                a = MOD(a, b)
+                a = XOR(a, b)
+                b = XOR(b, a)
+                a = XOR(a, b)
+            enddo
+            gcd = a
+            return
+
+        end function pyc_gcd
+
+        ! Implementation of math lcm function
+        function pyc_lcm(a, b) result(lcm)
+
+            implicit none
+            integer(kind=8), value :: a
+            integer(kind=8), value :: b
+            integer(kind=8)        :: lcm
+
+            lcm = a / pyc_gcd(a, b) * b
+            return
+
+        end function pyc_lcm
+
+        ! Implementation of math radians function
+        function pyc_radians(deg) result(rad)
+
+            implicit none
+            real(kind=8), value     :: deg
+            real(kind=8)            :: rad
+
+            rad = deg * (pi / 180.0)
+            return
+
+        end function pyc_radians
+
+        ! Implementation of math degrees function
+        function pyc_degrees(rad) result(deg)
+
+            implicit none
+            real(kind=8), value     :: rad
+            real(kind=8)            :: deg
+
+            deg = rad * (180.0 / pi)
+            return
+
+        end function pyc_degrees
+
+end module pyc_math
