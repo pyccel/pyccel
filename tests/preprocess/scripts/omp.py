0fc37e5a6f5b756b3e0e0b1a56bcb2cab2e3628c
mostafa <32802212+mostafaMamoni@users.noreply.github.com>
2020-12-03 10:47:38 +0100

    Use pythonic syntax in OpenMP pragmas (#523)
    Improve OpenMP pragmas to use Python's keywords (e.g. `#$ omp for` instead of `#$ omp do`) where appropriate:
    
    - Modify grammar of OpenMP pragmas;
    - Write unit tests that work for both C and Fortran.
    
    Co-authored-by: nhamidn <nhamid@student.1337.ma>

--- tests/preprocess/scripts/omp.py
+++ tests/preprocess/scripts/omp.py
@@ -3,43 +3,43 @@
 
 # this tests shows that OpenMP directives are moved to the appropriate parent
 
-#$ omp do schedule(runtime)
+#$ omp for schedule(runtime)
 for i in range(0, n):
     for j in range(0, m):
         a[i,j] = i-j
-#$ omp end do nowait
+#$ omp end for nowait
 
 def f(n,m,a):
-    #$ omp do schedule(runtime)
+    #$ omp for schedule(runtime)
     for i in range(0, n):
         for j in range(0, m):
             a[i,j] = i-j
-    #$ omp end do nowait
+    #$ omp end for nowait
 
 with toto:
-    #$ omp do schedule(runtime)
+    #$ omp for schedule(runtime)
     for i in range(0, n):
         for j in range(0, m):
             a[i,j] = i-j
-    #$ omp end do nowait
+    #$ omp end for nowait
 
 
 if init=='a':
-    #$ omp do schedule(runtime)
+    #$ omp for schedule(runtime)
     for i in range(0, n):
         for j in range(0, m):
             a[i,j] = i-j
-    #$ omp end do nowait
+    #$ omp end for nowait
 elif init=='b':
-    #$ omp do schedule(runtime)
+    #$ omp for schedule(runtime)
     for i in range(0, n):
         for j in range(0, m):
             b[i,j] = i-j
-    #$ omp end do nowait
+    #$ omp end for nowait
 else:
-    #$ omp do schedule(runtime)
+    #$ omp for schedule(runtime)
     for i in range(0, n):
         for j in range(0, m):
             c[i,j] = i-j
-    #$ omp end do nowait
+    #$ omp end for nowait
 
