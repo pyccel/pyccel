3137fdc1847d34527928a7517915e3c9595d8135
Redouane Mahjoubi <redouanemahjoubii@gmail.com>
2020-12-08 14:13:08 +0100

    add printing masked construct

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -1199,6 +1199,11 @@ class CCodePrinter(CodePrinter):
         omp_expr = '#pragma omp {}\n{{'.format(omp_expr)
         return omp_expr
 
+    def _print_OMP_Masked_Construct(self, expr):
+        omp_expr = str(expr.txt)
+        omp_expr = '#pragma omp {}\n{{'.format(omp_expr)
+        return omp_expr
+
     def _print_Omp_End_Clause(self, expr):
         return '}'
     #=====================================
