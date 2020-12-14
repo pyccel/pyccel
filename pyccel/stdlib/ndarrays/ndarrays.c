2c71487f2ebb20a15aab33027eba7dc0669d61a7
mostafa <mostafa-1470@live.fr>
2020-12-14 10:40:59 +0100

    fix notice format for c files

--- pyccel/stdlib/ndarrays/ndarrays.c
+++ pyccel/stdlib/ndarrays/ndarrays.c
@@ -1,4 +1,8 @@
-//This file is part of pyccel which is released under MIT License. See the LICENSE file or go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.
+/* --------------------------------------------------------------------------------------- */
+/* This file is part of Pyccel which is released under MIT License. See the LICENSE file   */
+/* or go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details. */
+/* --------------------------------------------------------------------------------------- */
+
 #include "ndarrays.h"
 
 /* 
