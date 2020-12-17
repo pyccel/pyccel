5cfcd0a0a3500c14d2d5c72a18f6d081b57a49be
mostafa <32802212+mostafaMamoni@users.noreply.github.com>
2020-12-17 14:04:26 +0100

    Update authors and add copyright (#592)
    Solve #582:
    - Add short copyright notice to each source file in project;
    - Update AUTHORS and LICENSE files.

--- pyccel/ast/utilities.py
+++ pyccel/ast/utilities.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
 
 import inspect
 
