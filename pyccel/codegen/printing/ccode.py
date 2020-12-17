aa4a502c73dbc7f9180abb6b4a95422f0749cbb8
saniqq <mouadelalj@gmail.com>
2020-12-17 11:31:03 +0100

    removed unnecessary line

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -1131,7 +1131,6 @@ class CCodePrinter(CodePrinter):
         # setting the pointer's is_view attribute to false so it can be ignored by the free_pointer function.
         if isinstance(expr.lhs, Variable) and expr.lhs.is_ndarray \
                 and isinstance(expr.rhs, Variable) and expr.lhs.is_ndarray and expr.rhs.is_pointer:
-            self._additional_imports.add("ndarrays")
             return 'alias_assign(&{}, {});'.format(lhs, rhs)
 
         return '{} = {};'.format(lhs, rhs)
