8dc50fbbcffcdf4e77e3d39417423ed16f5d154b
Christopher Albert <albert@alumni.tugraz.at>
2020-12-17 15:26:02 +0100

    Fix MPI tests on Windows (#597)
    - Fix MPI installation problems on Windows;
    - Ensure that errors are correctly raised when running pyccel in parallel.

--- pyccel/epyccel.py
+++ pyccel/epyccel.py
@@ -243,8 +243,8 @@ def epyccel( python_function_or_module, **kwargs ):
                 fun_name = python_function_or_module.__name__ if fun else None
                 success  = True
             # error handling carried out after broadcast to prevent deadlocks
-            except: # pylint: disable=bare-except
-                exc_info = sys.exc_info()
+            except BaseException as e: # pylint: disable=broad-except
+                exc_info = e
                 success  = False
 
         # Non-master processes initialize empty variables
