2f6a520de0ac17cf4920175ebfe182251dc0cba8
saniqq <mouadelalj@gmail.com>
2020-12-18 13:19:05 +0100

    reverted the test commit

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -753,7 +753,6 @@ class CCodePrinter(CodePrinter):
         return '{}\n{}'.format(free_code, alloc_code)
 
     def _print_Deallocate(self, expr):
-        return ''
         if expr.variable.is_pointer:
             return 'free_pointer({});'.format(self._print(expr.variable))
         return 'free_array({});'.format(self._print(expr.variable))
