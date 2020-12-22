765ebe0005f20a3f2516c4c94c0811c2edbc99f9
saidctb <said-ctb@live.fr>
2020-12-22 22:27:59 +0100

    make codacy happy

--- tests/epyccel/test_epyccel_python_annotations.py
+++ tests/epyccel/test_epyccel_python_annotations.py
@@ -1,8 +1,8 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
-import numpy as np
 from pyccel.epyccel import epyccel
 
 def test_module_1():
     import modules.python_annotations as mod
 
     modnew = epyccel(mod)
+    assert modnew.fib(10) == mod.fib(10)
