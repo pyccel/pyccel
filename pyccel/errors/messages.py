0319124f2e821fad26a4232eb85b367fb0021c3e
Said Hadjout <said-ctb@live.fr>
2020-12-28 13:35:00 +0000

    Support Python style type hints (#631)
    Accept Python annotations. For example:
    
    def fib(n: int) -> int:
        if n<=1:
            return 0
        elif n==2:
            return 1
        else:
            return fib(n-1) + fib(n-2)

--- pyccel/errors/messages.py
+++ pyccel/errors/messages.py
@@ -14,9 +14,11 @@ INVALID_IMPLICIT_RETURN = 'Implicit return in function which does not return'
 INCOMPATIBLE_RETURN_VALUE_TYPE = 'Incompatible return value type'
 RETURN_VALUE_EXPECTED = 'Return value expected'
 NO_RETURN_EXPECTED = 'Return statement in function which does not return'
-RECURSIVE_RESULTS_REQUIRED = ("A results type must be provided for recursive functions with one of the following two syntaxes:\n"
-    "@types('ARG_TYPES', results='RESULT_TYPES')\n"
-    "#$ header function FUNC_NAME(ARG_TYPES) results(RESULT_TYPES)\n")
+RECURSIVE_RESULTS_REQUIRED = ("A results type must be provided for recursive functions with one of the following three syntaxes:\n"
+                              "def FUNC_NAME(arg1_name:arg1_type, ...) -> RESULT_TYPES\n"
+                              "@types('ARG_TYPES', results='RESULT_TYPES')\n"
+                              "#$ header function FUNC_NAME(ARG_TYPES) results(RESULT_TYPES)\n")
+
 INCOMPATIBLE_TYPES = 'Incompatible types'
 INCOMPATIBLE_TYPES_IN_ASSIGNMENT = 'Incompatible types in assignment'
 INCOMPATIBLE_REDEFINITION = 'Incompatible redefinition'
