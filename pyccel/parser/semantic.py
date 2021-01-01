060146aa1ac87f92c8be5364750fc8946df5a4f9
menoIy <karim.daou.1993@gmail.com>
2021-01-01 12:36:01 +0100

    avoid multiple useless call to _insert_obj and and set import.ignore to True

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -2854,6 +2854,8 @@ class SemanticParser(BasicParser):
                     errors.report(IMPORTING_EXISTING_IDENTIFIED,
                                   bounding_box=(self._current_fst_node.lineno, self._current_fst_node.col_offset),
                                   severity='fatal')
+            expr.ignore = True
+            _insert_obj('imports', source, expr)
 
             if expr.target:
                 for (name, atom) in imports:
@@ -2862,10 +2864,8 @@ class SemanticParser(BasicParser):
                             _insert_obj('variables', name, atom)
                         else:
                             _insert_obj('functions', name, atom)
-                        _insert_obj('imports', source, expr)
             else:
                 _insert_obj('variables', source_target, imports)
-                _insert_obj('imports', source, expr)
         else:
 
             # in some cases (blas, lapack, openmp and openacc level-0)
