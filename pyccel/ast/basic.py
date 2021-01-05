40a77c14f84b8acabab5c9863a8d43c040c71491
Emily.Bourne <louise.bourne@gmail.com>
2021-01-05 17:25:13 +0100

    Add necessary functions to Basic class. TODO: Add Basic.__init__ call to every class. Does Basic+Expr inheritance cause problems?

--- pyccel/ast/basic.py
+++ pyccel/ast/basic.py
@@ -15,6 +15,45 @@ __all__ = ('Basic', 'PyccelAstNode')
 class Basic(sp_Basic):
     """Basic class for Pyccel AST."""
     _fst = None
+    def __init__(self, children):
+        self._parent = None
+        self._children = children
+        for c in children.values():
+            c.parent = self
+
+    def has_parent_of_type(self, search_type):
+        if isinstance(self._parent, search_type):
+            return True
+        elif self._parent:
+            return self._parent.has_parent_of_type(search_type)
+        else:
+            return False
+
+    def substitute(self, original, replacement, excluded_nodes = ()):
+        """
+        Substitute object original for object replacement in the code.
+        Any types in excluded_nodes will not be visited
+
+        Parameters
+        ==========
+        original    : object
+                      The original object to be replaced
+        replacement : object
+                      The object which will be inserted instead
+        excluded_nodes : object
+                      Types for which substitute should not be called
+        """
+        for n,v in self._children:
+            if v is original:
+                setattr(self,n, replacement)
+            elif not v.is_atomic and not isinstance(v, excluded_nodes):
+                v.substitute(original, replacement, excluded_nodes)
+
+    @property
+    def is_atomic(self):
+        """ Indicates whether the object has any children
+        """
+        return bool(self._children)
 
     def set_fst(self, fst):
         """Sets the python.ast fst."""
@@ -24,7 +63,15 @@ class Basic(sp_Basic):
     def fst(self):
         return self._fst
 
-class PyccelAstNode:
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent):
+        self._parent = parent
+
+class PyccelAstNode(Basic):
     stage      = None
     _shape     = None
     _rank      = None
