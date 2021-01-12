f3f5337fec9ca559ee3d4d12ab01f70a09626614
menoIy <karim.daou.1993@gmail.com>
2021-01-12 11:30:07 +0100

    simplify condition

--- pyccel/codegen/printing/fcode.py
+++ pyccel/codegen/printing/fcode.py
@@ -369,7 +369,7 @@ class FCodePrinter(CodePrinter):
     def _print_Import(self, expr):
 
         source = ''
-        if str(expr.source) in pyccel_builtin_import_registery or expr.ignore:
+        if expr.ignore:
             return ''
 
         if isinstance(expr.source, DottedName):
