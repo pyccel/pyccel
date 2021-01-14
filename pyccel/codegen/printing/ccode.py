ac250f5b1cd3df74671f52a68dbe9fb65c6348fa
rakati <ouhaddounouredine@gmail.com>
2021-01-14 17:01:32 +0100

    remove unused import of DottedVariable

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -13,7 +13,7 @@ from pyccel.ast.builtins  import PythonRange, PythonFloat, PythonComplex
 
 from pyccel.ast.core      import Declare, Slice, ValuedVariable
 from pyccel.ast.core      import FuncAddressDeclare, FunctionCall
-from pyccel.ast.core      import Deallocate, DottedVariable
+from pyccel.ast.core      import Deallocate
 from pyccel.ast.core      import FunctionAddress, PyccelArraySize
 from pyccel.ast.core      import IfTernaryOperator
 from pyccel.ast.core      import Assign, datatype, Variable, Import
