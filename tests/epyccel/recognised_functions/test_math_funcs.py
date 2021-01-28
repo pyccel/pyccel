efb6282d93b70f19a72d7962f8645749826cf075
rakati <ouhaddounouredine@gmail.com>
2021-01-28 20:21:29 +0100

    Add: flags to the test to make sure floor and ceil return the desired type

--- tests/epyccel/recognised_functions/test_math_funcs.py
+++ tests/epyccel/recognised_functions/test_math_funcs.py
@@ -354,7 +354,8 @@ def test_floor_call(language):
         from math import floor
         return floor(x)
 
-    f1 = epyccel(floor_call, language = language)
+    fflags = "-Werror -Wconversion"
+    f1 = epyccel(floor_call, language = language, fflags=fflags)
     x = rand()
     assert(isclose(f1(x) ,  floor_call(x), rtol=1e-14, atol=1e-15))
     assert(isclose(f1(-x) ,  floor_call(-x), rtol=1e-14, atol=1e-15))
@@ -366,7 +367,8 @@ def test_floor_phrase(language):
         a = floor(x)*floor(y)
         return a
 
-    f2 = epyccel(floor_phrase, language = language)
+    fflags = "-Werror -Wconversion"
+    f2 = epyccel(floor_phrase, language = language, fflags=fflags)
     x = rand()
     y = rand()
     assert(isclose(f2(x,y) ,  floor_phrase(x,y), rtol=1e-14, atol=1e-15))
@@ -387,13 +389,17 @@ def test_floor_return_type(language):
         a = floor(x)
         return a
 
-    f1 = epyccel(floor_return_type_int, language = language)
+    fflags = "-Werror -Wconversion"
+    f1 = epyccel(floor_return_type_int, language = language, fflags=fflags)
+
     x = randint(100)
     assert(isclose(f1(x) ,  floor_return_type_int(x), rtol=1e-14, atol=1e-15))
     assert(isclose(f1(-x) ,  floor_return_type_int(-x), rtol=1e-14, atol=1e-15))
     assert(type(f1(x))  == type(floor_return_type_int(x))) # pylint: disable=unidiomatic-typecheck
 
-    f1 = epyccel(floor_return_type_real, language = language)
+    fflags = "-Werror -Wconversion"
+    f1 = epyccel(floor_return_type_real, language = language, fflags=fflags)
+
     x = randint(100)
     assert(isclose(f1(x) ,  floor_return_type_real(x), rtol=1e-14, atol=1e-15))
     assert(isclose(f1(-x) ,  floor_return_type_real(-x), rtol=1e-14, atol=1e-15))
@@ -406,7 +412,9 @@ def test_ceil_call_r(language):
         from math import ceil
         return ceil(x)
 
-    f1 = epyccel(ceil_call, language = language)
+    fflags = "-Werror -Wconversion"
+    f1 = epyccel(ceil_call, language = language, fflags=fflags)
+
     x = rand()
     assert(ceil_call(x) == f1(x))
     assert(ceil_call(-x) == f1(-x))
@@ -419,7 +427,9 @@ def test_ceil_call_i(language):
         from math import ceil
         return ceil(x)
 
-    f1 = epyccel(ceil_call, language = language)
+    fflags = "-Werror -Wconversion"
+    f1 = epyccel(ceil_call, language = language, fflags=fflags)
+
     x = randint(10)
     assert(ceil_call(x) == f1(x))
     assert(ceil_call(-x) == f1(-x))
@@ -433,7 +443,9 @@ def test_ceil_phrase(language):
         a = ceil(x)*ceil(y)
         return a
 
-    f2 = epyccel(ceil_phrase, language = language)
+    fflags = "-Werror -Wconversion"
+    f2 = epyccel(ceil_phrase, language = language, fflags=fflags)
+
     x = rand()
     y = rand()
     assert(isclose(ceil_phrase(x, y), f2(x, y), rtol=1e-14, atol=1e-15))
