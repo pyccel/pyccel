fb338145ccee80ff79174340b7e348cec960850d
mostafa <mostafa-1470@live.fr>
2021-01-29 14:28:36 +0100

    typos

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -2381,7 +2381,7 @@ class SemanticParser(BasicParser):
         args_number = len(expr.arguments)
         templates = self.get_templates()
         if decorators['template']:
-            # load templates dict from decorators dict
+            # Load templates dict from decorators dict
             print(decorators['template']['template_dict'])
             templates.update(decorators['template']['template_dict'])
 
