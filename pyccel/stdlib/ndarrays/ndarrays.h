b06b6cad7fd672fc66c1099869cc5996da158e08
saniQQ <40796259+saniQQ@users.noreply.github.com>
2021-02-01 13:03:52 +0100

    Add stack_arrays in C (#710)
    - Add handling of stack_array decorator in C for numpy funcs: array, full, ones, zeros, empty:
      - `_init_stack_array` : function is called from `array_fill` and `copy_NumpyArray_Data` to do the initialization,
      - `arrayFill` : handles the initialization when calling the functions : full, ones, zeros, empty,
      - `copy_NumpyArray_Data` : handles the initialization when calling the functions: array;
     
    - Activate 1d stack_array epyccel tests;
    - Add 2d stack_array epyccel tests for C and Fortran.

--- pyccel/stdlib/ndarrays/ndarrays.h
+++ pyccel/stdlib/ndarrays/ndarrays.h
@@ -85,6 +85,7 @@ typedef struct  s_ndarray
 /* functions prototypes */
 
 /* allocations */
+void        stack_array_init(t_ndarray *arr);
 t_ndarray   array_create(int32_t nd, int64_t *shape, enum e_types type);
 void        _array_fill_int8(int8_t c, t_ndarray arr);
 void        _array_fill_int16(int16_t c, t_ndarray arr);
