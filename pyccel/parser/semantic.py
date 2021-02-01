8085e3217d708b0b49b998d14fa6d04da538c20c
Emily.Bourne <louise.bourne@gmail.com>
2021-02-01 12:26:32 +0100

    Ensure that pyccel objects are used in object creation. Use sympy objects in sympy calculations

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -2133,8 +2133,7 @@ class SemanticParser(BasicParser):
         indices = expr.indices
         dims    = []
         body    = expr.loops[1]
-        #TODO remove the line below after adding equivalent of atoms to Basic issue #694
-        indices = [sp_Symbol(i.name) for i in indices]
+        sp_indices = [sp_Symbol(i.name) for i in indices]
         idx_subs = dict()
 
         # The symbols created to represent unknown valued objects are temporary
@@ -2216,11 +2215,11 @@ class SemanticParser(BasicParser):
                 # Use the maximum value of the start
                 if start_idx[-1] > stop_idx[-1]:
                     s = start_idx.pop()
-                    min_size = min_size.subs(indices[s], dims[s][3])
+                    min_size = min_size.subs(sp_indices[s], dims[s][3])
                 # and the minimum value of the stop
                 else:
                     s = stop_idx.pop()
-                    min_size = min_size.subs(indices[s], dims[s][2])
+                    min_size = min_size.subs(sp_indices[s], dims[s][2])
 
             # While the min_size is not a known integer, assume that the bounds are positive
             j = 0
@@ -2234,8 +2233,8 @@ class SemanticParser(BasicParser):
                           severity='error')
 
             # sympy is necessary to carry out the summation
-            dim   = dim.subs(indices[i], start+step*indices[i])
-            dim   = Summation(dim, (indices[i], 0, size-1))
+            dim   = dim.subs(sp_indices[i], start+step*sp_indices[i])
+            dim   = Summation(dim, (sp_indices[i], 0, size-1))
             dim   = dim.doit()
 
         try:
@@ -2598,7 +2597,7 @@ class SemanticParser(BasicParser):
             all_assigned = [str(i) for i in all_assigned]
             assigned     = [str(i) for i in assigned]
 
-            apps = list(Tuple(*body.body).atoms(FunctionCall))
+            apps = list(body.children_of_type(FunctionCall))
             apps = [i for i in apps if (i.__class__.__name__
                     in self.get_parent_functions())]
 
