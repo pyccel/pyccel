5e7d13d5f1d0b88ba8ff49edca1614c35aa48239
mostafa <mostafa-1470@live.fr>
2021-02-04 00:12:11 +0100

    fix fainling test

--- pyccel/ast/numpyext.py
+++ pyccel/ast/numpyext.py
@@ -593,7 +593,7 @@ class NumpyFull(PyccelInternalFunction, NumpyNewArray):
         order = NumpyNewArray._process_order(order)
 
         # Cast fill_value to correct type
-        if fill_value:
+        if fill_value and not isinstance(fill_value, Nil):
             cast_func = DtypePrecisionToCastFunction[dtype.name][precision]
             fill_value = cast_func(fill_value)
         self._shape = shape
