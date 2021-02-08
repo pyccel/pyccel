9165f7c58de76b670352ff016fae87c1a9544a58
menoIy <karim.daou.1993@gmail.com>
2021-02-08 15:31:10 +0100

    Revert "add __str__ to funccall"
    This reverts commit ca9c836758bf05dfcd0f65243e226c4a779318d5.

--- pyccel/ast/core.py
+++ pyccel/ast/core.py
@@ -1792,9 +1792,6 @@ class FunctionCall(PyccelAstNode):
     def interface_name(self):
         return self._interface_name
 
-    def __str__(self):
-        return '{}({})'.format(self.func_name, ', '.join(str(a) for a in self.args))
-
     def __repr__(self):
         return '{}({})'.format(self.func_name, ', '.join(str(a) for a in self.args))
 
