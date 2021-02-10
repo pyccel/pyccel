237a1d8d4405a41de147641b32f5005eaca31f94
Noureddine Hamid <nhamid@student.1337.ma>
2021-02-10 13:20:27 +0100

    codacy fix

--- pyccel/ast/omp.py
+++ pyccel/ast/omp.py
@@ -3,6 +3,9 @@
 # This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
 # go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
 #------------------------------------------------------------------------------------------#
+"""
+This file contain the OpenMP types.
+"""
 
 from .basic import Basic
 
