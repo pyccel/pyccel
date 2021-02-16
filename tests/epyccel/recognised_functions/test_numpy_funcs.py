correct precision

tests/epyccel/recognised_functions/test_numpy_funcs.py
@@ -3623,8 +3623,8 @@ def test_numpy_real_scalar(language):
     assert f_complex64_output == test_complex64_output
     #assert (f_complex64_output == test_complex64_output) can't compare type issue #735
 
-    f_complex128_output = f_complex128(np.complex64(1+5j))
-    test_complex128_output = test_complex128(np.complex64(1+5j))
+    f_complex128_output = f_complex128(np.complex128(1+5j))
+    test_complex128_output = test_complex128(np.complex128(1+5j))
 
     assert f_complex128_output == test_complex128_output
     #assert (type(f_complex64_output) == type(test_complex64_output)) can't compare type issue #735