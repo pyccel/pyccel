6b64e87a573a0d46ef1e2159b44bbe2a4a634f2f
menoIy <karim.daou.1993@gmail.com>
2021-02-16 11:06:06 +0100

    update test

--- tests/epyccel/test_base.py
+++ tests/epyccel/test_base.py
@@ -199,7 +199,7 @@ def test_pass_if(language):
 def test_pass2_if(language):
     test = epyccel_test(base.pass2_if, lang=language)
     test.compare_epyccel(0.2)
-    test.compare_epyccel(0.2)
+    test.compare_epyccel(0.0)
 
 def test_use_optional(language):
     test = epyccel_test(base.use_optional, lang=language)
