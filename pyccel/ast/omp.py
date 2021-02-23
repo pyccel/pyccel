c85c1553c30f2d15fbc86028c99cf2062d394855
Noureddine Hamid <nhamid@student.1337.ma>
2021-02-23 09:33:57 +0100

    remove redefinition of __init__ function

--- pyccel/ast/omp.py
+++ pyccel/ast/omp.py
@@ -54,64 +54,60 @@ class OmpAnnotatedComment(Basic):
 class OMP_For_Loop(OmpAnnotatedComment):
     """ Represents an OpenMP Loop construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class OMP_Parallel_Construct(OmpAnnotatedComment):
     """ Represents an OpenMP Parallel construct. """
-    def __init__(self, txt, combined=None):
-        OmpAnnotatedComment.__init__(self, txt, combined)
+    pass
 
 class OMP_Task_Construct(OmpAnnotatedComment):
     """ Represents an OpenMP Task construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class OMP_Single_Construct(OmpAnnotatedComment):
     """ Represents an OpenMP Single construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class OMP_Critical_Construct(OmpAnnotatedComment):
     """ Represents an OpenMP Critical construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class OMP_Master_Construct(OmpAnnotatedComment):
     """ Represents OpenMP Master construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class OMP_Masked_Construct(OmpAnnotatedComment):
     """ Represents OpenMP Masked construct. """
-    def __init__(self, txt, combined=None):
-        OmpAnnotatedComment.__init__(self, txt, combined)
+    pass
 
 class OMP_Cancel_Construct(OmpAnnotatedComment):
     """ Represents OpenMP Cancel construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class OMP_Target_Construct(OmpAnnotatedComment):
     """ Represents OpenMP Target construct. """
-    def __init__(self, txt, combined=None):
-        OmpAnnotatedComment.__init__(self, txt, combined)
+    pass
 
 class OMP_Teams_Construct(OmpAnnotatedComment):
     """ Represents OpenMP Teams construct. """
-    def __init__(self, txt, combined=None):
-        OmpAnnotatedComment.__init__(self, txt, combined)
+    pass
 
 class OMP_Sections_Construct(OmpAnnotatedComment):
     """ Represents OpenMP Sections construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class OMP_Section_Construct(OmpAnnotatedComment):
     """ Represent OpenMP Section construct. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
 
 class Omp_End_Clause(OmpAnnotatedComment):
     """ Represents the End of an OpenMP block. """
     def __init__(self, txt):
-        OmpAnnotatedComment.__init__(self, txt)
+        super().__init__(txt)
