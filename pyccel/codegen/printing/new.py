0a997d1ac666b3288dbff4e93ee945ae14cbbacd
menoIy <karim.daou.1993@gmail.com>
2021-02-23 14:03:17 +0100

    add helper functions

--- pyccel/codegen/printing/new.py
+++ pyccel/codegen/printing/new.py
@@ -30,6 +30,52 @@ Print_FunctionDef():
 
 Print_Module():
 
+# --------------------------------------------------------------------
+#                       Helper functions
+# --------------------------------------------------------------------
+
+def get_new_name(self, used_names, requested_name):
+    """
+    """
+    if requested_name not in used_names:
+        used_names.add(requested_name)
+        return requested_name
+    else:
+        incremented_name, _ = create_incremented_string(used_names, prefix=requested_name)
+        return incremented_name
+
+def get_wrapper_name(self, used_names, function):
+    """
+    """
+    name = function.name
+    wrapper_name = self.get_new_name(used_names.union(self._global_names), name+"_wrapper")
+
+    self._function_wrapper_names[func.name] = wrapper_name
+    self._global_names.add(wrapper_name)
+    used_names.add(wrapper_name)
+
+    return wrapper_name
+
+def get_new_PyObject(self, name, used_names):
+    """
+    """
+    return Variable(dtype      = PyccelPyObject(),
+                    name       = self.get_new_name(used_names, name),
+                    is_pointer = True)
+
+
+def get_wrapper_arguments(self, used_names)
+    """
+    """
+    python_func_args    = self.get_new_PyObject("args"  , used_names)
+    python_func_kwargs  = self.get_new_PyObject("kwargs", used_names)
+    python_func_selfarg = self.get_new_PyObject("self"  , used_names)
+
+    return [python_func_selfarg, python_func_args, python_func_kwargs]
+
+
+def _print_Interface(self, expr):
+    # TODO
 
 def _print_FunctionDef(self, expr):
     # Save all used names
@@ -38,17 +84,13 @@ def _print_FunctionDef(self, expr):
                    + [expr.name])
 
     # Find a name for the wrapper function
-    wrapper_name = self._get_wrapper_name(used_names, expr)
-    used_names.add(wrapper_name)
-    python_func_args    = self.get_new_PyObject("args"  , used_names)
-    python_func_kwargs  = self.get_new_PyObject("kwargs", used_names)
-    python_func_selfarg = self.get_new_PyObject("self"  , used_names)
+    wrapper_name = self.get_wrapper_name(used_names, expr)
 
     # Collect arguments and results
-    wrapper_args    = [python_func_selfarg, python_func_args, python_func_kwargs]
+    wrapper_args    = get_wrapper_arguments(used_names)
     wrapper_results = [self.get_new_PyObject("result", used_names)]
 
-    arg_names         = [a.name for a in local_arg_vars]
+    arg_names         = [a.name for a in expr.arguments]
     keyword_list_name = self.get_new_name(used_names,'kwlist')
     keyword_list      = PyArgKeywords(keyword_list_name, arg_names)
 
@@ -91,3 +133,12 @@ def _print_FunctionDef(self, expr):
                                    local_varts = tuple(func_args + expr.results))
     
     return CCodePrinter._print_FunctionDef(self, wrapper_func)
+
+def _print_Module(self, expr):
+    self._global_names = set(f.name for f in expr.funcs)
+    self._module_name  = expr.name
+    
+    .
+    .
+    .
+    .
