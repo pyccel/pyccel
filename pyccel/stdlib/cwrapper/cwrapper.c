6767447d61c52f4eedc35b1a528385544f2f956f
menoIy <karim.daou.1993@gmail.com>
2021-02-24 18:24:11 +0100

    create python to c functions C side

--- pyccel/stdlib/cwrapper/cwrapper.c
+++ pyccel/stdlib/cwrapper/cwrapper.c
@@ -6,39 +6,180 @@
 
 // Python to C
 
-int PyComplex_to_Complex64(Pyobject *o)
+int	PyComplex_to_Complex64(Pyobject *o, float complex *c)
 {
-	float			real_part;
-	float			imag_part;
-	float complex	c;
+	float	real_part;
+	float	imag_part;
 
-	real_part = PyComplex_Real_AsDouble(o);
-	imag_part = PyComplex_Imag_AsDouble(o);
+	if (PyArray_IsScalar(o, Complex64))
+	{
+		PyArray_ScalarAsCtype(o, c);
+	}
 
-	c = CMPLXF(real_part, imag_part);
-	return c;
+	else if (PyComplex_Check(o))
+	{
+		real_part = (float)PyComplex_Real_AsDouble(o);
+		imag_part = (float)PyComplex_Imag_AsDouble(o);
+
+		*c = CMPLXF(real_part, imag_part);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 64 bit complex");
+		return 0;
+	}
+	return 1;
 }
 
-int	PyComplex_to_Complex128(Pyobject *o)
+int	PyComplex_to_Complex128(Pyobject *o, double complex *c)
 {
-	double			real_part;
-	double			imag_part;
-	double complex	c;
+	double	real_part;
+	double	imag_part;
 
-	real_part = PyComplex_Real_AsDouble(o);
-	imag_part = PyComplex_Imag_AsDouble(o);
+	if (PyArray_Is_Scalar(o, Complex128))
+	{
+		PyArray_ScalarAsCtype(o, c);
+	}
+	else if (PyComplex_Check(o))
+	{
+		real_part = PyComplex_Real_AsDouble(o);
+		imag_part = PyComplex_Imag_AsDouble(o);
 
-	c = CMPLX(real_part, imag_part);
-	return c;
+		*c = CMPLX(real_part, imag_part);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 128 bit complex");
+		return 0;
+	}
+	return 1;
 }
 
-int	PyBool_to_Bool(Pyobject *o)
+int	PyInt64_to_Int64(PyObject *o, int64_t *i)
 {
-	bool	b;
+	if (PyArray_Is_Scalar(o, Int64))
+	{
+		PyArray_ScalarAsCtype(o, i);
+	}
+	else if (PyLongCheck(o))
+	{
+		*i = (int64_t)PyLong_AsLongLong(o);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 64 bit integer");
+		return 0;
+	}
+	return 1;
+}
 
-	b = o == PyTrue;
+int	PyInt32_to_Int32(PyObject *o, int32_t *i)
+{
+	if (PyArray_Is_Scalar(o, Int32))
+	{
+		PyArray_ScalarAsCtype(o, i);
+	}
+	else if (PyLongCheck(o))
+	{
+		*i = (int32_t)PyLong_AsLong(o);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 32 bit integer");
+		return 0;
+	}
+	return 1;
+}
 
-	return b;
+int	PyInt16_to_Int16(PyObject *o, int16_t *i)
+{
+	if (PyArray_Is_Scalar(o, Int16))
+	{
+		PyArray_ScalarAsCtype(o, i);
+	}
+	else if (PyLongCheck(o))
+	{
+		*i = (int16_t)PyLong_AsLong(o);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 16 bit integer");
+		return 0;
+	}
+	return 1;
+}
+
+int	PyInt8_to_Int8(PyObject *o, int8_t *i)
+{
+	if (PyArray_Is_Scalar(o, Int16))
+	{
+		PyArray_ScalarAsCtype(o, i);
+	}
+	else if (PyLongCheck(o))
+	{
+		*i = (int8_t)PyLong_AsLong(o);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 8 bit integer");
+		return 0;
+	}
+	return 1;
+}
+
+
+int	PyBool_to_Bool(Pyobject *o, bool *b)
+{
+	if (PyArray_Is_Scalar(o, Bool))
+	{
+		PyArray_ScalarAsCtype(o, b);
+	}
+	else if (PyBool_Check(o))
+	{
+		*b = o == PyTrue;
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be a boolean");
+		return 0;
+	}
+	return 1;
+}
+
+int PyFloat_to_Float(Pyobject *o, float *f)
+{
+	if (PyArray_Is_Scalar(o, Float32))
+	{
+		PyArray_ScalarAsCtype(o, b);
+	{
+	else if (PyFloat_Check(o))
+	{
+		*f = (float)PyFloat_AsDouble(o);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 32 bit float");
+		return 0;
+	}
+	return 1;
+}
+
+int PyDouble_to_Double(PyObject *o, double *d)
+{
+	if (PyArray_Is_Scalar(o, Float64))
+	{
+		PyArray_ScalarAsCtype(o, b);
+	}
+	else if (PyFloat_Check(o))
+	{
+		*f = (float)PyFloat_AsDouble(o);
+	}
+	else
+	{
+		PyErr_SetString(PyExc_TypeError, "argument must be 64 bit float");
+		return 0;
+	}
+	return 1;
 }
 
 t_ndarray		PyArray_to_ndarray(PyObject *o)
