315b431828e83048e658247629552c2e81ff263b
EmilyBoune <louise.bourne@gmail.com>
2021-03-02 22:28:18 +0100

    Private decorator works in python

--- tests/epyccel/test_epyccel_decorators.py
+++ tests/epyccel/test_epyccel_decorators.py
@@ -5,6 +5,13 @@ import pytest
 from pyccel.epyccel import epyccel
 from pyccel.decorators import private
 
+@pytest.fixture(params=[
+    pytest.param('fortran', marks = pytest.mark.fortran),
+    pytest.param('c'      , marks = pytest.mark.c)
+)
+def language(request):
+    return request.param
+
 def test_private(language):
     @private
     def f():
