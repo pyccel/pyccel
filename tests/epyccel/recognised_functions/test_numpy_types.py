50f4e62797fda04fac5b5dfcc6afabb0860a673e
mostafa <mostafa-1470@live.fr>
2021-03-17 15:01:05 +0100

    reduce number of tests for complex types

--- tests/epyccel/recognised_functions/test_numpy_types.py
+++ tests/epyccel/recognised_functions/test_numpy_types.py
@@ -1753,21 +1753,37 @@ def test_numpy_double_array_like_2d(language):
         assert epyccel_func(fl64) == get_double(fl64)
     assert epyccel_func(fl32) == get_double(fl32)
 
-def test_numpy_complex64_scalar(language):
 
-    @types('bool')
-    @types('int')
-    @types('int8')
-    @types('int16')
-    @types('int32')
-    @types('int64')
-    @types('float')
-    @types('float32')
-    @types('float64')
-    def get_complex64(a):
-        from numpy import complex64
-        b = complex64(a)
-        return b
+@types('bool')
+@types('int')
+@types('int8')
+@types('int16')
+@types('int32')
+@types('int64')
+@types('float')
+@types('float32')
+@types('float64')
+def get_complex64(a):
+    from numpy import complex64
+    b = complex64(a)
+    return b
+
+@types('bool')
+@types('int')
+@types('int8')
+@types('int16')
+@types('int32')
+@types('int64')
+@types('float')
+@types('float32')
+@types('float64')
+def get_complex128(a):
+    from numpy import complex128
+    b = complex128(a)
+    return b
+
+@pytest.mark.parametrize( 'get_complex', [get_complex128, get_complex64])
+def test_numpy_complex(language, get_complex):
 
     integer8 = randint(min_int8, max_int8, dtype=np.int8)
     integer16 = randint(min_int16, max_int16, dtype=np.int16)
@@ -1781,13 +1797,13 @@ def test_numpy_complex64_scalar(language):
     fl64 = uniform(min_float64 / 2, max_float64 / 2)
 
 
-    epyccel_func = epyccel(get_complex64, language=language)
+    epyccel_func = epyccel(get_complex, language=language)
 
     f_bl_true_output = epyccel_func(True)
-    test_bool_true_output = get_complex64(True)
+    test_bool_true_output = get_complex(True)
 
     f_bl_false_output = epyccel_func(False)
-    test_bool_false_output = get_complex64(False)
+    test_bool_false_output = get_complex(False)
 
     assert f_bl_true_output == test_bool_true_output
     assert f_bl_false_output == test_bool_false_output
@@ -1796,25 +1812,25 @@ def test_numpy_complex64_scalar(language):
     assert matching_types(f_bl_false_output, test_bool_false_output)
 
     f_integer_output = epyccel_func(integer)
-    test_int_output  = get_complex64(integer)
+    test_int_output  = get_complex(integer)
 
     assert f_integer_output == test_int_output
     assert matching_types(f_integer_output, test_int_output)
 
     f_integer8_output = epyccel_func(integer8)
-    test_int8_output = get_complex64(integer8)
+    test_int8_output = get_complex(integer8)
 
     assert f_integer8_output == test_int8_output
     assert matching_types(f_integer8_output, test_int8_output)
 
     f_integer16_output = epyccel_func(integer16)
-    test_int16_output = get_complex64(integer16)
+    test_int16_output = get_complex(integer16)
 
     assert f_integer16_output == test_int16_output
     assert matching_types(f_integer16_output, test_int16_output)
 
     f_integer32_output = epyccel_func(integer32)
-    test_int32_output = get_complex64(integer32)
+    test_int32_output = get_complex(integer32)
 
     assert f_integer32_output == test_int32_output
     assert matching_types(f_integer32_output, test_int32_output)
@@ -1822,29 +1838,58 @@ def test_numpy_complex64_scalar(language):
     # the if block should be removed after resolving (https://github.com/pyccel/pyccel/issues/735).
     if sys.platform != 'win32':
         f_integer64_output = epyccel_func(integer64)
-        test_int64_output = get_complex64(integer64)
+        test_int64_output = get_complex(integer64)
 
         assert f_integer64_output == test_int64_output
         assert matching_types(f_integer64_output, test_int64_output)
 
     f_fl_output = epyccel_func(fl)
-    test_float_output = get_complex64(fl)
+    test_float_output = get_complex(fl)
 
     assert f_fl_output == test_float_output
     assert matching_types(f_fl_output, test_float_output)
 
     f_fl32_output = epyccel_func(fl32)
-    test_float32_output = get_complex64(fl32)
+    test_float32_output = get_complex(fl32)
 
     assert f_fl32_output == test_float32_output
     assert matching_types(f_fl32_output, test_float32_output)
 
     f_fl64_output = epyccel_func(fl64)
-    test_float64_output = get_complex64(fl64)
+    test_float64_output = get_complex(fl64)
 
     assert f_fl64_output == test_float64_output
     assert matching_types(f_fl64_output, test_float64_output)
 
+@types('bool[:,:]')
+@types('int[:,:]')
+@types('int8[:,:]')
+@types('int16[:,:]')
+@types('int32[:,:]')
+@types('int64[:,:]')
+@types('float[:,:]')
+@types('float32[:,:]')
+@types('float64[:,:]')
+def get_complex128_arr(arr):
+    from numpy import complex128, shape
+    a = complex128(arr)
+    s = shape(a)
+    return len(s), s[0], s[1], a[0,0], a[0,1]
+
+@types('bool[:,:]')
+@types('int[:,:]')
+@types('int8[:,:]')
+@types('int16[:,:]')
+@types('int32[:,:]')
+@types('int64[:,:]')
+@types('float[:,:]')
+@types('float32[:,:]')
+@types('float64[:,:]')
+def get_complex64_arr(arr):
+    from numpy import complex64, shape
+    a = complex64(arr)
+    s = shape(a)
+    return len(s), s[0], s[1], a[0,0], a[0,1]
 
 @pytest.mark.parametrize( 'language', (
         pytest.param("fortran", marks = [pytest.mark.fortran]),
@@ -1858,22 +1903,8 @@ def test_numpy_complex64_scalar(language):
     )
 )
 
-def test_numpy_complex64_array_like_1d(language):
-
-    @types('bool[:]')
-    @types('int[:]')
-    @types('int8[:]')
-    @types('int16[:]')
-    @types('int32[:]')
-    @types('int64[:]')
-    @types('float[:]')
-    @types('float32[:]')
-    @types('float64[:]')
-    def get_complex64(arr):
-        from numpy import complex64, shape
-        a = complex64(arr)
-        s = shape(a)
-        return len(s), s[0], a[0]
+@pytest.mark.parametrize( 'get_complex', [get_complex128_arr, get_complex64_arr])
+def test_numpy_complex_array_like_1d(language, get_complex):
 
     size = 5
 
@@ -1890,19 +1921,19 @@ def test_numpy_complex64_array_like_1d(language):
     fl32 = np.float32(fl32)
     fl64 = uniform(min_float64 / 2, max_float64 / 2, size = size)
 
-    epyccel_func = epyccel(get_complex64, language=language)
+    epyccel_func = epyccel(get_complex, language=language)
 
-    assert epyccel_func(bl) == get_complex64(bl)
-    assert epyccel_func(integer8) == get_complex64(integer8)
-    assert epyccel_func(integer16) == get_complex64(integer16)
-    assert epyccel_func(integer) == get_complex64(integer)
-    assert epyccel_func(integer32) == get_complex64(integer32)
+    assert epyccel_func(bl) == get_complex(bl)
+    assert epyccel_func(integer8) == get_complex(integer8)
+    assert epyccel_func(integer16) == get_complex(integer16)
+    assert epyccel_func(integer) == get_complex(integer)
+    assert epyccel_func(integer32) == get_complex(integer32)
     # the if block should be removed after resolving (https://github.com/pyccel/pyccel/issues/735).
     if sys.platform != 'win32':
-        assert epyccel_func(integer64) == get_complex64(integer64)
-        assert epyccel_func(fl) == get_complex64(fl)
-        assert epyccel_func(fl64) == get_complex64(fl64)
-    assert epyccel_func(fl32) == get_complex64(fl32)
+        assert epyccel_func(integer64) == get_complex(integer64)
+        assert epyccel_func(fl) == get_complex(fl)
+        assert epyccel_func(fl64) == get_complex(fl64)
+    assert epyccel_func(fl32) == get_complex(fl32)
 
 @pytest.mark.parametrize( 'language', (
         pytest.param("fortran", marks = [pytest.mark.fortran]),
@@ -1916,22 +1947,8 @@ def test_numpy_complex64_array_like_1d(language):
     )
 )
 
-def test_numpy_complex64_array_like_2d(language):
-
-    @types('bool[:,:]')
-    @types('int[:,:]')
-    @types('int8[:,:]')
-    @types('int16[:,:]')
-    @types('int32[:,:]')
-    @types('int64[:,:]')
-    @types('float[:,:]')
-    @types('float32[:,:]')
-    @types('float64[:,:]')
-    def get_complex64(arr):
-        from numpy import complex64, shape
-        a = complex64(arr)
-        s = shape(a)
-        return len(s), s[0], s[1], a[0,0], a[0,1]
+@pytest.mark.parametrize( 'get_complex', [get_complex128_arr, get_complex64_arr])
+def test_numpy_complex_array_like_2d(language, get_complex):
 
     size = (2, 5)
 
@@ -1948,226 +1965,16 @@ def test_numpy_complex64_array_like_2d(language):
     fl32 = np.float32(fl32)
     fl64 = uniform(min_float64 / 2, max_float64 / 2, size = size)
 
-    epyccel_func = epyccel(get_complex64, language=language)
+    epyccel_func = epyccel(get_complex, language=language)
 
-    assert epyccel_func(bl) == get_complex64(bl)
-    assert epyccel_func(integer8) == get_complex64(integer8)
-    assert epyccel_func(integer16) == get_complex64(integer16)
-    assert epyccel_func(integer) == get_complex64(integer)
-    assert epyccel_func(integer32) == get_complex64(integer32)
+    assert epyccel_func(bl) == get_complex(bl)
+    assert epyccel_func(integer8) == get_complex(integer8)
+    assert epyccel_func(integer16) == get_complex(integer16)
+    assert epyccel_func(integer) == get_complex(integer)
+    assert epyccel_func(integer32) == get_complex(integer32)
     # the if block should be removed after resolving (https://github.com/pyccel/pyccel/issues/735).
     if sys.platform != 'win32':
-        assert epyccel_func(integer64) == get_complex64(integer64)
-        assert epyccel_func(fl) == get_complex64(fl)
-        assert epyccel_func(fl64) == get_complex64(fl64)
-    assert epyccel_func(fl32) == get_complex64(fl32)
-
-def test_numpy_complex128_scalar(language):
-
-
-    @types('bool')
-    @types('int')
-    @types('int8')
-    @types('int16')
-    @types('int32')
-    @types('int64')
-    @types('float')
-    @types('float32')
-    @types('float64')
-    def get_complex128(a):
-        from numpy import complex128
-        b = complex128(a)
-        return b
-
-    integer8 = randint(min_int8, max_int8, dtype=np.int8)
-    integer16 = randint(min_int16, max_int16, dtype=np.int16)
-    integer = randint(min_int, max_int, dtype=np.int)
-    integer32 = randint(min_int32, max_int32, dtype=np.int32)
-    integer64 = randint(min_int64, max_int64, dtype=np.int64)
-
-    fl = uniform(min_float / 2, max_float / 2)
-    fl32 = uniform(min_float32 / 2, max_float32 / 2)
-    fl32 = np.float32(fl32)
-    fl64 = uniform(min_float64 / 2, max_float64 / 2)
-
-
-    epyccel_func = epyccel(get_complex128, language=language)
-
-    f_bl_true_output = epyccel_func(True)
-    test_bool_true_output = get_complex128(True)
-
-    f_bl_false_output = epyccel_func(False)
-    test_bool_false_output = get_complex128(False)
-
-    assert f_bl_true_output == test_bool_true_output
-    assert f_bl_false_output == test_bool_false_output
-
-    assert matching_types(f_bl_true_output, test_bool_true_output)
-    assert matching_types(f_bl_false_output, test_bool_false_output)
-
-    f_integer_output = epyccel_func(integer)
-    test_int_output  = get_complex128(integer)
-
-    assert f_integer_output == test_int_output
-    assert matching_types(f_integer_output, test_int_output)
-
-    f_integer8_output = epyccel_func(integer8)
-    test_int8_output = get_complex128(integer8)
-
-    assert f_integer8_output == test_int8_output
-    assert matching_types(f_integer8_output, test_int8_output)
-
-    f_integer16_output = epyccel_func(integer16)
-    test_int16_output = get_complex128(integer16)
-
-    assert f_integer16_output == test_int16_output
-    assert matching_types(f_integer16_output, test_int16_output)
-
-    f_integer32_output = epyccel_func(integer32)
-    test_int32_output = get_complex128(integer32)
-
-    assert f_integer32_output == test_int32_output
-    assert matching_types(f_integer32_output, test_int32_output)
-
-    # the if block should be removed after resolving (https://github.com/pyccel/pyccel/issues/735).
-    if sys.platform != 'win32':
-        f_integer64_output = epyccel_func(integer64)
-        test_int64_output = get_complex128(integer64)
-
-        assert f_integer64_output == test_int64_output
-        assert matching_types(f_integer64_output, test_int64_output)
-
-    f_fl_output = epyccel_func(fl)
-    test_float_output = get_complex128(fl)
-
-    assert f_fl_output == test_float_output
-    assert matching_types(f_fl_output, test_float_output)
-
-    f_fl32_output = epyccel_func(fl32)
-    test_float32_output = get_complex128(fl32)
-
-    assert f_fl32_output == test_float32_output
-    assert matching_types(f_fl32_output, test_float32_output)
-
-    f_fl64_output = epyccel_func(fl64)
-    test_float64_output = get_complex128(fl64)
-
-    assert f_fl64_output == test_float64_output
-    assert matching_types(f_fl64_output, test_float64_output)
-
-
-@pytest.mark.parametrize( 'language', (
-        pytest.param("fortran", marks = [pytest.mark.fortran]),
-        pytest.param("c", marks = [
-            pytest.mark.skip(reason="Arrays not handled yet."),
-            pytest.mark.c]
-        ),
-        pytest.param("python", marks = [
-            pytest.mark.python]
-        )
-    )
-)
-
-def test_numpy_complex128_array_like_1d(language):
-
-    @types('bool[:]')
-    @types('int[:]')
-    @types('int8[:]')
-    @types('int16[:]')
-    @types('int32[:]')
-    @types('int64[:]')
-    @types('float[:]')
-    @types('float32[:]')
-    @types('float64[:]')
-    def get_complex128(arr):
-        from numpy import complex128, shape
-        a = complex128(arr)
-        s = shape(a)
-        return len(s), s[0], a[0]
-
-    size = 5
-
-    bl = randint(0, 1, size=size, dtype= bool)
-
-    integer8 = randint(min_int8, max_int8, size=size, dtype=np.int8)
-    integer16 = randint(min_int16, max_int16, size=size, dtype=np.int16)
-    integer = randint(min_int, max_int, size=size, dtype=np.int)
-    integer32 = randint(min_int32, max_int32, size=size, dtype=np.int32)
-    integer64 = randint(min_int64, max_int64, size=size, dtype=np.int64)
-
-    fl = uniform(min_float / 2, max_float / 2, size = size)
-    fl32 = uniform(min_float32 / 2, max_float32 / 2, size = size)
-    fl32 = np.float32(fl32)
-    fl64 = uniform(min_float64 / 2, max_float64 / 2, size = size)
-
-    epyccel_func = epyccel(get_complex128, language=language)
-
-    assert epyccel_func(bl) == get_complex128(bl)
-    assert epyccel_func(integer8) == get_complex128(integer8)
-    assert epyccel_func(integer16) == get_complex128(integer16)
-    assert epyccel_func(integer) == get_complex128(integer)
-    assert epyccel_func(integer32) == get_complex128(integer32)
-    # the if block should be removed after resolving (https://github.com/pyccel/pyccel/issues/735).
-    if sys.platform != 'win32':
-        assert epyccel_func(integer64) == get_complex128(integer64)
-        assert epyccel_func(fl) == get_complex128(fl)
-        assert epyccel_func(fl64) == get_complex128(fl64)
-    assert epyccel_func(fl32) == get_complex128(fl32)
-
-@pytest.mark.parametrize( 'language', (
-        pytest.param("fortran", marks = [pytest.mark.fortran]),
-        pytest.param("c", marks = [
-            pytest.mark.skip(reason="Arrays not handled yet."),
-            pytest.mark.c]
-        ),
-        pytest.param("python", marks = [
-            pytest.mark.python]
-        )
-    )
-)
-
-def test_numpy_complex128_array_like_2d(language):
-
-    @types('bool[:,:]')
-    @types('int[:,:]')
-    @types('int8[:,:]')
-    @types('int16[:,:]')
-    @types('int32[:,:]')
-    @types('int64[:,:]')
-    @types('float[:,:]')
-    @types('float32[:,:]')
-    @types('float64[:,:]')
-    def get_complex128(arr):
-        from numpy import complex128, shape
-        a = complex128(arr)
-        s = shape(a)
-        return len(s), s[0], s[1], a[0,0], a[0,1]
-
-    size = (2, 5)
-
-    bl = randint(0, 1, size=size, dtype= bool)
-
-    integer8 = randint(min_int8, max_int8, size=size, dtype=np.int8)
-    integer16 = randint(min_int16, max_int16, size=size, dtype=np.int16)
-    integer = randint(min_int, max_int, size=size, dtype=np.int)
-    integer32 = randint(min_int32, max_int32, size=size, dtype=np.int32)
-    integer64 = randint(min_int64, max_int64, size=size, dtype=np.int64)
-
-    fl = uniform(min_float / 2, max_float / 2, size = size)
-    fl32 = uniform(min_float32 / 2, max_float32 / 2, size = size)
-    fl32 = np.float32(fl32)
-    fl64 = uniform(min_float64 / 2, max_float64 / 2, size = size)
-
-    epyccel_func = epyccel(get_complex128, language=language)
-
-    assert epyccel_func(bl) == get_complex128(bl)
-    assert epyccel_func(integer8) == get_complex128(integer8)
-    assert epyccel_func(integer16) == get_complex128(integer16)
-    assert epyccel_func(integer) == get_complex128(integer)
-    assert epyccel_func(integer32) == get_complex128(integer32)
-    # the if block should be removed after resolving (https://github.com/pyccel/pyccel/issues/735).
-    if sys.platform != 'win32':
-        assert epyccel_func(integer64) == get_complex128(integer64)
-        assert epyccel_func(fl) == get_complex128(fl)
-        assert epyccel_func(fl64) == get_complex128(fl64)
-    assert epyccel_func(fl32) == get_complex128(fl32)
+        assert epyccel_func(integer64) == get_complex(integer64)
+        assert epyccel_func(fl) == get_complex(fl)
+        assert epyccel_func(fl64) == get_complex(fl64)
+    assert epyccel_func(fl32) == get_complex(fl32)
