eaad00d39bd594e8c4219f2abf93f2239a6c8a97
Emily.Bourne <louise.bourne@gmail.com>
2021-03-17 16:49:29 +0100

    Add missing import

--- pyccel/complexity/memory.py
+++ pyccel/complexity/memory.py
@@ -37,7 +37,7 @@ from sympy import Poly, LT
 
 from pyccel.ast.basic        import Basic
 from pyccel.ast.builtins     import PythonTuple
-from pyccel.ast.core         import For, Assign, CodeBlock
+from pyccel.ast.core         import For, Assign, CodeBlock, FunctionDef
 from pyccel.ast.internals    import PyccelSymbol
 from pyccel.ast.numpyext     import NumpyZeros, NumpyOnes
 from pyccel.ast.sympy_helper import pyccel_to_sympy
