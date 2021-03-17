b33d74b372dfbdd5217bf639463ec26ab3bf51f6
mostafa <mostafa-1470@live.fr>
2021-03-17 13:41:46 +0100

    used imports

--- tests/epyccel/recognised_functions/test_numpy_types.py
+++ tests/epyccel/recognised_functions/test_numpy_types.py
@@ -12,6 +12,9 @@ from test_numpy_funcs import (min_int, max_int, min_int8, max_int8,
                                 min_int16, max_int16, min_int32, max_int32, max_int64, min_int64)
 from test_numpy_funcs import max_float, min_float, max_float32, min_float32,max_float64, min_float64
 
+
+from test_numpy_funcs import matching_types
+
 def test_numpy_bool_scalar(language):
 
     @types('bool')
