e2cf48b33e306424213e07789ce14fa636031b35
Emily.Bourne <louise.bourne@gmail.com>
2021-03-18 09:46:58 +0100

    add debug message

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,6 +28,7 @@ def pytest_runtest_teardown(item, nextitem):
     xdist_plugin = config.pluginmanager.getplugin("xdist")
     if xdist_plugin is None or "PYTEST_XDIST_WORKER_COUNT" not in os.environ \
             or os.getenv('PYTEST_XDIST_WORKER_COUNT') == 1:
+        print("Tearing down!")
         marks = [m.name for m in item.own_markers ]
         if 'parallel' not in marks:
             pyccel_clean(path_dir, remove_shared_libs = True)
