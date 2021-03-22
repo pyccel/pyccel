136fddef1e6d4f686801822116376c13478d7d52
Emily.Bourne <louise.bourne@gmail.com>
2021-03-22 10:40:18 +0100

    Prove that lock is working

--- pyccel/codegen/pipeline.py
+++ pyccel/codegen/pipeline.py
@@ -351,7 +351,8 @@ def execute_pyccel(fname, *,
                                             includes=[lib_dest_path])
                     try:
                         for f,l in zip(source_files, internal_modules):
-                            lock = FileLock(l + '.lock')
+                            lock = FileLock(l + '.lock', timeout=1)
+                            print("Locking ", l + '.lock')
                             with lock:
                                 compile_files(f, f90exec, flags,
                                                 binary=None,
--- pyccel/codegen/python_wrapper.py
+++ pyccel/codegen/python_wrapper.py
@@ -9,6 +9,7 @@ import subprocess
 import os
 import glob
 import warnings
+import time
 from filelock import FileLock
 
 from pyccel.ast.bind_c                      import as_static_module
@@ -131,10 +132,12 @@ def create_shared_library(codegen,
         if verbose:
             print(' '.join(cmd))
         lock_names = [d+'.lock' for d in dep_mods]
+        print(lock_names)
         locks = [FileLock(n) for n in lock_names]
         for l in locks:
             l.acquire()
         try:
+            time.sleep(10)
             p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
         finally:
             for l in locks:
