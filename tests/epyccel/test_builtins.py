c8b4218b8959f16c78d0701f6079060e85affb22
Emily.Bourne <louise.bourne@gmail.com>
2021-03-25 09:07:27 +0100

    [CODACY] Remove unused import

--- tests/epyccel/test_builtins.py
+++ tests/epyccel/test_builtins.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
 import pytest
 from numpy.random import randint, uniform
-from numpy import isclose, iinfo, finfo
+from numpy import iinfo, finfo
 import numpy as np
 
 from pyccel.epyccel import epyccel
