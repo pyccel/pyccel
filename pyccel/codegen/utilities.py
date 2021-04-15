757147b08f0135d03cfbef89a393decbf778adce
Emily.Bourne <louise.bourne@gmail.com>
2021-04-15 14:06:58 +0200

    Allow both gfortran.lib and libgfortran.a to be acceptable libraries on windows

--- pyccel/codegen/utilities.py
+++ pyccel/codegen/utilities.py
@@ -177,14 +177,17 @@ def get_gfortran_library_dir():
     """Provide the location of the gfortran libraries for linking
     """
     if sys.platform == "win32":
-        file_name = 'gfortran.lib'
+        file_name_list = ['gfortran.lib', 'libgfortran.a']
     else:
-        file_name = 'libgfortran.a'
-    file_location = subprocess.check_output([shutil.which('gfortran'), '-print-file-name='+file_name],
-            universal_newlines = True)
-    lib_dir = os.path.abspath(os.path.dirname(file_location))
-    if lib_dir:
-        if lib_dir not in sys.path:
-            # Add to sytem path
-            sys.path.insert(0, lib_dir)
+        file_name_list = ['libgfortran.a']
+    for f in file_name_list:
+        file_name = file_name_list[0]
+        file_location = subprocess.check_output([shutil.which('gfortran'), '-print-file-name='+file_name],
+                universal_newlines = True)
+        lib_dir = os.path.abspath(os.path.dirname(file_location))
+        if lib_dir:
+            if lib_dir not in sys.path:
+                # Add to sytem path
+                sys.path.insert(0, lib_dir)
+            break
     return lib_dir
