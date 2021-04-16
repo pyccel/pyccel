efa745571a06d33ade090bca4e04277a101d5950
Emily.Bourne <louise.bourne@gmail.com>
2021-04-16 15:18:16 +0200

    Try to choose mingw compiler

--- pyccel/codegen/python_wrapper.py
+++ pyccel/codegen/python_wrapper.py
@@ -127,6 +127,9 @@ def create_shared_library(codegen,
 
         setup_filename = os.path.join(pyccel_dirpath, setup_filename)
         cmd = [sys.executable, setup_filename, "build"]
+        if sys.platform == 'win32':
+            print(get_default_compiler())
+            cmd+=['--compiler=mingw32']
 
         if verbose:
             print(' '.join(cmd))
