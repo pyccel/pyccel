d20cdfab9f19077334b14a9a31a8b27737a64ff6
Emily.Bourne <louise.bourne@gmail.com>
2021-04-26 21:17:21 +0200

    Activate tests (require wrapper to pass)

--- tests/epyccel/test_epyccel_optional_args.py
+++ tests/epyccel/test_epyccel_optional_args.py
@@ -148,14 +148,6 @@ def test_f11(language):
     assert mod.call_optional_12() == modnew.call_optional_12()
 
 #------------------------------------------------------------------------------
-@pytest.mark.parametrize( 'language', (
-        pytest.param("fortran", marks = pytest.mark.fortran),
-        pytest.param("c", marks = [
-            pytest.mark.xfail(reason="Arrays cannot be optional. See #757"),
-            pytest.mark.c]
-        )
-    )
-)
 def test_optional_args_1d(language):
     @types( 'int[:]', 'int[:]')
     def f12(x, y = None):
@@ -174,14 +166,6 @@ def test_optional_args_1d(language):
     assert np.array_equal(x1, x2)
 
 #------------------------------------------------------------------------------
-@pytest.mark.parametrize( 'language', (
-        pytest.param("fortran", marks = pytest.mark.fortran),
-        pytest.param("c", marks = [
-            pytest.mark.xfail(reason="Arrays cannot be optional. See #757"),
-            pytest.mark.c]
-        )
-    )
-)
 def test_optional_2d_F(language):
     @types('int32[:,:](order=F)', 'int32[:,:](order=F)')
     def f13(x, y = None):
