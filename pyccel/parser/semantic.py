d08c0cc9adc32419e59e1c8818ced08d1176e784
Emily.Bourne <louise.bourne@gmail.com>
2021-05-05 09:40:20 +0200

    Add matmul operator to semantic stage

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -81,7 +81,7 @@ from pyccel.ast.builtins import python_builtin_datatype
 from pyccel.ast.builtins import (PythonRange, PythonZip, PythonEnumerate,
                                  PythonMap, PythonTuple, Lambda)
 
-from pyccel.ast.numpyext import NumpyZeros
+from pyccel.ast.numpyext import NumpyZeros, NumpyMatmul
 from pyccel.ast.numpyext import NumpyBool
 from pyccel.ast.numpyext import NumpyInt, NumpyInt8, NumpyInt16, NumpyInt32, NumpyInt64
 from pyccel.ast.numpyext import NumpyFloat, NumpyFloat32, NumpyFloat64
@@ -3305,6 +3305,11 @@ class SemanticParser(BasicParser):
         assert(var.rank==1)
         size = var.shape[0]
         return StarredArguments([var[i] for i in range(size)])
+
+    def _visit_NumpyMatmul(self, expr, **settings):
+        a = self._visit(expr.a)
+        b = self._visit(expr.b)
+        return NumpyMatmul(a, b)
 
 #==============================================================================
 
