445bedcb316509c1154f6910d036c3c753d0df7a
Emily.Bourne <louise.bourne@gmail.com>
2021-05-14 13:08:05 +0200

    Handle elif/else differences

--- tests/parser/test_comments.py
+++ tests/parser/test_comments.py
@@ -20,6 +20,15 @@ class Unparser(astunparse.Unparser):
     def _CommentMultiLine(self, node):
         self.fill(node.s)
 
+def get_indent(l):
+    i = 0
+    for li in l:
+        if li == ' ':
+            i+=1
+        else:
+            break
+    return i
+
 base_dir = os.path.dirname(os.path.realpath(__file__))
 path_dir = os.path.join(base_dir, 'scripts')
 
@@ -48,19 +57,56 @@ def test_parse(f):
     with open('out.py') as infile:
         copy = infile.read().strip()
 
+    # Get non-empty lines
     orig_lines = [l for l in orig.expandtabs(4).split('\n') if l!= '']
     copy_lines = [l for l in copy.expandtabs(4).split('\n') if l!= '']
 
-    for o,c in zip(orig_lines,copy_lines):
+    # Get indentation
+    orig_indents = [get_indent(l) for l in orig_lines]
+    copy_indents = [get_indent(l) for l in copy_lines]
+
+    # Remove spaces from lines
+    orig_lines = [l.replace(' ','') for l in orig_lines]
+    copy_lines = [l.replace(' ','') for l in copy_lines]
+
+    on = len(orig_lines)
+    cn = len(copy_lines)
+    oi = 0
+    ci = 0
+    extra_o_indent = 0
+    extra_c_indent = 0
+    while oi<on and ci<cn:
+        o = orig_lines[oi]
+        c = copy_lines[ci]
         # Check for the same indentation
-        for oi, ci in zip(o,c):
-            if oi == ' ' or ci == ' ':
-                assert(oi==ci)
-            else:
-                break
-        o = o.replace(' ','')
-        c = c.replace(' ','')
+        assert(orig_indents[oi] == copy_indents[ci])
+        # Handle change between elif and else
+        if o.startswith('else') and c.startswith('elif'):
+            oi += 1
+            o = orig_lines[oi].replace(' ','')
+            c = c[2:]
+            # Change indentation to match
+            indent = orig_indents[oi]
+            diff = (indent-orig_indents[oi-1])
+            i = oi
+            while i<len(orig_indents) and orig_indents[i]>=indent:
+                orig_indents[i]-=diff
+                i+=1
+        elif o.startswith('elif') and c.startswith('else'):
+            ci += 1
+            c = copy_lines[ci].replace(' ','')
+            o = o[2:]
+            # Change indentation to match
+            indent = copy_indents[ci]
+            diff = (indent-copy_indents[ci-1])
+            i = ci
+            while i<len(copy_indents) and copy_indents[i]>=indent:
+                copy_indents[i]-=diff
+                i+=1
+        # Check lines
         assert(o==c)
+        oi += 1
+        ci += 1
 
 ######################
 if __name__ == '__main__':
