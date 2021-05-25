d54b09217e64c5cb0fa833fffc8ba9ef84fa5d1a
Emily.Bourne <louise.bourne@gmail.com>
2021-05-25 23:51:23 +0200

    Disable unhelpful pylint messages

--- pyccel/ast/utilities.py
+++ pyccel/ast/utilities.py
@@ -90,7 +90,7 @@ builtin_import_registery = {'numpy': {
                             'math': {**math_functions, ** math_constants},
                             'pyccel.decorators': pyccel_decorators.__all__}
 if sys.version_info < (3, 10):
-    python_builtin_libs = [s[1:] if s[0]=='_' else s for s in sys.builtin_module_names]
+    python_builtin_libs = [s[1:] if s[0]=='_' else s for s in sys.builtin_module_names] # pylint: disable=no-member
 else:
     python_builtin_libs = list(sys.stdlib_module_names)
 
--- tests/errors/semantic/non_blocking/UNKNOWN_IMPORT2.py
+++ tests/errors/semantic/non_blocking/UNKNOWN_IMPORT2.py
@@ -1,1 +1,2 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring/, unused-import
 import abc
