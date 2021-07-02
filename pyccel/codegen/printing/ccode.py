a51cb288caac2ee2bdac7a5badb2e04d32561b35
Emily.Bourne <louise.bourne@gmail.com>
2021-07-02 17:13:03 +0200

    [CODACY] Unused import

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -9,7 +9,7 @@ import functools
 import operator
 
 from pyccel.ast.builtins  import PythonRange, PythonComplex
-from pyccel.ast.builtins  import PythonLen, PythonPrint
+from pyccel.ast.builtins  import PythonPrint
 from pyccel.ast.builtins  import PythonList, PythonTuple
 
 from pyccel.ast.core      import Declare, For, CodeBlock
