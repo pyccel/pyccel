78cb2d4a9c8c48691142e706412e374a5d53d414
Emily.Bourne <louise.bourne@gmail.com>
2021-07-27 11:41:30 +0200

    Add information known by python. E.g. math library and python includes/libs/etc

--- pyccel/compilers/generate_default.py
+++ pyccel/compilers/generate_default.py
@@ -1,12 +1,16 @@
 import json
 import os
 import sys
+import sysconfig
+from itertools import chain
+from numpy import get_include as get_numpy_include
 
 gfort_info = {'exec' : 'gfortran',
               'mpi_exec' : 'mpif90',
               'language': 'fortran',
               'module_output_flag': '-J',
               'debug_flags': ("-fcheck=bounds",),
+              'release_flags': ("-O3",),
               'standard_flags' : ('-std=f2003','-fPIC'),
               'mpi': {
                   },
@@ -30,6 +34,7 @@ ifort_info = {'exec' : 'ifort',
               'language': 'fortran',
               'module_output_flag': '-module',
               'debug_flags': ("-check=bounds",),
+              'release_flags': ("-O3",),
               'standard_flags' : ('-std=f2003','-fPIC'),
               'openmp': {
                   'flags' : ('-fopenmp','-nostandard-realloc-lhs'),
@@ -44,8 +49,8 @@ gcc_info = {'exec' : 'gcc',
             'mpi_exec' : 'mpicc',
             'language': 'c',
             'debug_flags': ("-g",),
+            'release_flags': ("-O3",),
             'standard_flags' : ('-std=c99','-fPIC'),
-            'libs' : ('m',),
             'openmp': {
                 'flags' : ('-fopenmp',),
                 'libs'  : ('gomp',),
@@ -63,8 +68,8 @@ icc_info = {'exec' : 'icc',
             'mpi_exec' : 'mpiicc',
             'language': 'c',
             'debug_flags': ("-g",),
+            'release_flags': ("-O3",),
             'standard_flags' : ('-std=c99','-fPIC'),
-            'libs' : ('m',),
             'openmp': {
                 'flags' : ('-fopenmp',),
                 },
@@ -73,11 +78,27 @@ icc_info = {'exec' : 'icc',
                 },
             }
 #------------------------------------------------------------
+python_info = {
+        "libs" : [s[2:] for s in sysconfig.get_config_var("LIBM").split()], # Strip -l from beginning
+        'python': {
+            'flags' : sysconfig.get_config_var("CFLAGS").split()\
+                + sysconfig.get_config_var("CC").split()[1:],
+            'includes' : [*sysconfig.get_config_var("INCLUDEPY").split(), get_numpy_include()],
+            'libs' : [s[2:] for s in sysconfig.get_config_var("LIBPYTHON").split()
+                                        +sysconfig.get_config_var("BLDLIBRARY").split()
+                                        +sysconfig.get_config_var("LIBS").split()], #Strip -l from beginning
+            'libdirs' : sysconfig.get_config_var("LIBPL").split(),
+            "linker_flags" : sysconfig.get_config_var("LDSHARED").split()[1:],
+            "shared_suffix" : sysconfig.get_config_var("EXT_SUFFIX"),
+            }
+        }
 
 save_folder = os.path.dirname(os.path.abspath(__file__))
 
 def print_json(filename, info):
-    print(json.dumps(info, indent=4), file=open(os.path.join(save_folder, filename),'w'))
+    print(json.dumps({k:v for k,v in chain(info.items(), python_info.items())},
+                     indent=4),
+          file=open(os.path.join(save_folder, filename),'w'))
 
 def generate_default():
     files = {
