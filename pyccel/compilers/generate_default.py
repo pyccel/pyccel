ecc0b71d35356407fbc62a76968e6e84f7630c51
Emily.Bourne <louise.bourne@gmail.com>
2021-08-02 13:24:01 +0200

    Remove gcc from linker flags

--- pyccel/compilers/generate_default.py
+++ pyccel/compilers/generate_default.py
@@ -94,7 +94,8 @@ icc_info = {'exec' : 'icc',
 #------------------------------------------------------------
 config_vars = sysconfig.get_config_vars()
 print(json.dumps(config_vars, indent=4))
-linker_flags = config_vars.get("BLDLIBRARY","").split()+config_vars.get("LDSHARED","").split()
+linker_flags = config_vars.get("BLDLIBRARY","").split() \
+                + config_vars.get("LDSHARED","").split()[1:]
 python_info = {
         "libs" : config_vars.get("LIBM","").split(), # Strip -l from beginning
         "libdirs" : config_vars.get("LIBDIR","").split(),
