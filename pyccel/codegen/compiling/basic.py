eb07a560e312c101732e13bef5d0a6473035bcaf
Emily.Bourne <louise.bourne@gmail.com>
2021-08-02 09:55:55 +0200

    Collect dependencies to avoid lock problems when file and dependencies share a dependency

--- pyccel/codegen/compiling/basic.py
+++ pyccel/codegen/compiling/basic.py
@@ -73,11 +73,11 @@ class CompileObj:
         self._libs         = list(libs or ())
         self._libdirs      = list(libdirs or ())
         self._accelerators = set(accelerators or ())
-        self._dependencies = []
+        self._dependencies = set()
         if dependencies:
             self.add_dependencies(*dependencies)
         self._is_module    = is_module
-        self._lock         = FileLock(os.path.splitext(self.source)[0]+'.lock')
+        self._lock         = FileLock(self.target+'.lock')
 
     @property
     def source(self):
@@ -158,7 +158,7 @@ class CompileObj:
         """
         if not all(isinstance(d, CompileObj) for d in args):
             raise TypeError("Dependencies require necessary compile information")
-        self._dependencies.extend(args)
+        self._dependencies = self._dependencies.union(args)
         for a in args:
             self._includes.extend(a.includes)
             self._libs.extend(a.libs)
@@ -171,7 +171,13 @@ class CompileObj:
         """
         self._lock.acquire()
         for d in self.dependencies:
-            d.acquire_lock()
+            d.acquire_simple_lock()
+
+    def acquire_simple_lock(self):
+        """
+        Lock the file to prevent race conditions but not its dependencies
+        """
+        self._lock.acquire()
 
     def release_lock(self):
         """
@@ -179,7 +185,13 @@ class CompileObj:
         """
         self._lock.release()
         for d in self.dependencies:
-            d.release_lock()
+            d.release_simple_lock()
+
+    def release_simple_lock(self):
+        """
+        Unlock the file
+        """
+        self._lock.release()
 
     @property
     def accelerators(self):
@@ -192,3 +204,9 @@ class CompileObj:
         """ Indicates if the result is a module or a program
         """
         return self._is_module
+
+    def __eq__(self, other):
+        return self.target == other.target
+
+    def __hash__(self):
+        return hash(self.target)
