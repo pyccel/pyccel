d63dd162d8a20a2f81be3458558930836bfc3c7f
Emily.Bourne <louise.bourne@gmail.com>
2021-08-13 16:15:16 +0200

    Add transpose tests

---
+++ tests/epyccel/test_epyccel_transpose.py
@@ -0,0 +1,101 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring/
+import pytest
+import numpy as np
+from numpy.random import randint
+
+from pyccel.epyccel import epyccel
+
+def test_transpose_shape(language):
+    def f1(x : 'int[:,:]'):
+        from numpy import transpose
+        y = transpose(x)
+        n, m = y.shape
+        return n, m, y[-1,0], y[0,-1]
+    def f2(x : 'int[:,:,:]'):
+        from numpy import transpose
+        y = transpose(x)
+        n, m, p = y.shape
+        return n, m, p, y[0,-1,0], y[0,0,-1], y[-1,-1,0]
+
+    x1 = randint(50, size=(2,5))
+    x2 = randint(50, size=(2,3,7))
+
+    f1_epyc = epyccel(f1, language=language)
+    assert f1( x1 ) == f1_epyc( x1 )
+
+    f2_epyc = epyccel(f2, language=language)
+    assert f2( x2 ) == f2_epyc( x2 )
+
+def test_transpose_property(language):
+    def f1(x : 'int[:,:]'):
+        y = x.T
+        n, m = y.shape
+        return n, m, y[-1,0], y[0,-1]
+    def f2(x : 'int[:,:,:]'):
+        y = x.T
+        n, m, p = y.shape
+        return n, m, p, y[0,-1,0], y[0,0,-1], y[-1,-1,0]
+
+    x1 = randint(50, size=(2,5))
+    x2 = randint(50, size=(2,3,7))
+
+    f1_epyc = epyccel(f1, language=language)
+    assert f1( x1 ) == f1_epyc( x1 )
+
+    f2_epyc = epyccel(f2, language=language)
+    assert f2( x2 ) == f2_epyc( x2 )
+
+def test_transpose_in_expression(language):
+    def f1(x : 'int[:,:]'):
+        from numpy import transpose
+        y = transpose(x)+3
+        n, m = y.shape
+        return n, m, y[-1,0], y[0,-1]
+    def f2(x : 'int[:,:,:]'):
+        y = x.T*3
+        n, m, p = y.shape
+        return n, m, p, y[0,-1,0], y[0,0,-1], y[-1,-1,0]
+
+    x1 = randint(50, size=(2,5))
+    x2 = randint(50, size=(2,3,7))
+
+    f1_epyc = epyccel(f1, language=language)
+    assert f1( x1 ) == f1_epyc( x1 )
+
+    f2_epyc = epyccel(f2, language=language)
+    assert f2( x2 ) == f2_epyc( x2 )
+
+def test_mixed_order(language):
+    def f1(x : 'int[:,:]'):
+        from numpy import transpose, ones
+        n, m = x.shape
+        y = ones((m,n), order='F')
+        z = x+transpose(y)
+        n, m = z.shape
+        return n, m, z[-1,0], z[0,-1]
+    def f2(x : 'int[:,:]'):
+        from numpy import transpose, ones
+        n, m = x.shape
+        y = ones((m,n), order='F')
+        z = transpose(x)+y
+        n, m = z.shape
+        return n, m, z[-1,0], z[0,-1]
+    def f3(x : 'int[:,:,:]'):
+        from numpy import transpose, ones
+        n, m, p = x.shape
+        y = ones((p,m,n))
+        z = transpose(x)+y
+        n, m, p = z.shape
+        return n, m, p, z[0,-1,0], z[0,0,-1], z[-1,-1,0]
+
+    x1 = randint(50, size=(2,5))
+    x2 = randint(50, size=(2,3,7))
+
+    f1_epyc = epyccel(f1, language=language)
+    assert f1( x1 ) == f1_epyc( x1 )
+
+    f2_epyc = epyccel(f2, language=language)
+    assert f2( x1 ) == f2_epyc( x1 )
+
+    f3_epyc = epyccel(f3, language=language)
+    assert f3( x2 ) == f3_epyc( x2 )
