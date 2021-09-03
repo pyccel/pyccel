925cee55cc6b0c6c8d1ad9023113ab2b63670d05
Emily.Bourne <louise.bourne@gmail.com>
2021-09-03 15:14:59 +0200

    Remove array functions. Make dataTypes extern

--- pyccel/stdlib/cwrapper/cwrapper.h
+++ pyccel/stdlib/cwrapper/cwrapper.h
@@ -28,22 +28,7 @@
 # define NO_TYPE_CHECK -1
 # define NO_ORDER_CHECK -1
 
-
-/*
- * Function: pyarray_checker
- * --------------------
- * Check Python Object (DataType, Rank, Order):
- *
- * Parameters :
- *     a 	 : python array object
- *     dtype : desired data type enum
- *     rank  : desired rank
- *     flag  : desired order flag
- *
- * Returns	  :
- *     return true if no error occurred otherwise it will return false
- */
-bool	pyarray_checker(PyArrayObject *o, int dtype, int rank, int flag);
+extern const char* dataTypes[17];
 
 /*
  * Functions : Cast functions
