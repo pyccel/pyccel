452f6be6979d043ac12308c8af51f1aa0c70a373
AbdelGhafour <aboukacem39@gmail.com>
2021-09-10 10:27:23 +0100

    added updated lapack header file and soubroutine tests

---
+++ tests/pyccel/scripts/lapack_subroutine.py
@@ -0,0 +1,40 @@
+# Matrix inverse test using dgetri and dgetrf subroutines
+
+from pyccel.stdlib.internal.lapack import dgetrf
+from pyccel.stdlib.internal.lapack  import dgetri
+from numpy import zeros,float64,int32,float32
+
+def dgetrf_test(A: 'float64[:,:](order=F)',piv:'int32[:]'):
+    n = int32(len(A))
+    info = int32(-1)
+    lda = int32(n)
+
+    dgetrf(n, n, A, lda, piv, info)
+
+
+def dgetri_test(A:'float64[:,:](order=F)'):
+    n = int32(len(A))
+
+    ipiv = zeros(n, dtype=int32,order='F')
+    
+    lda = int32(n)
+    info = int32(-1)
+
+    dgetrf_test(A,ipiv)
+    
+    lwork = int32(4 * n)
+    work  = zeros(lwork,dtype=float64)
+
+    dgetri(n, A, lda, ipiv, work, lwork, info)
+
+
+N = 4
+
+A = zeros((N,N),dtype=float64,order='F')
+
+for i in range(N):
+    A[i,i] = 1.0
+
+dgetri_test(A)
+
+print(A)
