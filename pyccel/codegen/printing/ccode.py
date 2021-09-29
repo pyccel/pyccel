31f320c502340c0b3473f3f9b5869beab5390b46
EmilyBourne <louise.bourne@gmail.com>
2021-09-29 08:17:19 +0200

    Bug fixes (#972)
    Fix bugs found by testing the pyccel-examples repository
    
    **Commit Summary**
    
    - Remove unnecessary line break in `_print_Return`
    - Handle if with allocations where 1st block doesn't allocate all variables
    - Modify test to encompass corner case

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -1352,7 +1352,7 @@ class CCodePrinter(CodePrinter):
             unneeded_var = not any(b in vars_in_deallocate_nodes for b in variables)
             if unneeded_var:
                 code = ''.join(self._print(a) for a in expr.stmt.body if a is not last_assign)
-                return code + '\nreturn {};\n'.format(self._print(last_assign.rhs))
+                return code + 'return {};\n'.format(self._print(last_assign.rhs))
             else:
                 code = ''+self._print(expr.stmt)
                 self._additional_declare.append(last_assign.lhs)
