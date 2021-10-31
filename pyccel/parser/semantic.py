34439051993f19b794c972987b9dc55843c67f9e
EmilyBoune <louise.bourne@gmail.com>
2021-10-31 14:22:59 +0100

    Create FunctionDef for all objects in header for imports without targets

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -247,15 +247,7 @@ class SemanticParser(BasicParser):
         # FunctionDef etc ...
 
         if self.is_header_file:
-            target = []
-
-            for parent in self.parents:
-                for (key, item) in parent.imports.items():
-                    if get_filename_from_import(key) == self.filename:
-                        target += item
-
-            target = set(target)
-            target_headers = target.intersection(self.namespace.headers.keys())
+            target_headers = self.namespace.headers.keys()
             # ARA : issue-999
             is_external = self.metavars.get('external', False)
             for name in list(target_headers):
