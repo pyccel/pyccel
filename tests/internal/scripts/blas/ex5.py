63ac961edc46750447ac4a5debb69e8ceb98c4e9
EmilyBoune <louise.bourne@gmail.com>
2021-11-01 21:45:28 +0100

    Add subroutine test

--- tests/internal/scripts/blas/ex5.py
+++ tests/internal/scripts/blas/ex5.py
@@ -6,3 +6,12 @@ def f(x: 'float32[:]'):
     n = np.int32(x.shape[0])
     incx = np.int32(1)
     return blas_mod.sasum (n, x, incx)
+
+def g(vect1 : 'float64[:]', vect2 : 'float64[:]'):
+    import pyccel.stdlib.internal.blas as blas_mod
+
+    sa = 3.5
+    n = np.int32(vect1.shape[0])
+
+    blas_mod.daxpy(n, sa, vect1, np.int32(1), vect2, np.int32(1))
+
