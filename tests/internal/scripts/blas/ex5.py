1d834faeeec8a4eaec17997497c91b3754ff8a29
Emily.Bourne <louise.bourne@gmail.com>
2021-11-02 10:39:30 +0100

    Add inline example test

--- tests/internal/scripts/blas/ex5.py
+++ tests/internal/scripts/blas/ex5.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
 import numpy as np
+from pyccel.decorators import inline
 
 def f(x: 'float32[:]'):
     import pyccel.stdlib.internal.blas as blas_mod
@@ -15,3 +16,13 @@ def g(vect1 : 'float64[:]', vect2 : 'float64[:]'):
 
     blas_mod.daxpy(n, sa, vect1, np.int32(1), vect2, np.int32(1))
 
+@inline
+def sasum(x: 'float32[:]',
+          incx: 'int64' = 1,
+         ):
+    import pyccel.stdlib.internal.blas as blas_mod
+    n = np.int32(x.shape[0])
+    return blas_mod.sasum (n, x, np.int32(incx))
+
+def h(x: 'float32[:]'):
+    return sasum (x)
