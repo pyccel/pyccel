afd7f936484458855ada2aec111a6d6fd84a093f
Emily.Bourne <louise.bourne@gmail.com>
2021-11-19 10:23:07 +0100

    Test all functions in runtest

--- tests/pyccel/scripts/runtest_decorators_inline.py
+++ tests/pyccel/scripts/runtest_decorators_inline.py
@@ -1,5 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
-from decorators_inline import power_4, get_powers, f, sin_base_1
+import numpy as np
+from decorators_inline import power_4, get_powers, f, sin_base_1, fill_pi, fill_pi
 
 def g(s : int):
     return f(s)/3
@@ -9,3 +10,6 @@ if __name__ == '__main__':
     print(power_4(5))
     print(f(4))
     print(sin_base_1(0.25))
+    arr = np.empty(4)
+    fill_pi(arr)
+    print(arr)
