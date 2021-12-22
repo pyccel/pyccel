11b568321ace1f830232d9d7e567ee462151a1c1
Emily.Bourne <louise.bourne@gmail.com>
2021-12-22 18:46:21 +0100

    Ensure all returns have same type

--- tests/epyccel/test_epyccel_functions.py
+++ tests/epyccel/test_epyccel_functions.py
@@ -308,8 +308,9 @@ def test_multiple_returns_f14(language):
 def test_decorator_f15(language):
     @types('bool', 'int8', 'int16', 'int32', 'int64')
     def f15(a,b,c,d,e):
+        import numpy as np
         if a:
-            return b + c
+            return np.int64(b + c)
         else:
             return d + e
 
