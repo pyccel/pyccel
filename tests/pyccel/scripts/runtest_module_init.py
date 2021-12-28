87af3de4f294f2fa3a8949aeab3d793cf6e1cf91
Emily.Bourne <louise.bourne@gmail.com>
2021-12-28 15:51:34 +0100

    Add to test

--- tests/pyccel/scripts/runtest_module_init.py
+++ tests/pyccel/scripts/runtest_module_init.py
@@ -1,7 +1,9 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
 from module_init import f
 from module_init import g
+from module_init import i
 
 if __name__ == '__main__':
     f()
     g()
+    print(i)
