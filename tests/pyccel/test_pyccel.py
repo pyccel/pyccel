8d801888d7b7a3ee5afea170527d7f31b91597ee
Emily.Bourne <louise.bourne@gmail.com>
2022-01-26 09:09:24 +0100

    Test if updated windows fixes lapack problems

--- tests/pyccel/test_pyccel.py
+++ tests/pyccel/test_pyccel.py
@@ -724,7 +724,7 @@ def test_classes( test_file ):
     pyccel_test(test_file, compile_with_pyccel = False)
 
 #------------------------------------------------------------------------------
-@pytest.mark.skipif( sys.platform == 'win32', reason="Compilation problem. On execution Windows raises: error while loading shared libraries: liblapack.dll: cannot open shared object file: No such file or directory" )
+#@pytest.mark.skipif( sys.platform == 'win32', reason="Compilation problem. On execution Windows raises: error while loading shared libraries: liblapack.dll: cannot open shared object file: No such file or directory" )
 @pytest.mark.parametrize( "test_file", ["scripts/lapack_subroutine.py",
                                         ] )
 def test_lapack( test_file ):
