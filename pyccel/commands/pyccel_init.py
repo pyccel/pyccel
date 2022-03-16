a9db8e003566f2b4c374bedab5449e3b61a42b79
Emily.Bourne <louise.bourne@gmail.com>
2022-03-16 14:28:21 +0100

    Add pyccel-init script

---
+++ pyccel/commands/pyccel_init.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+""" Module containing script to pyccelize internal files
+"""
+import os
+from pyccel.parser.parser import Parser
+from argparse import ArgumentParser
+
+def pyccel_init():
+    """ Pickle internal pyccel files
+    """
+    parser = ArgumentParser(description='Pickle internal pyccel files')
+    args = parser.parse_args()
+
+    folder = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','stdlib','internal'))
+    files = ['blas.pyh', 'dfftpack.pyh', 'fitpack.pyh',
+            'lapack.pyh', 'mpiext.py', 'mpi.pyh', 'openacc.pyh', 'openmp.pyh']
+
+    for f in files:
+        print(f)
+        parser = Parser(os.path.join(folder,f), show_traceback=False)
+        parser.parse(verbose=False)
