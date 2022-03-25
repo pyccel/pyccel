1be021dcc1a16cb9257616b88b12ff6238407f6c
Emily.Bourne <louise.bourne@gmail.com>
2022-03-25 14:20:56 +0100

    egg_info is sufficient for all types of installation

--- setup.py
+++ setup.py
@@ -2,23 +2,9 @@
 #!/usr/bin/env python
 import setuptools
 import sys
-from setuptools.command.dist_info import dist_info
 from setuptools.command.egg_info import egg_info
 
-class PickleHeadersInstall(dist_info):
-    """ Class to pickle headers in time for them to be collected
-    by the MANIFEST.in treatment during the install command
-    """
-    def run(self):
-        # Process .pyh headers and store their AST in .pyccel pickle files.
-        from pyccel.commands.pyccel_init import pyccel_init
-
-        pyccel_init()
-
-        # Execute the classic dist_info command
-        super().run()
-
-class PickleHeadersWheel(egg_info):
+class PickleHeaders(egg_info):
     """ Class to pickle headers in time for them to be collected
     by the MANIFEST.in treatment while building a wheel
     """
@@ -33,6 +19,5 @@ class PickleHeadersWheel(egg_info):
 
 if __name__ == "__main__":
     setuptools.setup(cmdclass={
-        "dist_info": PickleHeadersInstall,
-        "egg_info": PickleHeadersWheel,
+        "egg_info": PickleHeaders,
         })
