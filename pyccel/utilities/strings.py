f3869bb332a908c5791f49898635f19a2f564fa4
EmilyBoune <louise.bourne@gmail.com>
2022-04-08 22:46:09 +0200

    [CODACY] Documentation

--- pyccel/utilities/strings.py
+++ pyccel/utilities/strings.py
@@ -1,4 +1,10 @@
-
+# -*- coding: utf-8 -*-
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+""" Module containing helper functions for managing strings
+"""
 
 def create_incremented_string(forbidden_exprs, prefix = 'Dummy', counter = 1):
     """This function takes a prefix and a counter and uses them to construct
