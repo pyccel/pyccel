9ed2fda218411c72751005c6d8037dda28312982
Emily.Bourne <louise.bourne@gmail.com>
2022-04-15 09:03:32 +0200

    Add type checks

--- pyccel/ast/variable.py
+++ pyccel/ast/variable.py
@@ -526,6 +526,10 @@ class Variable(PyccelAstNode):
 
     @is_temp.setter
     def is_temp(self, is_temp):
+        if not isinstance(is_temp, bool):
+            raise TypeError("is_temp must be a boolean")
+        elif is_temp:
+            raise ValueError("Variables cannot become temporary")
         self._is_temp = is_temp
 
 class DottedName(Basic):
