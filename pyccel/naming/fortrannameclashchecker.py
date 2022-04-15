5af6dd2135e3700ce8bc872f0e8c99a9e73b5cfd
EmilyBoune <louise.bourne@gmail.com>
2022-04-16 01:16:00 +0200

    Correct collisions checks

--- pyccel/naming/fortrannameclashchecker.py
+++ pyccel/naming/fortrannameclashchecker.py
@@ -25,3 +25,21 @@ class FortranNameClashChecker(NameClashChecker):
             'wait', 'codimension', 'concurrent', 'contiguous',
             'critical', 'error', 'submodule', 'sync', 'lock',
             'unlock', 'test'])
+
+    def has_clash(self, name, symbols):
+        name = name.lower()
+        return any(name == k for k in self.keywords) or \
+               any(name == s.lower() for s in symbols)
+
+    def get_collisionless_name(self, name, symbols):
+        """ Get the name that will be used in the fortran code
+        """
+        prefix = name.lower()
+        coll_symbols = self.keywords.copy()
+        coll_symbols.update(s.lower() for s in symbols)
+        if prefix in coll_symbols:
+            counter = 1
+            new_name, counter = create_incremented_string(coll_symbols,
+                    prefix = prefix, counter = counter)
+            name = name+new_name[-5:]
+        return name
--- pyccel/naming/pythonnameclashchecker.py
+++ pyccel/naming/pythonnameclashchecker.py
@@ -1,4 +1,22 @@
 from .nameclashchecker import NameClashChecker
 
 class PythonNameClashChecker(NameClashChecker):
-    pass
+    keywords = set()
+
+    def has_clash(self, name, symbols):
+        return any(name == k for k in self.keywords) or \
+               any(name == s for s in symbols)
+
+    def get_collisionless_name(self, name, symbols):
+        """ Get the name that will be used in the fortran code
+        """
+        prefix = name
+        coll_symbols = self.keywords.copy()
+        coll_symbols.update(s.lower() for s in symbols)
+        if prefix in coll_symbols:
+            counter = 1
+            new_name, counter = create_incremented_string(coll_symbols,
+                    prefix = prefix, counter = counter)
+            name = name+new_name[-5:]
+        return name
+
