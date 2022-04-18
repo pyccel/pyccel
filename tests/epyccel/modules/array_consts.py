9e964aea1f3b3cbcb9d623edfabe4657a5bbf7e1
Emily.Bourne <louise.bourne@gmail.com>
2022-04-18 14:50:25 +0200

    Add test for array in module

---
+++ tests/epyccel/modules/array_consts.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.ones(6)
+b = np.array([1,2,3,4,5])
+c = np.zeros((2,3), dtype=np.int32)
+d = np.array([1+2j, 3+4j])
+e = np.empty((2,3,4))
+
+for i in range(2):
+    for j in range(3):
+        for k in range(4):
+            e[i,j,k] = i*12+j*4+k
