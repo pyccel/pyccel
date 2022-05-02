e0be8b1911d4b6a2863b168e1f220b6fef327249
Emily.Bourne <louise.bourne@gmail.com>
2022-05-02 11:26:05 +0200

    Add more tests

--- tests/epyccel/test_epyccel_round.py
+++ tests/epyccel/test_epyccel_round.py
@@ -9,10 +9,57 @@ def test_round_int(language):
     def round_int(x):
         return round(x)
 
-    f = epyccel(round_int, language=language)
+    f = epyccel(round_int, language=language, developer_mode=True)
     x = randint(100) / 10
 
     f_output = f(x)
     round_int_output = round_int(x)
     assert round_int_output == f_output
-    assert isinstance(round_int_output, type(f_output))
+    assert isinstance(f_output, type(round_int_output))
+
+    # Round down
+    x = 3.345
+
+    f_output = f(x)
+    round_int_output = round_int(x)
+    assert round_int_output == f_output
+    assert isinstance(f_output, type(round_int_output))
+
+    # Round up
+    x = 3.845
+
+    f_output = f(x)
+    round_int_output = round_int(x)
+    assert round_int_output == f_output
+    assert isinstance(f_output, type(round_int_output))
+
+def test_negative_round_int(language):
+    @types('real')
+    def round_int(x):
+        return round(x)
+
+    f = epyccel(round_int, language=language, developer_mode=True)
+    x = -randint(100) / 10
+
+    f_output = f(x)
+    round_int_output = round_int(x)
+    assert round_int_output == f_output
+    assert isinstance(f_output, type(round_int_output))
+
+    # Round up
+    x = -3.345
+
+    f_output = f(x)
+    round_int_output = round_int(x)
+    assert round_int_output == f_output
+    assert isinstance(f_output, type(round_int_output))
+
+    # Round down
+    x = -3.845
+
+    f_output = f(x)
+    round_int_output = round_int(x)
+    assert round_int_output == f_output
+    assert isinstance(f_output, type(round_int_output))
+
+#TODO: Add float tests
