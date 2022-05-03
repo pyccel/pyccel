130f7ca137368cbf5575facac9c4f1c28a22a099
Emily.Bourne <louise.bourne@gmail.com>
2022-05-03 11:06:09 +0200

    Correct test

--- tests/epyccel/test_arrays_multiple_assignments.py
+++ tests/epyccel/test_arrays_multiple_assignments.py
@@ -138,15 +138,16 @@ def test_creation_in_loop_stack(language):
     with pytest.raises(PyccelSemanticError):
         epyccel(f, language=language)
 
-    # Check that we got exactly 1 Pyccel error
+    # Check that we got exactly 2 Pyccel errors
     assert errors.has_errors()
     assert errors.num_messages() == 2
 
-    # Check that the error is correct
-    error_info = [*errors.error_info_map.values()][0][0]
+    # Check that the errors are correct
+    error_info_list = [*errors.error_info_map.values()][0]
+    error_info = error_info_list[0]
     assert error_info.symbol  == 'x'
     assert error_info.message == STACK_ARRAY_NON_LITERAL_SHAPE
-    error_info = [*errors.error_info_map.values()][0][1]
+    error_info = error_info_list[1]
     assert error_info.symbol  == 'x'
     assert error_info.message == STACK_ARRAY_DEFINITION_IN_LOOP
 
