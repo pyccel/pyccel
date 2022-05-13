1d0c7e10225466b2a0892074625310acc5a9fee1
EmilyBoune <louise.bourne@gmail.com>
2022-05-13 08:35:20 +0200

    Add pointer create function

--- pyccel/stdlib/ndarrays/ndarrays.c
+++ pyccel/stdlib/ndarrays/ndarrays.c
@@ -68,6 +68,61 @@ t_ndarray   array_create(int32_t nd, int64_t *shape, enum e_types type)
     return (arr);
 }
 
+t_ndarray   pointer_create(int32_t nd, int64_t *shape, enum e_types type)
+{
+    t_ndarray arr;
+
+    arr.nd = nd;
+    arr.type = type;
+    switch (type)
+    {
+        case nd_int8:
+            arr.type_size = sizeof(int8_t);
+            break;
+        case nd_int16:
+            arr.type_size = sizeof(int16_t);
+            break;
+        case nd_int32:
+            arr.type_size = sizeof(int32_t);
+            break;
+        case nd_int64:
+            arr.type_size = sizeof(int64_t);
+            break;
+        case nd_float:
+            arr.type_size = sizeof(float);
+            break;
+        case nd_double:
+            arr.type_size = sizeof(double);
+            break;
+        case nd_bool:
+            arr.type_size = sizeof(bool);
+            break;
+        case nd_cfloat:
+            arr.type_size = sizeof(float complex);
+            break;
+        case nd_cdouble:
+            arr.type_size = sizeof(double complex);
+            break;
+    }
+    arr.is_view = true;
+    arr.length = 1;
+    arr.shape = malloc(arr.nd * sizeof(int64_t));
+    for (int32_t i = 0; i < arr.nd; i++)
+    {
+        arr.length *= shape[i];
+        arr.shape[i] = shape[i];
+    }
+    arr.buffer_size = arr.length * arr.type_size;
+    arr.strides = malloc(nd * sizeof(int64_t));
+    for (int32_t i = 0; i < arr.nd; i++)
+    {
+        arr.strides[i] = 1;
+        for (int32_t j = i + 1; j < arr.nd; j++)
+            arr.strides[i] *= arr.shape[j];
+    }
+    return (arr);
+}
+
 void    stack_array_init(t_ndarray *arr)
 {
     switch (arr->type)
