ebd3d0becc8f48c8bd6e3dc11ddf234e374decc2
Soukaina Hikma <soukainahikma@gmail.com>
2022-05-26 10:40:48 +0100

    Fixing Modulo when one of the numbers is negative and the modulo is 0

--- pyccel/stdlib/math/pyc_math_c.h
+++ pyccel/stdlib/math/pyc_math_c.h
@@ -20,6 +20,6 @@ int64_t         pyc_factorial(int64_t n);
 int64_t         pyc_gcd (int64_t a, int64_t b);
 int64_t         pyc_lcm (int64_t a, int64_t b);
 double          pyc_radians(double degrees);
-double          pyc_degrees(double radians);
+double          pyc_degrees(double radians); 
 
 #endif
