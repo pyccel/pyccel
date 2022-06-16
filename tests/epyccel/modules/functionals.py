8eb3e85c194eed28c48d0ada4704a893e8316b83
EmilyBoune <louise.bourne@gmail.com>
2022-06-16 09:22:40 +0200

    Add failing tests. Activate tests in c and python

--- tests/epyccel/modules/functionals.py
+++ tests/epyccel/modules/functionals.py
@@ -10,6 +10,11 @@ def functional_for_1d_var(y):
     a = [yi for yi in y]
     return len(a), a[0], a[1], a[2], a[3]
 
+@types('int[:]', 'int')
+def functional_for_1d_const(y,z):
+    a = [z for _ in y]
+    return len(a), a[0], a[1], a[2], a[3]
+
 def functional_for_2d_range():
     a = [i*j for i in range(3) for j in range(2)]
     return len(a), a[0], a[1], a[2], a[3], a[4], a[5]
@@ -40,6 +45,10 @@ def functional_for_2d_dependant_range_3():
 def functional_for_2d_array_range(idx):
     a = [(x1, y1, z1)  for x1 in range(3) for y1 in range(x1,5) for z1 in range(y1,10)]
     return len(a), a[idx][0], a[idx][1], a[idx][2]
+
+def functional_for_2d_range_const():
+    a = [20 for _ in range(3) for _ in range(2)]
+    return len(a), a[0], a[1], a[2], a[3], a[4], a[5]
 
 def functional_for_3d_range():
     a = [i*j for i in range(1,3) for j in range(i,4) for k in range(i,j)]
--- tests/epyccel/test_functionals.py
+++ tests/epyccel/test_functionals.py
@@ -5,39 +5,47 @@ from numpy import equal
 from pyccel.epyccel import epyccel
 from modules import functionals
 
-def compare_epyccel(f, *args):
-    f2 = epyccel(f)
+def compare_epyccel(f, language, *args):
+    f2 = epyccel(f, language=language)
     out1 = f(*args)
     out2 = f2(*args)
     assert equal(out1, out2).all()
 
-def test_functional_for_1d_range():
-    compare_epyccel(functionals.functional_for_1d_range)
+def test_functional_for_1d_range(language):
+    compare_epyccel(functionals.functional_for_1d_range, language)
 
-def test_functional_for_1d_var():
+def test_functional_for_1d_var(language):
     y = randint(99,size = 4)
-    compare_epyccel(functionals.functional_for_1d_var, y)
+    compare_epyccel(functionals.functional_for_1d_var, language, y)
 
-def test_functional_for_2d_range():
-    compare_epyccel(functionals.functional_for_2d_range)
+def test_functional_for_1d_const(language):
+    y = randint(99,size = 4)
+    z = randint(99)
+    compare_epyccel(functionals.functional_for_1d_const, language, y, z)
 
-def test_functional_for_2d_var_range():
+def test_functional_for_2d_range(language):
+    compare_epyccel(functionals.functional_for_2d_range, language)
+
+def test_functional_for_2d_var_range(language):
     y = randint(99, size = 3)
-    compare_epyccel(functionals.functional_for_2d_var_range, y)
+    compare_epyccel(functionals.functional_for_2d_var_range, language, y)
 
-def test_functional_for_2d_var_var():
+def test_functional_for_2d_var_var(language):
     y = randint(99, size = 3)
     z = randint(99, size = 2)
-    compare_epyccel(functionals.functional_for_2d_var_var, y, z)
+    compare_epyccel(functionals.functional_for_2d_var_var, language, y, z)
 
-def test_functional_for_2d_dependant_range():
-    compare_epyccel(functionals.functional_for_2d_dependant_range_1)
-    compare_epyccel(functionals.functional_for_2d_dependant_range_2)
-    compare_epyccel(functionals.functional_for_2d_dependant_range_3)
+def test_functional_for_2d_dependant_range(language):
+    compare_epyccel(functionals.functional_for_2d_dependant_range_1, language)
+    compare_epyccel(functionals.functional_for_2d_dependant_range_2, language)
+    compare_epyccel(functionals.functional_for_2d_dependant_range_3, language)
 
-def test_functional_for_2d_array_range():
+def test_functional_for_2d_array_range(language):
     idx = randint(28)
-    compare_epyccel(functionals.functional_for_2d_array_range,idx)
+    compare_epyccel(functionals.functional_for_2d_array_range, language,idx)
 
-def test_functional_for_3d_range():
-    compare_epyccel(functionals.functional_for_3d_range)
+def test_functional_for_2d_array_range_const(language):
+    compare_epyccel(functionals.functional_for_2d_range_const, language)
+
+def test_functional_for_3d_range(language):
+    compare_epyccel(functionals.functional_for_3d_range, language)
