0bb467d4cda35f28b4c437aef27c3693fef90424
MostafaMamouni <mostafa-1470@live.fr>
2022-06-23 08:07:41 +0100

    update tests

--- tests/epyccel/test_epyccel_return_arrays.py
+++ tests/epyccel/test_epyccel_return_arrays.py
@@ -31,26 +31,24 @@ def test_return_arrays(language):
         x = array([a,b])
         return x
 
-    size = (1, 1)
+    bl = randint(0, 1, dtype= bool)
 
-    bl = randint(0, 1, size=size, dtype= bool)
+    integer8 = randint(min_int8, max_int8, dtype=np.int8)
+    integer16 = randint(min_int16, max_int16, dtype=np.int16)
+    integer = randint(min_int, max_int, dtype=int)
+    integer32 = randint(min_int32, max_int32, dtype=np.int32)
+    integer64 = randint(min_int64, max_int64, dtype=np.int64)
 
-    integer8 = randint(min_int8, max_int8, size=size, dtype=np.int8)
-    integer16 = randint(min_int16, max_int16, size=size, dtype=np.int16)
-    integer = randint(min_int, max_int, size=size, dtype=int)
-    integer32 = randint(min_int32, max_int32, size=size, dtype=np.int32)
-    integer64 = randint(min_int64, max_int64, size=size, dtype=np.int64)
-
-    fl = uniform(min_float / 2, max_float / 2, size = size)
-    fl32 = uniform(min_float32 / 2, max_float32 / 2, size = size)
+    fl = uniform(min_float / 2, max_float / 2)
+    fl32 = uniform(min_float32 / 2, max_float32 / 2)
     fl32 = np.float32(fl32)
-    fl64 = uniform(min_float64 / 2, max_float64 / 2, size = size)
+    fl64 = uniform(min_float64 / 2, max_float64 / 2)
 
-    cmplx128_from_float32 = uniform(low=min_float32 / 2, high=max_float32 / 2, size=size) + uniform(low=min_float32 / 2, high=max_float32 / 2, size=size) * 1j
+    cmplx128_from_float32 = uniform(low=min_float32 / 2, high=max_float32 / 2) + uniform(low=min_float32 / 2, high=max_float32 / 2) * 1j
     # the result of the last operation is a Python complex type which has 8 bytes in the alignment,
     # that's why we need to convert it to a numpy.complex64 the needed type.
     cmplx64 = np.complex64(cmplx128_from_float32)
-    cmplx128 = uniform(low=min_float64 / 2, high=max_float64 / 2, size=size) + uniform(low=min_float64 / 2, high=max_float64 / 2, size=size) * 1j
+    cmplx128 = uniform(low=min_float64 / 2, high=max_float64 / 2) + uniform(low=min_float64 / 2, high=max_float64 / 2) * 1j
 
     epyccel_func = epyccel(return_array, language=language)
 
@@ -64,4 +62,3 @@ def test_return_arrays(language):
     assert np.allclose(epyccel_func(fl32, fl32), return_array(fl32, fl32))
     assert np.allclose(epyccel_func(fl64, fl64), return_array(fl64, fl64))
     assert np.allclose(epyccel_func(cmplx64, cmplx64), return_array(cmplx64, cmplx64))
-    assert np.allclose(epyccel_func(cmplx128, cmplx128), return_array(cmplx128, cmplx128))
