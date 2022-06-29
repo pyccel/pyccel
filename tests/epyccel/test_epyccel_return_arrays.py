0b8d98ea97cc8e274adf84a9826913467e261a25
MostafaMamouni <mostafa-1470@live.fr>
2022-06-29 15:58:59 +0100

    unused import

--- tests/epyccel/test_epyccel_return_arrays.py
+++ tests/epyccel/test_epyccel_return_arrays.py
@@ -7,7 +7,6 @@ import pytest
 from recognised_functions.test_numpy_funcs import (min_int, max_int, min_int8, max_int8,
                                 min_int16, max_int16, min_int32, max_int32, max_int64, min_int64)
 from recognised_functions.test_numpy_funcs import max_float, min_float, max_float32, min_float32,max_float64, min_float64
-from recognised_functions.test_numpy_funcs import matching_types
 from pyccel.epyccel import epyccel
 from pyccel.decorators import types
 
