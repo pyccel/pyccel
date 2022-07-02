f1c1409372b21175c0105e9a3a2647a9aebf582c
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-07-02 19:50:17 +0000

    added Variable, IndexedElement imports

--- pyccel/ast/object_address.py
+++ pyccel/ast/object_address.py
@@ -3,7 +3,13 @@
 # go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
 #------------------------------------------------------------------------------------------#
 
+"""
+Module representing object address.
+"""
+
 from .basic import PyccelAstNode
+
+from pyccel.ast.variable import Variable, IndexedElement
 
 class ObjectAddress(PyccelAstNode):
     """Represents the address of an object.
@@ -11,20 +17,20 @@ class ObjectAddress(PyccelAstNode):
     ObjectAddress(Variable('int','a', memory_handling='alias'))   is   a
     """
 
-    __slots__ = ('_object',)
-    _attribute_nodes = ('_object',)
+    __slots__ = ('_obj',)
+    _attribute_nodes = ('_obj',)
 
-    def __init__(self, object):
-        if not isinstance(object, (Variable, IndexedElement, ObjectAddress)):
+    def __init__(self, obj):
+        if not isinstance(obj, (Variable, IndexedElement, ObjectAddress)):
             raise TypeError("object must be a ...") #TODO: to change
-        self._object = object
+        self._obj = obj
         super().__init__()
 
     @property
-    def object(self):
+    def obj(self):
         """The object whose address is of interest
         """
-        return self._object
+        return self._obj
 
 
 # Notes:
