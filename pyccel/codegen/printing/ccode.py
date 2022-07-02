1325a3447230470a2a60f4e2eee602a42c6e9b40
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-07-02 19:28:18 +0000

    added check for objects stored in c pointers -- incomplete: not added for all relevant functions

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -1169,12 +1169,25 @@ class CCodePrinter(CodePrinter):
 
         return Slice(start, stop, step)
 
+    # def _print_WithArrow(self, expr):
+    #     return '{}->'.format(self._print(expr.object))
+
+    # def _print_WithDot(self, expr):
+    #     return '{}.'.format(self._print(expr.object))
+
     def _print_NumpyArraySize(self, expr):
-        return '{}.length'.format(self._print(expr.arg))
+        arg = expr.arg
+        
+        if self.stored_in_c_pointer(arg):
+            return '{}->length'.format(self._print(ObjectAddress(arg)))
+        return '{}.length'.format(self._print(arg))
 
     def _print_PyccelArraySize(self, expr):
-        return '{}.shape[{}]'.format(self._print(expr.arg),
-            self._print(expr.index))
+        arg    = expr.arg
+
+        if self.stored_in_c_pointer(arg):
+            return '{}->shape[{}]'.format(self._print(ObjectAddress(arg)), self._print(expr.index))
+        return '{}.shape[{}]'.format(self._print(arg), self._print(expr.index))
 
     def _print_Allocate(self, expr):
         free_code = ''
