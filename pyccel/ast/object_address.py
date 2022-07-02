47ce3fe19083c142efbdcedcade062983afc89a8
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-07-02 19:42:31 +0000

    added module docstring, __slots__ from string to tuple

--- pyccel/ast/object_address.py
+++ pyccel/ast/object_address.py
@@ -1,19 +1,22 @@
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+
 from .basic import PyccelAstNode
 
-
 class ObjectAddress(PyccelAstNode):
-
     """Represents the address of an object.
     ObjectAddress(Variable('int','a'))                            is  &a
     ObjectAddress(Variable('int','a', memory_handling='alias'))   is   a
     """
 
-    __slots__ = ('_object')
+    __slots__ = ('_object',)
     _attribute_nodes = ('_object',)
 
     def __init__(self, object):
         if not isinstance(object, (Variable, IndexedElement, ObjectAddress)):
-            raise TypeError("object must be a ...")
+            raise TypeError("object must be a ...") #TODO: to change
         self._object = object
         super().__init__()
 
@@ -24,7 +27,6 @@ class ObjectAddress(PyccelAstNode):
         return self._object
 
 
-""" Notes:
-    self._print(ObjectAddress(Variable('int', 'x'))) where x is a Variable for example.
-    - Need a _print_ObjectAddress in the ccode.py
-"""
+# Notes:
+# self._print(ObjectAddress(Variable('int', 'x'))) where x is a Variable for example.
+# Need a _print_ObjectAddress in the ccode.py
