633df61c1be9d0ae85a98d550d8f5d79b3011e88
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-07-07 21:37:54 +0200

    removed old _print_ObjectAddress

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -1836,11 +1836,6 @@ class CCodePrinter(CodePrinter):
         else:
             raise NotImplementedError("Constant not implemented")
 
-    def _print_ObjectAddress(self, expr):
-        # if self.stored_in_c_pointer(expr.object):
-        #     return '&({})'.format(self._print(expr.object))
-        return '{}'.format(expr.obj)
-
     def _print_Variable(self, expr):
         if self.stored_in_c_pointer(expr):
             return '(*{0})'.format(expr.name)
