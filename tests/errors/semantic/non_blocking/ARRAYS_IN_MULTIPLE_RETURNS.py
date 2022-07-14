1fcebefa989b23016eab83585fc557da41ce643b
MostafaMamouni <mostafa-1470@live.fr>
2022-07-14 11:54:17 +0100

    remove ARRAYS_IN_MULTIPLE_RETURNS.py

--- tests/errors/semantic/non_blocking/ARRAYS_IN_MULTIPLE_RETURNS.py
+++
@@ -1,7 +0,0 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring
-from numpy import array
-
-def multi_returns():
-    x = array([1,2,3,4])
-    z = array([1,2,3,4])
-    return x, z
