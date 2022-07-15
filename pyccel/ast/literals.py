11236cd29ce9e57a4bed0b7e12a02518e26aa9bd
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-07-16 01:57:26 +0200

    removed unnecessary imported datatype

--- pyccel/ast/literals.py
+++ pyccel/ast/literals.py
@@ -8,7 +8,7 @@ from pyccel.utilities.metaclasses import Singleton
 
 from .basic              import PyccelAstNode, Basic
 from .datatypes          import (NativeGeneric, NativeInteger, NativeBool, NativeFloat,
-                                  NativeComplex, NativeString, NativeFloat, NativeVoid)
+                                  NativeComplex, NativeString, NativeFloat)
 
 __all__ = (
     'LiteralTrue',
