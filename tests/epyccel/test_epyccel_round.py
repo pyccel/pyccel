a3d80b9f17f6f01ffdfc75f0e3f9e5cf2cfe9001
EmilyBoune <louise.bourne@gmail.com>
2022-07-16 13:24:54 +0200

    Add tests. Add failing test in C

--- tests/epyccel/test_epyccel_round.py
+++ tests/epyccel/test_epyccel_round.py
@@ -1,11 +1,13 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
+import pytest
+import numpy as np
 from numpy.random import randint
 
 from pyccel.decorators import types
 from pyccel.epyccel import epyccel
 
 def test_round_int(language):
-    @types('real')
+    @types('float')
     def round_int(x):
         return round(x)
 
@@ -42,7 +44,7 @@ def test_round_int(language):
     assert isinstance(f_output, type(round_int_output))
 
 def test_negative_round_int(language):
-    @types('real')
+    @types('float')
     def round_int(x):
         return round(x)
 
@@ -78,4 +80,78 @@ def test_negative_round_int(language):
     assert round_int_output == f_output
     assert isinstance(f_output, type(round_int_output))
 
-#TODO: Add float tests
+def test_round_ndigits(language):
+    @types('float','int')
+    def round_ndigits(x, i):
+        return round(x,i)
+
+    f = epyccel(round_ndigits, language=language, developer_mode=True)
+    x = randint(100) / 10
+
+    f_output = f(x, 1)
+    round_ndigits_output = round_ndigits(x, 1)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
+
+    x = 3.343
+
+    f_output = f(x,2)
+    round_ndigits_output = round_ndigits(x,2)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
+
+    x = 3323.0
+
+    f_output = f(x,-2)
+    round_ndigits_output = round_ndigits(x, -2)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
+
+    x = -3390.0
+
+    f_output = f(x,-2)
+    round_ndigits_output = round_ndigits(x, -2)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
+
+@pytest.mark.parametrize( 'language', (
+    pytest.param('fortran', marks = pytest.mark.fortran),
+    pytest.param('c',       marks = [
+        pytest.mark.xfail(reason="Python implements bankers' round. But only for ndigits=0"),
+        pytest.mark.c]),
+    pytest.param('python', marks = pytest.mark.python),
+    )
+)
+def test_round_ndigits_half(language):
+    @types('float','int')
+    def round_ndigits(x, i):
+        return round(x,i)
+
+    f = epyccel(round_ndigits, language=language, developer_mode=True)
+    x = randint(100) / 10
+
+    f_output = f(x, 1)
+    round_ndigits_output = round_ndigits(x, 1)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
+
+    x = 3.345
+
+    f_output = f(x,2)
+    round_ndigits_output = round_ndigits(x,2)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
+
+    x = -3350.0
+
+    f_output = f(x,-2)
+    round_ndigits_output = round_ndigits(x, -2)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
+
+    x = 45.0
+
+    f_output = f(x,-1)
+    round_ndigits_output = round_ndigits(x,-1)
+    assert np.isclose(round_ndigits_output, f_output)
+    assert isinstance(f_output, type(round_ndigits_output))
