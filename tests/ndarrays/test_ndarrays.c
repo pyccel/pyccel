958d6a964b5a75078a4409ee98e4c87b49efd2eb
ohachim <othmanehachim@gmail.com>
2022-08-18 13:33:01 +0100

    adds indexing tests for order_f

--- tests/ndarrays/test_ndarrays.c
+++ tests/ndarrays/test_ndarrays.c
@@ -178,6 +178,35 @@ int32_t test_indexing_int64(void)
     return (0);
 }
 
+int32_t test_indexing_int64_order_f(void)
+{
+    int64_t m_1[] = {1, 6, 6, 2, 4, 0, 1, 5,
+                    7, 1, 3, 5, 3, 6, 4, 9,
+                    7, 6, 7, 1, 7, 7, 4, 4,
+                    7, 1, 9, 0, 1, 2, 4, 5,
+                    6, 1, 7, 3, 1, 5, 9, 6};
+    int64_t m_1_shape[] = {5, 8};
+    t_ndarray x;
+    int32_t index;
+    int32_t c_index;
+    int64_t value;
+    int64_t c_value;
+
+    x = array_create(2, m_1_shape, nd_int64, false, order_f);
+    memcpy(x.raw_data, m_1, x.buffer_size);
+    // testing the index [3, 2]
+    index = 3 * x.strides[0] + 2 * x.strides[1];
+    c_index = 13;
+    my_assert(index , c_index, "testing the strides");
+    my_assert(get_index(x, 3, 2) , c_index, "testing the indexing function");
+    // testing the value with the index [3, 2]
+    value = x.nd_int64[index];
+    c_value = 6;
+    my_assert(value , c_value, "testing the value");
+    free_array(x);
+    return (0);
+}
+
 int32_t test_indexing_int32(void)
 {
     int32_t m_1[] = {2, 3, 5, 5, 6, 7, 10, 11,
@@ -207,6 +236,36 @@ int32_t test_indexing_int32(void)
     return (0);
 }
 
+int32_t test_indexing_int32_order_f(void)
+{
+    int32_t m_1[] = {1, 6, 6, 2, 4, 0, 1, 5,
+                    7, 1, 3, 5, 3, 6, 4, 9,
+                    7, 6, 7, 1, 7, 7, 4, 4,
+                    7, 1, 9, 0, 1, 2, 4, 5,
+                    6, 1, 7, 3, 1, 5, 9, 6};
+    int64_t m_1_shape[] = {5, 8};
+    t_ndarray x;
+    int32_t index;
+    int32_t c_index;
+    int32_t value;
+    int32_t c_value;
+
+    x = array_create(2, m_1_shape, nd_int32, false, order_f);
+    memcpy(x.raw_data, m_1, x.buffer_size);
+    // testing the index [3, 2]
+    index = 3 * x.strides[0] + 2 * x.strides[1];
+    c_index = 13;
+    my_assert(index , c_index, "testing the strides");
+    my_assert(get_index(x, 3, 2) , c_index, "testing the indexing function");
+    // testing the value with the index [3, 2]
+    value = x.nd_int32[index];
+    c_value = 6;
+    my_assert(value , c_value, "testing the value");
+    free_array(x);
+    return (0);
+}
+
+
 int32_t test_indexing_int16(void)
 {
     int16_t m_1[] = {2, 3, 5, 5, 6, 7, 10, 11,
@@ -231,6 +290,35 @@ int32_t test_indexing_int16(void)
     // testing the value with the index [3, 2]
     value = x.nd_int16[index];
     c_value = 103;
+    my_assert(value , c_value, "testing the value");
+    free_array(x);
+    return (0);
+}
+
+int32_t test_indexing_int16_order_f(void)
+{
+    int16_t m_1[] = {1, 6, 6, 2, 4, 0, 1, 5,
+                    7, 1, 3, 5, 3, 6, 4, 9,
+                    7, 6, 7, 1, 7, 7, 4, 4,
+                    7, 1, 9, 0, 1, 2, 4, 5,
+                    6, 1, 7, 3, 1, 5, 9, 6};
+    int64_t m_1_shape[] = {5, 8};
+    t_ndarray x;
+    int32_t index;
+    int32_t c_index;
+    int16_t value;
+    int16_t c_value;
+
+    x = array_create(2, m_1_shape, nd_int16, false, order_f);
+    memcpy(x.raw_data, m_1, x.buffer_size);
+    // testing the index [3, 2]
+    index = 3 * x.strides[0] + 2 * x.strides[1];
+    c_index = 13;
+    my_assert(index , c_index, "testing the strides");
+    my_assert(get_index(x, 3, 2) , c_index, "testing the indexing function");
+    // testing the value with the index [3, 2]
+    value = x.nd_int16[index];
+    c_value = 6;
     my_assert(value , c_value, "testing the value");
     free_array(x);
     return (0);
@@ -265,6 +353,35 @@ int32_t test_indexing_int8(void)
     return (0);
 }
 
+int32_t test_indexing_int8_order_f(void)
+{
+    int8_t m_1[] = {1, 6, 6, 2, 4, 0, 1, 5,
+                    7, 1, 3, 5, 3, 6, 4, 9,
+                    7, 6, 7, 1, 7, 7, 4, 4,
+                    7, 1, 9, 0, 1, 2, 4, 5,
+                    6, 1, 7, 3, 1, 5, 9, 6};
+    int64_t m_1_shape[] = {5, 8};
+    t_ndarray x;
+    int32_t index;
+    int32_t c_index;
+    int8_t value;
+    int8_t c_value;
+
+    x = array_create(2, m_1_shape, nd_int8, false, order_f);
+    memcpy(x.raw_data, m_1, x.buffer_size);
+    // testing the index [3, 2]
+    index = 3 * x.strides[0] + 2 * x.strides[1];
+    c_index = 13;
+    my_assert(index , c_index, "testing the strides");
+    my_assert(get_index(x, 3, 2) , c_index, "testing the indexing function");
+    // testing the value with the index [3, 2]
+    value = x.nd_int8[index];
+    c_value = 6;
+    my_assert(value , c_value, "testing the value");
+    free_array(x);
+    return (0);
+}
+
 int32_t test_indexing_double(void)
 {
     double m_1[] = {2, 3, 5, 5, 6, 7, 10, 11,
@@ -294,6 +411,40 @@ int32_t test_indexing_double(void)
     return (0);
 }
 
+int32_t test_indexing_double_order_f(void)
+{
+    double m_1[] = {0.05751476201087158, 0.13745300160998386, 0.2079973756146688, 0.2141008701292113,
+                        0.2841626347062822, 0.4239597742653679, 0.6522693509949357, 0.9230925248474112,
+                    0.8525960970856643, 0.8138267834551158, 0.5210862950661376, 0.5971376449046766,
+                        0.21489965384896903, 0.39310326602036905, 0.8762355339594128, 0.6760453456313423,
+                    0.9424121921037278, 0.6475903689621096, 0.4663033100019591, 0.9317847307880955,
+                        0.6932017388898739, 0.2543468648524083, 0.6020488047717303, 0.5951783988465483,
+                    0.45982787529445157, 0.6810379211895127, 0.6125792953364687, 0.7803671441186834,
+                        0.24092775530531474, 0.3129576055158103, 0.6417260363979195, 0.029619310490432538,
+                    0.04414839864241349, 0.9515999281039909, 0.2694450127647845, 0.35691412053397653,
+                        0.790349738990523, 0.23743626350675473, 0.5085166254064067, 0.06735188687983484};
+    int64_t m_1_shape[] = {5, 8};
+    t_ndarray x;
+    int32_t index;
+    int32_t c_index;
+    double value;
+    double c_value;
+
+    x = array_create(2, m_1_shape, nd_double, false, order_f);
+    memcpy(x.raw_data, m_1, x.buffer_size);
+    // testing the index [3, 2]
+    index = 3 * x.strides[0] + 2 * x.strides[1];
+    c_index = 13;
+    my_assert(index , c_index, "testing the strides");
+    my_assert(get_index(x, 3, 2) , c_index, "testing the indexing function");
+    // testing the value with the index [3, 2]
+    value = x.nd_double[index];
+    c_value = 0.39310326602036905;
+    my_assert(value , c_value, "testing the value");
+    free_array(x);
+    return (0);
+}
+
 int32_t test_indexing_cdouble(void)
 {
     double complex m_1[] = {0.37 + 0.588*I,  0.92689451+0.57106791*I,
@@ -318,6 +469,35 @@ int32_t test_indexing_cdouble(void)
     // testing the value with the index [3, 1]
     value = x.nd_cdouble[index];
     c_value = 0.58532094+0.67890618*I;
+    my_assert(value , c_value, "testing the value");
+    free_array(x);
+    return (0);
+}
+
+int32_t test_indexing_cdouble_order_f(void)
+{
+    double complex m_1[] = {(0.8399107+0.39774981*I), (0.11668451+0.44048746*I),
+                            (0.82147723+0.65709434*I), (0.19273834+0.24206348*I),
+                            (0.88065081+0.34479325*I), (0.17994627+0.03374548*I),
+                            (0.22043986+0.16516544*I), (0.03286256+0.1403558*I),
+                            (0.02005023+0.59950758*I), (0.96582983+0.89097352*I)};
+    int64_t m_1_shape[] = {5, 2};
+    t_ndarray x;
+    int32_t index;
+    int32_t c_index;
+    double complex value;
+    double complex c_value;
+
+    x = array_create(2, m_1_shape, nd_cdouble, false, order_f);
+    memcpy(x.raw_data, m_1, x.buffer_size);
+    // testing the index [3, 1]
+    index = 3 * x.strides[0] + 1 * x.strides[1];
+    c_index = 8;
+    my_assert(index , c_index, "testing the strides");
+    my_assert(get_index(x, 3, 1) , c_index, "testing the indexing function");
+    // testing the value with the index [3, 1]
+    value = x.nd_cdouble[index];
+    c_value = 0.02005023+0.59950758*I;
     my_assert(value , c_value, "testing the value");
     free_array(x);
     return (0);
@@ -820,29 +1000,57 @@ int32_t test_array_zeros_cdouble(void)
 int32_t main(void)
 {
     /* indexing tests */
-    test_indexing_double();
-    test_indexing_int64();
-    test_indexing_int32();
-    test_indexing_int16();
-    test_indexing_int8();
-    test_indexing_cdouble();
-    /* slicing tests */
-    test_slicing_double();
-    test_slicing_int64();
-    test_slicing_int32();
-    test_slicing_int16();
-    test_slicing_int8();
-    test_slicing_cdouble();
-    /* array_fill tests */
-    test_array_fill_int64();
-    test_array_fill_int32();
-    test_array_fill_int16();
-    test_array_fill_int8();
-    test_array_fill_double();
-    test_array_fill_cdouble();
-    /* array_zeros tests */
-    test_array_zeros_int32();
-    test_array_zeros_double();
-    test_array_zeros_cdouble();
+    // test_indexing_double();
+    // test_indexing_int64();
+    // test_indexing_int32();
+    // test_indexing_int16();
+    // test_indexing_int8();
+    // test_indexing_cdouble();
+    // /* slicing tests */
+    // test_slicing_double();
+    // test_slicing_int64();
+    // test_slicing_int32();
+    // test_slicing_int16();
+    // test_slicing_int8();
+    // test_slicing_cdouble();
+    // /* array_fill tests */
+    // test_array_fill_int64();
+    // test_array_fill_int32();
+    // test_array_fill_int16();
+    // test_array_fill_int8();
+    // test_array_fill_double();
+    // test_array_fill_cdouble();
+    // /* array_zeros tests */
+    // test_array_zeros_int32();
+    // test_array_zeros_double();
+    // test_array_zeros_cdouble();
+
+    /*************ORDER F**********************/
+
+    /* indexing tests */
+    test_indexing_int64_order_f();
+    test_indexing_int32_order_f();
+    test_indexing_int16_order_f();
+    test_indexing_int8_order_f();
+    test_indexing_double_order_f();
+    test_indexing_cdouble_order_f();
+    // /* slicing tests */
+    // test_slicing_double_order_f();
+    // test_slicing_int64_order_f();
+    // test_slicing_int32_order_f();
+    // test_slicing_int16_order_f();
+    // test_slicing_int8_order_f();
+    // test_slicing_cdouble_order_f();
+    // /* array_fill tests */
+    // test_array_fill_int64_order_f();
+    // test_array_fill_int32_order_f();
+    // test_array_fill_int16_order_f();
+    // test_array_fill_int8_order_f();
+    // test_array_fill_double_order_f();
+    // test_array_fill_cdouble_order_f();
+    // /* array_zeros tests */
+    // test_array_zeros_int32_order_f();
+    // test_array_zeros_double_order_f();
+    // test_array_zeros_cdouble_order_f();
     return (0);
 }
