0d7df14298f46950eb46942b0c85a5c3a1da43fc
Anas Zouiten <azouiten@e1r5p12.1337.ma>
2022-09-22 14:14:42 +0100

    fixed append seg fault and changed handling of nested lists

--- pyccel/stdlib/lists/lists.c
+++ pyccel/stdlib/lists/lists.c
@@ -7,26 +7,26 @@ t_list   *allocate_list(size_t size, t_type type, void *elemnts) // va_arg could
 
     if (!(list = (t_list*)malloc(sizeof(t_list))))
         return NULL;
-    if (size == 0) // maybe a useless check
-        list->elements = NULL;
-    else if (!(list->elements = malloc(type * size)))
-        return NULL;
-    memcpy(list->elements, elemnts, type * size);
-    list->type = type;
     list->size = size * type;
-list->capacity = DEFAULT_CAP;
-    while (list->capacity <= size)
+    list->capacity = DEFAULT_CAP;
+    while (list->capacity <= list->size)
         list->capacity *= 2;
+    if (list->size == 0) // maybe a useless check
+        list->elements = NULL;
+    else if (!(list->elements = malloc(list->capacity * type)))
+        return NULL;
+    memcpy(list->elements, elemnts, type * list->size);
+    list->type = type;
     return list;
 }
 
-int     free_list(t_list **list)
+void     free_list(t_list **list)
 {
     free((*list)->elements);
     free(*list);
     *list = NULL;
 }
-#include <stdio.h>
+
 void    append(t_list** list1, t_list* list2)
 {
     size_t totalSize = (*list1)->size + list2->size;
@@ -36,26 +36,22 @@ void    append(t_list** list1, t_list* list2)
         t_list *tmp = *list1;
         elements = malloc(totalSize);
         memcpy(elements, (*list1)->elements, (*list1)->size);
-        printf("realloc[%d][%d][%d]{%d}\n", (*list1)->size, list2->size, totalSize, elements[(*list1)->size]);
-        memcpy(elements[(*list1)->size], list2->elements, list2->size);
-        *list1 = allocate_list(totalSize, (*list1)->type, elements);
+        memcpy(&elements[(*list1)->size - 1], list2->elements, list2->size);
+        free((*list1)->elements);
+        *list1 = allocate_list(totalSize/(*list1)->type, (*list1)->type, elements);
         free(tmp);
         free(elements);
     }
     else
     {
-        printf("no realloc\n");
         elements = (char*)(*list1)->elements;
-        memcpy(elements[(*list1)->size], list2->elements, list2->size);
+        memcpy(&elements[(*list1)->size], list2->elements, list2->size);
     }
 }
 
 void     clear(t_list* list)
 {
-    // free(list->elements);
-    // list->elements = NULL;
     list->size = 0;
-    // list->capacity = DEFAULT_CAP;
 }
 
 t_list*  copy(t_list* list)
@@ -67,11 +63,19 @@ size_t   count(t_list* list, void *item)
 {
     size_t index = 0;
     int count = 0;
-    char *elements = (char*)(list->elements);
+    char *elements = list->elements;
+    printf("omok\n");
     while (index < list->size)
     {
-        if (!(memcmp(item, elements[index], list->type)))
-            count += 1;
+        printf("and round we go\n");
+        if (list->type == lst_list)
+        {
+            t_list *tmp = (t_list*)&(elements[index]);
+            if (!(memcmp(item, tmp, sizeof(t_list))))
+                count += 1;
+        }
+        else if (!(memcmp(item, &(elements[index]), list->type)))
+                count += 1;
         index += list->type;
     }
     return (count);
@@ -85,8 +89,14 @@ int   lst_index(t_list* list, void* item)
     char *elements = (char*)(list->elements);
     while (index < list->size)
     {
-        if (!(memcmp(item, elements[index], list->type)))
-            return index/list->type;
+        if (list->type == lst_list)
+        {
+            t_list *tmp = (t_list*)(&elements[index]);
+            if (!(memcmp(item, tmp, sizeof(t_list))))
+                return index/list->type;
+        }
+        else if (!(memcmp(item, &elements[index], list->type)))
+                return index/list->type;
         index += list->type;
     }
     return (-1);
@@ -101,14 +111,14 @@ void     insert(t_list** list, size_t index, void* object)
         t_list *tmp = *list;
         elements = malloc((*list)->type * totalSize);
         memcpy(elements, (*list)->elements, (*list)->size);
-        memcpy(elements[(*list)->size], object, 1);
+        memcpy(&elements[(*list)->size], object, 1);
         *list = allocate_list(totalSize, (*list)->type, elements);
         free(tmp);
     }
     else
     {
         elements = (char*)(*list)->elements;
-        memcpy(elements[(*list)->size], object, 1);
+        memcpy(&elements[(*list)->size], object, 1);
     }
 }
 
@@ -119,7 +129,7 @@ void    pop(t_list* list, size_t index)
         char *elements = (char*)(list->elements);
         while ((index + (2 * list->type)) < list->size)
         {
-            memcpy(elements[index], elements[index + list->type], list->type);
+            memcpy(&elements[index], &elements[index + list->type], list->type);
             index += list->type;
         }
         list->size--;
@@ -140,29 +150,21 @@ void     reverse(t_list* list)
     size_t index = 0;
     while (index + list->type <= list->size/2)
     {
-        memcpy(buffer, elements[index], list->type);
-        memcpy(elements[index], elements[list->size - index - list->type], list->type);
-        memcpy(elements[list->size - index - list->type], buffer, list->type);
+        memcpy(buffer, &elements[index], list->type);
+        memcpy(&elements[index], &elements[list->size - index - list->type], list->type);
+        memcpy(&elements[list->size - index - list->type], buffer, list->type);
         index += list->type;
     }
     free(buffer);
 }
 
-// void     sort(t_list* list, cmp_function)
-// {
-//     int indx = 0, indy = 0;
-
-//     // while 
-// }
-
-// int      default_cmp_func(void* item1, void* item1, t_type type);
 
 void*   array_subscripting(t_list *list, size_t index)
 {
     if (list->size/list->type > index)
     {
         char *elements = (char*)(list->elements);
-        return (elements[index * list->type]);
+        return ((void*)(&elements[index * list->type]));
     }
     return (NULL);
 }
--- pyccel/stdlib/lists/lists.h
+++ pyccel/stdlib/lists/lists.h
@@ -1,20 +1,16 @@
+
+#ifndef _LISTS_
+#define _LISTS_
 #define DEFAULT_CAP 10
 
 #include <stddef.h>
 #include <string.h>
 #include <stdlib.h>
 
-typedef enum e_type t_type;
+// typedef enum e_type t_type;
 
-typedef struct  s_list
-{
-    void*       elements;
-    t_type      type;
-    size_t      capacity;
-    size_t      size;
-}               t_list;
 
-enum    e_type
+typedef enum    e_type
 {
     lst_bool   = 1  ,
     lst_int8   = 1  ,
@@ -23,26 +19,34 @@ enum    e_type
     lst_int64  = 8  ,
     lst_float  = 4  ,
     lst_double = 8  ,
-    lst_list   = sizeof(t_list)
-};
+    lst_list   = sizeof(int*)
+}       t_type;
 
+typedef struct  s_list
+{
+    void*       elements;
+    enum e_type      type;
+    size_t      capacity;
+    size_t      size;
+}               t_list;
 
 typedef  int(*cmp_function)(void*, void*, t_type);
 
-
 t_list   *allocate_list(size_t size, t_type type, void *elemnts);
-int     free_list(t_list **list);
+void     free_list(t_list **list);
 void    append(t_list** list1, t_list* list2);
 void     clear(t_list* list);
 t_list*  copy(t_list* list);
 size_t   count(t_list* list, void *item);
-void     extend(t_list* list, void* object); // redundant
+void     extend(t_list* list, void* object);
 int   lst_index(t_list* list, void* value);
 void     insert(t_list** list, size_t index, void* object);
 void     pop(t_list* list, size_t index);
 void     lst_remove(t_list* list, void* value);
 void     reverse(t_list* list);
 void     sort(t_list* list, cmp_function);
-int      default_cmp_func(void* item1, void* item2, t_type type); // hmm
+int      default_cmp_func(void* item1, void* item2, t_type type);
 
 void*   array_subscripting(t_list *list, size_t index);
+
+#endif
