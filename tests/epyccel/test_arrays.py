try assert_allclose instead of assert_array_almost_equal to fix Window's precision error

tests/epyccel/test_arrays.py
@@ -3837,7 +3837,7 @@ def test_numpy_arange_two_arg(language):
 def test_numpy_arange_full_arg(language):
     f1 = arrays.arr_arange_3
     f2 = epyccel(f1, language = language)
-    np.testing.assert_array_almost_equal(f1(), f2(), decimal=9)
+    np.testing.assert_allclose(f1(), f2(),  rtol=1e-9, atol=0)
 
 def test_numpy_arange_with_dtype(language):
     f1 = arrays.arr_arange_4
@@ -3847,12 +3847,12 @@ def test_numpy_arange_with_dtype(language):
 def test_numpy_arange_negative_step(language):
     f1 = arrays.arr_arange_5
     f2 = epyccel(f1, language = language)
-    np.testing.assert_array_almost_equal(f1(), f2(), decimal = 9)
+    np.testing.assert_allclose(f1(), f2(),  rtol=1e-9, atol=0)
 
 def test_numpy_arange_negative_step_2(language):
     f1 = arrays.arr_arange_6
     f2 = epyccel(f1, language = language)
-    np.testing.assert_array_almost_equal(f1(), f2(), decimal = 9)
+    np.testing.assert_allclose(f1(), f2(),  rtol=1e-9, atol=0)
 
 def test_iterate_slice(language):
     f1 = arrays.iterate_slice