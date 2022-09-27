448072fb46b738738d8c6ddaa00db66e231d99bc
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-09-27 11:56:29 +0100

    -

--- pyccel/stdlib/cwrapper_ndarrays/cwrapper_ndarrays.c
+++ pyccel/stdlib/cwrapper_ndarrays/cwrapper_ndarrays.c
@@ -321,10 +321,7 @@ PyObject* ndarray_to_pyarray(t_ndarray *o)
 PyObject* c_ndarray_to_pyarray(t_ndarray *o)
 {
     int FLAGS = NPY_ARRAY_C_CONTIGUOUS | NPY_ARRAY_WRITEABLE;
-
-    enum NPY_TYPES npy_type = get_numpy_type(o);
-
-    return PyArray_NewFromDescr(&PyArray_Type, PyArray_DescrFromType(npy_type),
+    return PyArray_NewFromDescr(&PyArray_Type, PyArray_DescrFromType(o->type),
             o->nd, _ndarray_to_numpy_shape(o->shape, o->nd),
             _ndarray_to_numpy_strides(o->strides, o->type_size, o->nd),
             o->raw_data, FLAGS, NULL);
