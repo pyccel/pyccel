500b251e7a3795b956070529a94a7ead8e478ce3
Youness FARINI <farini.work@gmail.com>
2022-09-30 09:46:11 +0000

    fixing codacy problems

--- tests/pyccel/scripts/exits/negative_exit1.py
+++ tests/pyccel/scripts/exits/negative_exit1.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
 
-from sys import exit
+from sys import exit as sys_exit
 
 if __name__ == "__main__":
-    exit(-1)
+    sys_exit(status=-1)
