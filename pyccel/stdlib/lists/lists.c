33654c55ab26cc296fcd6e35e21c3d7a69b6ab3a
azouiten <anas.zouiten1997@gmail.com>
2022-10-10 21:22:40 +0100

    refactored list functions

--- pyccel/stdlib/lists/lists.c
+++ pyccel/stdlib/lists/lists.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 
-int tSizes[8] = {
+size_t tSizes[8] = {
             1,
             sizeof(int8_t), 
             sizeof(int16_t), 
@@ -14,20 +14,19 @@ int tSizes[8] = {
 t_list   *allocate_list(size_t size, t_type type, void *elemnts) // va_arg could alow us to take in multiple list of elements
 {
     t_list *list;
+    size_t tsize = tSizes[type];
 
     if (!(list = (t_list*)malloc(sizeof(t_list))))
         return NULL;
-    list->size = size * tSizes[type];
+    list->size = size;
     list->type = type;
     list->capacity = DEFAULT_CAP;
     while (list->capacity <= list->size)
         list->capacity *= 2;
-    if (list->size == 0)
-        list->elements = NULL;
-    else if (!(list->elements = malloc(list->capacity * tSizes[type])))
+    if (!(list->elements = malloc(list->capacity * tsize)))
         return NULL;
     if (elemnts)
-        memcpy(list->elements, elemnts, tSizes[type] * list->size);
+        memcpy(list->elements, elemnts, tsize * list->size);
     return list;
 }
 
@@ -38,27 +37,21 @@ void     free_list(t_list **list)
     *list = NULL;
 }
 
-void    append(t_list** list1, t_list* list2)
+void    extend(t_list* list1, t_list* list2)
 {
-    size_t totalSize = (*list1)->size + list2->size;
-    char *elements = NULL;
-    if (totalSize >= (*list1)->capacity)
+    size_t totalSize = list1->size + list2->size;
+    size_t tsize = tSizes[list1->type];
+    char *elements = list1->elements;
+
+    if (totalSize >= list1->capacity)
     {
-        t_list *tmp = *list1;
-        elements = malloc(totalSize);
-        memcpy(elements, (*list1)->elements, (*list1)->size);
-        memcpy(&elements[(*list1)->size], list2->elements, list2->size);
-        free((*list1)->elements);
-        *list1 = allocate_list(totalSize/tSizes[(*list1)->type], (*list1)->type, elements);
-        free(tmp);
-        free(elements);
+        while (list1->capacity <= totalSize)
+            list1->capacity *= 2;
+        elements = realloc(elements, list1->capacity * tsize);
+        list1->elements = elements;
     }
-    else
-    {
-        elements = (char*)(*list1)->elements;
-        memcpy(&elements[(*list1)->size], list2->elements, list2->size);
-        (*list1)->size += tSizes[(*list1)->type];
-    }
+    memcpy(&elements[list1->size], list2->elements, list2->size * tsize);
+    list1->size = totalSize;
 }
 
 void     clear(t_list* list)
@@ -68,7 +61,7 @@ void     clear(t_list* list)
 
 t_list*  copy(t_list* list)
 {
-    return (allocate_list(list->size / tSizes[list->type], list->type, list->elements));
+    return (allocate_list(list->size, list->type, list->elements));
 }
 
 size_t   count(t_list* list, void *item)
@@ -80,35 +73,28 @@ size_t   count(t_list* list, void *item)
     {
         if (list->type == lst_list)
         {
-            t_list *tmp = (t_list*)&(elements[index]);
+            t_list *tmp = ((t_list**)elements)[index];
             if (!(memcmp(item, tmp, sizeof(t_list))))
                 count += 1;
         }
-        else if (!(memcmp(item, &(elements[index]), tSizes[list->type])))
+        else if (!(memcmp(item, &(elements[index * tSizes[list->type]]), tSizes[list->type])))
                 count += 1;
-        index += tSizes[list->type];
+        index += 1;
     }
     return (count);
 }
 
-void     extend(t_list** list, void* item)
+void     append(t_list* list, void* item) // this could just be an insert call
 {
-    size_t totalSize = (*list)->size + tSizes[(*list)->type];
-    char *elements = NULL;
-    if (totalSize > (*list)->capacity)
-    {
-        elements = malloc(totalSize);
-        memcpy(elements, (*list)->elements, (*list)->size);
-        memcpy(&elements[(*list)->size], item, 1);
-        *list = allocate_list(totalSize, (*list)->type, elements);
-        free(elements);
-    }
-    else
-    {
-        elements = (char*)(*list)->elements;
-        memcpy(&elements[(*list)->size], item, 1);
-    }
+    t_list listTmp;
+    listTmp.capacity = DEFAULT_CAP;
+    listTmp.elements = item;
+    listTmp.size = 1;
+    listTmp.type = list->type;
+    extend(list, &listTmp);
 }
+
+
 
 int   lst_index(t_list* list, void* item)
 {
@@ -118,79 +104,109 @@ int   lst_index(t_list* list, void* item)
     {
         if (list->type == lst_list)
         {
-            t_list *tmp = (t_list*)(&elements[index]);
-            if (!(memcmp(item, tmp, sizeof(t_list))))
-                return index/tSizes[list->type];
+            t_list *tmp = ((t_list**)elements)[index];
+            t_list *itmTmp = (t_list*)item;
+            // NEEDS TO COMPARE LITERALS (group node items)
+            if (tmp->size == itmTmp->size || 
+                !(memcmp(itmTmp->elements, tmp->elements, tmp->size * tSizes[tmp->type]))) 
+                return index;
         }
-        else if (!(memcmp(item, &elements[index], tSizes[list->type])))
-                return index/tSizes[list->type];
-        index += tSizes[list->type];
+        else if (!(memcmp(item, &elements[index * tSizes[list->type]], tSizes[list->type])))
+                return index;
+        index += 1;
     }
     return (-1);
 }
 
-void     insert(t_list** list, size_t index, void* item) // needs a redo
+size_t calculate_index(long int index, size_t size)
 {
-    size_t totalSize = (*list)->size + tSizes[(*list)->type];
-    index *= tSizes[(*list)->type];
-    if (index >= (*list)->size)
-        index = (*list)->size;
-    char *elements = NULL;
-    if (totalSize > (*list)->capacity)
+    if (index >= size)
+        index = size;
+    else if (index < 0)
     {
-        elements = malloc(totalSize);
-        memcpy(elements, &(*list)->elements, index);
-        memcpy(&elements[index], item, tSizes[(*list)->type]);
-        memcpy(&elements[index + tSizes[(*list)->type]], (int8_t*)(*list)->elements + index * (*list)->type, (*list)->size - index);
-        *list = allocate_list(totalSize / (*list)->type, (*list)->type, elements);
+        index += size + 1;
+        index = (index > 0) ? index : 0;
     }
-    else
-    {
-        elements = (*list)->elements;
-        size_t size = totalSize;
-        while (size > index)
-        {
-            memcpy(&elements[size], &elements[size - tSizes[(*list)->type]], (*list)->type);
-            size -= tSizes[(*list)->type];
-        }
-        memcpy(&elements[index], item, tSizes[(*list)->type]);
-    }
+    return index;
 }
 
-void    pop(t_list* list, size_t index)
+void     insert(t_list* list, long int index, void* item)
 {
-    index *= tSizes[list->type];
-    if (list->size > index)
-    {
-        char *elements = (char*)(list->elements);
+    char * elements = list->elements;
+    size_t totalSize = list->size + 1;
+    size_t tsize = tsize;
+    size_t ind = calculate_index(index, list->size) * tsize;
 
-        while ((index + tSizes[list->type]) <= list->size)
+    if (totalSize >= list->capacity)
+    {
+        list->capacity *= 2;
+        elements = realloc(elements, list->capacity * tsize);
+        list->elements = elements;
+    }
+    memmove(&(elements[ind + tsize]), &(elements[ind]), (list->size * tsize) - ind);
+    memcpy(&(elements[ind]), item, tsize);
+    list->size = totalSize; 
+}
+
+void    *pop(t_list* list, long int index)
+{
+
+    size_t tsize = tSizes[list->type];
+    char *elements = (char*)(list->elements);
+    t_pop_ret *ret_val = malloc(sizeof(t_pop_ret));
+
+    index = (index < 0) ? list->size + index : index;
+    if (index >= 0 && list->size > index)
+    {
+        ret_val->type = list->type;
+        if (list->type == lst_list)
+            ret_val->raw = ((t_list**)elements)[index];
+        else
         {
-            memcpy(&elements[index], &elements[index + tSizes[list->type]], tSizes[list->type]);
-            index += tSizes[list->type];
+            ret_val->raw = malloc(list->type);
+            memcpy(ret_val->raw, elements[index * tsize], tsize);
         }
-        list->size -= tSizes[list->type];
+        memmove(&elements[index * tsize], &elements[(index + 1) * tsize], list->size - index);
+        list->size -= 1;
+        return (ret_val);
+    }
+
+    return (NULL);
+}
+
+void    free_pop(t_pop_ret** pop_val)
+{
+    if (pop_val && (*pop_val))
+    {
+        free((*pop_val)->raw);
+        free(*pop_val);
+        *pop_val = NULL;
     }
 }
 
 void     lst_remove(t_list* list, void* item)
 {
     int index;
+    t_pop_ret *ret;
+
     if ((index = lst_index(list, item)) != -1)
-        pop(list, (size_t)index);
+        ret = pop(list, (size_t)index);
+    free_pop(ret);
 }
 
 void     reverse(t_list* list)
 {
-    char *elements = (char*)(list->elements);
-    void *buffer = malloc(tSizes[list->type]);
+    size_t tsize = tSizes[list->type];
     size_t index = 0;
-    while (index + tSizes[list->type] <= list->size/2)
+    char *elements = (char*)(list->elements);
+    void *buffer = malloc(tsize);
+
+    while (index + 1 <= list->size / 2)
     {
-        memcpy(buffer, &elements[index], tSizes[list->type]);
-        memcpy(&elements[index], &elements[list->size - index - tSizes[list->type]], tSizes[list->type]);
-        memcpy(&elements[list->size - index - tSizes[list->type]], buffer, tSizes[list->type]);
-        index += tSizes[list->type];
+        memcpy(buffer, &elements[index * tsize], tsize);
+        memcpy(&elements[index * tsize], &elements[(list->size - index - 1) * tsize], tsize);
+        memcpy(&elements[(list->size - index - 1) * tsize], buffer, tsize);
+        index++;
     }
     free(buffer);
 }
@@ -198,10 +214,9 @@ void     reverse(t_list* list)
 
 void*   array_subscripting(t_list *list, size_t index)
 {
-    if (list->size/tSizes[list->type] > index)
-    {
-        char *elements = (char*)(list->elements);
+    char *elements = (char*)(list->elements);
+
+    if (list->size > index)
         return ((void*)(&elements[index * tSizes[list->type]]));
-    }
     return (NULL);
 }
--- pyccel/stdlib/lists/lists.h
+++ pyccel/stdlib/lists/lists.h
@@ -29,23 +29,29 @@ typedef struct  s_list
     size_t      size;
 }               t_list;
 
+typedef struct s_pop_ret
+{
+    char *raw;
+    t_type type;
+}               t_pop_ret;
+
 typedef  int(*cmp_function)(void*, void*, t_type);
 
 t_list  *allocate_list(size_t size, t_type type, void *elemnts);
 void    free_list(t_list **list);
-void    append(t_list** list1, t_list* list2);
+void    free_pop(t_pop_ret** pop_val);
+size_t  calculate_index(long int index, size_t size);
+void    extend(t_list* list1, t_list* list2);
 void    clear(t_list* list);
 t_list* copy(t_list* list);
 size_t  count(t_list* list, void *item);
-void    extend(t_list** list, void* item);
+void    append(t_list* list, void* item);
 int     lst_index(t_list* list, void* item);
-void    insert(t_list** list, size_t index, void* item);
-void    pop(t_list* list, size_t index);
+void    insert(t_list* list, long int index, void* item);
+void    *pop(t_list* list, long int index);
 void    lst_remove(t_list* list, void* value);
 void    reverse(t_list* list);
-// void    sort(t_list* list, cmp_function);
 int     default_cmp_func(void* item1, void* item2, t_type type);
-
 void*   array_subscripting(t_list *list, size_t index);
 
 #endif
