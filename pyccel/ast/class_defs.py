d19b151b8c5b72a651b5f708fd7d7fc29b6afed5
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2022-10-14 20:30:29 +0100

    added append and pop FunctionDefs to ListClass

--- pyccel/ast/class_defs.py
+++ pyccel/ast/class_defs.py
@@ -5,6 +5,7 @@
 """
 This module contains all types which define a python class which is automatically recognised by pyccel
 """
+from pyccel.ast.internals import PyccelSymbol
 from .builtins  import PythonImag, PythonReal, PythonConjugate
 from .core      import ClassDef, FunctionDef
 from .datatypes import (NativeBool, NativeInteger, NativeFloat,
@@ -134,6 +135,8 @@ TupleClass = ClassDef('tuple',
 
 ListClass = ClassDef('list',
         methods=[
+            FunctionDef('append', [PyccelSymbol('arg1'), PyccelSymbol('arg2')], [], body=[]),
+            FunctionDef('pop', [PyccelSymbol('arg1'), PyccelSymbol('arg2')], [], body=[]),
             #index
             #count
             #and many more
