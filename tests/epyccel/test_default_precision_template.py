ad70478ab1a77935929089fdb332868321ce823c
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2022-10-20 05:25:29 +0100

    Update test_default_precision_template

--- tests/epyccel/test_default_precision_template.py
+++ tests/epyccel/test_default_precision_template.py
@@ -9,12 +9,14 @@ from pyccel.epyccel import epyccel
 
 
 def test_default_precision_template(language):
-    test_types = [np.int64 , np.float128]#', 'complex']
+
     @types('int[:]')
     @types('float[:]')
+    @types('complex[:]')
     def return_array_element(array):
         return array[0]
 
+    test_types = [np.int64 , np.float64 , np.complex128, 'int', 'float', 'complex']
     f1 = return_array_element
     f2 = epyccel(f1, language=language)
     for t in test_types:
@@ -23,4 +25,5 @@ def test_default_precision_template(language):
         python_result = f1(arr)
         pyccel_result = f2(arr)
 
+        assert type(pyccel_result) == type(python_result)
         assert isclose(pyccel_result, python_result)
