5444842adf8fdd0d3ea98a0db58b96ad3e6fe953
Emily.Bourne <louise.bourne@gmail.com>
2022-10-27 13:03:27 +0200

    Output debugging info

--- pyccel/codegen/compiling/compilers.py
+++ pyccel/codegen/compiling/compilers.py
@@ -145,7 +145,11 @@ class Compiler:
         prop.update(dict.fromkeys(self._info.get(key,())))
 
         for a in accelerators:
+            print (a)
+            print (self._info.get(a,{}))
             prop.update(dict.fromkeys(self._info.get(a,{}).get(key,())))
+
+        print(prop)
 
         return prop.keys()
 
@@ -189,6 +193,7 @@ class Compiler:
         accelerators : iterable or str
                        Accelerators used by the code
         """
+        print("_get_libdirs ", accelerators)
         return self._get_property('libdirs', libdirs, accelerators)
 
     def _get_dependencies(self, dependencies = (), accelerators = ()):
