4ec6b7fcc625cb297c82bddc513b1a107f1bcba2
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2022-10-28 14:03:40 +0100

    Comment calls to func_2d func_3d

--- tests/internal/scripts/ccuda/cuda_grid.py
+++ tests/internal/scripts/ccuda/cuda_grid.py
@@ -25,6 +25,8 @@ if __name__ == '__main__':
     arr = cuda.array([0, 1, 2, 3, 4])
     cuda.deviceSynchronize()
     func_1d[n_blocks, threads_per_block](arr)
-    func_2d[n_blocks, threads_per_block](arr)
-    func_3d[n_blocks, threads_per_block](arr)
+    # Since we dont support multi-dim n_block / threads_per_block
+    # func_2d and func_3d won't compile
+    # func_2d[n_blocks, threads_per_block](arr)
+    # func_3d[n_blocks, threads_per_block](arr)
     cuda.deviceSynchronize()
