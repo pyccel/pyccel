c4cc5a89679b4224afa1129b7606bb30c31df5fb
azouiten <azouiten.mail@gmail.com>
2022-11-18 17:06:20 +0100

    removed setup.py

--- pyccel/stdlib/cwrapper_lists/setup.py
+++
@@ -1,9 +0,0 @@
-
-from distutils.core import setup, Extension
- 
-module = Extension('listModule', sources = ['list_wrapper.c','wrapper_testing.c', '../lists/lists.c'])
- 
-setup (name = 'listModule',
-        version = '',
-        description = '',
-        ext_modules = [module])
