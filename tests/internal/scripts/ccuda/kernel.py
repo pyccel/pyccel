41fa42408d1ddd036b3d1455156ddb34e3422ce4
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2023-01-01 16:29:19 +0100

    Remove disable=django-not-configure

--- tests/internal/scripts/ccuda/kernel.py
+++ tests/internal/scripts/ccuda/kernel.py
@@ -1,5 +1,4 @@
 # pylint: disable=missing-function-docstring, disable=unused-variable, missing-module-docstring
-# pylint: disable=django-not-configure
 
 from pyccel.decorators import kernel, types
 from pyccel import cuda
