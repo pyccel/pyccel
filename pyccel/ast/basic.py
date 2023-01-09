b06dcdada8af4b7111d575ecd304419d360f80f6
jalalium <jalalium@hotmail.fr>
2023-01-09 16:09:36 +0100

    diagnosis

--- pyccel/ast/basic.py
+++ pyccel/ast/basic.py
@@ -75,10 +75,6 @@ class Basic:
         return c is None or isinstance(c, cls._ignored_types)
 
     def invalidate_node(self):
-        """ Indicate that this node is temporary.
-        This will allow it to remove itself from its attributes' users.
-        If an attribute subsequently has no users, invalidate_node is called recursively
-        """
         for c_name in self._my_attribute_nodes:
             c = getattr(self, c_name)
 
