a8c5db4779b6f2030eb1da75ce1191a1ee3df333
Aymane Benaissa <47903494+Pinkyboi@users.noreply.github.com>
2023-01-11 01:26:06 +0100

    Clean up cupy ravel tests

--- tests/internal/scripts/ccuda/cupy_ravel.py
+++ tests/internal/scripts/ccuda/cupy_ravel.py
@@ -4,17 +4,8 @@ import cupy as cp
 from pyccel.decorators import kernel, types
 from pyccel import cuda
 
-@kernel
-@types('int[:]', 'int[:]')
-def func(a, b):
-    i = cuda.threadIdx(0) + cuda.blockIdx(0) * cuda.blockDim(0)
-    print("Hello World! ", a[i], b[i])
-
 if __name__ == '__main__':
     threads_per_block = 32
     n_blocks = 1
     arr1 = cp.ravel([[1,2],[1,3]])
     arr2 = cp.ravel([1,2,3,4])
-    cuda.synchronize()
-    func[n_blocks, threads_per_block](arr1, arr2)
-    cuda.synchronize()
