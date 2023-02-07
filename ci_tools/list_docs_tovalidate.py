26768909b8258c615535d9a97c53164050af1bcb
jalalium <jalalium@hotmail.fr>
2023-02-07 16:55:34 +0100

    format comment

--- ci_tools/list_docs_tovalidate.py
+++ ci_tools/list_docs_tovalidate.py
@@ -27,25 +27,21 @@ objects = []
 for file, line_nos in changes.items():
     with open(file,'r', encoding="utf-8") as f:
         tree = ast.parse(f.read())
-    '''
-    The prefix variable stores the absolute dotted prefix of all
-    objects in the file.
-    for example: the objects in the file pyccel/ast/core.py will
-    have the prefix pyccel.ast.core
-    '''
+    # The prefix variable stores the absolute dotted prefix of all
+    # objects in the file.
+    # for example: the objects in the file pyccel/ast/core.py will
+    # have the prefix pyccel.ast.core
     prefix = '.'.join(PurePath(file).with_suffix('').parts)
 
     for node in ast.walk(tree):
-        '''
-        This loop walks the ast and explores all objects
-        present in the file.
-        If the object is an instance of a FunctionDef or
-        a ClassDef, a check is performed to see if any of
-        the updated lines are present within the object.
-        Additionally, The name of all objects present
-        inside a function or a class is updated to include
-        the name of the parent object
-        '''
+        # This loop walks the ast and explores all objects
+        # present in the file.
+        # If the object is an instance of a FunctionDef or
+        # a ClassDef, a check is performed to see if any of
+        # the updated lines are present within the object.
+        # Additionally, The name of all objects present
+        # inside a function or a class is updated to include
+        # the name of the parent object
         if isinstance(node, (FunctionDef, ClassDef)):
             if any((node.lineno <= x <= node.end_lineno
                     for x in line_nos)) and not node.name.startswith('inner_function'):
