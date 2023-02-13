5c12862fa998f7215cd334529f76f6b66754bfe7
EmilyBourne <louise.bourne@gmail.com>
2023-02-13 17:26:43 +0100

    Add comments. Wrap function results. Warn about wrapping function addresses.

--- pyccel/codegen/wrapper/fortran_to_c_wrapper.py
+++ pyccel/codegen/wrapper/fortran_to_c_wrapper.py
@@ -5,8 +5,11 @@
 #------------------------------------------------------------------------------------------#
 from pyccel.ast.bind_c import BindCFunctionDefArgument, BindCFunctionDefResult
 from pyccel.ast.bind_c import BindCPointer, BindCFunctionDef, C_F_Pointer
+from pyccel.ast.bind_c import CLocFunc
 from pyccel.ast.core import Module, Assign, FunctionCall, FunctionDefArgument
-from pyccel.ast.core import If, IfSection
+from pyccel.ast.core import Allocate, EmptyNode
+from pyccel.ast.core import If, IfSection, Import
+from pyccel.ast.datatypes import NativeInteger
 from pyccel.ast.internals import Slice
 from pyccel.ast.literals import LiteralInteger, Nil, LiteralTrue
 from pyccel.ast.operators import PyccelIsNot
@@ -15,13 +18,15 @@ from pyccel.parser.scope import Scope
 from .wrapper import Wrapper
 
 class FortranToCWrapper(Wrapper):
+    def __init__(self):
+        self._additional_exprs = []
 
     def _get_function_def_body(self, func, func_def_args, func_arg_to_call_arg, results, handled = ()):
         """ Get the body of the function definition by inserting if blocks
         to check the presence of optional variables
 
-        Arguments
-        ---------
+        Parameters
+        ----------
         func_def_args : list of FunctionDefArguments
                 The arguments received by the function
         func_arg_to_call_arg : dict
@@ -29,6 +34,11 @@ class FortranToCWrapper(Wrapper):
                 to be passed to the function call
         results : list of Variables
                 The Variables where the result of the function call will be saved
+
+        Results
+        -------
+        body : list
+                A list of Basic nodes describing the body of the function
         """
         optional = next((a for a in func_def_args if a.var.is_optional and a not in handled), None)
         if optional:
@@ -44,9 +54,33 @@ class FortranToCWrapper(Wrapper):
             args = [func_arg_to_call_arg[fa] for fa in func_def_args]
             body = [C_F_Pointer(fa.var, func_arg_to_call_arg[fa], fa.sizes)
                     for fa in func_def_args if isinstance(func_arg_to_call_arg[fa], IndexedElement)]
-            return body + [Assign(results, FunctionCall(func, args))]
+            func_call = Assign(results[0], FunctionCall(func, args)) if len(results) == 1 else \
+                        Assign(results, FunctionCall(func, args))
+            return body + [func_call]
 
     def _get_call_argument(self, original_arg, bind_c_arg):
+        """ Get the argument which should be passed to the function call
+
+        The FunctionDefArgument passed to the function may contain additional
+        information which should not be passed to the function being wrapped
+        (e.g. an array with strides should not pass the strides explicitly to
+        the function call, and nor should it pass the entire contiguous array).
+        This function extracts the necessary information and returns the object
+        which can be passed to the function call
+
+        Parameters
+        ----------
+        original_arg : FunctionDefArgument
+                       The argument to the function being wrapped
+        bind_c_arg : BindCFunctionDefArgument
+                        The argument to the wrapped bind_c_X function
+
+        Results
+        -------
+        var : PyccelAstNode
+                An object which can be passed to a function call of the function
+                being wrapped
+        """
         if original_arg.is_ndarray:
             new_var = original_arg.clone(self.scope.get_new_name(original_arg.name), is_argument = False, is_optional = False)
             self.scope.insert_variable(new_var)
@@ -70,12 +104,14 @@ class FortranToCWrapper(Wrapper):
         variable_getters = [self._wrap(f) for v in expr.variables if not v.is_private]
         init_func = self._wrap(expr.init_func) if expr.init_func else None
         free_func = self._wrap(expr.free_func) if expr.free_func else None
+        imports = [Import(expr.name, target = expr, mod=expr)]
 
-        name = mod_scope.get_new_name(f'bind_c_{expr.name}')
+        name = mod_scope.get_new_name(f'bind_c_{expr.name.target}')
 
         return Module(name, (), funcs + variable_getters,
                 init_func = init_func, free_func = free_func, 
                 interfaces = interfaces, classes = classes,
+                imports = imports,
                 scope = mod_scope)
 
     def _wrap_FunctionDef(self, expr):
@@ -83,15 +119,25 @@ class FortranToCWrapper(Wrapper):
         func_scope = self.scope.new_child_scope(name)
         self.set_scope(func_scope)
 
-        func_arguments = [self._wrap_FunctionDefArgument(a) for a in expr.arguments]
+        self._additional_exprs = []
+
+        if any(isinstance(a, FunctionAddress) for a in expr.arguments):
+            warnings.warn("Functions with functions as arguments cannot be wrapped by pyccel")
+            return EmptyNode()
+
+        func_arguments = [self._wrap(a) for a in expr.arguments]
         original_arguments = [a.var for a in expr.arguments]
         call_arguments = [self._get_call_argument(oa, fa) for oa, fa in zip(original_arguments, func_arguments)]
         func_to_call = {fa : ca for ca, fa in zip(call_arguments, func_arguments)}
 
-        results = [r.clone(self.scope.get_new_name(r.name)) for r in expr.results]
-        func_results = [BindCFunctionDefResult(r, func_scope) for r in expr.results]
+        func_results = [self._wrap_FunctionDefResult(r) for r in expr.results]
 
-        body = self._get_function_def_body(expr, func_arguments, func_to_call, results)
+        func_call_results = [self.scope.get_expected_name(r.name) for r in expr.results]
+
+        body = self._get_function_def_body(expr, func_arguments, func_to_call, func_call_results)
+
+        body.extend(self._additional_exprs)
+        self._additional_exprs.clear()
 
         return BindCFunctionDef(name, func_arguments, func_results, body, scope=func_scope, original_function = expr)
 
@@ -106,3 +152,37 @@ class FortranToCWrapper(Wrapper):
             new_var = var.clone(self.scope.get_new_name(expr.name))
             return FunctionDefArgument(new_var, value = expr.value,
                     kwonly = expr.is_kwonly, annotation = expr.annotation)
+
+    def _wrap_FunctionDefResult(self, expr):
+        name = expr.name
+        scope = self.scope
+        local_var = expr.clone(scope.get_new_name(name))
+
+        if local_var.rank:
+            # Allocatable is not returned so it must appear in local scope
+            self.scope.insert_variable(local_var)
+
+            # Save the shapes of the array
+            sizes   = [Variable(dtype=NativeInteger(),
+                                name=scope.get_new_name(f'{name}_shape_{i+1}'))
+                       for i in range(expr.rank)]
+            self._additional_exprs.extend([Assign(sizes[i], expr.shape[i]) for i in range(expr.rank)])
+            bind_var = Variable(dtype=BindCPointer(),
+                                name=scope.get_new_name('bound_'+name))
+            self.scope.insert_variable(bind_var)
+
+            # Create a C-compatible array variable
+            ptr_var = expr.clone(scope.get_new_name(name+'_ptr'),
+                                memory_handling='alias')
+            self.scope.insert_variable(ptr_var)
+
+            # Define the additional steps necessary to define and fill ptr_var
+            alloc = Allocate(ptr_var, shape=expr.shape,
+                             order=expr.order, status='unallocated')
+            copy = Assign(ptr_var, expr)
+            c_loc = CLocFunc(ptr_var, bind_var)
+            self._additional_exprs.extend([alloc, copy, c_loc])
+
+            return BindCFunctionDefResult(bind_var, sizes)
+        else:
+            return BindCFunctionDefResult(local_var)
