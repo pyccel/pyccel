47b808f2ae6b03d649be44f2b44fee8f1e26c095
Fatima-zahra RAMDANI <fatimazahra.ramdani@usmba.ac.ma>
2023-03-17 00:04:54 +0100

    Fix codacy issues

--- tests/cuda_test/test_print_deallocate.py
+++ tests/cuda_test/test_print_deallocate.py
@@ -1,6 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring/
+# pylint: disable=protected-access
 import pytest
-import numpy as np
 from pyccel.ast.variable import Variable
 from pyccel.codegen.printing.ccudacode import CcudaCodePrinter
 from pyccel.ast.core      import Deallocate
@@ -10,7 +10,6 @@ from pyccel.ast.core      import Deallocate
     ]
 )
 def test_print_deallocate(language):
-    
     # Test case 1: variable is in host memory
     var = Variable('int', 'y', memory_location='host')
     expr = Deallocate(var)
@@ -18,7 +17,6 @@ def test_print_deallocate(language):
     output = printer._print_Deallocate(expr)
     expected_output = "cuda_free_host(y);\n"
     assert output == expected_output
-
     # Test case 2: variable is in device memory
     var = Variable('int', 'y', memory_location='device')
     expr = Deallocate(var)
@@ -26,7 +24,6 @@ def test_print_deallocate(language):
     output = printer._print_Deallocate(expr)
     expected_output = "cuda_free(y);\n"
     assert output == expected_output
-
     #Test case 3: Memory location of variable is managed
     var = Variable('int', 'y', memory_location='managed')
     expr = Deallocate(var)
@@ -34,7 +31,6 @@ def test_print_deallocate(language):
     output = printer._print_Deallocate(expr)
     expected_output = "cuda_free(y);\n"
     assert output == expected_output
-
     #Test case 4: variable is an alias
     var = Variable('int', 'y', memory_handling='alias')
     expr = Deallocate(var)
