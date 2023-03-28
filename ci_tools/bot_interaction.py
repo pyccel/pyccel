1063258f4a49595fea470f071875a0a1e8fbc0aa
EmilyBourne <louise.bourne@gmail.com>
2023-03-28 14:39:30 +0200

    Improve bot

--- ci_tools/bot_interaction.py
+++ ci_tools/bot_interaction.py
@@ -7,6 +7,9 @@ from git_evaluation_tools import leave_comment, get_status_json
 #senior_reviewer = ['yguclu', 'ebourne']
 senior_reviewer = ['ebourne']
 
+test_keys = ['linux', 'windows', 'macosx', 'coverage', 'docs', 'pylint',
+             'lint', 'spelling']
+
 comment_folder = os.path.join(os.path.dirname(__file__), 'bot_messages')
 
 def run_tests(pr_id, event, outputs):
@@ -62,7 +65,7 @@ def mark_as_ready(pr_id, event, outputs):
     """
     pass
 
-def print_commands(pr_id, event, outputs):
+def print_commands(pr_id):
     """
     List the available bot commands.
 
@@ -78,7 +81,6 @@ def print_commands(pr_id, event, outputs):
     bot_commands = ("This bot reacts to all comments which begin with `/bot`. This phrase can be followed by any of these commands:\n"
             "- `run tests` : Triggers the tests for a draft pull request\n"
             "- `mark as ready` : Adds the appropriate review flag and requests reviews. This command should be used when the PR is first ready for review, or when a review has been answered.\n"
-            "- `trust user` : When written by a senior developer this allows the bot to run tests for a new contributor\n"
             "- `commands` : Shows this list detailing all the commands which are understood")
 
     leave_comment(pr_id, bot_commands)
@@ -88,7 +90,6 @@ def welcome(pr_id, event):
 
 bot_triggers = {'run tests' : run_tests,
                 'mark as ready': mark_as_ready,
-                'trust user': lambda pr_id, new_user: None,
                 'commands' : print_commands}
 
 
@@ -104,31 +105,6 @@ if __name__ == '__main__':
     with open(args.gitEvent, encoding="utf-8") as event_file:
         event = json.load(event_file)
 
-    if 'comment' in event and 'pull_request' in event['issue'] and event['comment']['body'].startswith('/bot'):
-        pr_id = event['issue']['number']
-        event_name = 'comment'
-    elif 'pull_request' in event:
-        pr_id = event['number']
-        event_name = 'pull_request'
-    else:
-        pr_id = None
-        event_name = None
-
-    if event['action'] == 'opened':
-        new_user = event['pull_request']['author_association'] in ('COLLABORATOR', 'FIRST_TIME_CONTRIBUTOR')
-        if new_user:
-            with open(os.path.join(comment_folder, 'welcome_newcomer.txt')) as msg_file:
-                comment = msg_file.read()
-        else:
-            with open(os.path.join(comment_folder, 'welcome_friend.txt')) as msg_file:
-                comment = msg_file.read()
-        with open(os.path.join(comment_folder, 'checklist.txt')) as msg_file:
-            comment += msg_file.read()
-
-        leave_comment(pr_id, comment)
-        sys.exit()
-
-
     outputs = {'run_linux': False,
                'run_windows': False,
                'run_macosx': False,
@@ -140,17 +116,52 @@ if __name__ == '__main__':
                'HEAD': '',
                'REF': ''}
 
+    if 'comment' in event and 'pull_request' in event['issue'] and event['comment']['body'].startswith('/bot'):
+        pr_id = event['issue']['number']
+
+        comment = event['comment']['body']
+        command = comment.split('/bot')[1].strip()
+        command_words = command.split()
+
+        if command_words[0] == 'run':
+            if command_words[1] == 'tests':
+                for k in test_keys:
+                    outputs[k] = True
+            else:
+                for k in command_words[1:]:
+                    outputs[k] = True
+        elif command == 'mark as ready':
+            pass
+        else:
+            print_commands(pr_id)
+
+    elif 'pull_request' in event and not event['pull_request']['draft']:
+        pr_id = event['number']
+        event_name = 'pull_request'
+
+        for k in test_keys:
+            outputs[k] = True
+
+    elif event['action'] == 'opened':
+        new_user = event['pull_request']['author_association'] in ('COLLABORATOR', 'FIRST_TIME_CONTRIBUTOR')
+        if new_user:
+            with open(os.path.join(comment_folder, 'welcome_newcomer.txt')) as msg_file:
+                comment = msg_file.read()
+        else:
+            with open(os.path.join(comment_folder, 'welcome_friend.txt')) as msg_file:
+                comment = msg_file.read()
+        with open(os.path.join(comment_folder, 'checklist.txt')) as msg_file:
+            comment += msg_file.read()
+
+        leave_comment(pr_id, comment)
+
     if pr_id is not None:
-        status = get_status_json(pr_id, 'headRefOid,baseRefName,isDraft,comments,reviews,potentialMergeCommit')
-        ref = status['headRefOid']
+        status = get_status_json(pr_id, 'baseRefName,potentialMergeCommit')
         merge_commit = status['potentialMergeCommit']
         outputs['HEAD'] = status['baseRefName']
         outputs['REF'] = f'+{merge_commit}:refs/remotes/pull/{pr_id}/merge'
 
         if event_name == 'comment':
-            command = args.command.split('/bot')[1].strip()
-
-            bot_triggers.get(command, print_commands)(args.pr_number, event, outputs)
 
     print(event)
 
