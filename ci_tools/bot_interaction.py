f4dc52b6eeaba82e2b4dbb7ffe6536bd7b3d5e23
EmilyBourne <louise.bourne@gmail.com>
2023-03-29 19:07:01 +0200

    Missing import

--- ci_tools/bot_interaction.py
+++ ci_tools/bot_interaction.py
@@ -4,6 +4,7 @@ import os
 import sys
 from git_evaluation_tools import leave_comment, get_status_json, github_cli, get_job_information
 from git_evaluation_tools import check_previous_comments, set_ready, set_draft, get_review_status
+from git_evaluation_tools import check_previous_contributions
 
 #senior_reviewer = ['yguclu', 'EmilyBourne']
 senior_reviewer = ['EmilyBourne']
