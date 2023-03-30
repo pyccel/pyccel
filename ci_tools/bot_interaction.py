875e876627f82c0130e4f4ffd0b005fdfa51e366
EmilyBourne <louise.bourne@gmail.com>
2023-03-30 13:54:04 +0200

    Fix args

--- ci_tools/bot_interaction.py
+++ ci_tools/bot_interaction.py
@@ -175,7 +175,7 @@ def mark_as_ready(pr_id):
     pr_id : int
         The number of the PR.
     """
-    job_data = get_status_json('statusCheckRollup')
+    job_data = get_status_json(pr_id, 'statusCheckRollup')
 
     success = [j['name'] for j in job_data if j['conclusion'] == 'SUCCESS']
     failures = [j['name'] for j in job_data if j['conclusion'] in ('FAILURE', 'ACTION_REQUIRED')]
