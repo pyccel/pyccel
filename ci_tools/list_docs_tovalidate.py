46073a78432ae7b8c46d208057a555ca01680a15
EmilyBourne <louise.bourne@gmail.com>
2023-04-28 13:00:32 +0200

    Remove debug print

--- ci_tools/list_docs_tovalidate.py
+++ ci_tools/list_docs_tovalidate.py
@@ -91,4 +91,3 @@ if __name__ == '__main__':
         with open(args.output, 'w', encoding="utf-8") as f:
             for obj in objects:
                 print(obj, file=f)
-                print(obj)
