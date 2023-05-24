e7576ddd2d8aed5998c2efbe1800005546a955c8
mkaddani linux <ledevloppeurmohammedkaddani@gmail.com>
2023-05-24 11:40:58 +0100

    Codacy error

--- tests/epyccel/test_arrays_multiple_assignments.py
+++ tests/epyccel/test_arrays_multiple_assignments.py
@@ -305,7 +305,7 @@ def test_Assign_between_nested_If(lang):
 @pytest.mark.skipif(sys.platform == 'win32', reason="Compilation problem. On execution Windows raises: error while loading shared libraries: libblas.dll: cannot open shared object file: No such file or directory" )
 def test_conda_flag_disable(language):
     def one():
-        return True;
+        return True
     with pytest.warns(None) as record1:
         epyccel(one, language='c', is_conda_warnings_disabled = True)
     assert len(record1) == 0 # Equals 0 on every platform
