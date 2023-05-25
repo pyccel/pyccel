a89e5d900545bfe193ff880f119a21ea2ab3884f
mkaddani linux <ledevloppeurmohammedkaddani@gmail.com>
2023-05-25 14:56:25 +0100

    fixing bad identation

--- pyccel/codegen/pipeline.py
+++ pyccel/codegen/pipeline.py
@@ -139,7 +139,7 @@ def execute_pyccel(fname, *,
         if verbose:
             print("Header file recognised, stopping after syntactic stage")
     if fname == "test.py":
-            raise ValueError("files called test can cause problems for some compilers and can't be imported from Python. see #issue: 1403")
+        raise ValueError("files called test can cause problems for some compilers and can't be imported from Python. see #issue: 1403")
     # Reset Errors singleton before parsing a new file
     errors = Errors()
     errors.reset()
