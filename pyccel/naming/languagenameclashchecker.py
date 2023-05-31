1fce244b35b12999a3836337a2f601ca3cb2b280
EmilyBourne <louise.bourne@gmail.com>
2023-05-31 18:22:59 +0200

    Correct condition

--- pyccel/naming/languagenameclashchecker.py
+++ pyccel/naming/languagenameclashchecker.py
@@ -40,7 +40,7 @@ class LanguageNameClashChecker(metaclass = Singleton):
         """
         coll_symbols = self.keywords.copy()
         coll_symbols.update(s.lower() for s in symbols)
-        if name in coll_symbols:
+        if self.has_clash(name, coll_symbols):
             counter = 1
             name, counter = create_incremented_string(coll_symbols,
                     prefix = name, counter = counter, name_clash_checker = self)
