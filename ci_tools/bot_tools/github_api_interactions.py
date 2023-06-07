9f2063f44e25f290dff4e50503e372e2eba97587
EmilyBourne <louise.bourne@gmail.com>
2023-06-07 11:47:37 +0200

    Correct syntax

--- ci_tools/bot_tools/github_api_interactions.py
+++ ci_tools/bot_tools/github_api_interactions.py
@@ -50,7 +50,7 @@ class GitHubAPIInteractions:
     def create_review(self, pr_id, commit, comment, comments = ()):
         status = 'APPROVE' if len(comments)==0 else 'REQUEST_CHANGES'
         url = f"https://api.github.com/repos/{self._org}/{self._repo}/pulls/{pr_id}/reviews"
-        review = {'commit_id':commit, 'body' = comment, 'event': status, 'comments': comments}
+        review = {'commit_id':commit, 'body': comment, 'event': status, 'comments': comments}
         return self._post_request("POST", url, json=review)
 
     def check_for_user_in_team(self, user, team):
