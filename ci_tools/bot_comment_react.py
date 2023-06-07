f8ada1a0e6c2fcb7a99b6fcec02b03f342c32b83
EmilyBourne <louise.bourne@gmail.com>
2023-06-07 11:15:32 +0200

    Missing files

---
+++ ci_tools/bot_comment_react.py
@@ -0,0 +1,61 @@
+import argparse
+import json
+import os
+
+pr_test_keys = ['linux', 'windows', 'macosx', 'coverage', 'docs', 'pylint',
+                'lint', 'spelling']
+
+def get_unique_test_list(keys):
+    tests = set(command_words[1:])
+    t = tests.pop('pr_tests')
+    if t:
+        tests.update(pr_test_keys)
+    if 'coverage' in tests:
+        tests.add('linux')
+
+# Parse event payload from $GITHUB_EVENT_PATH variable
+# (documented here : https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables)
+# The contents of this json file depend on the triggering event and are
+# described here :  https://docs.github.com/en/webhooks-and-events/webhooks/webhook-events-and-payloads
+with open(os.environ["GITHUB_EVENT_PATH"], encoding="utf-8") as event_file:
+    event = json.load(event_file)
+
+# If bot called explicitly (comment event)
+
+# Collect id from an issue_comment event with a created action
+pr_id = event['issue']['number']
+
+comment = event['comment']['body']
+command = comment.split('/bot')[1].strip()
+command_words = command.split()
+
+bot = Bot(os.environ["GITHUB_REPOSITORY"], pr_id)
+
+if command_words[:2] == ['show', 'tests']:
+    bot.show_tests()
+
+elif command_words[0] == 'run':
+    if bot.is_user_trusted(event['comment']['user']['login']):
+        bot.run_tests(get_unique_test_list(command_words[1:]))
+    else:
+        bot.warn_untrusted()
+
+elif command_words[0] == 'try':
+    if bot.is_user_trusted(event['comment']['user']['login']):
+        bot.run_tests(get_unique_test_list(command_words[1:]), command_words[1])
+    else:
+        bot.warn_untrusted()
+
+elif command_words[:3] == ['mark', 'as', 'ready']:
+    if bot.is_user_trusted(event['comment']['user']['login']):
+        bot.mark_as_ready()
+        bot.run_tests(pr_test_keys)
+    else:
+        bot.warn_untrusted()
+
+elif command_words[:2] == ['trust', 'user'] and len(command_words)==3 and event['comment']['user']['login'] in Bot.trust_givers:
+
+    bot.indicate_trust(command_words[2])
+
+else:
+    bot.show_commands()
