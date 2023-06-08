1e48e3e73c400ff638e189262fe58bad49b96f7c
EmilyBourne <louise.bourne@gmail.com>
2023-06-08 13:10:12 +0200

    Correct message

--- ci_tools/bot_tools/bot_funcs.py
+++ ci_tools/bot_tools/bot_funcs.py
@@ -68,7 +68,7 @@ class Bot:
 
     def run_tests(self, tests, python_version = None):
         if any(t not in default_python_versions for t in tests):
-            self._GAI.create_comment(self._pr_id, "There are unrecognised tests.\n"+message_from_file('bot_commands.txt'))
+            self._GAI.create_comment(self._pr_id, "There are unrecognised tests.\n"+message_from_file('show_tests.txt'))
         else:
             already_triggered = [c["name"] for c in self._GAI.get_check_runs(self._ref)['check_runs']]
             print(already_triggered)
