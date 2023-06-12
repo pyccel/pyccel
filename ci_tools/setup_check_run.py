fc30d50a55edcf88f23ecb650bfdcbe02ff1fa54
EmilyBourne <louise.bourne@gmail.com>
2023-06-12 12:53:52 +0200

    Correct check_run_id

--- ci_tools/setup_check_run.py
+++ ci_tools/setup_check_run.py
@@ -1,9 +1,9 @@
 import os
 from bot_tools.bot_funcs import Bot
 
-bot = Bot(check_run_id = os.environ["GITHUB_RUN_ID"], commit = os.environ["GITHUB_REF"])
-if os.environ["GITHUB_RUN_ID"]=="":
+bot = Bot(check_run_id = os.environ["GITHUB_CHECK_RUN_ID"], commit = os.environ["GITHUB_REF"])
+if os.environ["GITHUB_CHECK_RUN_ID"]=="":
     bot.create_in_progress_check_run()
 else:
-    print(os.environ["GITHUB_RUN_ID"])
+    print(os.environ["GITHUB_CHECK_RUN_ID"])
     bot.post_in_progress()
