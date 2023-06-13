4676de74e6a926a4a31044f95196a69ed6411e9d
EmilyBourne <louise.bourne@gmail.com>
2023-06-13 08:40:16 +0200

    Remove assertion parentheses

--- tests/ast/test_basic.py
+++ tests/ast/test_basic.py
@@ -20,13 +20,13 @@ def get_functions(filename):
     ast = pyccel.parse()
 
     # Assert syntactic success
-    assert(not errors.has_errors())
+    assert not errors.has_errors()
 
     settings = {}
     ast = pyccel.annotate(**settings)
 
     # Assert semantic success
-    assert(not errors.has_errors())
+    assert not errors.has_errors()
 
     return list(ast.scope.functions.values())
 
@@ -36,7 +36,7 @@ def test_get_attribute_nodes():
     fst = get_functions(filename)[0]
     atts = fst.get_attribute_nodes(Variable)
 
-    assert(all(isinstance(a, Variable) for a in atts))
+    assert all(isinstance(a, Variable) for a in atts)
 
     expected = [Variable('int', 'a'),
                 Variable('int', 'b'),
@@ -46,7 +46,7 @@ def test_get_attribute_nodes():
                 Variable('int', 'g')]
 
     for e in expected:
-        assert(e in atts)
+        assert e in atts
 
 def test_get_attribute_nodes_exclude():
     filename = os.path.join(path_dir, "math.py")
@@ -54,16 +54,16 @@ def test_get_attribute_nodes_exclude():
     fst = get_functions(filename)[0]
     atts = fst.get_attribute_nodes(PyccelOperator, excluded_nodes=(PyccelAdd,))
 
-    assert(all(isinstance(a, PyccelOperator) for a in atts))
-    assert(all(not isinstance(a, PyccelAdd) for a in atts))
-    assert(len(atts)==1)
+    assert all(isinstance(a, PyccelOperator) for a in atts)
+    assert all(not isinstance(a, PyccelAdd) for a in atts)
+    assert len(atts)==1
     minus = atts[0]
-    assert(isinstance(minus, PyccelMinus)==1)
+    assert isinstance(minus, PyccelMinus)==1
 
     a = Variable('int', 'a')
     b = Variable('int', 'b')
-    assert(minus.args[0] == a)
-    assert(minus.args[1] == b)
+    assert minus.args[0] == a
+    assert minus.args[1] == b
 
 def test_get_user_nodes():
     filename = os.path.join(path_dir, "math.py")
@@ -78,9 +78,9 @@ def test_get_user_nodes():
 
     sums = set(a_var.get_user_nodes((PyccelAdd, PyccelMinus)))
 
-    assert(all(isinstance(s, (PyccelAdd, PyccelMinus)) for s in sums))
+    assert all(isinstance(s, (PyccelAdd, PyccelMinus)) for s in sums)
 
-    assert(len(sums) == 3)
+    assert len(sums) == 3
 
 def test_get_user_nodes_excluded():
     filename = os.path.join(path_dir, "math.py")
@@ -94,7 +94,7 @@ def test_get_user_nodes_excluded():
     a_var = atts[0]
 
     plus_assign = a_var.get_user_nodes(Assign, excluded_nodes = PyccelMinus)
-    assert(len(plus_assign)==2)
+    assert len(plus_assign)==2
 
 def test_get_all_user_nodes():
     filename = os.path.join(path_dir, "math.py")
@@ -109,9 +109,9 @@ def test_get_all_user_nodes():
 
     users = set(a_var.get_all_user_nodes())
 
-    assert(all(isinstance(s, (PyccelMul, PyccelMinus, AugAssign, FunctionDefArgument)) for s in users))
+    assert all(isinstance(s, (PyccelMul, PyccelMinus, AugAssign, FunctionDefArgument)) for s in users)
 
-    assert(len(users) == 4)
+    assert len(users) == 4
 
 def test_get_direct_user_nodes():
     filename = os.path.join(path_dir, "math.py")
@@ -126,9 +126,9 @@ def test_get_direct_user_nodes():
 
     sums = set(a_var.get_direct_user_nodes(lambda x : isinstance(x,(PyccelAdd, PyccelMinus))))
 
-    assert(all(isinstance(s, (PyccelAdd, PyccelMinus)) for s in sums))
+    assert all(isinstance(s, (PyccelAdd, PyccelMinus)) for s in sums)
 
-    assert(len(sums) == 2)
+    assert len(sums) == 2
 
 def test_substitute():
     filename = os.path.join(path_dir, "math.py")
@@ -142,15 +142,15 @@ def test_substitute():
 
     a_var = atts[0]
     old_parents = a_var.get_user_nodes(Basic)
-    assert(len(a_var.get_user_nodes(Basic))>0)
+    assert len(a_var.get_user_nodes(Basic))>0
 
     fst.substitute(a_var, new_var)
 
-    assert(len(a_var.get_user_nodes(Basic))==0)
+    assert len(a_var.get_user_nodes(Basic))==0
 
     atts = set(fst.get_attribute_nodes(Variable))
-    assert(new_var in atts)
-    assert(set(new_var.get_user_nodes(Basic)) == set(old_parents))
+    assert new_var in atts
+    assert set(new_var.get_user_nodes(Basic)) == set(old_parents)
 
 def test_substitute_exclude():
     filename = os.path.join(path_dir, "math.py")
@@ -164,18 +164,18 @@ def test_substitute_exclude():
 
     a_var = atts[0]
     old_parents = set(a_var.get_user_nodes(Basic))
-    assert(len(a_var.get_user_nodes(Basic))>0)
+    assert len(a_var.get_user_nodes(Basic))>0
 
     fst.substitute(a_var, new_var, excluded_nodes=(PyccelMinus))
 
-    assert(len(a_var.get_user_nodes(Basic))==1)
+    assert len(a_var.get_user_nodes(Basic))==1
 
     atts = set(fst.get_attribute_nodes(Variable))
-    assert(new_var in atts)
+    assert new_var in atts
 
     new_parents = set(new_var.get_user_nodes(Basic))
-    assert(len(new_parents.difference(old_parents))==0)
-    assert(len(old_parents.difference(new_parents))==1)
+    assert len(new_parents.difference(old_parents))==0
+    assert len(old_parents.difference(new_parents))==1
 
 def test_recursive():
     filename = os.path.join(path_dir, "cyclic_dependence.py")
@@ -183,12 +183,12 @@ def test_recursive():
     fst = get_functions(filename)[0]
 
     atts = fst.get_attribute_nodes(PyccelMinus)
-    assert(len(set(atts))==2)
+    assert len(set(atts))==2
 
     var = atts[0].args[0]
 
     atts = var.get_user_nodes(FunctionDef)
-    assert(len(set(atts))==2) # The actual FunctionDef + the signature version
+    assert len(set(atts))==2 # The actual FunctionDef + the signature version
 
     fst.substitute(LiteralInteger(2), LiteralInteger(3))
 
