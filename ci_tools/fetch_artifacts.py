401deb0b868e373c254d6fbd00b230ccad8c1a7d
EmilyBourne <louise.bourne@gmail.com>
2023-06-14 11:17:58 +0200

    Missing parentheses

--- ci_tools/fetch_artifacts.py
+++ ci_tools/fetch_artifacts.py
@@ -7,4 +7,4 @@ artifact_urls = sys.argv[1:]
 GAI = GitHubAPIInteractions()
 for i,url in enumerate(artifact_urls):
     GAI.download_artifact(url)
-    shutil.move(file, f"{file}.{i}"
+    shutil.move(file, f"{file}.{i}")
