f2a1f2b17a11915ea7fad720382d046e6616dcd2
EmilyBourne <louise.bourne@gmail.com>
2023-06-14 15:42:00 +0200

    Correct call

--- ci_tools/bot_tools/setup_values.py
+++ ci_tools/bot_tools/setup_values.py
@@ -5,7 +5,7 @@ def get_pr_id(possible_pr_ids):
     GAI = GitHubAPIInteractions()
     for p in possible_pr_ids:
         n = p['number']
-        pr_details = self._GAI.get_pr_details()
+        pr_details = GAI.get_pr_details()
         if pr_details["state"] == "open":
             return n
     return possible_pr_ids[0]['number']
