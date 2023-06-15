fdf74aa32cf6b970654b6ab99d27d0b4bc4b0e2f
EmilyBourne <louise.bourne@gmail.com>
2023-06-15 13:47:12 +0200

    Remove bad format

--- ci_tools/json_pytest_output.py
+++ ci_tools/json_pytest_output.py
@@ -8,13 +8,7 @@ def get_json_status():
     data = {}
     summary = {}
     data['title'] = 'pytest results'
-    summary['pytest 1'] = sys.argv[1]
-    summary['pytest 2'] = sys.argv[2]
-    summary['pytest 3'] = sys.argv[3]
-    summary['pytest 4'] = sys.argv[4]
-    summary['pytest 5'] = sys.argv[5]
-    summary['pytest 6'] = sys.argv[6]
-    data['summary'] = summary
+    data['summary'] = 'text'
     return json.dumps(data)
 
 with open(output_file, 'a') as f:
