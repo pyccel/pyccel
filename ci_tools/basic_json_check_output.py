895445de8299b4ada1e5b0bbde3444fe95364408
EmilyBourne <louise.bourne@gmail.com>
2023-06-25 13:25:05 +0200

    Output errors for pickles

---
+++ ci_tools/basic_json_check_output.py
@@ -0,0 +1,26 @@
+import argparse
+import json
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('statuses', nargs='+', help="The outcome of each of the tests [success, failure, cancelled, or skipped]")
+    parser.add_argument('reasons', nargs='+', help="The reasons to print in case of failure")
+    p_args = parser.parse_args()
+
+    assert len(p_args.statuses) == len(p_args.reasons)
+
+    summary = "Congratulations! Tests are passing."
+    for status, reason in zip(p_args.statuses, p_args.reasons):
+        if status == "failure":
+            summary = reason
+            break
+        elif status != "success":
+            summary = "Tests cancelled"
+            break
+
+    json_ouput = {
+        "summary":summary
+    }
+
+    with open(output_file, 'w') as f:
+        json.dump(json_ouput,f)
