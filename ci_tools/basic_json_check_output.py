a110ad26c1bcf792cc363dc2852b555ee7e498a8
EmilyBourne <louise.bourne@gmail.com>
2023-06-25 13:43:55 +0200

    Fix json reporting

--- ci_tools/basic_json_check_output.py
+++ ci_tools/basic_json_check_output.py
@@ -3,8 +3,8 @@ import json
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('statuses', nargs='+', help="The outcome of each of the tests [success, failure, cancelled, or skipped]")
-    parser.add_argument('reasons', nargs='+', help="The reasons to print in case of failure")
+    parser.add_argument('--statuses', nargs='+', help="The outcome of each of the tests [success, failure, cancelled, or skipped]", required=True)
+    parser.add_argument('--reasons', nargs='+', help="The reasons to print in case of failure", required=True)
     p_args = parser.parse_args()
 
     assert len(p_args.statuses) == len(p_args.reasons)
@@ -17,6 +17,8 @@ if __name__ == '__main__':
         elif status != "success":
             summary = "Tests cancelled"
             break
+
+    output_file = 'test_json_result.json'
 
     json_ouput = {
         "summary":summary
