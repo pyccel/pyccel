718101b42168ab4c6687edecccdb3eaaf5c4a8e5
EmilyBourne <louise.bourne@gmail.com>
2023-07-07 10:34:03 +0200

    Add main to test which works

--- tests/codegen/fcode/scripts/concatenation.py
+++ tests/codegen/fcode/scripts/concatenation.py
@@ -1,7 +1,13 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
-x = [1, 2, 3]
-l = x + [4, 5, 6]
-s = [1, 2, 3] + [1, 2, 3]
 
-st = "Hello" + "World"
+if __name__ == '__main__':
+    x = [1, 2, 3]
+    l = x + [4, 5, 6]
+    s = [1, 2, 3] + [1, 2, 3]
 
+    st = "Hello" + "World"
+
+    print(x)
+    print(l)
+    print(s)
+    print(st)
