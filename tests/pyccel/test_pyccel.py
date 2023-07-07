d848b60e6d21412d70f6102c6a7bc18d10d7c0a6
EmilyBourne <louise.bourne@gmail.com>
2023-07-07 10:43:17 +0200

    Move to active tests

--- tests/pyccel/test_pyccel.py
+++ tests/pyccel/test_pyccel.py
@@ -1006,3 +1006,8 @@ def test_reserved_file_name():
         libname = str(random.choice(tuple(python_builtin_libs))) + ".py" # nosec B311
         execute_pyccel(fname=libname)
     assert str(exc_info.value) == f"File called {libname} has the same name as a Python built-in package and can't be imported from Python. See #1402"
+
+def test_concatentation():
+    pyccel_test("scripts/concatenation.py",
+                language = 'fortran',
+                output_dtype=[int]*15+[str])
