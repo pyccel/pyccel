cba881c842083ec213f6b201fb771a4bedde6719
EmilyBourne <louise.bourne@gmail.com>
2023-07-17 17:52:40 +0200

    Add missing main

--- ci_tools/summarise_pyspelling.py
+++ ci_tools/summarise_pyspelling.py
@@ -38,90 +38,91 @@ def find_all_words(file_path, search_word):
         return results
     return None
 
-parser = argparse.ArgumentParser(description='Create a neat markdown file to summarise the results')
-parser.add_argument('spelling', metavar='diffFile', type=str,
-                        help='File containing the pyspelling output')
-parser.add_argument('output', metavar='output', type=str,
-                        help='File where the markdown output will be printed')
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Create a neat markdown file to summarise the results')
+    parser.add_argument('spelling', metavar='diffFile', type=str,
+                            help='File containing the pyspelling output')
+    parser.add_argument('output', metavar='output', type=str,
+                            help='File where the markdown output will be printed')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-with open(args.spelling, encoding="utf-8") as f:
-    lines = f.readlines()
+    with open(args.spelling, encoding="utf-8") as f:
+        lines = f.readlines()
 
-lines = [l.strip() for l in lines[:-1]]
-lines = [l for l in lines if l != 'Misspelled words:' and any(c != '-' for c in l)]
+    lines = [l.strip() for l in lines[:-1]]
+    lines = [l for l in lines if l != 'Misspelled words:' and any(c != '-' for c in l)]
 
-errors = {}
-annotations = []
+    errors = {}
+    annotations = []
 
-n = len(lines)
-i = 0
-while i < n:
-    filename = lines[i].split()[1]
-    i+=1
-    words = set()
-    while i<n and not lines[i].startswith('<htmlcontent>'):
-        words.add(lines[i])
+    n = len(lines)
+    i = 0
+    while i < n:
+        filename = lines[i].split()[1]
         i+=1
+        words = set()
+        while i<n and not lines[i].startswith('<htmlcontent>'):
+            words.add(lines[i])
+            i+=1
 
-    if filename in errors:
-        errors[filename].update(words)
-    else:
-        errors[filename] = words
+        if filename in errors:
+            errors[filename].update(words)
+        else:
+            errors[filename] = words
 
-if errors:
-    all_words = set()
+    if errors:
+        all_words = set()
 
-    with open(os.path.join(os.path.dirname(__file__),'..','.dict_custom.txt'), encoding="utf-8") as d:
-        internal_dict = [w.strip() for w in d.readlines()]
+        with open(os.path.join(os.path.dirname(__file__),'..','.dict_custom.txt'), encoding="utf-8") as d:
+            internal_dict = [w.strip() for w in d.readlines()]
 
-    with open(args.output, 'w', encoding="utf-8") as f:
-        print("There are misspelled words", file=f)
-        for name, words in errors.items():
-            print("## `", name, "`", file=f)
-            for w in words:
-                words_list = find_all_words("../"+name.strip(":"), w)
-                suggestions = difflib.get_close_matches(w, internal_dict)
-                for line_no, column in words_list:
+        with open(args.output, 'w', encoding="utf-8") as f:
+            print("There are misspelled words", file=f)
+            for name, words in errors.items():
+                print("## `", name, "`", file=f)
+                for w in words:
+                    words_list = find_all_words("../"+name.strip(":"), w)
+                    suggestions = difflib.get_close_matches(w, internal_dict)
+                    for line_no, column in words_list:
+                        if suggestions:
+                            msg_cus = f" Misspelled word :  Did you mean {w} -> {suggestions}"
+                        else:
+                            msg_cus = f"Misspelled word {w}"
+                        annotation_1 = {
+                            "path": name,
+                            "start_line": line_no,
+                            "end_line": line_no,
+                            "start_column": column,
+                            "end_column": column + len(w),
+                            "annotation_level": "failure",
+                            "message": msg_cus,
+                            "title": "Misspelled word"
+                        }
+                        annotations.append(annotation_1)
                     if suggestions:
-                        msg_cus = f" Misspelled word :  Did you mean {w} -> {suggestions}"
+                        print("-   ", w, f"  :  Did you mean {w} -> {suggestions}", file=f)
                     else:
-                        msg_cus = f"Misspelled word {w}"
-                    annotation_1 = {
-                        "path": name,
-                        "start_line": line_no,
-                        "end_line": line_no,
-                        "start_column": column,
-                        "end_column": column + len(w),
-                        "annotation_level": "failure",
-                        "message": msg_cus,
-                        "title": "Misspelled word"
-                    }
-                    annotations.append(annotation_1)
-                if suggestions:
-                    print("-   ", w, f"  :  Did you mean {w} -> {suggestions}", file=f)
-                else:
-                    print("-   ", w, file=f)
-            print(file=f)
-            all_words.update(words)
+                        print("-   ", w, file=f)
+                print(file=f)
+                all_words.update(words)
 
-        print("These errors may be due to typos, capitalisation errors, or lack of quotes around code. If this is a false positive please add your word to `.dict_custom.txt`", file=f)
+            print("These errors may be due to typos, capitalisation errors, or lack of quotes around code. If this is a false positive please add your word to `.dict_custom.txt`", file=f)
 
-    # Generating a json file for github check runs
-    output_file = 'test_json_result.json'
-    md = ""
+        # Generating a json file for github check runs
+        output_file = 'test_json_result.json'
+        md = ""
 
-    with open(args.output, 'r', encoding="utf-8") as f:
-        md = f.read()
-    print(md)
-    json_ouput = {
-        "title":"Misspelling summary ",
-        "summary":md,
-        "annotations": annotations
-    }
-    with open(output_file, 'w', encoding="utf-8") as f:
-        json.dump(json_ouput,f)
-    sys.exit(1)
-else:
-    sys.exit(0)
+        with open(args.output, 'r', encoding="utf-8") as f:
+            md = f.read()
+        print(md)
+        json_ouput = {
+            "title":"Misspelling summary ",
+            "summary":md,
+            "annotations": annotations
+        }
+        with open(output_file, 'w', encoding="utf-8") as f:
+            json.dump(json_ouput,f)
+        sys.exit(1)
+    else:
+        sys.exit(0)
