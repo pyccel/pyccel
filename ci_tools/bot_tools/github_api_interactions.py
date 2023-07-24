c56db265392975bfd253971487c1c281dd934ad3
EmilyBourne <louise.bourne@gmail.com>
2023-07-24 11:49:58 +0200

    Update ids

--- ci_tools/bot_tools/github_api_interactions.py
+++ ci_tools/bot_tools/github_api_interactions.py
@@ -26,14 +26,14 @@ def get_authorization():
     # Issued at time
     # JWT expiration time (10 minutes maximum)
     # GitHub App's identifier
-    payload = {'iat': int(time.time()), 'exp': int(time.time()) + 60, 'iss': 337566}
+    payload = {'iat': int(time.time()), 'exp': int(time.time()) + 60, 'iss': 364561}
 
     jw_token=jwt.JWT().encode(payload, signing_key, alg='RS256')
 
     headers = {"Accept": "application/vnd.github+json", "Authorization": f"Bearer {jw_token}", "X-GitHub-Api-Version": "2022-11-28"}
 
     # Create JWT
-    reply = requests.post("https://api.github.com/app/installations/37820767/access_tokens", headers=headers)
+    reply = requests.post("https://api.github.com/app/installations/39885334/access_tokens", headers=headers)
     print(reply.text)
 
     token  = reply.json()["token"]
