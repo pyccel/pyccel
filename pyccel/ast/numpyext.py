f2a070873f89cc3a9fc000d54b0fa98392bdd643
Yaman Güçlü <yaman.guclu@gmail.com>
2023-07-24 17:14:02 +0200

    Extend docstring of NumpyZerosLike

--- pyccel/ast/numpyext.py
+++ pyccel/ast/numpyext.py
@@ -1236,10 +1236,38 @@ class NumpyOnesLike(PyccelInternalFunction):
 
 #==============================================================================
 class NumpyZerosLike(PyccelInternalFunction):
-    """ Represents a call to numpy.zeros_like for code generation.
+    """
+    Represents a call to numpy.zeros_like for code generation.
+
+    This wrapper class represents calls to the function numpy.zeros_like.
+    Objects of this class are never present in the Pyccel AST, because the
+    class constructor always returns an object of type `NumpyZeros`.
+
+    Parameters
+    ----------
+    a : PyccelAstNode
+        Numpy array which is used as a template.
+
+    dtype : ??, default=None
+        Type of the data contained in the new array. If None, a.dtype is used.
+
+    order : str, default='K'
+        Ordering used for the indices of a multi-dimensional array.
+
+    subok : bool, default=True
+        This parameter is currently ignored.
+
+    shape : PythonTuple of ??
+        Overrides the shape of the array.
+
+    See Also
+    --------
+    https://numpy.org/doc/stable/reference/generated/numpy.zeros_like.html
+
     """
     __slots__ = ()
     name = 'zeros_like'
+
     def __new__(cls, a, dtype=None, order='K', subok=True, shape=None):
 
         # NOTE: we ignore 'subok' argument
