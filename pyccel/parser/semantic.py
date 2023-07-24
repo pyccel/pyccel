8aa9377bae37d5a813f79aabed969058b8f3f12b
sboof911 <56886152+sboof911@users.noreply.github.com>
2023-07-24 10:31:20 +0000

    ConstructorCall inherit from DottedFunctionCall

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -2349,11 +2349,11 @@ class SemanticParser(BasicParser):
                 bounding_box=(self._current_fst_node.lineno, self._current_fst_node.col_offset),
                 severity='error')
             args = expr.args
+            cls_variable = expr.get_user_nodes(Assign)[0].lhs
 
             # TODO check compatibility
             # TODO treat parametrized arguments.
-
-            expr = ConstructorCall(method, args, cls_variable=None)
+            expr = ConstructorCall(method, args, cls_variable)
             #if len(stmts) > 0:
             #    stmts.append(expr)
             #    return CodeBlock(stmts)
