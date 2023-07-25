9ee9976bf853197c529cb53f3c8c82be448ffd2e
EmilyBourne <louise.bourne@gmail.com>
2023-07-25 15:58:37 +0200

    Import module from the correct branch

--- ci_tools/summarise_doccoverage.py
+++ ci_tools/summarise_doccoverage.py
@@ -32,7 +32,7 @@ if __name__ == '__main__':
         n = len(lines)
         i = 0
         while i < n:
-            modname = lines[i].split()[1].strip('"')[:-3].replace('/','.').split(f'.{branch}.', 1)[1]
+            modname = branch + '.' + lines[i].split()[1].strip('"')[:-3].replace('/','.').split(f'.{branch}.', 1)[1]
             i+=1
             while i<n and lines[i].startswith(' - '):
                 if lines[i].startswith(' - No module docstring'):
