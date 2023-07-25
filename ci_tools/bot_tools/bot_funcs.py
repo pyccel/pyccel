e1dc9a4ca6014180f286a7969f69bb4ff4c22774
EmilyBourne <louise.bourne@gmail.com>
2023-07-25 09:38:10 +0200

    List doesn't need unpacking

--- ci_tools/bot_tools/bot_funcs.py
+++ ci_tools/bot_tools/bot_funcs.py
@@ -285,6 +285,7 @@ class Bot:
                 assert p.returncode == 0
 
             commit_log = [o.split(' ')[0] for o in out.split('\n')]
+            print(commit_log)
 
             for t in tests:
                 pv = python_version or default_python_versions[t]
@@ -669,7 +670,7 @@ class Bot:
         """
         if commit is None:
             commit = self._ref
-        return self._GAI.get_check_runs(commit)['check_runs']
+        return self._GAI.get_check_runs(commit)
 
     def get_bot_review_comments(self):
         """
@@ -751,6 +752,7 @@ class Bot:
         """
         if base_commit is None:
             base_commit = self._base
+        assert bool(base_commit)
         cmd = [git, 'diff', f"{base_commit}..{self._ref}"]
         print(cmd)
         with subprocess.Popen(cmd + ['--name-only'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True) as p:
