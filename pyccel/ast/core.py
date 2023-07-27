e35c83833ec2454d333723e574c4a57fe01788c1
EmilyBourne <louise.bourne@gmail.com>
2023-07-27 11:52:40 +0200

    Revert changes

--- pyccel/ast/core.py
+++ pyccel/ast/core.py
@@ -2296,6 +2296,10 @@ class Return(Basic):
         super().__init__()
 
     @property
+    def expr(self):
+        return self._expr
+
+    @property
     def stmt(self):
         return self._stmt
 
