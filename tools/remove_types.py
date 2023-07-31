0c05409a4cf91a374225e2a69fdc782859946974
EmilyBourne <louise.bourne@gmail.com>
2023-07-31 10:47:00 +0200

    Correct algorithm

--- tools/remove_types.py
+++ tools/remove_types.py
@@ -37,10 +37,17 @@ if __name__ == '__main__':
                 elif 'def ' in lines[i]:
                     if len(current_types) == 1:
                         type_strs = current_types[0].split('(')[1].split(')')[0]
-                        quoted_types = type_annotation.findall(type_strs)
+                        quoted_types = [q.strip() for q in type_annotation.findall(type_strs)]
                         type_strs = type_annotation.sub('None', type_strs)
-                        non_quoted_types = [s.strip() for s in type_strs.split(',')]
-                        all_types = [q.strip() if s == 'None' else s for q,s in zip(quoted_types,non_quoted_types)]
+                        all_types = []
+                        j = 0
+                        for s in type_strs.split(','):
+                            s = s.strip()
+                            if s == 'None':
+                                all_types.append(quoted_types[j])
+                                j+=1
+                            else:
+                                all_types.append(s)
                         type_annotations = [s.replace('real', 'float').strip() for s in all_types]
                         start, tmp = lines[i].split('(')
                         args, end = tmp.split(')')
@@ -55,8 +62,7 @@ if __name__ == '__main__':
                     else:
                         new_lines.extend(current_types)
                         new_lines.append(lines[i])
-                        if len(current_types) != 0:
-                            uses_types = True
+                        uses_types = (len(current_types) != 0)
                     current_types = []
                 elif '@' in lines[i]:
                     if '(' in lines[i]:
@@ -68,7 +74,7 @@ if __name__ == '__main__':
                     import_types_line = i
                     if ',' in lines[i]:
                         imps = [i.strip() for i in lines[i].split('from pyccel.decorators import')[1].split(',')]
-                        new_lines.append('from pyccel.decorators import '+', '.join(i for  i in imps if i != 'types'))
+                        new_lines.append('from pyccel.decorators import '+', '.join(i for  i in imps if i != 'types')+'\n')
                 else:
                     assert len(current_types) == 0
                     new_lines.append(lines[i])
