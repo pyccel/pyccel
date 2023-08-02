bf079a7af9a957564643a27f2d540244b1c71c1d
EmilyBourne <louise.bourne@gmail.com>
2023-08-02 11:17:04 +0200

    Add to correct function. Correct while condition

--- ci_tools/bot_tools/github_api_interactions.py
+++ ci_tools/bot_tools/github_api_interactions.py
@@ -403,10 +403,12 @@ class GitHubAPIInteractions:
         url = f"https://api.github.com/repos/{self._org}/{self._repo}/pulls/{pr_id}/comments"
         results = []
         page = 1
-        while exit_status == 200 and len(results)%100 == 0:
+        new_results = [None]
+        while len(new_results) != 0:
             request = self._post_request("GET", url, params={'per_page': '100', 'page': str(page)})
-            results.extend(request.json())
-            exit_status = request.status_code
+            new_results = request.json()
+            results.extend(new_results)
+            page += 1
         return results
 
     def create_comment(self, pr_id, comment, reply_to = None):
@@ -638,7 +640,15 @@ class GitHubAPIInteractions:
             A dictionary describing the reviews.
         """
         url = f"https://api.github.com/repos/{self._org}/{self._repo}/pulls/{pr_id}/reviews"
-        return self._post_request("GET", url).json()
+        results = []
+        page = 1
+        new_results = [None]
+        while len(new_results) != 0:
+            request = self._post_request("GET", url, params={'per_page': '100', 'page': str(page)})
+            new_results = request.json()
+            results.extend(new_results)
+            page += 1
+        return results
 
     def get_events(self, pr_id, page = 1):
         """
