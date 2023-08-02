87b28511845206795ea5d765b2859eec39cd07e0
EmilyBourne <louise.bourne@gmail.com>
2023-08-02 10:55:47 +0200

    Init exit_status

--- ci_tools/bot_tools/github_api_interactions.py
+++ ci_tools/bot_tools/github_api_interactions.py
@@ -403,6 +403,7 @@ class GitHubAPIInteractions:
         url = f"https://api.github.com/repos/{self._org}/{self._repo}/pulls/{pr_id}/comments"
         results = []
         page = 1
+        exit_status = 200
         while exit_status == 200 and len(results)%100 == 0:
             request = self._post_request("GET", url, params={'per_page': '100', 'page': str(page)})
             results.extend(request.json())
@@ -640,6 +641,7 @@ class GitHubAPIInteractions:
         url = f"https://api.github.com/repos/{self._org}/{self._repo}/pulls/{pr_id}/reviews"
         results = []
         page = 1
+        exit_status = 200
         while exit_status == 200 and len(results)%100 == 0:
             request = self._post_request("GET", url, params={'per_page': '100', 'page': str(page)})
             results.extend(request.json())
