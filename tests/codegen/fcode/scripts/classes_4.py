121e20bb68ee70c72ddefc4e3f08e0b16c162948
EmilyBourne <louise.bourne@gmail.com>
2023-09-07 21:12:23 +0200

    PEP 484

--- tests/codegen/fcode/scripts/classes_4.py
+++ tests/codegen/fcode/scripts/classes_4.py
@@ -3,27 +3,27 @@
 
 class Shape:
 
-    def __init__(self : Shape, x : float, y : float):
+    def __init__(self : 'Shape', x : float, y : float):
         self.x = x
         self.y = y
         self.description = "This shape has not been described yet"
         self.author = "Nobody has claimed to make this shape yet"
 
-    def area(self : Shape) -> float:
+    def area(self : 'Shape') -> float:
         y = self.x * self.y
         return y
 
-    def perimeter(self : Shape) -> float:
+    def perimeter(self : 'Shape') -> float:
         x = 2 * self.x + 2 * self.y
         return x
 
-    def describe(self : Shape, text : str):
+    def describe(self : 'Shape', text : str):
         self.description = text
 
-    def authorName(self : Shape, text : str):
+    def authorName(self : 'Shape', text : str):
         self.author = text
 
-    def scaleSize(self : Shape, scale : float):
+    def scaleSize(self : 'Shape', scale : float):
         self.x = self.x * scale
         self.y = self.y * scale
 
