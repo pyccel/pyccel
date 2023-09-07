b4968078930bdbee3e54202044f8709a8785bad0
EmilyBourne <louise.bourne@gmail.com>
2023-09-07 21:41:35 +0200

    PEP 484

--- tests/pyccel/scripts/classes/generic_methods.py
+++ tests/pyccel/scripts/classes/generic_methods.py
@@ -2,15 +2,15 @@
 from pyccel.decorators import template
 
 class Point(object):
-    def __init__(self : Point, x : float, y : float):
+    def __init__(self : 'Point', x : float, y : float):
         self.x = x
         self.y = y
 
-    def __del__(self : Point):
+    def __del__(self : 'Point'):
         pass
 
     @template('T', [int, float])
-    def translate(self : Point, a : 'T', b : 'T'):
+    def translate(self : 'Point', a : 'T', b : 'T'):
         self.x = self.x + a
         self.y = self.y + b
 
