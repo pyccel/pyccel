137b66db48ca43c3893c186e8e69419faab9b436
EmilyBourne <louise.bourne@gmail.com>
2023-09-07 21:21:01 +0200

    PEP 484

--- tests/errors/known_bugs/inheritance.py
+++ tests/errors/known_bugs/inheritance.py
@@ -1,21 +1,21 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 
 class Point2d:
-    def __init__(self : Point2d, x : float, y : float):
+    def __init__(self : 'Point2d', x : float, y : float):
         self.x = x
         self.y = y
 
-    def translate(self : Point2d, a : float, b : float):
+    def translate(self : 'Point2d', a : float, b : float):
         self.x = self.x + a
         self.y = self.y + b
 
 class Point3d(Point2d):
-    def __init__(self : Point3d, x : float, y : float, z : float):
+    def __init__(self : 'Point3d', x : float, y : float, z : float):
         self.z = z
         Point2d.__init__(self,x, y)
         #super().__init__(x, y)
 
-    def translate(self : Point3d, a : float, b : float, c : float):
+    def translate(self : 'Point3d', a : float, b : float, c : float):
         self.z = self.z + c
         Point2d.translate(self,a,b)
         #super().translate(a,b)
