2bcca891b8084f0902a2358776ece6f4a238b889
EmilyBourne <louise.bourne@gmail.com>
2023-09-07 21:06:37 +0200

    PEP 484

--- tests/codegen/fcode/scripts/classes.py
+++ tests/codegen/fcode/scripts/classes.py
@@ -1,14 +1,14 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 
 class Point(object):
-    def __init__(self : Point, x : float, y : float):
+    def __init__(self : 'Point', x : float, y : float):
         self.x = x
         self.y = y
 
-    def __del__(self : Point):
+    def __del__(self : 'Point'):
         pass
 
-    def translate(self : Point, a : float, b : float):
+    def translate(self : 'Point', a : float, b : float):
         self.x = self.x + a
         self.y = self.y + b
 
