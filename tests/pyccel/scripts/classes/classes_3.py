a43f8a1c9f3e5ecac038fb8f6d0e85610a09e564
EmilyBourne <louise.bourne@gmail.com>
2023-09-07 21:40:39 +0200

    PEP 484

--- tests/pyccel/scripts/classes/classes_3.py
+++ tests/pyccel/scripts/classes/classes_3.py
@@ -2,14 +2,14 @@
 # coding: utf-8
 
 class Point1:
-    def __init__(self : Point1, x : float):
+    def __init__(self : 'Point1', x : float):
         self.x = x
 
 class Point2:
-    def __init__(self : Point2, y : float):
+    def __init__(self : 'Point2', y : float):
         self.y = y
 
-    def test_func(self : Point2):
+    def test_func(self : 'Point2'):
         p = Point1(self.y)
         print(p.x)
 
