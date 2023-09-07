7d32d8b5ff8434125da82063f491026449bb5854
EmilyBourne <louise.bourne@gmail.com>
2023-09-07 21:40:09 +0200

    PEP 484

--- tests/pyccel/scripts/classes/classes_2_C.py
+++ tests/pyccel/scripts/classes/classes_2_C.py
@@ -2,13 +2,13 @@
 import numpy as np
 
 class Point:
-    def __init__(self : Point):
+    def __init__(self : 'Point'):
         pass
 
-    def addition(self : Point, a : float, b : float):
+    def addition(self : 'Point', a : float, b : float):
         return a + b
 
-    def subtraction(self : Point, a : 'float[:]', b : 'float[:]'):
+    def subtraction(self : 'Point', a : 'float[:]', b : 'float[:]'):
         return a - b
 
 if __name__ == '__main__':
