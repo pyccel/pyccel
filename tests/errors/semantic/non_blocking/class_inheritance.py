0a0abbb46aaa1bd2c8824a19e3808ee28d672a48
EmilyBourne <louise.bourne@gmail.com>
2023-09-07 18:36:32 +0200

    Use type annotations

--- tests/errors/semantic/non_blocking/class_inheritance.py
+++ tests/errors/semantic/non_blocking/class_inheritance.py
@@ -1,7 +1,5 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
-#$ header class Point(public)
-#$ header method __init__(Point)
 
 class Point(NonExistantSuperClass): # pylint: disable=undefined-variable
-    def __init__(self):
+    def __init__(self : Point):
         pass
