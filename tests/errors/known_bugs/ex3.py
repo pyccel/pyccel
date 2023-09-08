647d80b1f8194aac0aebec31f4429cf62e4e027a
EmilyBourne <louise.bourne@gmail.com>
2023-09-08 08:24:13 +0200

    Use type annotations

--- tests/errors/known_bugs/ex3.py
+++ tests/errors/known_bugs/ex3.py
@@ -6,14 +6,14 @@
 #$ header method __del__(Line)
 
 class Line(object):
-    def __init__(self, a, b, step):
+    def __init__(self : 'Line', a : int, b : int, step : int):
         self.a = a
         self.b = b
         self.step = step
 
         self.indices = range(self.a, self.b, self.step)
 
-    def __del__(self):
+    def __del__(self : 'Line'):
         pass
 
 p = Line (0, 5, 1)
