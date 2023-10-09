a8b7f08146eef5fb5adbfff75732b50bf91a9634
EmilyBourne <louise.bourne@gmail.com>
2023-10-09 22:06:40 +0200

    Fix docstring

--- pyccel/ast/builtins.py
+++ pyccel/ast/builtins.py
@@ -406,7 +406,17 @@ class PythonInt(PyccelAstNode):
 
 #==============================================================================
 class PythonTuple(PyccelAstNode):
-    """ Represents a call to Python's native tuple() function.
+    """
+    Class representing a call to Python's native tuple() function.
+
+    Class representing a call to Python's native tuple() function
+    which either converts an object to a tuple or initialises a
+    literal tuple.
+
+    Parameters
+    ----------
+    *args : tuple of PyccelAstNode
+        The arguments passed to the tuple function.
     """
     __slots__ = ('_args','_inconsistent_shape','_is_homogeneous',
             '_dtype','_precision','_rank','_shape','_order')
