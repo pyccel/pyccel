17c6819ff26c90b8bf622e10aa9b0d61749b5c90
EmilyBourne <louise.bourne@gmail.com>
2023-10-13 14:22:00 +0200

    Add Intel tests to devel branch (#1547)
    Add the configuration files to run tests with an Intel compiler. Add
    an environment variable `PYCCEL_DEFAULT_COMPILER` which
    specifies the default compiler.
    
    There are 3 failing tests due to:
    - passing NaN as an argument
    - #1540 
    
    Additionally some array tests were modified to use `isclose` for
    comparisons instead of `np.all_equal` these tests all involve divisions
    and should have been using `isclose` anyway.
    
    This is related to #542 but does not fully fix the issue as it only adds
    Intel.
    
    ---------
    
    Co-authored-by: Yaman Güçlü <yaman.guclu@gmail.com>

--- tests/epyccel/test_epyccel_openmp.py
+++ tests/epyccel/test_epyccel_openmp.py
@@ -380,6 +380,7 @@ def test_omp_arraysum_combined(language):
     assert f1(x) == np.sum(x)
 
 @pytest.mark.external
+@pytest.mark.xfail(os.environ.get('PYCCEL_DEFAULT_COMPILER', None) == 'intel', reason='#1539')
 def test_omp_range_sum_critical(language):
     f1 = epyccel(openmp.omp_range_sum_critical, fflags = '-Wall', accelerators=['openmp'], language=language)
 
