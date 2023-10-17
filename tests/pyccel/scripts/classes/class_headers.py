4d0287e05078c3a9f07c3e7e907a8e0b6314070d
EmilyBourne <louise.bourne@gmail.com>
2023-10-17 17:28:38 +0200

    Add method header test

--- tests/pyccel/scripts/classes/class_headers.py
+++ tests/pyccel/scripts/classes/class_headers.py
@@ -1,8 +1,13 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 
+#$ header method get_4(A) results(int)
+
 class A:
     def __init__(self : 'A'):
         self.x = 3
+
+    def get_4(self):
+        return 4
 
 if __name__ == '__main__':
     #$ header variable myA A
