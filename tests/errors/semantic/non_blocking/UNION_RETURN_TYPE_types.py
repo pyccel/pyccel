a7f65e116af87f6b61abd263b96844e7c7e849b3
EmilyBourne <louise.bourne@gmail.com>
2023-10-18 12:12:22 +0200

    Missing import

--- tests/errors/semantic/non_blocking/UNION_RETURN_TYPE_types.py
+++ tests/errors/semantic/non_blocking/UNION_RETURN_TYPE_types.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
+from pyccel.decorators import types
 
 @types('int', 'int', results=['int'])
 @types('float', 'float', results=['float'])
