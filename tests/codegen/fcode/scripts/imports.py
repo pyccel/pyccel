d4473f8981827fd90aa569fd873c00fb15b51c47
sboof911 <56886152+sboof911@users.noreply.github.com>
2023-10-26 16:26:06 +0000

    fix codacy

--- tests/codegen/fcode/scripts/imports.py
+++ tests/codegen/fcode/scripts/imports.py
@@ -4,12 +4,12 @@
 #
 #from expressions import ad, bd
 #xd = ad * bd
+from numpy import ones
 xi = 2
 
 from decorators_types import decr
 yi = decr(xi)
 
 from classes_2 import Point
-from numpy import ones
 
 p = Point(ones(4))
