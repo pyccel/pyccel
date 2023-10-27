3f8bf5861e310c00aff4ce2ccd6849cf8326d6f4
sboof911 <56886152+sboof911@users.noreply.github.com>
2023-10-27 11:34:41 +0000

    move classes_interface to non_blocking

--- tests/errors/semantic/blocking/classes_interface.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
-
-class MyClass:
-    def __init__(self : 'MyClass', param1 : 'int'):
-        self.param1 = param1
-        print("MyClass Object created!")
-
-    def help(self : 'MyClass', param1 : 'int|float'):
-        self.param1 = param1
