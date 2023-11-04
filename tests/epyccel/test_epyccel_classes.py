787b7513720683ecb8e98e159506f14e036b1539
EmilyBourne <louise.bourne@gmail.com>
2023-11-04 22:16:19 +0100

    Add test

--- tests/epyccel/test_epyccel_classes.py
+++ tests/epyccel/test_epyccel_classes.py
@@ -1,5 +1,11 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
+import pytest
 from pyccel.epyccel import epyccel
+import modules.expose_classes as mod
+
+@pytest.fixture(scope="module")
+def modnew(language):
+    return epyccel(mod, language = language)
 
 def test_class_import(language):
     class A:
@@ -9,3 +15,15 @@ def test_class_import(language):
     epyc_A = epyccel(A)
 
     assert isinstance(epyc_A, type)
+
+
+def test_class_return(modnew):
+    a = modnew.get_A()
+    assert isinstance(a, modnew.A)
+    a_new, i = modnew.get_A_int()
+    a_new2, i2 = mod.get_A_int()
+    assert isinstance(a_new, modnew.A)
+    assert i == i2
+
+    b = modnew.get_B(3.0)
+    assert isinstance(b, modnew.B)
