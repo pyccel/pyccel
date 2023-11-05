c4490f1d70bfe240eea923c0533bf7e01f21bec0
sboof911 <56886152+sboof911@users.noreply.github.com>
2023-11-05 13:08:20 +0000

    add dalloc class alias

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -1507,11 +1507,11 @@ class CCodePrinter(CodePrinter):
         if isinstance(expr.variable, InhomogeneousTupleVariable):
             return ''.join(self._print(Deallocate(v)) for v in expr.variable)
         variable_address = self._print(ObjectAddress(expr.variable))
-        if expr.variable.is_alias:
-            return f'free_pointer({variable_address});\n'
         if isinstance(expr.variable.dtype, CustomDataType):
             Pyccel__del = expr.variable.cls_base.scope.find('__del__').name
             return f"{Pyccel__del}({variable_address});\n"
+        if expr.variable.is_alias:
+            return f'free_pointer({variable_address});\n'
         return f'free_array({variable_address});\n'
 
     def _print_Slice(self, expr):
--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -511,8 +511,6 @@ class SemanticParser(BasicParser):
                 if isinstance(i, DottedVariable):
                     if isinstance(i.lhs.dtype, CustomDataType) and self._current_function != '__del__':
                         continue
-                if isinstance(i.dtype, CustomDataType) and i.is_alias:
-                    continue
                 deallocs.append(Deallocate(i))
         self._allocs.pop()
         return deallocs
