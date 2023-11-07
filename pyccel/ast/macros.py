081734a904ff6f6465583e778da09a8e6f08537d
EmilyBourne <louise.bourne@gmail.com>
2023-11-07 11:16:15 +0100

    Add docstrings

--- pyccel/ast/macros.py
+++ pyccel/ast/macros.py
@@ -26,14 +26,23 @@ __all__ = (
 
 #==============================================================================
 class Macro(TypedAstNode):
-    """."""
+    """
+    Class representing a macro, ie an inline definition.
+
+    Class representing a macro, ie an inline definition. TO BE DEPRECATED.
+
+    Parameters
+    ----------
+    argument : PyccelSymbol | Variable
+        The argument passed to the macro.
+    """
     __slots__ = ('_argument',)
     _name = '__UNDEFINED__'
     _attribute_nodes = ()
 
     def __init__(self, argument):
         if not isinstance(argument, (PyccelSymbol, Variable)):
-            raise TypeError("Argument must be a Pyccelsymbol or a Variable not {}".format(type(argument)))
+            raise TypeError("Argument must be a Pyccelsymbol or a Variable not {type(argument)}")
 
         self._argument = argument
         super().__init__()
@@ -48,7 +57,18 @@ class Macro(TypedAstNode):
 
 #==============================================================================
 class MacroShape(Macro):
-    """."""
+    """
+    A macro indicating the shape of a variable.
+
+    A macro indicating the shape of a variable. TO BE DEPRECATED.
+
+    Parameters
+    ----------
+    argument : PyccelSymbol | Variable
+        The variable whose shape we are accessing.
+    index : int | LiteralInteger
+        The index of the element of the shape we are accessing.
+    """
     __slots__ = ('_index','_rank','_shape')
     _name      = 'shape'
     _dtype     = NativeInteger()
@@ -75,14 +95,22 @@ class MacroShape(Macro):
 
     def __str__(self):
         if self.index is None:
-            return 'MacroShape({})'.format(str(self.argument))
+            return 'MacroShape({self.argument})'
         else:
-            return 'MacroShape({}, {})'.format(str(self.argument),
-                                               str(self.index))
+            return 'MacroShape({self.argument}, {self.index})'
 
 #==============================================================================
 class MacroType(Macro):
-    """."""
+    """
+    A macro representing the type of a variable.
+
+    A macro representing the type of a variable. TO BE DEPRECATED.
+
+    Parameters
+    ----------
+    argument : PyccelSymbol | Variable
+        The variable whose datatype we are accessing.
+    """
     __slots__ = ()
     _name      = 'dtype'
     _dtype     = NativeGeneric()
@@ -93,11 +121,21 @@ class MacroType(Macro):
     _class_type = NativeGeneric()
 
     def __str__(self):
-        return 'MacroType({})'.format(str(self.argument))
+        return 'MacroType({self.argument})'
 
 #==============================================================================
 class MacroCount(Macro):
-    """."""
+    """
+    A macro representing the total number of elements in a variable.
+
+    A macro representing the total number of elements in a variable.
+    TO BE DEPRECATED.
+
+    Parameters
+    ----------
+    argument : PyccelSymbol | Variable
+        The variable whose size we are accessing.
+    """
     __slots__ = ()
     _name      = 'count'
     _dtype     = NativeInteger()
@@ -108,7 +146,7 @@ class MacroCount(Macro):
     _class_type = NativeInteger()
 
     def __str__(self):
-        return 'MacroCount({})'.format(str(self.argument))
+        return 'MacroCount({self.argument})'
 
 
 
