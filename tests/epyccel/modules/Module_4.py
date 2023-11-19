9abb8f89a59a566611b513b174ca90210b868076
EmilyBourne <louise.bourne@gmail.com>
2023-11-19 18:04:13 +0100

    Remove debug prints

--- tests/epyccel/modules/Module_4.py
+++ tests/epyccel/modules/Module_4.py
@@ -1,7 +1,6 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 
 def basic_optional(a  : 'int' =  None):
-    print("basic_optional : ", a is None)
     if a is  None :
         return 2
     return a
@@ -10,7 +9,6 @@ def call_optional_1():
     return basic_optional()
 
 def call_optional_2(b  : 'int' =  None):
-    print(b is None)
     return basic_optional(b)
 
 def change_optional(a : int = None):
