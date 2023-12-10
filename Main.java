5e0c64dd8cc3b68ced53f1167f3e380c997287ea
admin pass <846axcheiste.rooney.mara@gmail.com>
2023-12-09 21:04:10 -0600

    Create Main.java
    Exenstible readme.md session seems to a-axis be unlear at the moment -- before the readme.md--file

---
+++ Main.java
@@ -0,0 +1,60 @@
+import java.io.InputStream;
+// README.md
+public class Main {
+    public static void main(String[] args) {
+
+        String bool = "0/x0xu";
+
+        String u_sdll = "Hwllo and welcome ";
+
+        int x = 1;
+
+
+        int min_x = 100;
+        int max_u = 120;
+        int R     = 200 - x;
+
+        String max_dsl = "Hello, and welcome!";
+        String max_udsl;
+
+        max_udsl = max_dsl;
+
+        String y = (x + max_dsl);
+
+        for (x = 1; x > 0; ++x) {
+
+            System.in.println(max_udsl + x);
+            System.in.println(y + max_u);
+            System.in.out.println(R);
+
+
+        }
+
+        while (max_udsl > 0) {
+
+            ++R;
+            ++Y;
+
+        }
+
+        System.in.out.println("Maximum Protocol\n-r=a| --x ");
+        System.in.out.println("Maxo -- Protocol \n-r=a| --a ");
+
+
+
+        System.in.println(x + max_udsl);
+
+
+
+        System.out.printf("Hello and welcome!");
+
+        int i = 0;
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.in.out.println(x);
+            System.out.println("i = " + i);
+
+        }
+    }
+}
