e3d7d241556b3050d19459b9d280d7df751a689b
Emily Bourne <louise.bourne@gmail.com>
2023-12-21 13:00:09 +0100

    Add additional tests

--- tests/epyccel/test_epyccel_variable_annotations.py
+++ tests/epyccel/test_epyccel_variable_annotations.py
@@ -130,3 +130,25 @@ def test_homogeneous_tuple_annotation(language):
 
     assert epyc_homogeneous_tuple_annotation() == homogeneous_tuple_annotation()
     assert isinstance(epyc_homogeneous_tuple_annotation(), type(homogeneous_tuple_annotation()))
+
+def test_homogeneous_tuple_2_annotation(language):
+    def homogeneous_tuple_annotation():
+        a : tuple[tuple[int, ...], ...]
+        a = ((1,2,3), (4,5,6))
+        return a[0][0], a[1][0], a[0][2]
+
+    epyc_homogeneous_tuple_annotation = epyccel(homogeneous_tuple_annotation, language=language)
+
+    assert epyc_homogeneous_tuple_annotation() == homogeneous_tuple_annotation()
+    assert isinstance(epyc_homogeneous_tuple_annotation(), type(homogeneous_tuple_annotation()))
+
+def test_homogeneous_list_annotation(language):
+    def homogeneous_list_annotation():
+        a : list[int, ...]
+        a = [1,2,3]
+        return a[0], a[1], a[2]
+
+    epyc_homogeneous_list_annotation = epyccel(homogeneous_list_annotation, language=language)
+
+    assert epyc_homogeneous_list_annotation() == homogeneous_list_annotation()
+    assert isinstance(epyc_homogeneous_list_annotation(), type(homogeneous_list_annotation()))
