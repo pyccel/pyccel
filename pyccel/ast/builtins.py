2038303603b4fa2dbe4715358062c379f6c2b684
Emily Bourne <louise.bourne@gmail.com>
2023-12-22 17:46:44 +0100

    Unused import

--- pyccel/ast/builtins.py
+++ pyccel/ast/builtins.py
@@ -25,7 +25,7 @@ from .literals  import Literal, LiteralImaginaryUnit, convert_to_literal
 from .literals  import LiteralString
 from .operators import PyccelAdd, PyccelAnd, PyccelMul, PyccelIsNot
 from .operators import PyccelMinus, PyccelUnarySub, PyccelNot
-from .variable  import IndexedElement, InhomogeneousTupleVariable, Variable
+from .variable  import IndexedElement, InhomogeneousTupleVariable
 
 pyccel_stage = PyccelStage()
 
