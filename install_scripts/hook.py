c229133c2081644b37ae9de07d005a11daea0f86
EmilyBourne <louise.bourne@gmail.com>
2024-01-03 17:19:34 +0100

    Add docstrings and delete old files before rebuilding .pyccel files

--- install_scripts/hook.py
+++ install_scripts/hook.py
@@ -1,3 +1,5 @@
+""" A script to provide a hook which allows artifacts to be generated during installation of the package.
+"""
 import os
 import subprocess
 import sys
@@ -5,11 +7,53 @@ import sys
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class CustomBuildHook(BuildHookInterface):
+    """
+    A class which allows code to be run while building the package.
+
+    A class inheriting from BuildHookInterface which allows code to be run to create artifacts during the build stage
+    of the package installation using hatch.
+    See <https://hatch.pypa.io/latest/plugins/build-hook/reference> for more details.
+
+    Parameters
+    ----------
+    root : str
+        The root folder for the package being installed.
+    config : dict
+        See hatch documentation.
+    build_config : BuilderConfigBound
+        See hatch documentation.
+    metadata : ProjectMetadata
+        See hatch documentation.
+    directory : str
+        See hatch documentation.
+    target_name : str
+        The name of the target being built.
+    app : Application
+        See hatch documentation.
+    """
 
     def initialize(self, version, build_data):
+        """
+        Script run before creating the build target.
+
+        See <https://hatch.pypa.io/latest/plugins/build-hook/reference/#hatchling.builders.hooks.plugin.interface.BuildHookInterface.initialize>.
+
+        Parameters
+        ----------
+        version : str
+            See hatch documentation.
+
+        build_data : dict
+            See hatch documentation.
+        """
+        folder = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','pyccel','stdlib','internal'))
+        files_stubs = ['blas', 'dfftpack', 'fitpack',
+                'lapack', 'mpi', 'openacc', 'openmp']
+        output_files = [os.path.join(folder, f)+'.pyccel' for f in files_stubs]
+        for f in output_files:
+            if os.path.isfile(f):
+                os.remove(f)
+
         subprocess.run([sys.executable, "-c", "from pyccel.commands.pyccel_init import pyccel_init; pyccel_init()"],
                 cwd = self.root, check=True)
-        folder = os.path.abspath(os.path.join(os.path.dirname(__file__),'..','pyccel','stdlib','internal'))
-        files = ['blas', 'dfftpack', 'fitpack',
-                'lapack', 'mpi', 'openacc', 'openmp']
-        build_data['artifacts'].extend(os.path.join(folder, f'{f}.pyccel') for f in files)
+        build_data['artifacts'].extend(output_files)
