bc3e5db18cedde4d8b49d3681e4320ef90ca24f1
El alj Mouad <mouadelalj@gmail.com>
2024-01-15 13:28:49 +0100

    add language and compiler support, created CudaCodePrinter

---
+++ pyccel/codegen/printing/cucode.py
@@ -0,0 +1,78 @@
+# coding: utf-8
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+
+from pyccel.codegen.printing.ccode import CCodePrinter, c_imports
+
+from pyccel.errors.errors   import Errors
+from pyccel.errors.messages import (PYCCEL_RESTRICTION_TODO, INCOMPATIBLE_TYPEVAR_TO_FUNC,
+                                    PYCCEL_RESTRICTION_IS_ISNOT, UNSUPPORTED_ARRAY_RANK)
+
+
+errors = Errors()
+
+# TODO: add examples
+
+__all__ = ["CudaCodePrinter", "cucode"]
+
+class CudaCodePrinter(CCodePrinter):
+    """
+    A printer for printing code in Cuda.
+
+    A printer to convert Pyccel's AST to strings of cuda code.
+    As for all printers the navigation of this file is done via _print_X
+    functions.
+
+    Parameters
+    ----------
+    filename : str
+            The name of the file being pyccelised.
+    prefix_module : str
+            A prefix to be added to the name of the module.
+    """
+    printmethod = "_cucode"
+    language = "cuda"
+
+    def __init__(self, filename, prefix_module = None):
+
+        errors.set_target(filename, 'file')
+
+        super().__init__(filename)
+        self.prefix_module = prefix_module
+        self._additional_imports = {'stdlib':c_imports['stdlib']}
+        self._additional_code = ''
+        self._additional_args = []
+        self._temporary_args = []
+        self._current_module = None
+        self._in_header = False
+
+
+
+def cucode(expr, filename, assign_to=None, **settings):
+    """Converts an expr to a string of cuda code
+
+    expr : Expr
+        A pyccel expression to be converted.
+    filename : str
+        The name of the file being translated. Used in error printing
+    assign_to : optional
+        When given, the argument is used as the name of the variable to which
+        the expression is assigned. Can be a string, ``Symbol``,
+        ``MatrixSymbol``, or ``Indexed`` type. This is helpful in case of
+        line-wrapping, or for expressions that generate multi-line statements.
+    precision : integer, optional
+        The precision for numbers such as pi [default=15].
+    user_functions : dict, optional
+        A dictionary where keys are ``FunctionClass`` instances and values are
+        their string representations. Alternatively, the dictionary value can
+        be a list of tuples i.e. [(argument_test, cfunction_string)]. See below
+        for examples.
+    dereference : iterable, optional
+        An iterable of symbols that should be dereferenced in the printed code
+        expression. These would be values passed by address to the function.
+        For example, if ``dereference=[a]``, the resulting code would print
+        ``(*a)`` instead of ``a``.
+    """
+    return CudaCodePrinter(filename, **settings).doprint(expr, assign_to)
---
+++ pyccel/naming/cudanameclashchecker.py
@@ -0,0 +1,69 @@
+# coding: utf-8
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+"""
+Handles name clash problems in Ccuda
+"""
+from pyccel.utilities.metaclasses import Singleton
+from pyccel.utilities.strings import create_incremented_string
+
+class CudaNameClashChecker(metaclass = Singleton):
+    """ Class containing functions to help avoid problematic names in Ccuda
+    """
+    # Keywords as mentioned on https://en.cppreference.com/w/c/keyword
+    keywords = set(['auto', 'break', 'case', 'char', 'const', 'bool',
+        'continue', 'default', 'do', 'double', 'else', 'enum',
+        'extern', 'float', 'for', 'goto', 'if', 'inline', 
+        'int', 'int8_t', 'int16_t', 'int32_t', 'int64_t',
+        'long', 'register', 'restrict', 'return', 'short', 'signed',
+        'sizeof', 'static', 'struct', 'switch', 'typedef', 'union',
+        'unsigned', 'void', 'volatile', 'while', '_Alignas',
+        '_Alignof', '_Atomic', '_Bool', '_Complex', 'Decimal128',
+        '_Decimal32', '_Decimal64', '_Generic', '_Imaginary', '__global__',
+        '_Noreturn', '_Static_assert', '_Thread_local', 't_ndarray',
+        'array_create', 'new_slice', 'array_slicing', 'alias_assign',
+        'transpose_alias_assign', 'array_fill', 't_slice',
+        'GET_INDEX_EXP1', 'GET_INDEX_EXP2', 'GET_INDEX_EXP2',
+        'GET_INDEX_EXP3', 'GET_INDEX_EXP4', 'GET_INDEX_EXP5',
+        'GET_INDEX_EXP6', 'GET_INDEX_EXP7', 'GET_INDEX_EXP8',
+        'GET_INDEX_EXP9', 'GET_INDEX_EXP10', 'GET_INDEX_EXP11',
+        'GET_INDEX_EXP12', 'GET_INDEX_EXP13', 'GET_INDEX_EXP14',
+        'GET_INDEX_EXP15', 'NUM_ARGS_H1', 'NUM_ARGS',
+        'GET_INDEX_FUNC_H2', 'GET_INDEX_FUNC', 'GET_INDEX',
+        'INDEX', 'GET_ELEMENT', 'free_array', 'free_pointer',
+        'get_index', 'numpy_to_ndarray_strides',
+        'numpy_to_ndarray_shape', 'get_size',
+        'cuda_free_array', 'cuda_free_pointer', 'cuda_array_create', 
+        'threadIdx', 'blockIdx', 'blockDim', 'gridDim', 
+        'cuda_array_fill_double', 'cuda_array_fill_int64', 
+        'cuda_array_fill_int32', 'cuda_array_fill_int8',
+        'cuda_array_arange_double', 'cuda_array_arange_int64', 
+        'cuda_array_arange_int32', 'cuda_array_arange_int8',
+        'cudaMallocManaged', 'cudaSynchronize'])
+
+    def has_clash(self, name, symbols):
+        """ Indicate whether the proposed name causes any clashes
+        """
+        return any(name == k for k in self.keywords) or \
+               any(name == s for s in symbols)
+
+    def get_collisionless_name(self, name, symbols):
+        """ Get the name that will be used in the fortran code
+        """
+        if len(name)>4 and all(name[i] == '_' for i in (0,1,-1,-2)):
+            # Ignore magic methods
+            return name
+        if name[0] == '_':
+            name = 'private'+name
+        prefix = name
+        coll_symbols = self.keywords.copy()
+        coll_symbols.update(s.lower() for s in symbols)
+        if prefix in coll_symbols:
+            counter = 1
+            new_name, counter = create_incremented_string(coll_symbols,
+                    prefix = prefix, counter = counter)
+            name = name+new_name[-5:]
+        return name
+
