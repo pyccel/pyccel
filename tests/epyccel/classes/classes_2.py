349e507d6a38f83f5f14d8feb91846c03e417b33
EmilyBourne <louise.bourne@gmail.com>
2024-01-18 15:53:06 +0100

    Add tests

---
+++ tests/epyccel/classes/classes_2.py
@@ -0,0 +1,12 @@
+# pylint: disable=missing-class-docstring,  missing-function-docstring, missing-module-docstring
+import numpy as np
+
+class Point:
+    def __init__(self : 'Point'):
+        pass
+
+    def addition(self : 'Point', a : float, b : float):
+        return a + b
+
+    def subtraction(self : 'Point', a : 'float[:]', b : 'float[:]'):
+        return a - b
