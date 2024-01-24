a7a6bfbd2388341129e19c1277acd1415bf67f28
EmilyBourne <louise.bourne@gmail.com>
2024-01-24 17:29:57 +0100

    Add test

---
+++ tests/errors/semantic/blocking/pointer_to_temporary.py
@@ -0,0 +1,30 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
+from numpy import ones
+
+class Point(object):
+    def __init__(self : 'Point', x : 'float[:]'):
+        self.x = x
+
+    def __del__(self : 'Point'):
+        pass
+
+    def translate(self : 'Point', a : 'float[:]'):
+        self.x = self.x + a
+
+
+class Points(object):
+    def __init__(self : 'Points', x : Point):
+        self.x = x
+
+    def __del__(self : 'Points'):
+        pass
+
+x = ones(4)
+P1 = Point(x)
+P2 = Points(P1)
+P3 = P2.x
+P4 = P2
+P5 = P2.x.x
+print(x,P5)
+
+
