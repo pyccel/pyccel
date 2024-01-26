9e8be097d5699ff114b16ca424c4a4e62fdfaef6
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-01-26 10:27:47 +0100

    getting last devel updates into my branch

---
+++ pyccel/codegen/fech.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+a = np.array([42])
+# b = np.array([1, 2, 3, 4, 5])
+# c = np.array([[1, 2, 3], [4, 5, 6]])
+# d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+# print(a.ndim)
+# print(b.ndim)
+# print(c.ndim)
+# print(d.ndim) 
