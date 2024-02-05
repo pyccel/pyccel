441b7ca491d85e9299df696649357f0f22a2b120
El alj Mouad <mouadelalj@gmail.com>
2024-02-05 13:37:20 +0100

    fixing cudanameclashchecker

--- pyccel/naming/cudanameclashchecker.py
+++ pyccel/naming/cudanameclashchecker.py
@@ -4,22 +4,17 @@
 # go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
 #------------------------------------------------------------------------------------------#
 """
-Handles name clash problems in Ccuda.
-
-This modules contains the reserved names for the Cuda language or the internal function
-that are used by pyccel.
+Handles name clash problems in Cuda
 """
-from pyccel.utilities.metaclasses import Singleton
-from pyccel.utilities.strings import create_incremented_string
+from .languagenameclashchecker import LanguageNameClashChecker
 
-class CudaNameClashChecker(metaclass = Singleton):
+class CudaNameClashChecker(LanguageNameClashChecker):
     """
-    Class containing functions to help avoid problematic names in Fortran.
+    Class containing functions to help avoid problematic names in Cuda.
 
     A class which provides functionalities to check or propose variable names and
     verify that they do not cause name clashes. Name clashes may be due to
-    capitalisation (as Fortran is not case-sensitive), or due to the use of reserved
-    keywords.
+    new variables, or due to the use of reserved keywords.
     """
     # Keywords as mentioned on https://en.cppreference.com/w/c/keyword
     keywords = set(['isign', 'fsign', 'csign', 'auto', 'break', 'case', 'char', 'const',
@@ -45,33 +40,18 @@ class CudaNameClashChecker(metaclass = Singleton):
         'numpy_to_ndarray_shape', 'get_size', 'order_f', 'order_c', 'array_copy_data'])
 
     def has_clash(self, name, symbols):
-        """
-        Indicate whether the proposed name causes any clashes.
-
-        Check if the name has any clash with the symbols or the keywords.
-
-        Parameters
-        ----------
-        name : str
-            The suggested name.
-        symbols : set
-            Symbols which should be considered as collisions.
-
-        Returns
-        -------
-        str
-            A new name which is collision free.
+        """ Indicate whether the proposed name causes any clashes
         """
         return any(name == k for k in self.keywords) or \
                any(name == s for s in symbols)
 
     def get_collisionless_name(self, name, symbols):
         """
-        Get a valid name which doesn't collision with symbols or C keywords.
+        Get a valid name which doesn't collision with symbols or Cuda keywords.
 
         Find a new name based on the suggested name which will not cause
-        conflicts with C keywords, does not appear in the provided symbols,
-        and is a valid name in C code.
+        conflicts with Cuda keywords, does not appear in the provided symbols,
+        and is a valid name in Cuda code.
 
         Parameters
         ----------
@@ -91,4 +71,3 @@ class CudaNameClashChecker(metaclass = Singleton):
         if name[0] == '_':
             name = 'private'+name
         return self._get_collisionless_name(name, symbols)
-
