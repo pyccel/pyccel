7f9caa60f965478b9ed308b3eaf822ba23529848
El alj Mouad <mouadelalj@gmail.com>
2024-02-07 12:21:06 +0100

    added a program test in the codegen for cucode

---
+++ tests/codegen/cucode/scripts/prog.py
@@ -0,0 +1,7 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring
+#==============================================================================
+
+if __name__ == "__main__":
+    a = 191
+    b = 7
+    b = b*a
