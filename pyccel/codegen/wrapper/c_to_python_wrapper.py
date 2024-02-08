e3e70d472888602837b735975e67d7a85697cf86
EmilyBourne <louise.bourne@gmail.com>
2024-02-08 13:14:00 +0100

    Ensure corect assgn

--- pyccel/codegen/wrapper/c_to_python_wrapper.py
+++ pyccel/codegen/wrapper/c_to_python_wrapper.py
@@ -1172,7 +1172,9 @@ class CToPythonWrapper(Wrapper):
             body.append(FunctionCall(expr, func_call_args))
         elif n_c_results == 1:
             res = c_results[0]
-            if original_func.results[0].var.is_alias:
+            if original_func.results[0].var.is_alias and not is_bind_c_function_def:
+                if isinstance(res, PointerCast):
+                    res = res.obj
                 body.append(AliasAssign(res, FunctionCall(expr, func_call_args)))
             else:
                 body.append(Assign(res, FunctionCall(expr, func_call_args)))
