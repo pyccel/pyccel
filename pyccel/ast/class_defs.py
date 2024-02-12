8d649bfdb127979ea3223f0bfc80ba57ca5efacc
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2024-02-12 11:12:01 +0100

    Remove parentheses

--- pyccel/ast/class_defs.py
+++ pyccel/ast/class_defs.py
@@ -6,7 +6,7 @@
 This module contains all types which define a python class which is automatically recognised by pyccel
 """
 
-from pyccel.ast.builtin_objects.list_functions import (ListPop)
+from pyccel.ast.builtin_objects.list_functions import ListPop
 from .builtins  import PythonImag, PythonReal, PythonConjugate
 from .core      import ClassDef, PyccelFunctionDef
 from .c_concepts import CStackArray
