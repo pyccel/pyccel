8e921a7c9a042604974a2e1d53a18cfc8b3e8d3f
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2024-02-12 11:29:51 +0100

    Save the results to avoid repeating calls in the test functions

--- tests/epyccel/test_epyccel_lists.py
+++ tests/epyccel/test_epyccel_lists.py
@@ -15,12 +15,14 @@ from  pyccel.epyccel import epyccel
 )
 
 def test_pop_last_element(language) :
-    def po():
+    def pop_last_element():
         a = [1,3,45]
         return a.pop()
-    re = epyccel(po, language = language)
-    assert isinstance(po(), type(re()))
-    assert po() == re()
+    epyc_last_element = epyccel(pop_last_element, language = language)
+    pyccel_result = epyc_last_element()
+    python_result = pop_last_element()
+    assert isinstance(python_result, type(pyccel_result))
+    assert python_result == pyccel_result
 
 @pytest.mark.parametrize( 'language', (
         pytest.param("fortran", marks = [
@@ -35,12 +37,14 @@ def test_pop_last_element(language) :
 )
 
 def test_pop_specific_index(language) :
-    def po():
+    def pop_specific_index():
         a = [1j,3j,45j]
         return a.pop(1)
-    re = epyccel(po, language = language)
-    assert isinstance(po(), type(re()))
-    assert po() == re()
+    epyc_specific_index = epyccel(pop_specific_index, language = language)
+    python_result = pop_specific_index()
+    pyccel_result = epyc_specific_index()
+    assert isinstance(python_result, type(pyccel_result))
+    assert python_result == pyccel_result
 
 @pytest.mark.parametrize( 'language', (
         pytest.param("fortran", marks = [
@@ -55,12 +59,14 @@ def test_pop_specific_index(language) :
 )
 
 def test_pop_negative_index(language) :
-    def po():
+    def pop_negative_index():
         a = [1j,3j,45j]
         return a.pop(-1)
-    re = epyccel(po, language = language)
-    assert isinstance(po(), type(re()))
-    assert po() == re()
+    epyc_negative_index = epyccel(pop_negative_index, language = language)
+    python_result = pop_negative_index()
+    pyccel_result = epyc_negative_index()
+    assert isinstance(python_result, type(pyccel_result))
+    assert python_result == pyccel_result
 
 @pytest.mark.parametrize( 'language', (
         pytest.param("fortran", marks = [
@@ -75,10 +81,12 @@ def test_pop_negative_index(language) :
 )
 
 def test_pop_2(language) :
-    def po():
+    def pop_2():
         a = [1.7,2.7,45.0]
         a.pop()
         return a.pop(-1)
-    re = epyccel(po, language = language)
-    assert isinstance(po(), type(re()))
-    assert po() == re()
+    pop_2_epyc = epyccel(pop_2, language = language)
+    python_result = pop_2()
+    pyccel_result = pop_2_epyc()
+    assert isinstance(python_result, type(pyccel_result))
+    assert python_result == pyccel_result
