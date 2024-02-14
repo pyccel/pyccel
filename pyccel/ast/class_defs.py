171959d2b18275af819ccdfc36a0a3618a261fec
Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
2024-02-14 16:00:23 +0100

    add ListExtend method to ListClass ClassDef

--- pyccel/ast/class_defs.py
+++ pyccel/ast/class_defs.py
@@ -6,7 +6,7 @@
 This module contains all types which define a python class which is automatically recognised by pyccel
 """
 
-from pyccel.ast.builtin_methods.list_methods import ListAppend, ListInsert
+from pyccel.ast.builtin_methods.list_methods import ListAppend, ListInsert, ListExtend
 
 
 from .builtins  import PythonImag, PythonReal, PythonConjugate
@@ -140,6 +140,8 @@ ListClass = ClassDef('list', class_type = NativeHomogeneousList(),
             PyccelFunctionDef('append', func_class = ListAppend,
                 decorators = {}),
             PyccelFunctionDef('insert', func_class = ListInsert,
+                decorators = {}),
+            PyccelFunctionDef('extend', func_class = ListExtend,
                 decorators = {}),
         ])
 
