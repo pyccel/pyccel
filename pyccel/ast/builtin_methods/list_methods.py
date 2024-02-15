a563d080794af0ee722c86043e4bcc417d064ce9
Mustapha Belbiad <67117140+mustapha-belbiad@users.noreply.github.com>
2024-02-15 18:43:05 +0100

    List support: pop method (#1710)
    Add support for the pop() method of Python lists to the semantic stage.
    Add handling of that method to the Python printer. This fixes #1690.
    
    ---------
    
    Co-authored-by: Farouk-Echaref <farouk.echcharef@usmba.ac.ma>
    Co-authored-by: EmilyBourne <louise.bourne@gmail.com>

--- pyccel/ast/builtin_methods/list_methods.py
+++ pyccel/ast/builtin_methods/list_methods.py
@@ -14,7 +14,7 @@ from pyccel.ast.datatypes import NativeVoid, NativeGeneric, NativeHomogeneousLis
 from pyccel.ast.internals import PyccelInternalFunction
 
 
-__all__ = ('ListAppend',)
+__all__ = ('ListAppend','ListPop')
 
 
 class ListAppend(PyccelInternalFunction):
@@ -79,3 +79,54 @@ class ListAppend(PyccelInternalFunction):
         Get the argument which is passed to append().
         """
         return self._append_arg
+
+#==============================================================================
+class ListPop(PyccelInternalFunction) :
+    """
+    Represents a call to the .pop() method.
+    
+    Represents a call to the .pop() method which
+    removes the item at the specified index. 
+    The method also returns the removed item.
+
+    Parameters
+    ----------
+    list_variable : TypedAstNode
+        The name of the list.
+
+    index_element : TypedAstNode
+        The current index value for the element to be popped.
+    """
+    __slots__ = ('_dtype','_precision', '_index','_list_variable')
+    _attribute_nodes = ('_index','_list_variable')
+    _rank = 0
+    _order = None
+    _shape = None
+    _class_type = NativeHomogeneousList()
+    name = 'pop'
+
+    def __init__(self, list_variable, index_element=None):
+        self._index = index_element
+        self._list_variable = list_variable
+        self._dtype = list_variable.dtype
+        self._precision = list_variable.precision
+        super().__init__()
+
+    @property
+    def pop_index(self):
+        """
+        The current index value for the element to be popped.
+
+        The current index value for the element to be popped.
+        """
+        return self._index
+
+    @property
+    def list_variable(self):
+        """
+        Provide the name of the list as the return value.
+        
+        Provide the name of the list as the return value.
+        """
+        return self._list_variable
+
