da5fc01b54798844bea411e71adaa5a11d75ce14
said <said-ctb@live.fr>
2024-02-17 00:00:15 +0100

    making function uniontype error blocking

--- tests/errors/semantic/non_blocking/INCOMPATIBLE_ARGUMENT.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring
-
-def f(x : int):
-    y = x + 1
-    return y
-
-z = f(2.3)
-print(z)
--- tests/errors/semantic/non_blocking/INCOMPATIBLE_ARGUMENT2.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring
-
-def f(x : 'int[:]'):
-    y = x + 1
-    return y[0]
-
-x = 3.9
-z = f(x)
-print(z)
