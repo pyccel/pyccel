3b59ce017ca3594bf48e5d344745ab9848bee346
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2024-02-21 11:07:15 +0100

    add method for sets

---
+++ pyccel/ast/builtin_methods/set_methods.py
@@ -0,0 +1,75 @@
+# coding: utf-8
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
+#------------------------------------------------------------------------------------------#
+"""
+The Set container has a number of built-in methods that are 
+always available.
+
+This module contains objects which describe these methods within Pyccel's AST.
+"""
+from pyccel.ast.datatypes import NativeVoid, NativeGeneric, NativeHomogeneousSet
+from pyccel.ast.internals import PyccelInternalFunction
+from pyccel.ast.builtins import PythonTuple
+
+__all__ = ('SetAdd',)
+
+class SetAdd(PyccelInternalFunction) :
+    """
+    Represents a call to the .add() method.
+    
+    Represents a call to the .add() method which add an element
+    to the set if it's not already present.
+    If the element is already in the set, the set remains unchanged.
+    Parameters
+    ----------
+    set_variable : TypedAstNode
+        The name of the set.
+
+    index_element : TypedAstNode
+        The current index value for the element to be add.
+    """
+    __slots__ = ("_set_variable", "_add_arg")
+    _attribute_nodes = ("_set_variable", "_add_arg")
+    _dtype = NativeVoid()
+    _shape = None
+    _order = None
+    _rank = 0
+    _precision = None
+    _class_type = NativeVoid()
+    name = 'add'
+
+    def __init__(self, set_variable, new_elem) -> None:
+        if (new_elem.rank > 0 and not isinstance(new_elem, PythonTuple)) :
+                raise TypeError(f"Can't add an unhashable type")
+        is_homogeneous = (
+            new_elem.dtype is not NativeGeneric() and
+            set_variable.dtype is not NativeGeneric() and
+            set_variable.dtype == new_elem.dtype and
+            set_variable.precision == new_elem.precision and
+            set_variable.rank - 1 == new_elem.rank
+        )
+        if not is_homogeneous:
+            raise TypeError("Expecting an argument of the same type as the elements of the set")
+        self._set_variable = set_variable
+        self._add_arg = new_elem
+        super().__init__()
+
+    @property
+    def set_variable(self):
+        """
+        Get the variable representing the set.
+
+        Get the variable representing the set.
+        """
+        return self._set_variable
+
+    @property
+    def add_argument(self):
+        """
+        Get the argument which is passed to add().
+
+        Get the argument which is passed to add().
+        """
+        return self._add_arg
