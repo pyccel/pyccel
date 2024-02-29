ffc6ea9442e09603a0bf75998fa847d6b97c81fb
said mazouz <smazouz@student.1337.ma>
2024-02-29 13:31:43 +0000

    trying to fix documentation errors in CUDA module classes

--- pyccel/ast/cuda.py
+++ pyccel/ast/cuda.py
@@ -14,24 +14,27 @@ __all__ = (
     'KernelCall',
     'IndexedFunctionCall'
 )
+
 class KernelCall(FunctionCall):
     """
     Represents a kernel function call in the code.
-    # Example of a method call with array-like indexing: a[c, b]().
-    
+
     Parameters
-    ==========
-    numBlocks : NativeInteger
-        The number of blocks.
-                   
-    tpblock : NativeInteger
-        The number of threads per block.
-                   
+    ----------
     func : FunctionDef
         The definition of the function being called.
-                   
+
     args : tuple
         The arguments being passed to the function.
+
+    numBlocks : NativeInteger
+        The number of blocks.
+
+    tpblock : NativeInteger
+        The number of threads per block.
+
+    launch_config : object
+        Launch configuration of kernel call.
     """
     __slots__ = ('_numBlocks','_tpblock','_func', '_args', '_launch_config')
     _attribute_nodes = (*FunctionCall._attribute_nodes, '_numBlocks', '_tpblock', '_launch_config')
@@ -43,27 +46,23 @@ class KernelCall(FunctionCall):
 
     @property
     def numBlocks(self):
-        """ The number of blocks in the kernel being called
-        """
+        """ The number of blocks in the kernel being called. """
         return self._numBlocks
+
     @property
     def tpblock(self):
-        """ The number of threads per block.
-        """
+        """ The number of threads per block. """
         return self._tpblock
+
     @property
     def launch_config(self):
-        """ launch configuration of kernel call.
-        """
+        """ Launch configuration of kernel call. """
         return self._launch_config
 
 class IndexedFunctionCall(FunctionCall):
     """
-    represents a indexed funtion call in the code 
-    
-    A node with hild all information necessary to represent a indexed function
-    call in the code
-    
+    Represents an indexed function call in the code.
+
     Parameters
     ----------
     func : FunctionDef
@@ -71,6 +70,9 @@ class IndexedFunctionCall(FunctionCall):
 
     args : list of FunctionCallArgument
         The arguments passed to the function.
+
+    launch_config : object
+        Launch configuration of kernel call.
     """
     __slots__ = ('_launch_config',)
     def __init__(self, func, args, launch_config):
@@ -81,6 +83,5 @@ class IndexedFunctionCall(FunctionCall):
 
     @property
     def launch_config(self):
-        """ launch configuration of kernel call.
-        """
+        """ Launch configuration of kernel call. """
         return self._launch_config
