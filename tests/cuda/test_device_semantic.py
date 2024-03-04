c3008b6709beca80611da1c6bfa4b3cc51d006d4
said mazouz <smazouz@student.1337.ma>
2024-03-04 11:40:35 +0000

    feat: handle device not called from non-kernel or non-device and write a test for that

---
+++ tests/cuda/test_device_semantic.py
@@ -0,0 +1,31 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring
+import pytest
+
+from pyccel.epyccel import epyccel
+from pyccel.decorators import kernel, device
+from pyccel.errors.errors import Errors, PyccelSemanticError
+from pyccel.errors.messages import (MISSING_KERNEL_CONFIGURATION,
+                                    )
+
+@pytest.mark.cuda
+def test_invalid_launch_configuration():
+    def invalid_launch_configuration():
+        @device
+        def add_one(x : int)
+            return x + 1
+        @kernel
+        def kernel_call():
+            pass
+        x = add_one(1)
+        kernel_call[1, 1]()
+
+    errors = Errors()
+
+    with pytest.raises(PyccelSemanticError):
+        epyccel(invalid_block_number, language="cuda")
+
+    assert errors.has_errors()
+    assert errors.num_messages() == 1
+
+    error_info = [*errors.error_info_map.values()][0][0]
+    assert MISSING_KERNEL_CONFIGURATION == error_info.message
