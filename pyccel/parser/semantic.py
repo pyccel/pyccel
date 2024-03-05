adc22f5d1cff3bdfa32749149f11a67630fc58a2
said mazouz <smazouz@student.1337.ma>
2024-03-05 09:06:57 +0000

    remove extra line in handle kernel

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -1197,7 +1197,6 @@ class SemanticParser(BasicParser):
             return new_expr
 
     def _handle_kernel(self, expr, func, args, **settings):
-
         """
         Create the node representing the kernel function call.
 
