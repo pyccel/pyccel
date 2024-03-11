8e513535f2dbb5d46bf9ea37a156c1732645781b
mustapha-belbiad <mustapha.belbiad10@gmail.com>
2024-03-11 10:27:57 +0000

    Solve Codacy Problem: Unused importe

--- pyccel/ast/builtin_methods/set_methods.py
+++ pyccel/ast/builtin_methods/set_methods.py
@@ -11,7 +11,6 @@ This module contains objects which describe these methods within Pyccel's AST.
 """
 from pyccel.ast.datatypes import NativeVoid, NativeGeneric
 from pyccel.ast.internals import PyccelInternalFunction
-from pyccel.ast.builtins import PythonRange
 
 __all__ = ('SetAdd', 'SetClear', 'SetMethod', 'SetRemove')
 
