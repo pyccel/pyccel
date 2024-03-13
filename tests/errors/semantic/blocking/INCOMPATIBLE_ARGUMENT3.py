35415b5dad3eb8237a82373ed65391632fd5b84b
Said Hadjout <said-ctb@live.fr>
2024-03-13 15:36:39 +0100

    Update tests/errors/semantic/blocking/INCOMPATIBLE_ARGUMENT3.py
    Co-authored-by: Yaman Güçlü <yaman.guclu@gmail.com>

--- tests/errors/semantic/blocking/INCOMPATIBLE_ARGUMENT3.py
+++ tests/errors/semantic/blocking/INCOMPATIBLE_ARGUMENT3.py
@@ -1,8 +1,9 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 
 from pyccel.decorators import inline
-@inline
-def add(a:int,b:'int|complex'):
-    return a+b
 
-z = add(3.,2.)
+@inline
+def add(a: int, b: 'int | complex'):
+    return a + b
+
+z = add(3., 2.)
