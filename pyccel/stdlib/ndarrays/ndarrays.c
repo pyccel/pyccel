37a8af9a14ba9c7226d02cb2b58906740a94ce0f
Varadarajan R <varadarajanrengaraj@outlook.com>
2024-03-14 16:49:07 +0100

    address review comments and add test cases to test all possible code path

--- pyccel/stdlib/ndarrays/ndarrays.c
+++ pyccel/stdlib/ndarrays/ndarrays.c
@@ -512,96 +512,96 @@ bool is_same_shape(t_ndarray a, t_ndarray b)
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (int8_t)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_int8[i + offset] = (int8_t)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (int8_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_int8[element_index(*dest, i, dest->nd) + offset] = (int8_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
             case nd_int16: \
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (int16_t)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_int16[i + offset] = (int16_t)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (int16_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_int16[element_index(*dest, i, dest->nd) + offset] = (int16_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
             case nd_int32: \
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (int32_t)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_int32[i + offset] = (int32_t)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (int32_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_int32[element_index(*dest, i, dest->nd) + offset] = (int32_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
             case nd_int64: \
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (int64_t)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_int64[i + offset] = (int64_t)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (int64_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_int64[element_index(*dest, i, dest->nd) + offset] = (int64_t)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
             case nd_float: \
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (float)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_float[i + offset] = (float)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (float)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_float[element_index(*dest, i, dest->nd) + offset] = (float)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
             case nd_double: \
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (double)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_double[i + offset] = (double)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (double)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_double[element_index(*dest, i, dest->nd) + offset] = (double)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
             case nd_cfloat: \
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (float complex)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_cfloat[i + offset] = (float complex)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (float complex)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_cfloat[element_index(*dest, i, dest->nd) + offset] = (float complex)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
             case nd_cdouble: \
                 if(elem_wise_cp == false)\
                 { \
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[i + offset] = (double complex)src.nd_##SRC_TYPE[i]; \
+                        dest->nd_cdouble[i + offset] = (double complex)src.nd_##SRC_TYPE[i]; \
                 }\
                 else \
                 {\
                     for(int64_t i = 0; i < src.length; i++) \
-                        dest->nd_bool[element_index(*dest, i, dest->nd) + offset] = (double complex)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
+                        dest->nd_cdouble[element_index(*dest, i, dest->nd) + offset] = (double complex)src.nd_##SRC_TYPE[element_index(src, i, src.nd)]; \
                 }\
                 break; \
         } \
