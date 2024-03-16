59fc8cd107373c456432b8a3c4d96223e372c682
EmilyBourne <louise.bourne@gmail.com>
2024-03-16 16:01:14 +0100

    Add test failing in main

--- tests/pyccel/scripts/print_tuples.py
+++ tests/pyccel/scripts/print_tuples.py
@@ -1,5 +1,9 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 # ------------------------------- Strings ------------------------------------
+
+def f():
+    return 1, True
+
 if __name__ == '__main__':
     print(())
     print((1,))
@@ -10,3 +14,4 @@ if __name__ == '__main__':
     print((1, True))
     print((1, False), sep=",")
     print((1, True), end="!\n")
+    print(f())
