aff55ca8830c05e5d0f24973e439ad47530868ac
EmilyBourne <louise.bourne@gmail.com>
2024-04-03 10:37:31 +0200

    Remove debug print

--- pyccel/ast/operators.py
+++ pyccel/ast/operators.py
@@ -941,7 +941,6 @@ class PyccelComparisonOperator(PyccelBinaryOperator):
         dtype = PythonNativeBool()
         possible_class_types = set(a.class_type for a in (arg1, arg2) \
                         if isinstance(a.class_type, ContainerType))
-        print(possible_class_types)
         if len(possible_class_types) == 0:
             class_type = dtype
         elif len(possible_class_types) == 1:
