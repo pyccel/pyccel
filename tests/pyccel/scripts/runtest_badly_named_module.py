bc973ccf1d7e5214d6be9f462a195aa587dbac2f
EmilyBourne <louise.bourne@gmail.com>
2024-04-25 19:01:36 +0200

    Add test

---
+++ tests/pyccel/scripts/runtest_badly_named_module.py
@@ -0,0 +1,9 @@
+import numpy as np
+from endif import matmul
+
+if __name__ == '__main__':
+    a = np.ones((3,4))
+    b = np.ones((4,3), order='F')
+    c = np.empty((3,3))
+    matmul(a, b, c)
+    print(c)
