4e28b31fa9986b14b0bddb74384a3881dfc191ea
EmilyBourne <louise.bourne@gmail.com>
2024-04-25 18:44:01 +0200

    Ensure Python name of module exists in the scope. Don't use AsName for module name.

--- pyccel/parser/syntactic.py
+++ pyccel/parser/syntactic.py
@@ -356,7 +356,8 @@ class SyntaxParser(BasicParser):
         # Define the name of the module
         # The module name allows it to be correctly referenced from an import command
         mod_name = os.path.splitext(os.path.basename(self._filename))[0]
-        name = AsName(mod_name, self.scope.get_new_name(mod_name))
+        self.scope.insert_symbol(mod_name)
+        name = self.scope.get_expected_name(mod_name)
 
         body = [b for i in body for b in (i.body if isinstance(i, CodeBlock) else [i])]
         return Module(name, [], functions, init_func = CodeBlock(body), scope = self.scope,
