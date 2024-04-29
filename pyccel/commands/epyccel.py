9dfc28254ff7ab4929a11d64e64846d40d28a7f6
EmilyBourne <louise.bourne@gmail.com>
2024-04-29 17:59:00 +0200

    Module docstring

--- pyccel/commands/epyccel.py
+++ pyccel/commands/epyccel.py
@@ -3,7 +3,8 @@
 # This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
 # go to https://github.com/pyccel/pyccel/blob/master/LICENSE for full license details.     #
 #------------------------------------------------------------------------------------------#
-
+""" File containing functions for calling Pyccel interactively (epyccel and epyccel_seq)
+"""
 
 import inspect
 import importlib
