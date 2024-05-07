6ca076a3f10541a3b759c9323347e4a211a3e829
EmilyBourne <louise.bourne@gmail.com>
2024-05-07 16:36:29 +0200

    Correct required workflows

--- ci_tools/wait_for_main_workflows.py
+++ ci_tools/wait_for_main_workflows.py
@@ -2,6 +2,8 @@ import os
 from bot_tools.github_api_interactions import GitHubAPIInteractions
 
 GAI = GitHubAPIInteractions()
-required_workflows = ('Pickled-installation', 'Wheel-pickled-installation')
+required_workflows = ('Anaconda-Linux', 'Anaconda-Windows', 'Intel unit tests',
+                      'Linux unit tests', 'MacOSX unit tests', 'Windows unit tests',
+                      'Pickled-installation', 'Wheel-pickled-installation')
 assert GAI.wait_for_runs(os.environ['COMMIT'], required_workflows)
 
