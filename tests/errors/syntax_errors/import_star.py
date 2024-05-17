869f607cba93b892e6a6735198b9d758cbcb3a83
EmilyBourne <louise.bourne@gmail.com>
2024-05-17 11:26:45 +0200

    Ignore wildcard-import error on wildcard-import test

--- tests/errors/syntax_errors/import_star.py
+++ tests/errors/syntax_errors/import_star.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring
+# pylint: disable=missing-function-docstring, missing-module-docstring, wildcard-import
 from nargs import *
 
 print(f())
