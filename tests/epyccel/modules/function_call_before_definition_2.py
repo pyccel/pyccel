41234f52181ede1c1f55c2e518c2fec016059520
EmilyBourne <louise.bourne@gmail.com>
2024-06-14 09:29:26 +0200

    Add test with renamed function call

--- tests/epyccel/modules/function_call_before_definition_2.py
+++ tests/epyccel/modules/function_call_before_definition_2.py
@@ -1,9 +1,12 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 def f():
-    return g()
+    return g() + do()
 
 def g():
     return 2
+
+def do():
+    return 3
 
 a = f()
 
