9376632c077388db60db9fd6aadec3ce1eed89cb
mustapha-belbiad <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>
2024-06-24 16:15:17 +0100

    Delete the .c file and implement the pop function in the header file.

--- pyccel/extensions/STC_Extensions/Set_extensions.h
+++ pyccel/extensions/STC_Extensions/Set_extensions.h
@@ -1,7 +1,14 @@
-#ifndef EXTENSIONS_H
-#define EXTENSIONS_H
+#ifndef SET_EXTENSIONS_H
+#define SET_EXTENSIONS_H
 
-#define Set_pop_macro(type, key) \
-    key Set_pop(type *l);
+
+#define _c_MEMB(name) c_JOIN(i_type, name)
+
+static inline i_key _c_MEMB(_pop)(i_type* self) {
+    _c_MEMB(_iter) itr = _c_MEMB(_begin)(self);
+    i_key value = *(itr.ref);
+    _c_MEMB(_erase_at)(self, itr);
+    return value;
+}
 
 #endif
