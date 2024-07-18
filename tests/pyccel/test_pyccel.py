a52f4a22a5f8eaeff0c9e63ffd44e89db06cb1ce
smazouz42 <smazouz@student.1337.ma>
2024-07-18 17:23:49 +0100

    debuging perpose

--- tests/pyccel/test_pyccel.py
+++ tests/pyccel/test_pyccel.py
@@ -62,8 +62,13 @@ def compile_pyccel(path_dir, test_file, options = ""):
     cmd = [shutil.which("pyccel"), test_file]
     if options != "":
         cmd += options.strip().split()
-    p = subprocess.Popen(cmd, universal_newlines=True, cwd=path_dir)
+    p = subprocess.Popen(cmd, universal_newlines=True, cwd=path_dir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     p.wait()
+    stdout, stderr = p.communicate()
+    if p.returncode != 0:
+        print(f"Command failed with return code {p.returncode}")
+        print(f"Standard Output:\n{stdout}")
+        print(f"Standard Error:\n{stderr}")
     # assert p.returncode==0
 
 #------------------------------------------------------------------------------
