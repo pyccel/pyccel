23ee44786cd13ba93efafa28a7944c3de8a652a2
EmilyBourne <louise.bourne@gmail.com>
2024-08-06 21:00:54 +0200

    Add more tests

--- tests/container_leaks/list_check.py
+++ tests/container_leaks/list_check.py
@@ -4,5 +4,16 @@ def allocate_list_of_list():
     a : 'list[list[int]]' = [[1, 2], [3,4]]
     print(a[0][0])
 
+def allocate_list_of_pointers():
+    a = [1,2,3]
+    b = [a]
+    print(b[0][0])
+
+def get_list_element():
+    a = [[[1,2], [3,4]], [[5,6], [7,8]]]
+    return a[1][0][1]
+
 if __name__ == '__main__':
     allocate_list_of_list()
+    allocate_list_of_pointers()
+    elem = get_list_element()
