6f299d1de71ecd4fa2e6112ced24d29104603646
EmilyBourne <louise.bourne@gmail.com>
2024-08-21 10:02:04 +0200

    Add declaration of CStackArray

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -21,6 +21,8 @@ from pyccel.ast.core      import Assign, Import, AugAssign, AliasAssign
 from pyccel.ast.core      import SeparatorComment
 from pyccel.ast.core      import Module, AsName
 
+from pyccel.ast.c_concepts import CStackArray
+
 from pyccel.ast.operators import PyccelAdd, PyccelMul, PyccelMinus, PyccelLt, PyccelGt
 from pyccel.ast.operators import PyccelAssociativeParenthesis, PyccelMod
 from pyccel.ast.operators import PyccelUnarySub, IfTernaryOperator
@@ -1342,6 +1344,8 @@ class CCodePrinter(CodePrinter):
             if isinstance(expr.class_type, (HomogeneousSetType, HomogeneousListType, DictType)):
                 dtype = self.get_c_type(expr.class_type)
                 return dtype
+            if isinstance(expr.class_type, CStackArray):
+                return self.get_c_type(expr.class_type.element_type)
             if isinstance(expr.class_type,(HomogeneousTupleType, NumpyNDArrayType)):
                 if expr.rank > 15:
                     errors.report(UNSUPPORTED_ARRAY_RANK, symbol=expr, severity='fatal')
@@ -1379,14 +1383,22 @@ class CCodePrinter(CodePrinter):
         return f'{ret_type} (*{name})({arg_code});\n'
 
     def _print_Declare(self, expr):
-        if isinstance(expr.variable, InhomogeneousTupleVariable):
-            return ''.join(self._print_Declare(Declare(v,intent=expr.intent, static=expr.static)) for v in expr.variable)
+        var = expr.variable
+        if isinstance(var, InhomogeneousTupleVariable):
+            return ''.join(self._print_Declare(Declare(v,intent=expr.intent, static=expr.static)) for v in var)
 
-        declaration_type = self.get_declare_type(expr.variable)
-        variable = self._print(expr.variable.name)
+        declaration_type = self.get_declare_type(var)
+        variable = self._print(var.name)
 
-        if expr.variable.is_stack_array:
-            preface, init = self._init_stack_array(expr.variable,)
+        if isinstance(var.class_type, CStackArray):
+            preface = ''
+            if isinstance(var.shape[0], int):
+                init = f'[{var.shape[0]}]'
+            else:
+                declaration_type += '*'
+                init = ''
+        elif var.is_stack_array:
+            preface, init = self._init_stack_array(var)
         elif declaration_type == 't_ndarray' and not self._in_header:
             preface = ''
             init    = ' = {.shape = NULL}'
