70b829c7cc799e8bbe1ec9cd84f692b36065fdc9
EmilyBourne <louise.bourne@gmail.com>
2024-09-25 12:20:31 +0200

    Correct cast recognition

--- pyccel/ast/builtins.py
+++ pyccel/ast/builtins.py
@@ -288,8 +288,7 @@ class PythonComplex(PyccelFunction):
         return super().__new__(cls)
 
     def __init__(self, arg0, arg1 = LiteralFloat(0)):
-        self._is_cast = isinstance(arg0.dtype.primitive_type, PrimitiveComplexType) and \
-                        isinstance(arg1, Literal) and arg1.python_value == 0
+        self._is_cast = isinstance(arg1, Literal) and arg1.python_value == 0
 
         if self._is_cast:
             self._real_part = self._real_cast(arg0)
