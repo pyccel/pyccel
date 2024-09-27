42120ffcd3f6289a771460e69d30ee5ace21a9f6
EmilyBourne <louise.bourne@gmail.com>
2024-09-27 10:34:31 +0200

    Unused import

--- pyccel/codegen/wrapper/c_to_python_wrapper.py
+++ pyccel/codegen/wrapper/c_to_python_wrapper.py
@@ -36,7 +36,7 @@ from pyccel.ast.internals     import Slice
 from pyccel.ast.literals      import Nil, LiteralTrue, LiteralString, LiteralInteger
 from pyccel.ast.literals      import LiteralFalse, convert_to_literal
 from pyccel.ast.numpytypes    import NumpyNDArrayType, NumpyInt64Type
-from pyccel.ast.numpy_wrapper import PyArray_DATA, PyArray_SHAPE
+from pyccel.ast.numpy_wrapper import PyArray_DATA
 from pyccel.ast.numpy_wrapper import PyArray_BASE
 from pyccel.ast.numpy_wrapper import get_strides_and_size_from_numpy_array
 from pyccel.ast.numpy_wrapper import get_strides_and_shape_from_numpy_array, PyccelPyArrayObject
