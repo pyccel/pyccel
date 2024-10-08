546d729f7082fb366468fb91c6ebff7091835aac
EmilyBourne <louise.bourne@gmail.com>
2024-10-08 17:26:58 +0200

    No need to duplicate tests

--- tests/epyccel/test_epyccel_sets.py
+++ tests/epyccel/test_epyccel_sets.py
@@ -16,33 +16,33 @@ from pyccel import epyccel
 def python_only_language(request):
     return request.param
 
-def test_add_literal_int(python_only_language) :
+def test_add_literal_int(language) :
     def add_int():
         a = {1,3,45}
         a.add(4)
-        return a
-    epyc_add_element = epyccel(add_int, language = python_only_language)
+        return len(a)
+    epyc_add_element = epyccel(add_int, language = language)
     pyccel_result = epyc_add_element()
     python_result = add_int()
     assert python_result == pyccel_result
 
-def test_add_literal_complex(python_only_language) :
+def test_add_literal_complex(language) :
     def add_complex():
         a = {6j,7j,8j}
         a.add(9j)
-        return a
-    epyc_add_element = epyccel(add_complex, language = python_only_language)
+        return len(a)
+    epyc_add_element = epyccel(add_complex, language = language)
     pyccel_result = epyc_add_element()
     python_result = add_complex()
     assert python_result == pyccel_result
 
-def test_add_variable_int(python_only_language):
+def test_add_variable_int(language):
     def add_element_range():
         a = {1, 2, 3}
         for i in range(50, 100):
             a.add(i)
-        return a
-    epyc_add_element = epyccel(add_element_range, language = python_only_language)
+        return len(a)
+    epyc_add_element = epyccel(add_element_range, language = language)
     pyccel_result = epyc_add_element()
     python_result = add_element_range()
     assert python_result == pyccel_result
@@ -383,37 +383,3 @@ def test_Pop_complex(language_without_fortran):
     pyccel_result = set(epyccel_remove())
     python_result = set(Pop_complex())
     assert python_result == pyccel_result
-
-@pytest.mark.parametrize( 'language', (
-        pytest.param("fortran",marks = [
-            pytest.mark.skip(reason="pop method not implemented in fortran"),
-            pytest.mark.fortran]),
-        pytest.param("c", marks = pytest.mark.c),
-        pytest.param("python", marks = pytest.mark.python)
-    )
-)
-
-def test_add_int(language):
-    def add_int():
-        a = {1, 2, 1} #pylint: disable=duplicate-value
-        size_1 = len(a)
-        a.add(5)
-        size_2 = len(a)
-        return size_1, size_2
-    epyccel_remove = epyccel(add_int, language = language)
-    pyccel_result = epyccel_remove()
-    python_result = add_int()
-    assert python_result == pyccel_result
-
-def test_add_float(language):
-    def add_float():
-        a = {4.6, 2.8, 8.4}
-        size_1 = len(a)
-        a.add(5)
-        size_2 = len(a)
-        return size_1, size_2
-    epyccel_remove = epyccel(add_float, language = language)
-    pyccel_result = epyccel_remove()
-    python_result = add_float()
-    assert python_result == pyccel_result
-
