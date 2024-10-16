f09bf78bffa7d790de1d307d869fa7589122de87
EmilyBourne <louise.bourne@gmail.com>
2024-10-16 13:01:13 +0200

    Add an AugAssign test

--- tests/epyccel/test_epyccel_sets.py
+++ tests/epyccel/test_epyccel_sets.py
@@ -484,3 +484,16 @@ def test_set_union_operator(language):
     python_result = union_int()
     assert python_result[0] == pyccel_result[0]
     assert set(python_result[1:]) == set(pyccel_result[1:])
+
+def test_set_union_augoperator(language):
+    def union_int():
+        a = {1,2,3,4}
+        b = {5,6,7,2}
+        a |= b
+        return len(c), c.pop(), c.pop(), c.pop(), c.pop(), c.pop(), c.pop(), c.pop()
+
+    epyccel_func = epyccel(union_int, language = language)
+    pyccel_result = epyccel_func()
+    python_result = union_int()
+    assert python_result[0] == pyccel_result[0]
+    assert set(python_result[1:]) == set(pyccel_result[1:])
