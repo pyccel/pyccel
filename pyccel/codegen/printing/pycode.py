d075f736e17d22a6f4306a2c165b3177bd7beb92
EmilyBourne <louise.bourne@gmail.com>
2024-10-16 13:02:59 +0200

    Fix Python

--- pyccel/codegen/printing/pycode.py
+++ pyccel/codegen/printing/pycode.py
@@ -524,7 +524,8 @@ class PythonCodePrinter(CodePrinter):
         return ''
 
     def _print_DottedName(self, expr):
-        return '.'.join(self._print(n) for n in expr.name)
+        # A DottedName can only contain LiteralStrings at the printing stage
+        return '.'.join(n.python_value for n in expr.name)
 
     def _print_FunctionCall(self, expr):
         func = expr.funcdef
