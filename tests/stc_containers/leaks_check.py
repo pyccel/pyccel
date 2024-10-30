0c71a90ec98cacce946b94f9fd8c8f360f1870ed
jalalium <jalalium@hotmail.fr>
2024-10-30 15:16:44 +0100

    fix codacy errors

--- tests/stc_containers/leaks_check.py
+++ tests/stc_containers/leaks_check.py
@@ -1,43 +1,44 @@
+
 # pylint: disable=missing-function-docstring, missing-module-docstring
 def create_list():
-    a = [1,2,3]# pylint: disable=missing-function-docstring, missing-module-docstring
+    a = [1,2,3]# pylint: disable=unused-variable
 
 def create_set():
-    a = {1,2,3}# pylint: disable=missing-function-docstring, missing-module-docstring
+    a = {1,2,3}# pylint: disable=unused-variable
 
 def create_dict():
-    a = {1:1,2:2,3:3}# pylint: disable=missing-function-docstring, missing-module-docstring
+    a = {1:1,2:2,3:3}# pylint: disable=unused-variable
 
 def list_reassign():
-    a = [1,2,3]# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = [1,2,3,4]# pylint: disable=missing-function-docstring, missing-module-docstring
+    a = [1,2,3]# pylint: disable=unused-variable
+    a = [1,2,3,4]# pylint: disable=unused-variable
 
 def set_reassign():
-    a = {1,2,3}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1,2}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1,2,3,4}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1,2,3,4,5}# pylint: disable=missing-function-docstring, missing-module-docstring
+    a = {1,2,3}# pylint: disable=unused-variable
+    a = {1,2}# pylint: disable=unused-variable
+    a = {1,2,3,4}# pylint: disable=unused-variable
+    a = {1,2,3,4,5}# pylint: disable=unused-variable
 
 def dict_reassign():
-    a = {1:1,2:2,3:3}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1:1,2:2,3:3,4:4}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1:1,2:2}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1:1,2:2,3:3,4:4,5:5}# pylint: disable=missing-function-docstring, missing-module-docstring
+    a = {1:1,2:2,3:3}# pylint: disable=unused-variable
+    a = {1:1,2:2,3:3,4:4}# pylint: disable=unused-variable
+    a = {1:1,2:2}# pylint: disable=unused-variable
+    a = {1:1,2:2,3:3,4:4,5:5}# pylint: disable=unused-variable
 
 def conditional_list(b1: bool):
     if (b1):
-        a = [1,2,3]# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = [1,2,3,4]# pylint: disable=missing-function-docstring, missing-module-docstring
+        a = [1,2,3]# pylint: disable=unused-variable
+    a = [1,2,3,4]# pylint: disable=unused-variable
 
 def conditional_set(b1: bool):
     if (b1):
-        a = {1,2,3}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1,2,3,4}# pylint: disable=missing-function-docstring, missing-module-docstring
+        a = {1,2,3}# pylint: disable=unused-variable
+    a = {1,2,3,4}# pylint: disable=unused-variable
 
 def conditional_dict(b1: bool):
     if (b1):
-        a = {1:1,2:2,3:3}# pylint: disable=missing-function-docstring, missing-module-docstring
-    a = {1:1,2:2,3:3,4:4,5:5}# pylint: disable=missing-function-docstring, missing-module-docstring
+        a = {1:1,2:2,3:3}# pylint: disable=unused-variable
+    a = {1:1,2:2,3:3,4:4,5:5}# pylint: disable=unused-variable
 
 if __name__ == '__main__':
     create_list()
