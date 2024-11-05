bc358df7794ce024212081b54dcdb75795fa11f6
EmilyBourne <louise.bourne@gmail.com>
2024-11-05 11:22:56 +0100

    Correct pointer check

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -1401,7 +1401,8 @@ class SemanticParser(BasicParser):
             d_lhs['memory_handling'] = 'alias'
             rhs.internal_var.is_target = True
 
-        if isinstance(rhs, Variable) and (rhs.rank > 0 or isinstance(rhs.class_type, CustomDataType)):
+        if isinstance(rhs, Variable) and (rhs.rank > 0 or isinstance(rhs.class_type, CustomDataType)) \
+                and not isinstance(rhs.class_type, TupleType):
             d_lhs['memory_handling'] = 'alias'
             rhs.is_target = not rhs.is_alias
 
