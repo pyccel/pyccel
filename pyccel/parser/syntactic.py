c5952b92948952d459af511ec64d29c53c7c1ab5
EmilyBourne <louise.bourne@gmail.com>
2024-11-27 14:49:21 +0100

    Handle consecutive comments

--- pyccel/parser/syntactic.py
+++ pyccel/parser/syntactic.py
@@ -1032,6 +1032,8 @@ class SyntaxParser(BasicParser):
                 visited_i.arguments[0].bound_argument = True
             elif isinstance(visited_i, (Pass, Comment)):
                 continue
+            elif isinstance(visited_i, CodeBlock) and all(isinstance(x, Comment) for x in visited_i.body):
+                continue
             elif isinstance(visited_i, AnnotatedPyccelSymbol):
                 attributes.append(visited_i)
             elif isinstance(visited_i, CommentBlock):
