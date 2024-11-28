d5ba1ffb82ff3aa492d80472c1cc1539e4f747ba
EmilyBourne <louise.bourne@gmail.com>
2024-11-28 20:57:33 +0100

    Filter out properties without setters

--- pyccel/codegen/printing/fcode.py
+++ pyccel/codegen/printing/fcode.py
@@ -3735,7 +3735,7 @@ class FCodePrinter(CodePrinter):
 
     def _print_BindCClassDef(self, expr):
         funcs = [expr.new_func, *expr.methods, *[f for i in expr.interfaces for f in i.functions],
-                 *[a.getter for a in expr.attributes], *[a.setter for a in expr.attributes]]
+                 *[a.getter for a in expr.attributes], *[a.setter for a in expr.attributes if a.setter]]
         sep = f'\n{self._print(SeparatorComment(40))}\n'
         return '', sep.join(self._print(f) for f in funcs)
 
