750290442e5a7cb9114223d5099ce1e792a6070d
EmilyBourne <louise.bourne@gmail.com>
2024-12-12 00:20:56 +0100

    Simplify test

--- tests/epyccel/test_epyccel_sets.py
+++ tests/epyccel/test_epyccel_sets.py
@@ -701,9 +701,7 @@ def test_set_arg(stc_language):
 def test_set_return(language):
     def set_return():
         a = {1,2,3,4,5}
-        b = {4,5,6}
-        c = a.union(b) # Use union to avoid #2084
-        return c
+        return a
 
     epyccel_func = epyccel(set_return, language = language)
     pyccel_result = epyccel_func()
