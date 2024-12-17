e47afbe74a82d59f45e4b2ed31dc7d62dc957cd4
EmilyBourne <louise.bourne@gmail.com>
2024-12-17 15:34:46 +0100

    Fix macro

--- pyccel/stdlib/cwrapper/cwrapper.h
+++ pyccel/stdlib/cwrapper/cwrapper.h
@@ -50,7 +50,7 @@ void capsule_cleanup(PyObject *capsule);
  * c_order : True if the data is in C order, False otherwise.
  * release_memory : If true a Capsule is created to automatically free the data when the created PyArrayObject goes out of scope.
  */
-#ifdef _WIN32 && (PyArray_RUNTIME_VERSION >= NPY_2_0_API_VERSION)
+#if defined(_WIN32) && (PyArray_RUNTIME_VERSION >= NPY_2_0_API_VERSION)
 PyObject* to_pyarray(int nd, enum NPY_TYPES typenum, void* data, int32_t shape[], bool c_order, bool release_memory);
 #else
 PyObject* to_pyarray(int nd, enum NPY_TYPES typenum, void* data, int64_t shape[], bool c_order, bool release_memory);
