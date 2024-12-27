c7e7bb40c00531caa23dafa9ed239320e8d18207
EmilyBourne <louise.bourne@gmail.com>
2024-12-27 18:19:21 +0100

    Use template syntax

--- tests/errors/syntax_errors/functions_in_template.py
+++ tests/errors/syntax_errors/functions_in_template.py
@@ -1,8 +1,7 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 
-#$ header template S(int|real)
-#$ header template T((int)(int)|(real)(real))
-#$ header function f(T, S)
-def f(g, a):
+@template('S', [int, float])
+@template('T', ['(int)(int)', '(real)(real)'])
+def f(g : 'T', a : 'S'):
     return g(a)
 
