2e959a6fb7c4dc8678964919a9bd5729dab450d3
EmilyBourne <louise.bourne@gmail.com>
2025-01-04 19:21:06 +0100

    Activate tests

--- tests/epyccel/test_strings.py
+++ tests/epyccel/test_strings.py
@@ -3,31 +3,26 @@ import inspect
 import pytest
 
 from pyccel import epyccel
-from modules import strings as strings_module
+from modules import strings_module
 
 string_funcs = [(f, getattr(strings_module,f)) for f in strings_module.__all__ if inspect.isfunction(getattr(strings_module,f))]
 
-#failing_tests = {
-#        'homogenous_string_string':'String has no precision',
-#        'string_multi_indexing_1':'Multi object part of numpy array stored in sympy Tuple',
-#        'string_multi_indexing_2':'Multi object part of numpy array stored in sympy Tuple',
-#        'string_homogeneous_return':"Can't return a string",
-#        'string_inhomogeneous_return':"Can't return a string",
-#        'string_visitation_inhomogeneous':"Can't iterate over an inhomogeneous string",
-#        }
-failing_tests = dict()
+failing_tests = {
+        'concatenate':'C does not support string concatenation',
+        'concatenate_multiple':'C does not support string concatenation',
+        'concatenate_expr':'C does not support string concatenation',
+        }
 
 marks = [f[1] if f[0] not in failing_tests else
-        pytest.param(f[1], marks = pytest.mark.xfail(reason=failing_tests[f[0]])) for f in string_funcs]
+        pytest.param(f[1], marks = pytest.mark.skip(reason=failing_tests[f[0]])) for f in string_funcs]
 
-@pytest.mark.skip(reason="Strings are arrays of chars. We cannot return arrays")
 @pytest.mark.parametrize('test_func',marks)
-def test_strings(test_func):
+def test_strings(test_func, language):
     f1 = test_func
-    f2 = epyccel( f1 )
+    f2 = epyccel( f1, language = language )
 
     python_out = f1()
-    pyccel_out = (f2()).decode("utf-8")
+    pyccel_out = f2()
     print(python_out)
     print(pyccel_out)
     assert python_out == pyccel_out.strip()
