09293bb856e714e924d9364305891338d088ba85
Emily Bourne <louise.bourne@gmail.com>
2025-01-15 09:46:45 +0100

    Fix class constness (#2146)
    Fix the constness of class instances in functions and class methods.
    Fixes #2145.

--- pyccel/ast/numpyext.py
+++ pyccel/ast/numpyext.py
@@ -695,7 +695,7 @@ class NumpyArray(NumpyNewArray):
         have.
     """
     __slots__ = ('_arg','_shape')
-    _attribute_nodes = ('_arg',)
+    _attribute_nodes = NumpyNewArray._attribute_nodes + ('_arg',)
     name = 'array'
 
     def __init__(self, arg, dtype=None, order='K', ndmin=None):
@@ -1288,6 +1288,7 @@ class NumpyFull(NumpyNewArray):
         (row- or column-wise) order in memory.
     """
     __slots__ = ('_fill_value','_shape')
+    _attribute_nodes = NumpyNewArray._attribute_nodes + ('_shape',)
     name = 'full'
 
     def __init__(self, shape, fill_value, dtype=None, order='C'):
