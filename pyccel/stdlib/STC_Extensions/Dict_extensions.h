0558368a070bfe5cb95e636abfd00560a3222da3
EmilyBourne <louise.bourne@gmail.com>
2025-01-24 18:20:56 +0100

    Update syntax

--- pyccel/stdlib/STC_Extensions/Dict_extensions.h
+++ pyccel/stdlib/STC_Extensions/Dict_extensions.h
@@ -1,3 +1,4 @@
+#include <stc/priv/template.h>
 
 /**
  * Removes and returns the value associated with the key in the dictionary.
@@ -6,7 +7,7 @@
  * @param rkey : The raw key to be removed from the dictionary.
  * @return The value associated with the key.
  */
-static inline i_val _c_MEMB(_pop)(i_type* self, i_keyraw rkey) {
+static inline i_val _c_MEMB(_pop)(Self* self, i_keyraw rkey) {
     i_val value = *_c_MEMB(_at)(self, rkey);
     _c_MEMB(_erase)(self, rkey);
     return value;
@@ -21,13 +22,11 @@ static inline i_val _c_MEMB(_pop)(i_type* self, i_keyraw rkey) {
  * @param default_val : The default value to return if the key is not found.
  * @return The value associated with the key, or the default value if the key is not found.
  */
-static inline i_val _c_MEMB(_pop_with_default)(i_type* self, i_keyraw rkey, i_valraw default_val) {
+static inline i_val _c_MEMB(_pop_with_default)(Self* self, i_keyraw rkey, i_valraw default_val) {
     if (!_c_MEMB(_contains)(self, rkey)) {
         return default_val;
     }
     return _c_MEMB(_pop)(self, rkey);
 }
 
-#undef i_type
-#undef i_key
 #include <stc/priv/template2.h>
