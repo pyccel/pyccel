be114b4d336f71d5581f816d19f21423a750d07a
EmilyBourne <louise.bourne@gmail.com>
2025-01-28 16:11:01 +0100

    Add warning test

---
+++ tests/warnings/semantic/UNNECESSARY_ALLOCATION_IN_FUNCTION.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def Mi(x: float, M0: 'float[:, :]') -> 'float[:, :]':
+    M1 = x * M0 + 1.    
+    return M1
+
+def M_GEN(x: float) -> 'float[:, :, :]':
+    M = np.ones((4, 4), dtype = float)
+    M2 = np.empty((3, 4, 4), dtype = float)
+    for i in range(3):
+        M2[i] = Mi(x * i, M)
+    return M2
