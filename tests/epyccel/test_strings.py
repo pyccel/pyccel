9d690601108c84ace1f5db663f6e659a9734d916
EmilyBourne <louise.bourne@gmail.com>
2025-02-03 14:59:58 +0100

    Activate all relevant tests

--- tests/epyccel/test_strings.py
+++ tests/epyccel/test_strings.py
@@ -5,7 +5,7 @@ import pytest
 from pyccel import epyccel
 from modules import strings_module
 
-string_funcs = [(f, getattr(strings_module,f)) for f in strings_module.__all__ if inspect.isfunction(getattr(strings_module,f))]
+string_funcs = [getattr(strings_module,f) for f in strings_module.__all__ if inspect.isfunction(getattr(strings_module,f))]
 
 failing_tests = {
         'concatenate':'C does not support string concatenation',
@@ -13,11 +13,11 @@ failing_tests = {
         'concatenate_expr':'C does not support string concatenation',
         }
 
-marks = [f[1] if f[0] not in failing_tests else
-        pytest.param(f[1], marks = pytest.mark.skip(reason=failing_tests[f[0]])) for f in string_funcs]
-
-@pytest.mark.parametrize('test_func',marks)
+@pytest.mark.parametrize('test_func', string_funcs)
 def test_strings(test_func, language):
+    if test_func.__name__ in failing_tests and language=='c':
+        pytest.xfail(failing_tests[test_func.__name__])
+
     f1 = test_func
     f2 = epyccel( f1, language = language )
 
