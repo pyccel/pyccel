c1f6db557c2b03ff0f38e83b95d988dfb02d4e2b
EmilyBourne <louise.bourne@gmail.com>
2025-02-18 10:44:06 +0100

    Remove unnecessary pylint disable

--- pyccel/errors/errors.py
+++ pyccel/errors/errors.py
@@ -325,7 +325,7 @@ class Errors(metaclass = Singleton):
         if symbol is not None:
             if isinstance(symbol, ast.AST):
                 ast_node = symbol
-                symbol = ast.unparse(ast_node) # pylint: disable=no-member
+                symbol = ast.unparse(ast_node)
             elif isinstance(symbol, PyccelAstNode):
                 ast_node = symbol.python_ast
 
