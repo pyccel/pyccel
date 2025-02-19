78268864fa6e62e8828d7f889aa954800890c3cb
EmilyBourne <louise.bourne@gmail.com>
2025-02-19 14:17:36 +0100

    Remove debug output

--- tests/epyccel/test_epyccel_openmp.py
+++ tests/epyccel/test_epyccel_openmp.py
@@ -475,7 +475,7 @@ def test_omp_barrier(language):
 @pytest.mark.external
 def test_combined_for_simd(language):
     # Intel compiler has a bug in debug mode
-    f1 = epyccel(openmp.combined_for_simd, fflags = '-Wall', accelerators=['openmp'], language=language, verbose=True, debug=False)
+    f1 = epyccel(openmp.combined_for_simd, fflags = '-Wall', accelerators=['openmp'], language=language, debug=False)
     f2 = openmp.combined_for_simd
     assert f1() == f2()
 
