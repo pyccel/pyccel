20e2ff6743c46b8845234b98ab9158f765a2d5bb
EmilyBourne <louise.bourne@gmail.com>
2025-02-24 09:18:42 +0100

    Fix Python return printing

--- pyccel/codegen/printing/pycode.py
+++ pyccel/codegen/printing/pycode.py
@@ -379,18 +379,19 @@ class PythonCodePrinter(CodePrinter):
 
         if expr.expr is None:
             return 'return\n'
-        elif isinstance(expr.expr.class_type, InhomogeneousTupleType):
-            elems = [self.scope.collect_tuple_element(v) for v in expr.expr]
-            expr_return_vars = [assigns.get(e, e) for e in elems]
-            return_expr = ', '.join(self._print(i) for i in expr_return_vars)
-            if len(expr_return_vars) < 2:
-                return_expr += ','
-            return_expr = f'({return_expr})'
-        else:
-            expr_return_vars = [assigns.get(expr.expr, expr.expr)]
-            return_expr = ', '.join(self._print(i) for i in expr_return_vars)
 
-        return prelude + f'return {return_expr}\n'
+        def get_return_code(return_var):
+            if isinstance(return_var.class_type, InhomogeneousTupleType):
+                elem_code = [get_return_code(self.scope.collect_tuple_element(elem)) for elem in return_var]
+                return_expr = ', '.join(elem_code)
+                if len(elem_code) < 2:
+                    return_expr += ','
+                return f'({return_expr})'
+            else:
+                return_expr = assigns.get(return_var, return_var)
+                return self._print(return_expr)
+
+        return prelude + f'return {get_return_code(expr.expr)}\n'
 
     def _print_Program(self, expr):
         mod_scope = self.scope
