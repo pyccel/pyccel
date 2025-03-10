2f122c0a38a192306767a2c5cc28d6806932cfb8
EmilyBourne <louise.bourne@gmail.com>
2025-03-10 23:06:36 +0100

    Correct result type

--- pyccel/stdlib/STC_Extensions/List_extensions.h
+++ pyccel/stdlib/STC_Extensions/List_extensions.h
@@ -1,23 +1,23 @@
 #include <stc/priv/template.h>
 
 #if defined _i_is_arc
-#define UNPACK_ITR(itr) (itr.ref)->get
+#define UNPACK_ITR(itr) *((itr.ref)->get)
 #else
 #define UNPACK_ITR(itr) *(itr.ref)
 #endif
 
 // This function represents a call to the .pop() method.
 // i_type: Class type (e.g., hset_int64_t).
-// i_key: Data type of the elements in the set (e.g., int64_t).
+// i_keyraw: Data type of the elements in the set (e.g., int64_t).
 
-static inline i_key _c_MEMB(_pull_elem)(Self* self, intptr_t pop_idx) {
+static inline i_keyraw _c_MEMB(_pull_elem)(Self* self, intptr_t pop_idx) {
     // Get the iterator for the specified element using (_advance) and (_begin)
     _c_MEMB(_iter) itr = _c_MEMB(_advance)(_c_MEMB(_begin)(self), pop_idx);
 
     // If the element is found then remove it from the list
     if (itr.ref) 
     {
-        i_key value = UNPACK_ITR(itr);
+        i_keyraw value = UNPACK_ITR(itr);
         _c_MEMB(_erase_at)(self, itr); // Remove the element by value using "_erase_at".
         return value;
     }
@@ -27,8 +27,8 @@ static inline i_key _c_MEMB(_pull_elem)(Self* self, intptr_t pop_idx) {
 
 #if defined (i_use_cmp) 
 // Function to get the minimum element from the vector
-static inline i_key _c_MEMB(_min)(const Self* self) {
-    i_key min_val = *_c_MEMB(_front)(self);
+static inline i_keyraw _c_MEMB(_min)(const Self* self) {
+    i_keyraw min_val = *_c_MEMB(_front)(self);
     c_foreach(it, Self, *self) {
         if (i_less(it.ref, &min_val)) {
             min_val = UNPACK_ITR(it);
@@ -38,8 +38,8 @@ static inline i_key _c_MEMB(_min)(const Self* self) {
 }
 
 // Function to get the maximum element from the vector
-static inline i_key _c_MEMB(_max)(const Self* self) {
-    i_key max_val = *_c_MEMB(_front)(self);
+static inline i_keyraw _c_MEMB(_max)(const Self* self) {
+    i_keyraw max_val = *_c_MEMB(_front)(self);
     c_foreach(it, Self, *self) {
         if (i_less(&max_val, it.ref)) {
             max_val = UNPACK_ITR(it);
@@ -48,9 +48,7 @@ static inline i_key _c_MEMB(_max)(const Self* self) {
     return max_val;
 }
 #endif
-#undef i_type
-#undef i_key
-#undef i_use_cmp
+
 #undef UNPACK_ITR
 
 #include <stc/priv/template2.h>
