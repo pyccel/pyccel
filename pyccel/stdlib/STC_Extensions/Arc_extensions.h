8c05a355d88170b0b229384a37338ce67f1dfd40
EmilyBourne <louise.bourne@gmail.com>
2025-03-15 16:36:22 +0100

    Correct file to work for generic types

--- pyccel/stdlib/STC_Extensions/Arc_extensions.h
+++ pyccel/stdlib/STC_Extensions/Arc_extensions.h
@@ -2,7 +2,7 @@
 #include <stc/priv/template.h>
 
 static inline Self _c_MEMB(_share)(i_keyraw x) {
-    element_vec_vec_int64_t out = element_vec_vec_int64_t_make(x);
+    Self out = _c_MEMB(_make)(x);
     (*out.use_count)++;
     return out;
 }
