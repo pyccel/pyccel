91830ea338beabcde3005ad1b6d344e897e936ca
EmilyBourne <louise.bourne@gmail.com>
2025-03-17 14:31:29 +0100

    Uncomment

--- tests/stc_containers/nested_leaks_check.py
+++ tests/stc_containers/nested_leaks_check.py
@@ -1,43 +1,43 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring, unused-variable
 
 
-#def create_nested_list():
-#    a = [[1,2,3], [4,5,6]]
+def create_nested_list():
+    a = [[1,2,3], [4,5,6]]
 
 def create_nested_list2():
     a = [[[1,2],[3,4]], [[5,6],[7,8]]]
 
-#def create_nested_dict():
-#    a = {1:[1,2,3], 2:[4,5,6]}
-#
-#def create_nested_dict2():
-#    a = {1:{True: [1,2], False: [3,4]}, 2:{True: [5,6], False: [7,8]}}
-#
-#def nested_list_reassign():
-#    a = [[1,2,3], [4,5,6]]
-#    a = [[7,8], [9, 10], [11, 12]]
-#
-#def nested_dict_reassign():
-#    a = {1:[1,2,3], 2:[4,5,6]}
-#    a = {3:[1,2], 4:[4,5], 6:[7,8]}
-#
-#def nested_list_ref():
-#    a = [1,2]
-#    b = [3,4]
-#    c = [a,b]
-#
-#def nested_list_ref_pop():
-#    a = [1,2]
-#    b = [3,4]
-#    c = [a,b]
-#    d = c.pop()
+def create_nested_dict():
+    a = {1:[1,2,3], 2:[4,5,6]}
+
+def create_nested_dict2():
+    a = {1:{True: [1,2], False: [3,4]}, 2:{True: [5,6], False: [7,8]}}
+
+def nested_list_reassign():
+    a = [[1,2,3], [4,5,6]]
+    a = [[7,8], [9, 10], [11, 12]]
+
+def nested_dict_reassign():
+    a = {1:[1,2,3], 2:[4,5,6]}
+    a = {3:[1,2], 4:[4,5], 6:[7,8]}
+
+def nested_list_ref():
+    a = [1,2]
+    b = [3,4]
+    c = [a,b]
+
+def nested_list_ref_pop():
+    a = [1,2]
+    b = [3,4]
+    c = [a,b]
+    d = c.pop()
 
 if __name__ == '__main__':
-    #create_nested_list()
+    create_nested_list()
     create_nested_list2()
-    #create_nested_dict()
-    #create_nested_dict2()
-    #nested_list_reassign()
-    #nested_dict_reassign()
-    #nested_list_ref()
-    #nested_list_ref_pop()
+    create_nested_dict()
+    create_nested_dict2()
+    nested_list_reassign()
+    nested_dict_reassign()
+    nested_list_ref()
+    nested_list_ref_pop()
