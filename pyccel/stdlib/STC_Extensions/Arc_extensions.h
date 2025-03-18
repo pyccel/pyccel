299204a2faf6c87c10748d0ec5dece265b356cbe
EmilyBourne <louise.bourne@gmail.com>
2025-03-18 11:40:37 +0100

    Save idea

--- pyccel/stdlib/STC_Extensions/Arc_extensions.h
+++ pyccel/stdlib/STC_Extensions/Arc_extensions.h
@@ -1,6 +1,11 @@
 #define _i_is_arc
 #include <stc/priv/template.h>
 
+struct {
+    bool is_owning;
+    i_key* ptr;
+};
+
 static inline Self _c_MEMB(_share)(i_keyraw x) {
     Self out = _c_MEMB(_make)(x);
     (*out.use_count)++;
