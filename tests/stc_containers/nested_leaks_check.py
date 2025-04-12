395fad52eb8474acd984190c71491d9a73a6905f
EmilyBourne <louise.bourne@gmail.com>
2025-04-12 17:34:13 +0200

    Remove pylint unused-variable by printing elements

--- tests/stc_containers/nested_leaks_check.py
+++ tests/stc_containers/nested_leaks_check.py
@@ -1,40 +1,51 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring, unused-variable
+# pylint: disable=missing-function-docstring, missing-module-docstring
 
 
 def create_nested_list():
     a = [[1,2,3], [4,5,6]]
+    print(a[0][0])
 
 def create_nested_list2():
     a = [[[1,2],[3,4]], [[5,6],[7,8]]]
+    print(a[0][0])
 
 def create_nested_dict():
     a = {1:[1,2,3], 2:[4,5,6]}
+    print(a[1][0])
 
 def create_nested_dict2():
     a = {1:{True: [1,2], False: [3,4]}, 2:{True: [5,6], False: [7,8]}}
+    print(a[2][False][1])
 
 def nested_list_reassign():
     a = [[1,2,3], [4,5,6]]
+    print(a[0][0])
     a = [[7,8], [9, 10], [11, 12]]
+    print(a[0][0])
 
 def nested_dict_reassign():
     a = {1:[1,2,3], 2:[4,5,6]}
+    print(a[1][0])
     a = {3:[1,2], 4:[4,5], 6:[7,8]}
+    print(a[3][0])
 
 def nested_list_ref():
     a = [1,2]
     b = [3,4]
     c = [a,b]
+    print(c[0][0])
 
 def nested_list_ref_pop():
     a = [1,2]
     b = [3,4]
     c = [a,b]
     d = c.pop()
+    print(d[0])
 
 def nested_list_ref_pop_temp():
     a = [[1,2,3], [4,5,6]]
     b = a.pop()
+    print(b[0])
 
 if __name__ == '__main__':
     create_nested_list()
