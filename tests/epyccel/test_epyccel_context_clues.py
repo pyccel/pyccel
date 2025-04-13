1411ea2f642670dbb261f3dbe5417c59740c9a66
EmilyBourne <louise.bourne@gmail.com>
2025-04-13 10:58:27 +0200

    Add TypeVar error test. Add enclosed function test

--- tests/epyccel/test_epyccel_context_clues.py
+++ tests/epyccel/test_epyccel_context_clues.py
@@ -1,8 +1,10 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 import sys
+from typing import TypeVar
 import numpy as np
 import pytest
 from pyccel import epyccel
+from pyccel.errors.errors import PyccelError
 
 def test_numpy_context(language):
     def f():
@@ -25,7 +27,7 @@ def test_literal_context(language):
 def test_type_alias_context(language):
     T = int
 
-    def f(a : T):
+    def f(a : T) -> T:
         return 2*a
 
     epyc_f = epyccel(f, language=language)
@@ -44,3 +46,31 @@ def test_type_union_context(language):
     assert isinstance(f(2), type(epyc_f(2)))
     assert f(3.5) == epyc_f(3.5)
     assert isinstance(f(2.3), type(epyc_f(2.3)))
+
+def test_type_enclosing_context(language):
+    def get_func(b : bool):
+        T = int if b else float
+
+        def f(a : T):
+            return 2*a
+
+        return f
+
+    epyc_f = epyccel(get_func(True), language=language)
+    py_f = get_func(True)
+    assert py_f(3) == epyc_f(3)
+    assert isinstance(py_f(2), type(epyc_f(2)))
+    epyc_f = epyccel(get_func(False), language=language)
+    py_f = get_func(False)
+    assert py_f(3.5) == epyc_f(3.5)
+    assert isinstance(py_f(2.3), type(epyc_f(2.3)))
+
+def test_bad_type_var_context(language):
+    T = TypeVar('T', int, float)
+    S = TypeVar('T', int, float)
+
+    def f(a : T, b : S) -> T:
+        return 2*a
+
+    with pytest.raises(PyccelError):
+        epyc_f = epyccel(f, language=language)
