329548c9d2ec0c1f94bde8f191060f51dec63878
Yaman Güçlü <yaman.guclu@gmail.com>
2025-04-15 14:40:57 +0200

    Avoid duplicated calls in test_bitwise.py.

--- tests/epyccel/test_bitwise.py
+++ tests/epyccel/test_bitwise.py
@@ -4,146 +4,187 @@ import pytest
 from pyccel import epyccel
 from modules import bitwise
 
-@pytest.mark.parametrize("a, b",[(True, False),(True, True)])
+@pytest.mark.parametrize("a, b", [(True, False), (True, True)])
 def test_right_shift_b_b(language, a, b):
     f1 = bitwise.right_shift_b_b
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(1, 1),(1, 2)])
+@pytest.mark.parametrize("a, b", [(1, 1), (1, 2)])
 def test_right_shift_i_i(language, a, b):
     f1 = bitwise.right_shift_i_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(True, 2),(True, 1)])
+@pytest.mark.parametrize("a, b", [(True, 2), (True, 1)])
 def test_right_shift_b_i(language, a, b):
     f1 = bitwise.right_shift_b_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(True, 1),(True, 2)])
+@pytest.mark.parametrize("a, b", [(True, 1), (True, 2)])
 def test_left_shift_b_i(language, a, b):
     f1 = bitwise.right_shift_b_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(1, 1),(1, 2)])
+@pytest.mark.parametrize("a, b", [(1, 1), (1, 2)])
 def test_left_shift_i_i(language, a, b):
     f1 = bitwise.right_shift_i_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(True, False),(True, True)])
+@pytest.mark.parametrize("a, b", [(True, False), (True, True)])
 def test_left_shift_b_b(language, a, b):
     f1 = bitwise.right_shift_b_b
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
 def test_bit_xor_b_b(language):
     f1 = bitwise.bit_xor_b_b
-    f2 = epyccel( f1, language = language )
-    assert f1(True, False) == f2(True, False)
-    assert type(f1(True, False)) is type(f2(True, False))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(True, False)
+    r2 = f2(True, False)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b, c",[(True, False, False),(True, True, True)])
+@pytest.mark.parametrize("a, b, c", [(True, False, False), (True, True, True)])
 def test_bit_xor_b_b_b(language, a, b, c):
     f1 = bitwise.bit_xor_b_b_b
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b, c) == f2(a, b, c)
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b, c)
+    r2 = f2(a, b, c)
+    assert r1 == r2
 
-@pytest.mark.parametrize("a, b",[(1, 1),(1, 2)])
+@pytest.mark.parametrize("a, b", [(1, 1), (1, 2)])
 def test_bit_xor_i_i(language, a, b):
     f1 = bitwise.bit_xor_i_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(False, 2), (True, 1)])
+@pytest.mark.parametrize("a, b", [(False, 2), (True, 1)])
 def test_bit_xor_b_i(language, a, b):
     f1 = bitwise.bit_xor_b_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(1, False),(1, True)])
+@pytest.mark.parametrize("a, b", [(1, False), (1, True)])
 def test_bit_or_i_b(language, a, b):
     f1 = bitwise.bit_or_i_b
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(1, 1),(1, 2)])
+@pytest.mark.parametrize("a, b", [(1, 1), (1, 2)])
 def test_bit_or_i_i(language, a, b):
     f1 = bitwise.bit_or_i_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
 def test_bit_or_b_b(language):
     f1 = bitwise.bit_or_b_b
-    f2 = epyccel( f1, language = language )
-    assert f1(False, True) == f2(False, True)
-    assert type(f1(False, True)) is type(f2(False, True))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(False, True)
+    r2 = f2(False, True)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(1, True),(1, False)])
+@pytest.mark.parametrize("a, b", [(1, True), (1, False)])
 def test_bit_and_i_b(language, a, b):
     f1 = bitwise.bit_and_i_b
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b",[(1, 1),(1, 2)])
+@pytest.mark.parametrize("a, b", [(1, 1), (1, 2)])
 def test_bit_and_i_i(language, a, b):
     f1 = bitwise.bit_and_i_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b) == f2(a, b)
-    assert type(f1(a, b)) is type(f2(a, b))
-
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b)
+    r2 = f2(a, b)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
 def test_bit_and_b_b(language):
     f1 = bitwise.bit_and_b_b
-    f2 = epyccel( f1, language = language )
-    assert f1(True, True) == f2(True, True)
-    assert type(f1(True, True)) is type(f2(True, True))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(True, True)
+    r2 = f2(True, True)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b, c",[(1, 0, 4), (1, 0, 4)])
+@pytest.mark.parametrize("a, b, c", [(1, 0, 4), (1, 0, 4)])
 def test_bit_and_i_i_i(language, a, b, c):
     f1 = bitwise.bit_and_i_i_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b, c) == f2(a, b, c)
-    assert type(f1(a, b, c)) is type(f2(a, b, c))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b, c)
+    r2 = f2(a, b, c)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
-@pytest.mark.parametrize("a, b, c",[(True, True, 4), (True, False, 4)])
+@pytest.mark.parametrize("a, b, c", [(True, True, 4), (True, False, 4)])
 def test_bit_and_b_b_i(language, a, b, c):
     f1 = bitwise.bit_and_b_b_i
-    f2 = epyccel( f1, language = language )
-    assert f1(a, b, c) == f2(a, b, c)
-    assert type(f1(a, b, c)) is type(f2(a, b, c))
+    f2 = epyccel(f1, language=language)
+    r1 = f1(a, b, c)
+    r2 = f2(a, b, c)
+    assert r1 == r2
+    assert type(r1) is type(r2)
 
 def test_invert_b(language):
     f1 = bitwise.invert_b
-    f2 = epyccel( f1, language = language )
-    for a in [True,False]:
-        assert f1(a) == f2(a)
-        assert type(f1(a)) is type(f2(a))
+    f2 = epyccel(f1, language=language)
+    for a in [True, False]:
+        r1 = f1(a)
+        r2 = f2(a)
+        assert r1 == r2
+        assert type(r1) is type(r2)
 
 def test_invert_i(language):
     f1 = bitwise.invert_i
-    f2 = epyccel( f1, language = language )
+    f2 = epyccel(f1, language=language)
     for a in [0, 1, 60, -45]:
-        assert f1(a) == f2(a)
-        assert type(f1(a)) is type(f2(a))
+        r1 = f1(a)
+        r2 = f2(a)
+        assert r1 == r2
+        assert type(r1) is type(r2)
 
 def test_or_ints(language):
     f1 = bitwise.or_ints
-    f2 = epyccel( f1, language = language )
+    f2 = epyccel(f1, language=language)
     for a in [0, 1, 60, -45]:
-        assert f1(a) == f2(a)
-        assert type(f1(a)) is type(f2(a))
+        r1 = f1(a)
+        r2 = f2(a)
+        assert r1 == r2
+        assert type(r1) is type(r2)
