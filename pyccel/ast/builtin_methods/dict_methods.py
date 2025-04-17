ca7e5613e8870465caedfbcd6e90f8d85eef4297
EmilyBourne <louise.bourne@gmail.com>
2025-04-17 22:50:38 +0200

    Complete __all__

--- pyccel/ast/builtin_methods/dict_methods.py
+++ pyccel/ast/builtin_methods/dict_methods.py
@@ -26,6 +26,7 @@ __all__ = ('DictClear',
            'DictPop',
            'DictPopitem',
            'DictSetDefault',
+           'DictValues',
            )
 
 #==============================================================================
