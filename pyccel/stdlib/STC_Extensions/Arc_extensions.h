5b86aa177e24cc1fb10d16752bf3305590f96ad6
EmilyBourne <louise.bourne@gmail.com>
2025-04-18 00:50:55 +0200

    Correct possession

--- pyccel/stdlib/STC_Extensions/Arc_extensions.h
+++ pyccel/stdlib/STC_Extensions/Arc_extensions.h
@@ -9,7 +9,7 @@ STC_INLINE Self _c_MEMB(_from_heap)(i_key raw)
 STC_INLINE c_JOIN(i_key, _value) _c_MEMB(_release)(Self mem) {
     c_JOIN(i_key, _value)* out_ptr = mem.get->get;
     c_assert(mem.get->is_owning);
-    mem.get->get = NULL;
+    mem.get->is_owning = false;
     _c_MEMB(_drop)(&mem);
     c_JOIN(i_key, _value) out = *out_ptr;
     i_free(out_ptr, c_sizeof out);
