c52a2eeb4f048092df72d43367f1e82522f710aa
EmilyBourne <louise.bourne@gmail.com>
2025-04-24 18:47:10 +0200

    Move tuple tests to join containers

--- tests/pyccel/scripts/print_builtin_containers.py
+++ tests/pyccel/scripts/print_builtin_containers.py
@@ -1,6 +1,21 @@
+# pylint: disable=missing-module-docstring
+
+def f():
+    return 1, True
 
 
 if __name__ == '__main__':
+    print(())
+    print((1,))
+    print((1,2,3))
+    print(((1,2),3))
+    print(((1,2),(3,)))
+    print((((1,),2),(3,)))
+    print((1, True))
+    print((1, False), sep=",")
+    print((1, True), end="!\n")
+    print(f())
+
     print([1,2,3])
     a = [4,5,6]
     print(a)
@@ -10,3 +25,4 @@ if __name__ == '__main__':
     print(c)
 
     print({1,2,3})
+
--- tests/pyccel/test_pyccel.py
+++ tests/pyccel/test_pyccel.py
@@ -721,11 +721,6 @@ def test_print_integers(language):
     pyccel_test("scripts/print_integers.py", language=language, output_dtype=types)
 
 #------------------------------------------------------------------------------
-def test_print_tuples(language):
-    types = str
-    pyccel_test("scripts/print_tuples.py", language=language, output_dtype=types)
-
-#------------------------------------------------------------------------------
 def test_print_sp_and_end(language):
     types = str
     pyccel_test("scripts/print_sp_and_end.py", language=language, output_dtype=types)
