81d38762dbe99b187e408503b269494ad2c78a53
EmilyBourne <louise.bourne@gmail.com>
2025-04-25 10:33:07 +0200

    Add class to represent a TypeVar

--- pyccel/ast/typingext.py
+++ pyccel/ast/typingext.py
@@ -61,10 +61,37 @@ class TypingTypeAlias(TypedAstNode):
     _static_type = TypeAlias()
 
 #==============================================================================
+class TypingTypeVar(TypedAstNode):
+    """
+    Class representing a call to the typing.TypeVar construct.
+
+    Class representing a call to the typing.TypeVar construct. This object
+    is a type annotation.
+    """
+    __slots__ = ('_name', '_possible_types')
+    _attribute_nodes = ()
+    _class_type = TypeAlias()
+    _shape = None
+
+    def __init__(self, name, *constraints, bound=None, covariant=False, contravariant=False):
+        if covariant or contravariant:
+            raise TypeError("Covariant and contravariant types are not currently supported")
+        if len(constraints) == 0:
+            raise TypeError(f"The possible types for {name} must be specified")
+        self._name = name
+        self._possible_types = constraints
+        super().__init__()
+
+    @property
+    def type_list(self):
+        return self._possible_types
+
+#==============================================================================
 
 typing_funcs = {
         'Final': PyccelFunctionDef('Final', TypingFinal),
         'TypeAlias': PyccelFunctionDef('TypeAlias', TypingTypeAlias),
+        'TypeVar' : PyccelFunctionDef('TypeVar', TypingTypeVar),
     }
 
 typing_mod = Module('typing',
