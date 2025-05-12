0675d4ff88720c7ecb19ea845c7f284b39e82ec0
rmahjoubi <redouanemahjoubii@gmail.com>
2025-05-12 20:07:02 +0100

    doc strings and cleanup

--- pyccel/plugins/Openmp/omp.py
+++ pyccel/plugins/Openmp/omp.py
@@ -6,15 +6,41 @@ errors = Errors()
 
 
 class OmpAnnotatedComment(PyccelAstNode):
-    """Parent class for all openmp classes, including classes used in the grammar."""
+    """
+    Parent class for all openmp classes, including classes used in the grammar.
+    
+    Parameters
+    ----------
+    parent : omp object
+
+    omp_version : float
+        The version of OpenMP chosen by the user.
+
+    raw : str
+        The raw syntax of the section of the comment that represent the object, as provided in the source code.
+
+    position : tuple
+        The start and end positions of the OpenMP syntax in the source code, used to print errors.
+
+    line : int
+        The line number in the source code where the OpenMP syntax occurs.
+
+    VERSION : float | list
+        The OpenMP version required for the annotated comment.
+        If the syntax contains sections with different versions, they are stored in a list by textx.
+
+    DEPRECATED : float
+        The OpenMP version in which the syntax was deprecated. Defaults to infinity.
+    
+    """
 
     __slots__ = ("_parent", "_raw", "_position", "_line", "_version", "_deprecated", "_omp_version")
     _attribute_nodes = ()
 
-    def __init__(self, **kwargs):
+    def __init__ (self, parent=None, omp_version=None, raw=None, position=None, line=None, VERSION=None, DEPRECATED=None):
         super().__init__()
-        self._parent = kwargs.pop("parent", None)
-        self._omp_version = kwargs.pop("omp_version", None)
+        self._parent=parent
+        self._omp_version = omp_version
 
         if self._omp_version is None:
             # fetch the omp version from the root model, it will not be fetched in after the syntactic stage
@@ -27,23 +53,29 @@ class OmpAnnotatedComment(PyccelAstNode):
                 raise NotImplementedError(
                     "OpenMP version not set"
                 )
-        self._raw = kwargs.pop("raw", None)
-        self._position = kwargs.pop('position', None)
-        self._line = kwargs.pop('line', None)
+        self._raw = raw
+        self._position = position
+        self._line = line
         if self._position is None and hasattr(self, '_tx_position') and hasattr(self, '_tx_position_end'):
             self._position = (self._tx_position, self._tx_position_end)
         assert self._raw or self._position
-        self._version = kwargs.pop("VERSION", 0.0) or 0.0
+        self._version = VERSION or 0.0
         if isinstance(self._version, list):
             self._version = max(self._version)
-        self._deprecated = float(kwargs.pop("DEPRECATED", "inf") or "inf")
+        self._deprecated = DEPRECATED or float("inf")
         if self._version > self._omp_version:
-            raise NotImplementedError(
-                f"Syntax not supported in OpenMP version {self._omp_version}"
+            errors.report(
+                f"Syntax not supported in OpenMp version {self._omp_version}",
+                symbol=self,
+                column=self._position[0],
+                severity="fatal",
             )
         if self._deprecated <= self._omp_version:
-            raise NotImplementedError(
-                f"Syntax deprecated in OpenMP version {self._deprecated}"
+            errors.report(
+                f"Syntax deprecated in OpenMp version {self._omp_version}",
+                symbol=self,
+                column=self._position[0],
+                severity="fatal",
             )
 
     @property
@@ -52,7 +84,7 @@ class OmpAnnotatedComment(PyccelAstNode):
         returns the parent of the omp object.
         """
         omp_user_nodes = self.get_user_nodes((OmpClause, OmpDirective, OmpConstruct),
-                                             excluded_nodes=(OmpEndDirective))
+                                             excluded_nodes=(OmpEndDirective,))
         return omp_user_nodes[0] if len(omp_user_nodes) else self._parent
 
     @property
@@ -109,11 +141,31 @@ class OmpAnnotatedComment(PyccelAstNode):
             'line': self.line,
             'raw': self.raw,
             'VERSION': self.VERSION,
-            'deprecated': self.deprecated,
+            'DEPRECATED': self.deprecated,
             'omp_version': self.omp_version,
         }
 
 class OmpConstruct(PyccelAstNode):
+    """
+    Represents an OpenMP Construct.
+
+    An OpenMP construct is a block of code , delimited by an OpenMP Directive at the
+    start and potentially an end directive.
+
+    Parameters
+    ----------
+    start : OmpDirective
+        The directive that marks the start of the construct.
+
+    body : CodeBlock
+        The block of code (code statements) or syntax forming the body within the construct.
+
+    end : OmpEndDirective, optional
+        The directive that marks the end of the construct, if applicable.
+
+    omp_version : float
+        The OpenMP version chosen by the user.
+    """
     __slots__ = ("_start", "_end", "_body", "_omp_version")
     _attribute_nodes = ("_start", "_end", "_body")
 
@@ -139,18 +191,14 @@ class OmpConstruct(PyccelAstNode):
         """Returns the end directive that marks the end of the construct"""
         return self._end
 
-    @end.setter
-    def end(self, end):
-        """Used by the printers to tweak the expression of the construct"""
-        self._end = end
-
     @property
     def name(self):
+        """Returns the name of the construct"""
         return self._start.name
 
     @property
     def omp_version(self):
-        """returns the openmp version used"""
+        """returns the openmp version chosen by the user"""
         return self._omp_version
 
 class OmpDirective(OmpAnnotatedComment):
@@ -165,18 +213,19 @@ class OmpDirective(OmpAnnotatedComment):
     is_construct bool
           True if the directive is syntactically incorrect without a corresponding end directive
 
-    clauses: OmpClause
+    clauses: tuple of OmpClause
               Clauses passed to the directive
     """
     __slots__ = ("_name", "_clauses", "_is_construct", "_tx_clauses", "_invalid_clauses")
     _attribute_nodes = ("_clauses",)
 
-    def __init__(self, **kwargs):
+    def __init__(self, name=None, clauses=(), is_construct=False, parent=None, omp_version=None, raw=None,
+                     position=None, line=None, VERSION=None, DEPRECATED=None):
 
-        self._name = kwargs.pop("name")
-        self._is_construct = kwargs.pop("is_construct", False)
-        self._clauses = kwargs.pop("clauses", [])
-        super().__init__(**kwargs)
+        self._name = name
+        self._is_construct = is_construct
+        self._clauses = clauses
+        super().__init__(parent=parent, omp_version=omp_version, raw=raw, position=position, line=line, VERSION=VERSION, DEPRECATED=DEPRECATED)
 
     @property
     def name(self):
@@ -205,28 +254,29 @@ class OmpDirective(OmpAnnotatedComment):
             p = self
             while hasattr(p, 'parent'):
                 p = p.parent
-            return re.sub(r"#\s*\$\s*omp\s*(end)?", "", p.raw)
+            self._raw = re.sub(r"#\s*\$\s*omp\s*(end)?", "", p.raw)
+            return self._raw
 
     def has_clause(self, clause_name):
         """checks if the directive has a clause with the name clause_name"""
         return any(c.name == clause_name for c in self.clauses)
 
     @classmethod
-    def from_directive(cls, directive, **kwargs):
+    def from_directive(cls, directive, parent=None, clauses=None):
         """Takes a directive and returns a directive that keeps its unchangeable state"""
-        clauses = kwargs.pop('clauses', directive.clauses)
-        parent = kwargs.pop('parent', directive.parent)
-        coresponding_directive = kwargs.pop('coresponding_directive', None)
+        if not clauses:
+            clauses = directive.clauses
+        if not parent:
+            parent = directive.parent
         d_attrs = directive.get_fixed_state()
         return cls(clauses=clauses,
                    is_construct=directive.is_construct,
                    name=directive.name,
                    parent=parent,
-                   coresponding_directive=coresponding_directive,
                    **d_attrs, )
 
     @classmethod
-    def from_tx_directive(cls, directive, fst=None):
+    def from_tx_directive(cls, directive, omp_version, fst=None):
         """Takes a tx object that represent a directive and returns a directive"""
         is_construct = directive.is_construct
         if is_construct is None:
@@ -237,12 +287,11 @@ class OmpDirective(OmpAnnotatedComment):
         tx_clauses = [c for c in tx_clauses if c]
         tx_clauses = [c.clause if hasattr(c, 'clause') else c for c in tx_clauses]
         # Transform the tx clauses to OmpClause
-        clauses = [OmpClause.from_tx_clause(c, parent=directive) if not isinstance(c, OmpList) else c for
-                   c in tx_clauses]
+        clauses = tuple(OmpClause.from_tx_clause(c, omp_version, parent=directive) for c in tx_clauses)
         # Get a list containing all the versions of the childs omp objcets,
         # necessary to calculate the directives versions.
         omp_objs = [directive] + tx_clauses
-        VERSION = [getattr(o, 'VERSION') for o in omp_objs if getattr(o, 'VERSION', None)]
+        VERSION = [getattr(o, 'VERSION') for o in omp_objs if hasattr(o, 'VERSION')]
         VERSION = [v for l_v in VERSION for v in (l_v if isinstance(l_v, list) else [l_v])]
         # Get the position of the directive from tx attributes.
         position = (directive._tx_position, directive._tx_position_end)
@@ -263,9 +312,8 @@ class OmpDirective(OmpAnnotatedComment):
                                           VERSION=VERSION,
                                           DEPRECATED=getattr(directive, 'deprecated', float('inf')),
                                           position=position,
-                                          fst=fst,
-                                          line=None,
-                                          raw=None)
+                                          # fst=fst,
+                                          omp_version=omp_version)
         else:
             return cls(clauses=clauses,
                        is_construct=is_construct,
@@ -274,39 +322,42 @@ class OmpDirective(OmpAnnotatedComment):
                        VERSION=VERSION,
                        DEPRECATED=getattr(directive, 'deprecated', float('inf')),
                        position=position,
-                       fst=fst,
-                       line=None,
-                       raw=None)
+                       # fst=fst,
+                       omp_version=omp_version)
 
 class OmpEndDirective(OmpDirective):
     """Represents an OpenMP End Directive."""
 
-    __slots__ = ("_coresponding_directive",)
-    _attribute_nodes = ("_coresponding_directive",)
-
-
-    def __init__(self, **kwargs):
-        self._coresponding_directive = kwargs.pop("coresponding_directive", None)
-        super().__init__(**kwargs)
-
-    @property
-    def coresponding_directive(self):
-        return self._coresponding_directive
-
+    def __init__(self, name=None, clauses=[], is_construct=False, parent=None, omp_version=None, raw=None,
+        position=None, line=None, VERSION=None, DEPRECATED=None):
+        super().__init__(name=name, clauses=clauses, is_construct=is_construct, parent=parent, omp_version=omp_version, raw=raw, position=position, line=line, VERSION=VERSION, DEPRECATED=DEPRECATED)
 
 class OmpClause(OmpAnnotatedComment):
-    """Represents an OpenMP Clause in the code."""
+    """
+    Represents an OpenMP Clause.
+
+    Parameters
+    ----------
+    name: str
+          The name of the clause
+
+    omp_exprs: tuple
+          OpenMP expressions passed to the clause
+
+    allowed_parents: set or None
+          A set of allowed parent directives for this clause or None if there are no restrictions
+    """
     __slots__ = ("_omp_exprs", "_name", "_allowed_parents")
     _attribute_nodes = ("_omp_exprs",)
 
-    def __init__(self, **kwargs):
-        self._omp_exprs = kwargs.pop('omp_exprs', [])
-        if not isinstance(self._omp_exprs, list):
-            self._omp_exprs = [self.omp_exprs]
-        self._name = kwargs.pop('name', None)
-        super().__init__(**kwargs)
+    def __init__ (self, name=None, omp_exprs=(), allowed_parents=None, parent=None, omp_version=None, raw=None, position=None, line=None, VERSION=None, DEPRECATED=None):
+        self._omp_exprs = omp_exprs
+        if not isinstance(self._omp_exprs, tuple):
+            self._omp_exprs = (self.omp_exprs,)
+        self._name = name
+        super().__init__(parent=parent, omp_version=omp_version, raw=raw, position=position, line=line, VERSION=VERSION, DEPRECATED=DEPRECATED)
         # check if the parent directive accepts the current clause.
-        self._allowed_parents = kwargs.pop('allowed_parents', None)
+        self._allowed_parents = allowed_parents
         if self._allowed_parents is not None:
             if self.parent.name not in self._allowed_parents:
                 errors.report(
@@ -316,10 +367,12 @@ class OmpClause(OmpAnnotatedComment):
                 )
 
     @classmethod
-    def from_clause(cls, clause, **kwargs):
+    def from_clause(cls, clause, omp_exprs=None, parent=None):
         """Takes a clause and returns a clause that keeps its unchangeable state"""
-        omp_exprs = kwargs.pop('omp_exprs', clause.omp_exprs)
-        parent = kwargs.pop('parent', clause.parent)
+        if not omp_exprs:
+            omp_exprs = clause.omp_exprs
+        if not parent:
+            parent = clause.parent
         d_attrs = clause.get_fixed_state()
         return cls(omp_exprs=omp_exprs,
                    name=clause.name,
@@ -328,11 +381,12 @@ class OmpClause(OmpAnnotatedComment):
                    **d_attrs, )
 
     @classmethod
-    def from_tx_clause(cls, tx_clause, **kwargs):
+    def from_tx_clause(cls, tx_clause, omp_version, parent=None):
         """Takes a tx object that represent a clause and returns an OmpClause"""
+        if not parent:
+            parent = getattr('parent', tx_clause, None)
         # Get the all the Allowed parents of the omp objects used inside the clause.
         # sets allowed parents to None if there are no restrictions about the allowed_parents
-        parent = kwargs.pop('parent', tx_clause.parent)
         omp_objs = [tx_clause] + [getattr(tx_clause, attr_name) for attr_name in tx_clause._tx_attrs]
         allowed_parents = [set(getattr(obj, 'allowed_parents')) for obj in omp_objs
                            if getattr(obj, 'allowed_parents', None)]
@@ -340,14 +394,13 @@ class OmpClause(OmpAnnotatedComment):
         # Get the position of the clause
         position = (tx_clause._tx_position, tx_clause._tx_position_end)
         return cls(position=position,
-                   omp_exprs=getattr(tx_clause, 'omp_exprs', []),
+                   omp_exprs=getattr(tx_clause, 'omp_exprs', tuple()),
                    name=getattr(tx_clause, 'name', None),
                    parent=parent,
                    allowed_parents=allowed_parents,
                    VERSION=getattr(tx_clause, 'VERSION', 0.0),
                    DEPRECATED=getattr(tx_clause, 'deprecated', float('inf')),
-                   raw=None,
-                   line=None)
+                   omp_version=omp_version,)
 
     @property
     def omp_exprs(self):
@@ -365,15 +418,28 @@ class OmpClause(OmpAnnotatedComment):
         return self._allowed_parents
 
 
-class OmpExpr(OmpClause):
+class OmpExpr(OmpAnnotatedComment):
     """Parent class of omp object that should represent a python expression"""
+
+    """
+    Parent class of omp object that should represent a python expression
+
+    Parameters
+    ----------
+    value: str or any
+          The value or Python expression represented by this object
+
+    throw_away: any
+          A placeholder for additional processing or transient data, if any
+    """
 
     __slots__ = ("_value",)
     _attribute_nodes = ()
 
-    def __init__(self, **kwargs):
-        self._value = kwargs.pop("value", None)
-        super().__init__(**kwargs)
+    def __init__ (self, value=None, throw_away=None, parent=None, omp_version=None, raw=None, position=None, line=None, VERSION=None, DEPRECATED=None):
+        # self._value = kwargs.pop("value", None)
+        self._value = value
+        super().__init__(parent=parent, omp_version=omp_version, raw=raw, position=position, line=line, VERSION=VERSION, DEPRECATED=DEPRECATED)
 
     @property
     def value(self):
@@ -408,9 +474,10 @@ class OmpIntegerExpr(OmpExpr):
 
 class OmpList(OmpExpr):
     """Represents a list"""
-
     @property
-    def raw(self):
-        """Returns a tweaked raw that represents a tuple"""
-        raw = super().raw
-        return f"({raw},)"
+    def value(self):
+        """Returns the expression, or the tweaked raw that should represent a python expression"""
+        if self._value:
+            return self._value
+        else:
+            return f"({self.raw},)"
