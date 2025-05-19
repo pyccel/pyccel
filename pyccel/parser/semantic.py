cdc2e93c7bd86747e6972caa7dc98154a3b3a3bb
EmilyBourne <louise.bourne@gmail.com>
2025-05-19 10:46:54 +0200

    Don't use dunder

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -3576,7 +3576,7 @@ class SemanticParser(BasicParser):
                     recognised_mod = recognised_source(mod_name)
                 elif mod_name is None and isinstance(env_var, BuiltinFunctionType):
                     # Handling of BuiltinFunctionType is necessary for Python 3.9 (NumPy 1.* doesn't specify __module__)
-                    mod_name = env_var.__str__().split(' of ')[-1].split(' object ')[0]
+                    mod_name = str(env_var).split(' of ')[-1].split(' object ')[0]
                     while mod_name and not recognised_source(mod_name):
                         mod_name = mod_name.rsplit('.', 1)[0]
 
