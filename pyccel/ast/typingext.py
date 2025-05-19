d88f91c6b5084bbc83af8a830d02127f4e40ff37
EmilyBourne <louise.bourne@gmail.com>
2025-05-19 09:16:42 +0200

    Add extra arguments for Python 3.12

--- pyccel/ast/typingext.py
+++ pyccel/ast/typingext.py
@@ -76,24 +76,34 @@ class TypingTypeVar(TypedAstNode):
     *constraints : PyccelAstNode
         The possible annotations that this TypeVar can represent.
     bound : PyccelAstNode
-        The superclass from which the type must inherit.
+        The superclass from which the type must inherit. See PEP 484.
     covariant : bool
-        Indicates if the TypeVar can represent superclasses of the constraints.
+        Indicates if the TypeVar can represent superclasses of the constraints. See PEP 484.
     contravariant : bool
-        Indicates if the TypeVar can represent subclasses of the constraints.
+        Indicates if the TypeVar can represent subclasses of the constraints. See PEP 484.
+    infer_variance : bool
+        Indicates if the variance (see covariant/contravariant) should be inferred from
+        use. See PEP 695.
+    default : TypedAstNode
+        The type that should be chosen if the type cannot be deduced from the call.
+        This can sometimes be the case for parametrised classes. See PEP 696.
     """
-    __slots__ = ('_name', '_possible_types')
+    __slots__ = ('_name', '_possible_types', '_default')
     _attribute_nodes = ()
     _class_type = TypeAlias()
     _shape = None
 
-    def __init__(self, name, *constraints, bound=None, covariant=False, contravariant=False):
+    def __init__(self, name, *constraints, bound=None, covariant=False, contravariant=False,
+            infer_variance=False, default=None):
         if covariant or contravariant or bound:
             raise TypeError("Covariant, contravariant and bound types are not currently supported")
         if len(constraints) == 0:
             raise TypeError(f"The possible types for {name} must be specified")
+        if default is not None and default not in constraints:
+            raise TypeError("The default value of the TypeVar must be one of the constraints.")
         self._name = name
         self._possible_types = constraints
+        self._default = default
         super().__init__()
 
     @property
