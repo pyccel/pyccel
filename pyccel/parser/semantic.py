29c3a56198bd8556d095231cc1666d16722a9c2f
EmilyBourne <louise.bourne@gmail.com>
2025-05-30 15:38:07 +0200

    Ensure __init__ is visited sufficiently early

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -2666,6 +2666,10 @@ class SemanticParser(BasicParser):
         for c in expr.classes:
             self._visit(c)
 
+        # Visit all __init__ methods only once everything is inserted into scope
+        for c in expr.classes:
+            self._visit(c.get_method('__init__'))
+
         init_func_body += self._visit(expr.init_func).body
         mod_name = self.metavars.get('module_name', None)
         if mod_name is None:
