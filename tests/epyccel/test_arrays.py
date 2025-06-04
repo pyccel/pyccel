a85a75d0b950d441069e777cf43501323cedb74b
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2025-06-04 10:11:36 +0200

    Commented pytestmark = pytest.mark.skip_llvm

--- tests/epyccel/test_arrays.py
+++ tests/epyccel/test_arrays.py
@@ -8,7 +8,7 @@ from pyccel import epyccel
 from modules        import arrays
 
 # Skip all tests if PYCCEL_DEFAULT_COMPILER=LLVM
-pytestmark = pytest.mark.skip_llvm
+# pytestmark = pytest.mark.skip_llvm
 
 RTOL = 1e-12
 ATOL = 1e-16
