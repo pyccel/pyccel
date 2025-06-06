68f0a733c2fb1245b6fce070e2d75cfdfd8ae3c6
Emily Bourne <louise.bourne@gmail.com>
2025-06-06 18:58:09 +0200

    handle python 3.9

--- tests/pyccel/scripts/import_syntax/user_mod2.py
+++ tests/pyccel/scripts/import_syntax/user_mod2.py
@@ -1,4 +1,4 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
 
-def user_func(x1 : int | float, x2 : float):
+def user_func(x1 : 'int | float', x2 : float):
     return x1+x2
