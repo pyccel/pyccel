7731303dc8201dd0859bd05fb63c08aa7880c4f8
EmilyBourne <louise.bourne@gmail.com>
2025-06-10 13:21:54 +0200

    Add myst_parser to extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '*'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc",
+extensions = ["sphinx.ext.todo", "sphinx.ext.viewcode", "sphinx.ext.autodoc", "myst_parser",
               "sphinx.ext.coverage", "sphinx.ext.doctest", "sphinx.ext.napoleon", "sphinx.ext.autosummary"]
 
 # Add any paths that contain templates here, relative to this directory.
