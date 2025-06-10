1460ad85973eac6bdffeebfec90e3efb11377f33
EmilyBourne <louise.bourne@gmail.com>
2025-06-10 12:56:39 +0200

    Not needed/helpful for Python

--- pyccel/codegen/codegen.py
+++ pyccel/codegen/codegen.py
@@ -159,7 +159,7 @@ class Codegen:
 
     def get_printer_imports(self):
         """return the imports of the current codeprinter"""
-        additional_imports = self._printer.get_additional_imports()
+        additional_imports = self._printer.get_additional_imports().copy()
         if self._parser.metavars['printer_imports']:
             for i in self._parser.metavars['printer_imports'].split(','):
                 additional_imports.setdefault(i, None)
@@ -234,9 +234,10 @@ class Codegen:
                 f.write(code)
 
         code = printer_registry['python'](self.parser.filename).doprint(module_header)
-        printer_imports = ','.join(self.get_printer_imports().keys())
-        if printer_imports:
-            code = f'#$ header metavar printer_imports="{printer_imports}"\n' + code
+        if self.language != 'python':
+            printer_imports = ','.join(self.get_printer_imports().keys())
+            if printer_imports:
+                code = f'#$ header metavar printer_imports="{printer_imports}"\n' + code
         with open(pyi_filename, 'w', encoding="utf-8") as f:
             f.write(code)
 
