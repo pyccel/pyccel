5755633934924a5443e80be88b80687f93076f4d
EmilyBourne <louise.bourne@gmail.com>
2025-06-11 10:22:09 +0200

    Any arguments are allowed for inline

--- tests/errors/semantic/blocking/INCOMPATIBLE_ARGUMENT3.py
+++
@@ -1,9 +0,0 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring
-
-from pyccel.decorators import inline
-
-@inline
-def add(a: int, b: 'int | complex'):
-    return a + b
-
-z = add(3., 2.)
