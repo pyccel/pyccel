b1f9fb1b18545e787da4cca40cd30617d96b8fa9
EmilyBourne <louise.bourne@gmail.com>
2025-06-12 18:05:42 +0200

    Update coverage

--- tests/pyccel/scripts/runtest_decorators_inline.py
+++ tests/pyccel/scripts/runtest_decorators_inline.py
@@ -2,6 +2,9 @@
 import numpy as np
 from decorators_inline import power_4, get_powers, f, sin_base_1, fill_pi, positron_charge
 
+# Set a different value for pi to ensure the expected global variable is used
+pi = 42
+
 def g(s : int):
     return f(s)/3
 
