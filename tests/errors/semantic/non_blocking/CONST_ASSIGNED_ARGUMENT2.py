bbf6e93527c65e7542e2f85c9c9322abd70afe5e
EmilyBourne <louise.bourne@gmail.com>
2025-06-12 15:46:56 +0200

    Missing import

--- tests/errors/semantic/non_blocking/CONST_ASSIGNED_ARGUMENT2.py
+++ tests/errors/semantic/non_blocking/CONST_ASSIGNED_ARGUMENT2.py
@@ -1,4 +1,5 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
+from typing import Final #pylint: disable=unused-import
 
 def array_int32_1d_add_const( x : 'Final[int32[:]]', y : 'int32[:]' ):
     x[:] += y
