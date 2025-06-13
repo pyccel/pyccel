1de79a98cb876dc8c50bd289a07d16b26f69ea72
EmilyBourne <louise.bourne@gmail.com>
2025-06-13 13:44:27 +0200

    Replace lapack macros with inline functions

--- pyccel/stdlib/external/lapack.py
+++ pyccel/stdlib/external/lapack.py
@@ -8,18 +8,63 @@ Module exposing the lapack library functions to pyccel (see http://www.netlib.or
 """
 
 #$ header metavar ignore_at_import=True
+import numpy as np
+from pyccel.decorators import inline
+import pyccel.stdlib.internal.lapack as lapack
 
-from pyccel.stdlib.internal.lapack import dgetrf
-from pyccel.stdlib.internal.lapack import dgetrs
-from pyccel.stdlib.internal.lapack import dgbtrf
-from pyccel.stdlib.internal.lapack import dgbtrs
+@inline
+def dgetrf(a : 'float64[:,:]', overwrite_a : bool = False):
+    ipiv = np.empty(np.int32(a.shape[1]), dtype = np.int32)
+    info : np.int32
+    if overwrite_a:
+        lapack.dgetrf(np.int32(a.shape[1]), np.int32(a.shape[0]), a.T, np.int32(a.shape[1]), ipiv, info)
+        return a, ipiv, info
+    else:
+        lu = np.array(a)
+        lapack.dgetrf(np.int32(a.shape[1]), np.int32(a.shape[0]), lu.T, np.int32(a.shape[1]), ipiv, info)
+        return lu, ipiv, info
 
+@inline
+def dgetrs(lu : 'float64[:,:]', piv : 'int32[:]', b : 'float64[:,:]',
+           trans : str ='N', overwrite_b : bool = False):
+    ipiv = np.empty(m, dtype = np.int32)
+    info : np.int32
+    if not overwrite_b:
+        storage_b = np.array(b)
+        b_ptr = storage_b.T
+    else:
+        b_ptr = b.T
 
+    lapack.dgetrs(s, np.int32(lu.shape[0]), np.int32(b.shape[0]), lu.T,
+            np.int32(lu.shape[1]), piv, b_ptr, np.int32(b.shape[1]), info)
+    return b, info
 
-#$ header macro (ab(:,:), IPIV(ab.shape[0]), info), dgetrf(ab) := dgetrf(ab.shape[0], ab.shape[1], ab, ab.shape[0], IPIV, info)
-#$ header macro (b(:,:), info), dgetrs(ab, piv, b, s='N') := dgetrs(s, ab.shape[1], 1, ab, ab.shape[0], piv, b, b.count, info)
+@inline
+def dgbtrf(ab : 'float64[:,:]', kl : int, ku : int,
+        m : int = None, n : int = None, ldab : int = None,
+        overwrite_ab : bool = False):
+    m_val = np.int32(ab.shape[0] if m is None else m)
+    n_val = np.int32(ab.shape[0] if n is None else n)
+    ldab_val = np.int32(max(ab.shape[1], 1) if ldab is None else ldab)
+    ipiv = np.empty(min(m, n), dtype = np.int32)
+    info : np.int32
+    if overwrite_ab:
+        lapack.dgbtrf(m_val, n_val, np.int32(kl), np.int32(ku), ab.T, ldab_val, ipiv, info)
+        return ab, ipiv, info
+    else:
+        lu = np.array(ab)
+        lapack.dgbtrf(m_val, n_val, np.int32(kl), np.int32(ku), lu.T, ldab_val, ipiv, info)
+        return lu, ipiv, info
 
-#$ header macro (ab(:,:), IPIV(ab.shape[0]), info), dgbtrf(ab, lab, uab) := dgbtrf(ab.shape[0], ab.shape[1], lab, uab, ab, ab.shape[0], IPIV, info)
-#$ header macro (b(:,:), info), dgbtrs(ab, lab, uab, b, piv, s='N') := dgbtrs(s, ab.shape[1], lab, uab, 1, ab, ab.shape[0], piv, b, b.count, info)
+@inline
+def dgbtrs(ab : 'float64[:,:]', kl : int, ku : int, b : 'float64[:,:]',
+           ipiv : np.int32, trans : str ='N', n : int = None,
+           ldab : int = None, ldb : int = None, overwrite_b : bool = False):
+    n_val = np.int32(ab.shape[0] if n is None else n)
+    ldab_val = np.int32(max(ab.shape[1], 1) if ldab is None else ldab)
+    ldb_val = np.int32(ab.shape[0] if ldb is None else ldb)
+    info : np.int32
+    lapack.dgbtrs(trans, n_val, kl, ku, b.shape[0], ab.T, ldab_val, ipiv, b.T, ldb_val, info)
+    return b, info
 
 
