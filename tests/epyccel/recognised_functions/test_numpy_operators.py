4ecf113d6a4e587e3a6e6480cea6bf2afa436229
EmilyBourne <louise.bourne@gmail.com>
2025-06-20 10:25:14 +0200

    Increase test coverage

--- tests/epyccel/recognised_functions/test_numpy_operators.py
+++ tests/epyccel/recognised_functions/test_numpy_operators.py
@@ -12,6 +12,10 @@ IT2 = TypeVar('IT2', *int_types)
 def test_numpy_bit_and(language):
     def f(a : 'IT[:,:,:]', b : 'IT2[:,:,:]'):
         return a & b
+    def g(a : 'IT[:,:,:]', b : 'IT2'):
+        return a & b
+    def h(a : 'IT', b : 'IT2[:,:,:]'):
+        return a & b
 
     for t_x in int_types:
         for t_y in int_types:
@@ -19,14 +23,32 @@ def test_numpy_bit_and(language):
             y = randint(2 if t_y is bool else 127, size=(2,3,4), dtype = t_y)
 
             epyc_f = epyccel(f, language=language)
+            epyc_g = epyccel(g, language=language)
+            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
             assert np.array_equal(epyc_z, z)
             assert z.dtype is epyc_z.dtype
 
+            y = randint(2 if t_y is bool else 127, dtype = t_y)
+
+            z2 = g(x,y)
+            epyc_z2 = epyc_g(x,y)
+            assert np.array_equal(epyc_z2, z2)
+            assert z2.dtype is epyc_z2.dtype
+
+            z3 = h(y,x)
+            epyc_z3 = epyc_h(y,x)
+            assert np.array_equal(epyc_z3, z3)
+            assert z3.dtype is epyc_z3.dtype
+
 def test_numpy_bit_or(language):
     def f(a : 'IT[:,:,:]', b : 'IT2[:,:,:]'):
+        return a | b
+    def g(a : 'IT[:,:,:]', b : 'IT2'):
+        return a | b
+    def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a | b
 
     for t_x in int_types:
@@ -35,14 +57,32 @@ def test_numpy_bit_or(language):
             y = randint(2 if t_y is bool else 127, size=(2,3,4), dtype = t_y)
 
             epyc_f = epyccel(f, language=language)
+            epyc_g = epyccel(g, language=language)
+            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
             assert np.array_equal(epyc_z, z)
             assert z.dtype is epyc_z.dtype
 
+            y = randint(2 if t_y is bool else 127, dtype = t_y)
+
+            z2 = g(x,y)
+            epyc_z2 = epyc_g(x,y)
+            assert np.array_equal(epyc_z2, z2)
+            assert z2.dtype is epyc_z2.dtype
+
+            z3 = h(y,x)
+            epyc_z3 = epyc_h(y,x)
+            assert np.array_equal(epyc_z3, z3)
+            assert z3.dtype is epyc_z3.dtype
+
 def test_numpy_bit_xor(language):
     def f(a : 'IT[:,:,:]', b : 'IT2[:,:,:]'):
+        return a ^ b
+    def g(a : 'IT[:,:,:]', b : 'IT2'):
+        return a ^ b
+    def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a ^ b
 
     for t_x in int_types:
@@ -51,14 +91,32 @@ def test_numpy_bit_xor(language):
             y = randint(2 if t_y is bool else 127, size=(2,3,4), dtype = t_y)
 
             epyc_f = epyccel(f, language=language)
+            epyc_g = epyccel(g, language=language)
+            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
             assert np.array_equal(epyc_z, z)
             assert z.dtype is epyc_z.dtype
 
+            y = randint(2 if t_y is bool else 127, dtype = t_y)
+
+            z2 = g(x,y)
+            epyc_z2 = epyc_g(x,y)
+            assert np.array_equal(epyc_z2, z2)
+            assert z2.dtype is epyc_z2.dtype
+
+            z3 = h(y,x)
+            epyc_z3 = epyc_h(y,x)
+            assert np.array_equal(epyc_z3, z3)
+            assert z3.dtype is epyc_z3.dtype
+
 def test_numpy_bit_lshift(language):
     def f(a : 'IT[:,:,:]', b : 'IT2[:,:,:]'):
+        return a << b
+    def g(a : 'IT[:,:,:]', b : 'IT2'):
+        return a << b
+    def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a << b
 
     for t_x in int_types:
@@ -67,14 +125,35 @@ def test_numpy_bit_lshift(language):
             y = randint(2 if t_y is bool else 5, size=(2,3,4), dtype = t_y)
 
             epyc_f = epyccel(f, language=language)
+            epyc_g = epyccel(g, language=language)
+            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
             assert np.array_equal(epyc_z, z)
             assert z.dtype is epyc_z.dtype
 
+            y = randint(2 if t_y is bool else 5, dtype = t_y)
+
+            z2 = g(x,y)
+            epyc_z2 = epyc_g(x,y)
+            assert np.array_equal(epyc_z2, z2)
+            assert z2.dtype is epyc_z2.dtype
+
+            x = randint(2 if t_x is bool else 32, dtype = t_x)
+            y = randint(2 if t_y is bool else 5, size=(2,3,4), dtype = t_y)
+
+            z3 = h(x,y)
+            epyc_z3 = epyc_h(x,y)
+            assert np.array_equal(epyc_z3, z3)
+            assert z3.dtype is epyc_z3.dtype
+
 def test_numpy_bit_rshift(language):
     def f(a : 'IT[:,:,:]', b : 'IT2[:,:,:]'):
+        return a >> b
+    def g(a : 'IT[:,:,:]', b : 'IT2'):
+        return a >> b
+    def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a >> b
 
     for t_x in int_types:
@@ -83,11 +162,28 @@ def test_numpy_bit_rshift(language):
             y = randint(2 if t_y is bool else 5, size=(2,3,4), dtype = t_y)
 
             epyc_f = epyccel(f, language=language)
+            epyc_g = epyccel(g, language=language)
+            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
             assert np.array_equal(epyc_z, z)
             assert z.dtype is epyc_z.dtype
+
+            y = randint(2 if t_y is bool else 5, dtype = t_y)
+
+            z2 = g(x,y)
+            epyc_z2 = epyc_g(x,y)
+            assert np.array_equal(epyc_z2, z2)
+            assert z2.dtype is epyc_z2.dtype
+
+            x = randint(2 if t_x is bool else 32, dtype = t_x)
+            y = randint(2 if t_y is bool else 5, size=(2,3,4), dtype = t_y)
+
+            z3 = h(x,y)
+            epyc_z3 = epyc_h(x,y)
+            assert np.array_equal(epyc_z3, z3)
+            assert z3.dtype is epyc_z3.dtype
 
 def test_numpy_bit_invert(language):
     def f(a : 'IT[:,:,:]'):
