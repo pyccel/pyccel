3b7fe8526534cab38ba77c98cbf778467b4dfc10
EmilyBourne <louise.bourne@gmail.com>
2025-06-20 11:30:16 +0200

    Translation should only be done once

--- tests/epyccel/recognised_functions/test_numpy_operators.py
+++ tests/epyccel/recognised_functions/test_numpy_operators.py
@@ -19,14 +19,14 @@ def test_numpy_bit_and(language):
     def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a & b
 
+    epyc_f = epyccel(f, language=language)
+    epyc_g = epyccel(g, language=language)
+    epyc_h = epyccel(h, language=language)
+
     for t_x in int_types:
         for t_y in int_types:
             x = randint(2 if t_x is bool else 127, size=(2,3,4), dtype = t_x)
             y = randint(2 if t_y is bool else 127, size=(2,3,4), dtype = t_y)
-
-            epyc_f = epyccel(f, language=language)
-            epyc_g = epyccel(g, language=language)
-            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
@@ -53,14 +53,14 @@ def test_numpy_bit_or(language):
     def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a | b
 
+    epyc_f = epyccel(f, language=language)
+    epyc_g = epyccel(g, language=language)
+    epyc_h = epyccel(h, language=language)
+
     for t_x in int_types:
         for t_y in int_types:
             x = randint(2 if t_x is bool else 127, size=(2,3,4), dtype = t_x)
             y = randint(2 if t_y is bool else 127, size=(2,3,4), dtype = t_y)
-
-            epyc_f = epyccel(f, language=language)
-            epyc_g = epyccel(g, language=language)
-            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
@@ -87,14 +87,14 @@ def test_numpy_bit_xor(language):
     def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a ^ b
 
+    epyc_f = epyccel(f, language=language)
+    epyc_g = epyccel(g, language=language)
+    epyc_h = epyccel(h, language=language)
+
     for t_x in int_types:
         for t_y in int_types:
             x = randint(2 if t_x is bool else 127, size=(2,3,4), dtype = t_x)
             y = randint(2 if t_y is bool else 127, size=(2,3,4), dtype = t_y)
-
-            epyc_f = epyccel(f, language=language)
-            epyc_g = epyccel(g, language=language)
-            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
@@ -121,14 +121,14 @@ def test_numpy_bit_lshift(language):
     def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a << b
 
+    epyc_f = epyccel(f, language=language)
+    epyc_g = epyccel(g, language=language)
+    epyc_h = epyccel(h, language=language)
+
     for t_x in int_types:
         for t_y in int_types:
             x = randint(2 if t_x is bool else 32, size=(2,3,4), dtype = t_x)
             y = randint(2 if t_y is bool else 5, size=(2,3,4), dtype = t_y)
-
-            epyc_f = epyccel(f, language=language)
-            epyc_g = epyccel(g, language=language)
-            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
@@ -158,14 +158,14 @@ def test_numpy_bit_rshift(language):
     def h(a : 'IT', b : 'IT2[:,:,:]'):
         return a >> b
 
+    epyc_f = epyccel(f, language=language)
+    epyc_g = epyccel(g, language=language)
+    epyc_h = epyccel(h, language=language)
+
     for t_x in int_types:
         for t_y in int_types:
             x = randint(2 if t_x is bool else 32, size=(2,3,4), dtype = t_x)
             y = randint(2 if t_y is bool else 5, size=(2,3,4), dtype = t_y)
-
-            epyc_f = epyccel(f, language=language)
-            epyc_g = epyccel(g, language=language)
-            epyc_h = epyccel(h, language=language)
 
             z = f(x,y)
             epyc_z = epyc_f(x,y)
@@ -193,10 +193,10 @@ def test_numpy_bit_invert(language):
     def f(a : 'IT[:,:,:]'):
         return ~a
 
+    epyc_f = epyccel(f, language=language)
+
     for t_x in int_types:
         x = randint(2 if t_x is bool else 127, size=(2,3,4), dtype = t_x)
-
-        epyc_f = epyccel(f, language=language)
 
         z = f(x)
         epyc_z = epyc_f(x)
