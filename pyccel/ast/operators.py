30882c3a1339698845266a23c48932c2942959de
EmilyBourne <louise.bourne@gmail.com>
2025-06-24 16:24:14 +0200

    Missing comma

--- pyccel/ast/operators.py
+++ pyccel/ast/operators.py
@@ -37,7 +37,7 @@ __all__ = (
     'PyccelOperator',
     'PyccelUnaryOperator',
     # --- Operator classes ---
-    'IfTernaryOperator'
+    'IfTernaryOperator',
     'PyccelAdd',
     'PyccelAnd',
     'PyccelAssociativeParenthesis',
