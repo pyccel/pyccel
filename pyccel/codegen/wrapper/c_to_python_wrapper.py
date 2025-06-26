e02b28d6f4181cf3c3748f80a954a540c004c61f
EmilyBourne <louise.bourne@gmail.com>
2025-06-26 17:47:16 +0200

    Fix #2306 by using the class type as key instead of a description of the class type

--- pyccel/codegen/wrapper/c_to_python_wrapper.py
+++ pyccel/codegen/wrapper/c_to_python_wrapper.py
@@ -469,17 +469,15 @@ class CToPythonWrapper(Wrapper):
         for i, py_arg in enumerate(args):
             # Get the relevant typed arguments from the original functions
             interface_args = [func.arguments[i].var for func in orig_funcs]
-            # Get the type key
-            interface_types = [(str(a.dtype), a.rank, a.order) for a in interface_args]
             # Get a dictionary mapping each unique type key to an example argument
-            type_to_example_arg = dict(zip(interface_types, interface_args))
+            type_to_example_arg = {a.class_type : a for a in interface_args}
             # Get a list of unique keys
             possible_types = list(type_to_example_arg.keys())
 
             n_possible_types = len(possible_types)
             if n_possible_types != 1:
                 # Update argument_type_flags with the index of the type key
-                for func, t in zip(funcs, interface_types):
+                for func, t in zip(funcs, type_to_example_arg):
                     index = possible_types.index(t)*step
                     argument_type_flags[func] += index
 
