d3215fe4191f2f93771e825f74f3c8147fc84f38
EmilyBourne <louise.bourne@gmail.com>
2025-06-26 22:19:02 +0200

    Add a pyccel_make command

---
+++ pyccel/commands/pyccel_make.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/devel/LICENSE for full license details.      #
+#------------------------------------------------------------------------------------------#
+
+import argparse
+import sys
+from pathlib import Path
+
+def pyccel_make_command():
+    parser = argparse.ArgumentParser(description="Pyccel's project command line interface.",
+            add_help = False)
+
+    #... Help and Version
+    import pyccel
+    version = pyccel.__version__
+    libpath = pyccel.__path__[0]
+    python  = 'python {}.{}'.format(*sys.version_info)
+    message = "pyccel {} from {} ({})".format(version, libpath, python)
+
+    group = parser.add_argument_group('Basic options')
+    group.add_argument('-h', '--help', action='help', help='Show this help message and exit.')
+    group.add_argument('-V', '--version', action='version', help='Show version and exit.', version=message)
+
+    # ...
+    group = parser.add_argument_group('File specification',
+            description = "One of the below methods can be used to specify which files should be translated."
+            ).add_mutually_exclusive_group()
+    group.add_argument('-f', '--files', nargs='+', type=Path,
+            help="A list of files to be translated as a project.")
+    group.add_argument('-g', '--glob', nargs=1, type=str,
+            help="A glob that should be used to recognise files to be translated as a project.")
+    group.add_argument('-d', '--file-descr', nargs=1, type=Path,
+            help="A file containing a list of all the files to be translated as a project.")
+
+    # ... compiler syntax, semantic and codegen
+    group = parser.add_argument_group('Pyccel compiling stages')
+    group.add_argument('-x', '--syntax-only', action='store_true',
+                       help='Stop Pyccel after syntactic parsing, before semantic analysis or code generation.')
+    group.add_argument('-e', '--semantic-only', action='store_true',
+                       help='Stop Pyccel after semantic analysis, before code generation.')
+    group.add_argument('-t', '--convert-only', action='store_true',
+                       help='Stop Pyccel after translation to the target language, before build.')
+    # ...
+
+    # ... backend compiler options
+    group = parser.add_argument_group('Backend selection')
+
+    group.add_argument('--language', choices=('fortran', 'c', 'python'), default='Fortran',
+                       help='Target language for translation, i.e. the main language of the generated code (default: Fortran).',
+                       type=str.lower)
+
+    # ... Compiler options
+    group = parser.add_argument_group('Compiler configuration (mutually exclusive options)')
+    compiler_group = group.add_mutually_exclusive_group(required=False)
+    compiler_group.add_argument('--compiler-family',
+                                type=str,
+                                default='GNU',
+                                metavar='FAMILY',
+                                help='Compiler family {GNU,intel,PGI,nvidia,LLVM} (default: GNU).')
+    compiler_group.add_argument('--compiler-config',
+                                type=Path,
+                                default=None,
+                                metavar='CONFIG.json',
+                                help='Load all compiler information from a JSON file with the given path (relative or absolute).')
+    group.add_argument('--debug', action=argparse.BooleanOptionalAction, default=None,
+                        help='Compile the code with debug flags, or not.\n' \
+                        ' Overrides the environment variable PYCCEL_DEBUG_MODE, if it exists. Otherwise default is False.')
+    group.add_argument('--output', type=Path, default = None,\
+                       help="Folder in which the output is stored (default: FILE's folder).")
+
+    # ... Accelerators
+    group = parser.add_argument_group('Accelerators options')
+    group.add_argument('--mpi', action='store_true', \
+                       help='Use MPI.')
+    group.add_argument('--openmp', action='store_true', \
+                       help='Use OpenMP.')
+#    group.add_argument('--openacc', action='store_true', \
+#                       help='Use OpenACC.') # [YG 17.06.2025] OpenACC is not supported yet
+    # ...
+
+    # ... Other options
+    group = parser.add_argument_group('Other options')
+    group.add_argument('-v', '--verbose', action='count', default = 0,\
+                        help='Increase output verbosity (use -v, -vv, -vvv for more detailed output).')
+    group.add_argument('--time-execution', action='store_true', \
+                        help='Print the time spent in each section of the execution.')
+    group.add_argument('--developer-mode', action='store_true', \
+                        help='Show internal messages.')
+    group.add_argument('--conda-warnings', choices=('off', 'basic', 'verbose'),
+                        help='Specify the level of Conda warnings to display (default: basic).')
+    # ...
+    # ...
+    args = parser.parse_args()
