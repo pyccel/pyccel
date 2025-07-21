c795e32dfe30900191cb927cee799603b80622c7
EmilyBourne <louise.bourne@gmail.com>
2025-07-21 17:23:32 +0200

    Pylint

--- tests/pyccel/scripts/runtest_varargs.py
+++ tests/pyccel/scripts/runtest_varargs.py
@@ -1,8 +1,8 @@
-
+# pylint: disable=missing-function-docstring, missing-module-docstring
 
 def f(*a : int):
     for ai in a:
-        print(a)
+        print(ai)
 
 if __name__ == '__main__':
     f(1,2,3)
