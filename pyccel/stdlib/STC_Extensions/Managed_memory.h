852715de43e335a7e8562215242d3dc6e38e2bfa
EmilyBourne <louise.bourne@gmail.com>
2025-07-28 10:48:42 +0200

    Correct malloc size

--- pyccel/stdlib/STC_Extensions/Managed_memory.h
+++ pyccel/stdlib/STC_Extensions/Managed_memory.h
@@ -30,7 +30,7 @@ STC_INLINE Self _c_MEMB(_init)(void) { return c_literal(Self){false, NULL}; }
 STC_INLINE Self _c_MEMB(_make)(_m_value val) {
     Self owned;
     owned.is_owning = true;
-    owned.get = i_malloc(1);
+    owned.get = i_malloc(c_sizeof(_m_value));
     *owned.get = val;
     return owned;
 }
