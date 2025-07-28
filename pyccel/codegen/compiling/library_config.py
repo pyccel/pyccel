17117d6bc38fd159eb33ce0ab011fd54a3c82de8
EmilyBourne <louise.bourne@gmail.com>
2025-07-28 08:40:50 +0200

    Print compiler name

--- pyccel/codegen/compiling/library_config.py
+++ pyccel/codegen/compiling/library_config.py
@@ -343,6 +343,7 @@ class STCInstaller(ExternalLibInstaller):
                     os.makedirs(install_dir)
                     os.makedirs(libdir)
                     os.makedirs(libdir / 'pkgconfig')
+                    print("Using compiler : ", Path(compiler.get_exec({}, "c")).name)
                     p = subprocess.run([make, 'lib', f'CC={Path(compiler.get_exec({}, "c")).name}', f'BUILDDIR={build_dir}', '-C', self._src_dir],
                                    check=False, cwd=pyccel_dirpath, capture_output = (verbose <= 1))
                     print(p.stdout)
