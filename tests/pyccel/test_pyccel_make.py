3a7a97a839ca89a90777d4b49554a24601cfda11
EmilyBourne <louise.bourne@gmail.com>
2025-07-29 16:01:03 +0200

    Mark up tests correctly

--- tests/pyccel/test_pyccel_make.py
+++ tests/pyccel/test_pyccel_make.py
@@ -39,19 +39,25 @@ def test_project_abs_imports(language, build_system):
                      language, build_system, ['-g', '**/*.py'])
 
 #------------------------------------------------------------------------------
-def test_project_abs_mod_imports(language, build_system):
-    pyccel_make_test('runtest.py', current_folder / 'project_abs_mod_imports',
-                     language, build_system, ['-g', '**/*.py'])
-
-#------------------------------------------------------------------------------
 def test_project_class_imports(language, build_system):
+    # Failing due to repeated renaming
     pyccel_make_test('runtest.py', current_folder / 'project_class_imports',
                      language, build_system, ['-g', '**/*.py'])
 
 #------------------------------------------------------------------------------
+@pytest.mark.parametrize( 'language', (
+        pytest.param("fortran", marks = pytest.mark.fortran),
+        pytest.param("python", marks = pytest.mark.python),
+        pytest.param("c", marks = [
+            pytest.mark.skip(reason="Collisions are not handled"),
+            pytest.mark.c]
+        )
+    )
+)
 def test_project_multi_imports(language, build_system):
-    pyccel_make_test('runtest.py', current_folder / 'project_multi_imports',
-                     language, build_system, ['-g', '**/*.py'])
+    pyccel_make_test('file4.py', current_folder / 'project_multi_imports',
+                     language, build_system, ['-f', 'file1.py', 'file2.py', 'file3.py', 'file4.py'],
+                     output_dtype = str)
 
 #------------------------------------------------------------------------------
 def test_project_rel_imports(language, build_system):
