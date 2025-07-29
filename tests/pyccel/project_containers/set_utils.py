51704b603f0a7fb9ef5c8b05f87edf35d425d484
EmilyBourne <louise.bourne@gmail.com>
2025-07-29 17:59:37 +0200

    Avoid #2406

--- tests/pyccel/project_containers/set_utils.py
+++ tests/pyccel/project_containers/set_utils.py
@@ -3,5 +3,6 @@ def unique_elements(values: set[int]) -> set[int]:
     return set(values)
 
 def common_elements(set1: set[int], set2: set[int]) -> set[int]:
-    return set1 & set2
+    set3 = set1 & set2
+    return set3
 
