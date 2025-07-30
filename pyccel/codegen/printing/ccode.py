55adae9a4c0ae39f75392b16cc5be34d815bef83
Emily Bourne <louise.bourne@gmail.com>
2025-07-30 21:44:30 +0200

    Update STC to Version 6.0 beta2 (#2404)
    Update STC to Version 6.0 beta2. This will help test STC before v6.0 is
    released (hopefully in time for v2.1 of Pyccel). But this is also
    extremely useful for #2401 which is needed for #2383.

--- pyccel/codegen/printing/ccode.py
+++ pyccel/codegen/printing/ccode.py
@@ -1247,7 +1247,7 @@ class CCodePrinter(CodePrinter):
                            AsName(VariableTypeAnnotation(class_type), container_type),
                            ignore_at_print=True))
 
-                decl_line = f'#define i_type {container_type}\n'
+                decl_line = f'#define T {container_type}\n'
                 if isinstance(class_type, DictType):
                     key_decl_line = self._get_stc_element_type_decl(class_type.key_type, expr)
                     val_decl_line = self._get_stc_element_type_decl(class_type.value_type, expr, 'val')
@@ -1752,7 +1752,7 @@ class CCodePrinter(CodePrinter):
                 indices.append('{'+self._print(idx)+'}')
             indices_code = ', '.join(indices)
             base_code = self._print(ObjectAddress(base))
-            return f'cspan_slice({c_type}, {base_code}, {indices_code})'
+            return f'cspan_slice({base_code}, {c_type}, {indices_code})'
 
         for i, ind in enumerate(inds):
             if is_literal_integer(ind) and int(ind) < 0:
@@ -2596,7 +2596,7 @@ class CCodePrinter(CodePrinter):
                 rhs = self._print(rhs_address)
                 rhs_type = self.get_c_type(rhs_var.class_type)
                 slicing = ', '.join(['{c_ALL}']*lhs_var.rank)
-                code = f'{lhs} = cspan_slice({rhs_type}, {rhs}, {slicing});\n'
+                code = f'{lhs} = cspan_slice({rhs}, {rhs_type}, {slicing});\n'
                 if lhs_var.order != rhs_var.order:
                     code += f'cspan_transpose({lhs_ptr});\n'
                 return code
