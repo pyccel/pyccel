b3eb194def64b7505b7d790e4e183a1ed8882b5e
EmilyBourne <louise.bourne@gmail.com>
2025-07-31 13:37:54 +0200

    Go back to filecmp to compare contents

--- pyccel/codegen/compiling/library_config.py
+++ pyccel/codegen/compiling/library_config.py
@@ -6,6 +6,7 @@
 """
 This module contains tools useful for handling the compilation of stdlib imports.
 """
+import filecmp
 import os
 from pathlib import Path
 import shutil
@@ -100,13 +101,12 @@ class StdlibInstaller:
                 to_delete = False
             else:
                 # If folder exists check if it needs updating
-                to_copy = lib_dest_path.lstat().st_mtime < self._src_dir.lstat().st_mtime
-                print(lib_dest_path, self._src_dir)
-                print(lib_dest_path.lstat().st_mtime, self._src_dir.lstat().st_mtime, lib_dest_path.lstat().st_mtime < self._src_dir.lstat().st_mtime)
+                src_files = list(self._src_dir.glob('*'))
+                _, mismatch, _ = filecmp.cmpfiles(lib_dest_path, self._src_dir, src_files)
+                to_copy = len(mismatch) != 0
                 to_delete = to_copy
 
             if to_delete:
-                print("DELETING")
                 shutil.rmtree(lib_dest_path)
                 assert False
 
