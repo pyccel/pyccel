c8ab15f37b0d357827cca1d0e19dc3c54d331c9a
EmilyBourne <louise.bourne@gmail.com>
2025-08-05 09:30:20 +0200

    Add a ReadOnlyDict class to enforce expected usage of dictionaries

---
+++ pyccel/utilities/tools.py
@@ -0,0 +1,22 @@
+#------------------------------------------------------------------------------------------#
+# This file is part of Pyccel which is released under MIT License. See the LICENSE file or #
+# go to https://github.com/pyccel/pyccel/blob/devel/LICENSE for full license details.      #
+#------------------------------------------------------------------------------------------#
+"""
+File containing basic tools.
+"""
+
+__all__ = (
+        'ReadOnlyDict',
+        )
+
+class ReadOnlyDict(dict):
+    """
+    A read-only version of a dictionary.
+
+    A read-only version of a dictionary. This is useful to prevent objects
+    being used in a way which was not intended.
+    """
+    def __setitem__(self, key, value):
+        raise TypeError("Can't modify read-only dictionary")
+        return super().__setitem__(key, value)
