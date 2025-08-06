658f38e56570b91719857d438487106dce24b6bf
EmilyBourne <louise.bourne@gmail.com>
2025-08-06 10:29:02 +0200

    Use insert_function

--- pyccel/codegen/wrapper/c_to_python_wrapper.py
+++ pyccel/codegen/wrapper/c_to_python_wrapper.py
@@ -1030,7 +1030,7 @@ class CToPythonWrapper(Wrapper):
         function = PyFunctionDef(func_name, func_args, body, func_results, scope=func_scope,
                 docstring = init_function.docstring, original_function = original_func)
 
-        self.scope.functions[func_name] = function
+        self.scope.insert_function(function, func_name)
         self._python_object_map[init_function] = function
         self._error_exit_code = Nil()
 
@@ -1096,7 +1096,7 @@ class CToPythonWrapper(Wrapper):
         function = PyFunctionDef(func_name, [FunctionDefArgument(func_arg)], body, scope=func_scope,
                 original_function = original_func)
 
-        self.scope.functions[func_name] = function
+        self.scope.insert_function(function, func_name)
         self._python_object_map[del_function] = function
 
         return function
