6fe386168219379df280f17fdb3956fea3fc94bb
EmilyBourne <louise.bourne@gmail.com>
2025-08-14 17:38:23 +0200

    Fix assertion

--- pyccel/ast/numpyext.py
+++ pyccel/ast/numpyext.py
@@ -188,7 +188,7 @@ def get_shape_of_multi_level_container(expr, shape_prefix = ()):
         return new_shape
     elif isinstance(expr, (PythonTuple, PythonList)):
         possible_shapes = [get_shape_of_multi_level_container(a, new_shape) for a in expr.args]
-        assert all(len(set(s[i] for s in possible_shapes if isinstance(s, LiteralInteger))) == 1
+        assert all(len(set(s[i] for s in possible_shapes if isinstance(s, LiteralInteger))) <= 1
                    for i in range(class_type.rank-1))
         return possible_shapes[0]
     elif isinstance(expr, (Variable, IndexedElement)):
