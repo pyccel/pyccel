5da8f91e677bd50263ed860dabf2b2e619076452
EmilyBourne <louise.bourne@gmail.com>
2025-08-14 17:42:27 +0200

    Pylint

--- tests/errors/semantic/blocking/MISSING_TYPE_ANNOTATION.py
+++ tests/errors/semantic/blocking/MISSING_TYPE_ANNOTATION.py
@@ -1,4 +1,5 @@
-# Missing type annotation. Pyccel needs arguments to be annotated with types to know which types are required in the translated code. See https://github.com/pyccel/pyccel/blob/devel/docs/quickstart.md#type-annotations for information on how to specify the type information.
+# Missing type annotation. Pyccel needs arguments to be annotated with types to know which types are required in the translated code.
+# pylint: disable=missing-function-docstring, missing-module-docstring
 
 def f(n):
     pass
