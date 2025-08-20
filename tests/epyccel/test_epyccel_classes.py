67e04d1454bce76657f03251a8ba643e7433f02b
EmilyBourne <louise.bourne@gmail.com>
2025-08-20 19:07:40 +0200

    Add test

--- tests/epyccel/test_epyccel_classes.py
+++ tests/epyccel/test_epyccel_classes.py
@@ -501,3 +501,25 @@ def test_class_globals_visitation_order(language):
     a_l = modnew.A()
 
     assert a_py.x == a_l.x
+
+def test_class_variables(language):
+    import classes.class_variables as mod
+    modnew = epyccel(mod, language = language)
+
+    a_py = mod.A(True)
+    a_l = modnew.A(True)
+
+    assert a_py.x == a_l.x
+    assert a_py.z == a_l.z
+    assert a_py.get_4() == a_l.get_4()
+    assert np.array_equal(a_py.y, a_l.y)
+    assert a_py.get_y_len() == a_l.get_y_len()
+
+    a_py = mod.A(False)
+    a_l = modnew.A(False)
+
+    assert a_py.x == a_l.x
+    assert a_py.z == a_l.z
+    assert a_py.get_4() == a_l.get_4()
+    assert np.array_equal(a_py.y, a_l.y)
+    assert a_py.get_y_len() == a_l.get_y_len()
