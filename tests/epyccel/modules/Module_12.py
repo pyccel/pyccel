17e4d4fbeb781b159afd78fbab90c8f9986fc6fa
EmilyBourne <louise.bourne@gmail.com>
2025-08-20 18:39:53 +0200

    Pylint

--- tests/epyccel/modules/Module_12.py
+++ tests/epyccel/modules/Module_12.py
@@ -1,10 +1,9 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
-import numpy as np
 
 def add_2(a : float):
     return a + 2
 
-def times_3(a : float|complex):
+def times_3(a : 'float|complex'):
     b = 1.0
     b = add_2(b)
     a *= b
