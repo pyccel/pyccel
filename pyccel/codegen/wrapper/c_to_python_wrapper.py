3e62f125afc3025d087159750556a1b0af235843
EmilyBourne <louise.bourne@gmail.com>
2025-08-20 18:32:20 +0200

    Catch another call

--- pyccel/codegen/wrapper/c_to_python_wrapper.py
+++ pyccel/codegen/wrapper/c_to_python_wrapper.py
@@ -1481,7 +1481,7 @@ class CToPythonWrapper(Wrapper):
             a_var = a.var
             func_scope.insert_symbol(getattr(a_var, 'original_var', a_var).name)
 
-        in_interface = len(expr.get_user_nodes(Interface)) > 0
+        in_interface = len(expr.get_user_nodes(Interface, (FunctionCall,))) > 0
 
         # Get variables describing the arguments and results that are seen from Python
         python_args = expr.arguments
