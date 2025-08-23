065982177fc217ff8de5b1f8e8e3b04f34cd97c3
EmilyBourne <louise.bourne@gmail.com>
2025-08-23 12:11:48 +0200

    C not C++

--- pyccel/stdlib/cwrapper/cwrapper.c
+++ pyccel/stdlib/cwrapper/cwrapper.c
@@ -312,19 +312,19 @@ static char* _check_pyarray_order(PyArrayObject *a, int flag)
 	if (flag == NO_ORDER_CHECK)
 		return NULL;
 
-    bool valid(true);
+    bool valid = true;
     if (flag == NPY_ARRAY_C_CONTIGUOUS) {
         int nd = PyArray_NDIM(a);
         npy_intp* np_strides = PyArray_STRIDES(a);
-        for (int i(1); i<nd; ++i) {
-            valid = valid & np_strides[i-1] >= np_strides[i]
+        for (int i = 1; i<nd; ++i) {
+            valid = valid & np_strides[i-1] >= np_strides[i];
         }
     }
     else if (flag == NPY_ARRAY_F_CONTIGUOUS) {
         int nd = PyArray_NDIM(a);
         npy_intp* np_strides = PyArray_STRIDES(a);
-        for (int i(1); i<nd; ++i) {
-            valid = valid & np_strides[i-1] <= np_strides[i]
+        for (int i = 1; i<nd; ++i) {
+            valid = valid & np_strides[i-1] <= np_strides[i];
         }
     }
     else {
