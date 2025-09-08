27a24a7793515041641e589325f2709fe0a7706a
EmilyBourne <louise.bourne@gmail.com>
2025-09-08 15:20:01 +0200

    Correct insertion key

--- pyccel/codegen/wrapper/c_to_python_wrapper.py
+++ pyccel/codegen/wrapper/c_to_python_wrapper.py
@@ -1100,7 +1100,7 @@ class CToPythonWrapper(Wrapper):
         function = PyFunctionDef(func_name, [FunctionDefArgument(func_arg)], body, scope=func_scope,
                 original_function = original_func)
 
-        self.scope.insert_function(function, func_name)
+        self.scope.insert_function(function, self.scope.get_python_name(func_name))
         self._python_object_map[del_function] = function
 
         return function
