b5daefb65d68424eb9d2e4523570ce7b098fa7a4
EmilyBourne <louise.bourne@gmail.com>
2025-09-09 14:29:49 +0200

    Static inline needed

--- tests/pyccel/wrap_scripts/c_tests/class_property.h
+++ tests/pyccel/wrap_scripts/c_tests/class_property.h
@@ -12,7 +12,7 @@ struct Counter {
 void Counter__create(struct Counter* self, int64_t start);
 void Counter__free(const struct Counter* self);
 void Counter__increment(struct Counter* self);
-inline int64_t Counter__get_value(const struct Counter* self) {
+static inline int64_t Counter__get_value(const struct Counter* self) {
     return self->value;
 }
 
