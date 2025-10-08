e5331b67b448bc0462d16347431b1d1c164e71e4
EmilyBourne <louise.bourne@gmail.com>
2025-10-08 10:50:55 +0200

    Pylint : docs in tests

--- tests/epyccel/modules/Module_13.py
+++ tests/epyccel/modules/Module_13.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 import numpy as np
 from pyccel.decorators import inline
 
--- tests/epyccel/test_imports.py
+++ tests/epyccel/test_imports.py
@@ -1,4 +1,4 @@
-# pylint: disable=missing-function-docstring, missing-module-docstring
+# pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 
 import pytest
 from numpy import ones
