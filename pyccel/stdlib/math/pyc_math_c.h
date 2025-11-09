f71740b70928752bf8a47f9b361fbc66492e807c
learning-dev <ayaz.mohammed.me@gmail.com>
2025-11-09 20:12:05 +0530

    add csign priting functionality

--- pyccel/stdlib/math/pyc_math_c.h
+++ pyccel/stdlib/math/pyc_math_c.h
@@ -46,7 +46,6 @@ inline double        pyc_fmodulo(double a, double b)
 
 long long int isign(long long int x);
 double fsign(double x);
-double complex csign(double complex x);
 
 double fpyc_bankers_round(double arg, int64_t ndigits);
 int64_t ipyc_bankers_round(int64_t arg, int64_t ndigits);
