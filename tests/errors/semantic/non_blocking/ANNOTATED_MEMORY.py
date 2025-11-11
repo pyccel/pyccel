bddf0593197880ad0f51c824d39a05d1c81f2248
EmilyBourne <louise.bourne@gmail.com>
2025-11-11 16:00:27 +0100

    Unused imports

--- tests/errors/semantic/non_blocking/ANNOTATED_MEMORY.py
+++ tests/errors/semantic/non_blocking/ANNOTATED_MEMORY.py
@@ -1,7 +1,6 @@
 # An object cannot be both a pointer to an object stored elsewhere and a stack allocated object.
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 from typing import Annotated
-import numpy as np
 
 a : Annotated['int[:]', 'stack', 'pointer']
 
--- tests/errors/semantic/non_blocking/NESTED_ANNOTATED.py
+++ tests/errors/semantic/non_blocking/NESTED_ANNOTATED.py
@@ -1,6 +1,5 @@
 # Nested Annotated[] type modifiers are not handled.
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 from typing import Annotated
-import numpy as np
 
 a : Annotated[Annotated['int[:]', 'stack'], '>0']
