498b88e5761c781b862a3e57be8cba470161398c
EmilyBourne <louise.bourne@gmail.com>
2025-11-13 09:40:27 +0100

    Use import name when printing

--- pyccel/codegen/printing/fcode.py
+++ pyccel/codegen/printing/fcode.py
@@ -2312,7 +2312,11 @@ class FCodePrinter(CodePrinter):
         return f"{iterable_type}_Iterator"
 
     def _print_CustomDataType(self, expr):
-        return expr.low_level_name
+        try:
+            name = self.scope.get_import_alias(expr, 'cls_constructs')
+        except RuntimeError:
+            name = expr.low_level_name
+        return name
 
     def _print_DataType(self, expr):
         return self._print(expr.name)
