308247a6c7050e3937ff3d4a3565bb75e8daba5e
EmilyBourne <louise.bourne@gmail.com>
2025-11-13 08:32:49 +0100

    Leave changes to operators for #2493

--- pyccel/ast/operators.py
+++ pyccel/ast/operators.py
@@ -360,17 +360,8 @@ class PyccelUnarySub(PyccelUnary):
     ----------
     arg : TypedAstNode
         The argument passed to the operator.
-    simplify : bool
-        True if the expression should be simplified to be as compact/readable as
-        possible. False if the arguments should be preserved as they are.
     """
     __slots__ = ()
-
-    def __new__(cls, arg, simplify = False):
-        if simplify and isinstance(arg, PyccelUnarySub):
-            return arg.args[0]
-        else:
-            return super().__new__(cls)
 
     def __repr__(self):
         return f'-{repr(self.args[0])}'
@@ -793,11 +784,8 @@ class PyccelMinus(PyccelArithmeticOperator):
                 dtype = cls._calculate_type(arg1, arg2)
                 return convert_to_literal(arg1.python_value - arg2.python_value,
                                           dtype)
-            elif isinstance(arg1, PyccelAdd) and arg1.args[1] == arg2:
+            if isinstance(arg1, PyccelAdd) and arg1.args[1] == arg2:
                 return arg1.args[0]
-            elif arg1 == 0:
-                return PyccelUnarySub(arg2)
-
         if isinstance(arg1, LiteralFloat) and \
             isinstance(arg2, LiteralComplex) and \
             arg2.real == LiteralFloat(0):
