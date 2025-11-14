4c0923ed766939c04c9efdb8b547ceb17049f5fb
EmilyBourne <louise.bourne@gmail.com>
2025-11-14 17:45:07 +0100

    Make test more complex to show errors

--- tests/pyccel/wrap_scripts/fortran_tests/runtest_class_renaming.py
+++ tests/pyccel/wrap_scripts/fortran_tests/runtest_class_renaming.py
@@ -1,8 +1,18 @@
 # pylint: disable=missing-module-docstring
+from typing import Final
 from class_renaming import Counter
 
-if __name__ == "__main__":
+def foo():
     c = Counter(10)
     c.increment()
     print(c.value)   # expected 11
 
+def bar(c : Final[Counter]):
+    print(c.value)
+
+if __name__ == "__main__":
+    foo()
+    c = Counter(10)
+    c.increment()
+    c.increment()
+    bar(c)
