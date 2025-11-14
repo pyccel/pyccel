7ee55cb9d2c9600a7f9bff5c43c4461755497e40
EmilyBourne <louise.bourne@gmail.com>
2025-11-14 18:05:57 +0100

    Cleaner

--- tests/pyccel/wrap_scripts/fortran_tests/runtest_class_renaming.py
+++ tests/pyccel/wrap_scripts/fortran_tests/runtest_class_renaming.py
@@ -2,17 +2,11 @@
 from typing import Final
 from class_renaming import Counter
 
-def foo():
-    c = Counter(10)
-    c.increment()
-    print(c.value)   # expected 11
-
-def bar(c : Final[Counter]):
+def print_val(c : Final[Counter]):
     print(c.value)
 
 if __name__ == "__main__":
-    foo()
     c = Counter(10)
     c.increment()
     c.increment()
-    bar(c)
+    print_val(c)
