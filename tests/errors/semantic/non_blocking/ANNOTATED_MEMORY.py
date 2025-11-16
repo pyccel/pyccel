b4bf57e8c6518cff220f6334451b6581bf154384
Emily Bourne <louise.bourne@gmail.com>
2025-11-16 16:22:42 +0100

    correct error message

--- tests/errors/semantic/non_blocking/ANNOTATED_MEMORY.py
+++ tests/errors/semantic/non_blocking/ANNOTATED_MEMORY.py
@@ -1,4 +1,4 @@
-# An object cannot be both an alias to an object stored elsewhere and a stack allocated object.
+# An object cannot be both an alias for an object stored elsewhere and a stack allocated object.
 # pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
 from typing import Annotated
 
