02550a32cd2ffea54841d5ea66d4065138605fc0
EmilyBourne <louise.bourne@gmail.com>
2025-11-20 14:49:53 +0100

    Fix identical cmath bug

--- pyccel/parser/semantic.py
+++ pyccel/parser/semantic.py
@@ -5994,6 +5994,11 @@ class SemanticParser(BasicParser):
         TypedAstNode
             A node describing the result of a call to the `cmath.sqrt` function.
         """
+        syntactic_func = func_call.funcdef
+        if isinstance(syntactic_func, PyccelFunctionDef):
+            func = syntactic_func
+        else:
+            func = self.scope.find(syntactic_func, 'functions', raise_if_missing=True)
         func = self.scope.find(func_call.funcdef, 'functions')
         arg = func_call_args[0]
         if isinstance(arg.value, PyccelMul):
