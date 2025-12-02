436bf63ebc578653ac04e47d57126f3842772548
EmilyBourne <louise.bourne@gmail.com>
2025-12-02 09:13:09 +0100

    Clean up and add docs

--- pyccel/commands/argparse_helpers.py
+++ pyccel/commands/argparse_helpers.py
@@ -9,12 +9,16 @@ import os
 import pathlib
 import sys
 
+from pyccel import __version__ as pyccel_version, __path__ as pyccel_path
+from ..compilers.default_compilers import available_compilers
+
 __all__ = (
         'file_type',
         'AcceleratorAction',
         )
 
-def file_type(suffixes):
+# -----------------------------------------------------------------------------------------
+def check_file_type(suffixes):
     """
     Check if the input is a type with one of the suffixes.
 
@@ -38,38 +42,69 @@ def file_type(suffixes):
         if not path.is_file():
             raise argparse.ArgumentTypeError(f"File not found: {path}")
         if path.suffix not in suffixes:
-            raise argparse.ArgumentTypeError("Wrong file extension. Expecting one of: {', '.join(suffixes)}")
+            raise argparse.ArgumentTypeError(f"Wrong file extension. Expecting one of: {', '.join(suffixes)}")
         return path.absolute()
     return check_path
 
+# -----------------------------------------------------------------------------------------
 def add_basic_functionalities(parser):
-    #... Help and Version
-    import pyccel
-    version = pyccel.__version__
-    libpath = pyccel.__path__[0]
+    """
+    Add basic functionalities to argument parser.
+
+    Add basic functionalities such as a help and a version message to argument parser.
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        The parser to be modified.
+    """
+    version = pyccel_version
+    libpath = pyccel_path[0]
     python  = f'python {sys.version_info.major}.{sys.version_info.minor}'
     message = f'pyccel {version} from {libpath} ({python})'
 
     group = parser.add_argument_group('Basic options')
     group.add_argument('-h', '--help', action='help', help='Show this help message and exit.')
     group.add_argument('-V', '--version', action='version', help='Show version and exit.', version=message)
-    # ...
 
+# -----------------------------------------------------------------------------------------
 def add_compiler_selection(parser):
+    """
+    Add compiler selection flags to argument parser.
+
+    Add flags to argument parser to select a compiler. This can be done by family
+    or with a json config file.
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        The parser to be modified.
+    """
+    default_compiler = os.environ.get('PYCCEL_DEFAULT_COMPILER', 'GNU')
     group = parser.add_argument_group('Compiler configuration (mutually exclusive options)')
     compiler_group = group.add_mutually_exclusive_group(required=False)
     compiler_group.add_argument('--compiler-family',
-                                type=str,
-                                default=os.environ.get('PYCCEL_DEFAULT_COMPILER', 'GNU'),
-                                metavar='FAMILY',
-                                help='Compiler family {GNU,intel,PGI,nvidia,LLVM} (default: GNU).')
+                                choices=available_compilers.keys(),
+                                default=default_compiler,
+                                help=f'Compiler family (default: {default_compiler}).')
     compiler_group.add_argument('--compiler-config',
-                                type=pathlib.Path,
+                                type=check_file_type(('.json',)),
                                 default=None,
                                 metavar='CONFIG.json',
                                 help='Load all compiler information from a JSON file with the given path (relative or absolute).')
 
+# -----------------------------------------------------------------------------------------
 def add_accelerator_selection(parser):
+    """
+    Add accelerator flags to argument parser.
+
+    Add flags to argument parser to select any accelerators to be used in compilation.
+
+    Parameters
+    ----------
+    parser : argparse.ArgumentParser
+        The parser to be modified.
+    """
     group = parser.add_argument_group('Accelerators options')
     group.add_argument('--mpi', action=AcceleratorAction, nargs=0,
                        help='Use MPI.')
@@ -78,6 +113,7 @@ def add_accelerator_selection(parser):
 #    group.add_argument('--openacc', action=AcceleratorAction, nargs=0,
 #                       help='Use OpenACC.') # [YG 17.06.2025] OpenACC is not supported yet
 
+# -----------------------------------------------------------------------------------------
 class AcceleratorAction(argparse.Action):
     """
     A class to describe the action which groups accelerators into one output.
