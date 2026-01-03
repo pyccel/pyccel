19f8b1dfbd597a570702cf02604cc19bfbf0dfdb
EmilyBourne <louise.bourne@gmail.com>
2026-01-03 11:38:41 +0100

    Add error tests

---
+++ tests/errors/semantic/blocking/NON_LITERAL_AXIS_ABC.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+if __name__ == '__main__':
+    a = np.array([[1,2,3],[4,5,6]])
+    b = np.array([[6,5,4],[3,2,1]])
+    axis = 0
+    c = np.cross(a, b, axisa=axis, axisb=axis, axisc=axis)
