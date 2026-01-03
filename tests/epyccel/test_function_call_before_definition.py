784cc6cb37d22438ebced93c1e483340c294b4db
EmilyBourne <louise.bourne@gmail.com>
2026-01-03 23:02:01 +0100

    Activate tests

--- tests/epyccel/test_function_call_before_definition.py
+++ tests/epyccel/test_function_call_before_definition.py
@@ -11,6 +11,6 @@ def test_fun_1(language):
     assert mod1.x2 == modnew.x2
     assert mod1.x3 == modnew.x3
 
-def test_fun_2(language):
-    modnew = epyccel(mod2, language = language)
+def test_fun_2(experimental_language):
+    modnew = epyccel(mod2, language = experimental_language)
     assert mod2.a == modnew.a
