868b42d99d4a92ad8dc7449ea2d2acfe0e29aa26
EmilyBourne <louise.bourne@gmail.com>
2026-01-03 23:03:30 +0100

    Activate tests

--- tests/epyccel/test_kind.py
+++ tests/epyccel/test_kind.py
@@ -3,7 +3,7 @@ import platform
 import pytest
 from pyccel import epyccel
 
-def test_or_boolean(language):
+def test_or_boolean(experimental_language):
     def or_bool(a : 'bool', b : 'bool'):
         c = False
         if (a):
@@ -11,61 +11,61 @@ def test_or_boolean(language):
         if (b):
             c = True
         return c
-    epyc_or_bool = epyccel(or_bool, language=language)
+    epyc_or_bool = epyccel(or_bool, language = experimental_language)
 
     assert epyc_or_bool(True,True)==or_bool(True,True)
     assert epyc_or_bool(True,False)==or_bool(True,False)
     assert epyc_or_bool(False,False)==or_bool(False,False)
 
-def test_real_greater_bool(language):
+def test_real_greater_bool(experimental_language):
     def real_greater_bool(x0 : 'float', x1 : 'float'):
         greater = False
         if x0 > x1:
             greater = True
         return greater
 
-    epyc_real_greater_bool = epyccel(real_greater_bool, language=language)
+    epyc_real_greater_bool = epyccel(real_greater_bool, language = experimental_language)
 
     assert real_greater_bool(1.0,2.0)==epyc_real_greater_bool(1.0,2.0)
     assert real_greater_bool(1.5,1.2)==epyc_real_greater_bool(1.5,1.2)
 
 # Skip test if PYCCEL_DEFAULT_COMPILER=LLVM
 @pytest.mark.skip_llvm
-def test_input_output_matching_types(language):
+def test_input_output_matching_types(experimental_language):
     def add_real(a : 'float', b : 'float'):
         c = a+b
         return c
 
     flags="-Werror -Wconversion"
-    if language=="fortran":
+    if experimental_language=="fortran":
         flags=flags+"-extra"
-    if platform.system() == 'Darwin' and language=='c': # If macosx
+    if platform.system() == 'Darwin' and experimental_language=='c': # If macosx
         flags=flags+" -Wno-error=unused-command-line-argument"
-    epyc_add_real = epyccel(add_real, flags=flags, language=language)
+    epyc_add_real = epyccel(add_real, flags=flags, language = experimental_language)
 
     assert add_real(1.0,2.0)==epyc_add_real(1.0,2.0)
 
-def test_output_types_1(language):
+def test_output_types_1(experimental_language):
     def cast_to_int(a : 'float'):
         b = int(a)
         return b
 
-    f = epyccel(cast_to_int, language = language)
+    f = epyccel(cast_to_int, language = experimental_language)
     assert(type(cast_to_int(5.2)) == type(f(5.2))) # pylint: disable=unidiomatic-typecheck
 
-def test_output_types_2(language):
+def test_output_types_2(experimental_language):
     def cast_to_float(a : 'int'):
         b = float(a)
         return b
 
-    f = epyccel(cast_to_float,language= language)
+    f = epyccel(cast_to_float, language = experimental_language)
     assert(type(cast_to_float(5)) == type(f(5)))    # pylint: disable=unidiomatic-typecheck
 
-def test_output_types_3(language):
+def test_output_types_3(experimental_language):
     def cast_to_bool(a : 'int'):
         b = bool(a)
         return b
 
-    f = epyccel(cast_to_bool, language=language)
+    f = epyccel(cast_to_bool, language = experimental_language)
     assert cast_to_bool(1) == f(1)
 
