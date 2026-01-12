992c3481cdf4d7a33a882080e719c09e2a44eff9
EmilyBourne <louise.bourne@gmail.com>
2026-01-12 10:11:02 +0100

    Add tests

---
+++ tests/epyccel/classes/inheritance_dispatch.py
@@ -0,0 +1,10 @@
+# pylint: disable=missing-function-docstring, missing-module-docstring, missing-class-docstring
+class A:
+    def f(self) -> int:
+        return 1
+
+class B(A):
+    def f(self) -> int:
+        return super().f() + 1
+
+
---
+++ tests/errors/syntax_errors/multiple_inheritance.py
@@ -0,0 +1,4 @@
+class A: pass
+class B: pass
+class C(A, B): pass
+
