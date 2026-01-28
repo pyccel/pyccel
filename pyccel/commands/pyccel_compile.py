f7cce5f1b82ad0fa7d2664518d41e1fad967f6b4
EmilyBourne <louise.bourne@gmail.com>
2026-01-28 16:25:14 +0100

    More specific

--- pyccel/commands/pyccel_compile.py
+++ pyccel/commands/pyccel_compile.py
@@ -101,7 +101,7 @@ def setup_pyccel_compile_parser(parser):
     add_common_settings(group)
     group.add_argument('--export-compiler-config', action='store_true',
                         help='Export all compiler information to a JSON file with the given path (relative or absolute). '
-                       'This flag is deprecated and will be removed in v2.3. Please use `pyccel config` instead.')
+                       'This flag is deprecated and will be removed in v2.3. Please use `pyccel config export` instead.')
     # ...
 
 
