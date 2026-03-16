9175195012d28db6edd68f2fe6d314f9684e3547
EmilyBourne <louise.bourne@gmail.com>
2026-03-16 18:27:37 +0100

    Black format

--- pyccel/__init__.py
+++ pyccel/__init__.py
@@ -3,6 +3,7 @@ Pyccel : write Python code, get Fortran speed
 
 Find documentation at pyccel.github.io/pyccel/
 """
+
 from .version import __version__
 from .commands.epyccel import epyccel
 from .commands.lambdify import lambdify
