adddc27e760fa23a41e3feaa97dadae654127a2c
EmilyBourne <louise.bourne@gmail.com>
2026-03-16 17:59:15 +0100

    Add pyccel base imports to __all__

--- pyccel/__init__.py
+++ pyccel/__init__.py
@@ -1,3 +1,9 @@
 from .version import __version__
 from .commands.epyccel import epyccel
 from .commands.lambdify import lambdify
+
+__all__ = (
+    "__version__",
+    "epyccel",
+    "lambdify",
+)
