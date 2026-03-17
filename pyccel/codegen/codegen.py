e4b67fa20b66d249ef6360a45da612ebf77ef571
Yaman Güçlü <yaman.guclu@gmail.com>
2026-03-17 20:19:46 +0100

    Add docstring to pyccel/codegen/codegen.py

--- pyccel/codegen/codegen.py
+++ pyccel/codegen/codegen.py
@@ -1,10 +1,17 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 # ------------------------------------------------------------------------- #
 # This file is part of Pyccel which is released under MIT License. See the  #
 # LICENSE file or go to https://github.com/pyccel/pyccel/blob/devel/LICENSE #
 # for full license details.                                                 #
 # ------------------------------------------------------------------------- #
+"""
+Module containing the `Codegen` class which handles the generation of code
+for a Python program or module. It takes the Pyccel semantic parser, which
+contains the Pyccel AST annotated through the semantic stage as well as the
+scoping information, and uses the appropriate `CodePrinter` to generate code
+in the target language.
+See developer_docs/codegen_stage.md for more details on the codegen stage.
+"""
+
 import os
 
 from pyccel.codegen.printing.fcode import FCodePrinter
