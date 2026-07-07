cbc0d8f795f7711555b8a5bb8a2655cf6a52b7d5
EmilyBourne <louise.bourne@gmail.com>
2026-07-07 11:08:22 +0200

    Add test

---
+++ tests/epyccel/modules/Module_docstring.py
@@ -0,0 +1,12 @@
+# Licence info line 1
+# Licence info line 2
+"""
+This is a module to test that module docstrings are correctly detected.
+"""
+
+# a = 4
+a = 4
+
+"""
+Multi-line comment is not in module docstring.
+"""
