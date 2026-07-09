4b66d69b925a1442e511a81e68a5fc45c6d2207f
EmilyBourne <louise.bourne@gmail.com>
2026-07-09 11:26:01 +0200

    Use epyccel_module_with_fallback for test_epyccel_compile_time_if (#808 is fixed)

--- tests/epyccel/test_epyccel_compile_time_if.py
+++ tests/epyccel/test_epyccel_compile_time_if.py
@@ -3,103 +3,40 @@ import numpy as np
 import pytest
 
 from pyccel import epyccel
+from modules import epyccel_compile_time_if
+from utilities import epyccel_module_with_fallback
 
 
-@pytest.mark.parametrize(
-    "language",
-    (
-        pytest.param("fortran", marks=[pytest.mark.fortran]),
-        pytest.param("c", marks=[pytest.mark.c]),
-        pytest.param(
-            "python",
-            marks=[
-                pytest.mark.skip(
-                    reason=(
-                        "isinstance is evaluated during translation so Python translation "
-                        "gives wrong results. See #802"
-                    )
-                ),
-                pytest.mark.python,
-            ],
-        ),
-    ),
-)
-def test_rank_differentiation_1(language):
-    def f(a: "int[:] | int[:,:]"):
-        if len(a.shape) == 1:
-            return a[0]
-        else:
-            return a[0, 0]
+@pytest.fixture(scope="module")
+def epyc_epyccel_compile_time_if_mod(language):
+    return epyccel_module_with_fallback(epyccel_compile_time_if, language)
 
+
+
+
+def test_rank_differentiation_1(epyc_epyccel_compile_time_if_mod):
+    f = epyccel_compile_time_if.rank_differentiation_1
     x = np.arange(10, dtype=int)
     y = np.array(np.reshape(x[::-1], (2, 5)), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_compile_time_if_mod.rank_differentiation_1
     assert f_epyc(x) == f(x)
     assert f_epyc(y) == f(y)
 
 
-@pytest.mark.parametrize(
-    "language",
-    (
-        pytest.param("fortran", marks=[pytest.mark.fortran]),
-        pytest.param("c", marks=[pytest.mark.c]),
-        pytest.param(
-            "python",
-            marks=[
-                pytest.mark.skip(
-                    reason=(
-                        "isinstance is evaluated during translation so Python translation "
-                        "gives wrong results. See #802"
-                    )
-                ),
-                pytest.mark.python,
-            ],
-        ),
-    ),
-)
-def test_rank_differentiation_2(language):
-    def f(a: "int[:] | int[:,:]"):
-        if len(a.shape) != 2:
-            return a[0]
-        else:
-            return a[0, 0]
-
+def test_rank_differentiation_2(epyc_epyccel_compile_time_if_mod):
+    f = epyccel_compile_time_if.rank_differentiation_2
     x = np.arange(10, dtype=int)
     y = np.array(np.reshape(x[::-1], (2, 5)), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_compile_time_if_mod.rank_differentiation_2
     assert f_epyc(x) == f(x)
     assert f_epyc(y) == f(y)
 
 
-@pytest.mark.parametrize(
-    "language",
-    (
-        pytest.param("fortran", marks=[pytest.mark.fortran]),
-        pytest.param("c", marks=[pytest.mark.c]),
-        pytest.param(
-            "python",
-            marks=[
-                pytest.mark.skip(
-                    reason=(
-                        "isinstance is evaluated during translation so Python translation "
-                        "gives wrong results. See #802"
-                    )
-                ),
-                pytest.mark.python,
-            ],
-        ),
-    ),
-)
-def test_type_differentiation(language):
-    def f(a: "int | float"):
-        if isinstance(a, int):
-            return a * 2
-        else:
-            return -a
-
-    f_epyc = epyccel(f, language=language)
+def test_type_differentiation(epyc_epyccel_compile_time_if_mod):
+    f = epyccel_compile_time_if.type_differentiation
+    f_epyc = epyc_epyccel_compile_time_if_mod.type_differentiation
     assert f_epyc(3) == f(3)
     assert isinstance(f_epyc(3), type(f(3)))
     assert f_epyc(4.0) == f(4.0)
