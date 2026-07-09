064aa7b64a39a83b1eaaccc94beddc3640b0e3d0
EmilyBourne <louise.bourne@gmail.com>
2026-07-09 12:40:36 +0200

    Use epyccel_module_with_fallback for test_epyccel_expressions

--- tests/epyccel/test_epyccel_expressions.py
+++ tests/epyccel/test_epyccel_expressions.py
@@ -1,21 +1,28 @@
 # pylint: disable=missing-function-docstring, missing-module-docstring
+import pytest
 import numpy as np
 from numpy import iinfo
 from numpy.random import rand, randint
 
 from pyccel import epyccel
+from modules import epyccel_expressions
+from utilities import epyccel_module_with_fallback
+
+
+@pytest.fixture(scope="module")
+def epyc_epyccel_expressions_mod(language):
+    return epyccel_module_with_fallback(epyccel_expressions, language)
+
+
 
 # Use int32 for Windows compatibility
 min_int = iinfo(np.int32).min
 max_int = iinfo(np.int32).max
 
 
-def test_swap_basic(language):
-    def swp(a: int, b: int):
-        a, b = (b, a)
-        return a, b
-
-    f = epyccel(swp, language=language)
+def test_swap_basic(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.swp
+    f = epyc_epyccel_expressions_mod.swp
     assert f(2, 4) == swp(2, 4)
     assert f(-2, 4) == swp(-2, 4)
     assert f(4, 100) == swp(4, 100)
@@ -24,12 +31,9 @@ def test_swap_basic(language):
     assert f(x, y) == swp(x, y)
 
 
-def test_swap_basic_2(language):
-    def swp(a: int, b: int):
-        a, b = b, a
-        return a, b
-
-    f = epyccel(swp, language=language)
+def test_swap_basic_2(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.swap_basic_2
+    f = epyc_epyccel_expressions_mod.swap_basic_2
     assert f(2, 4) == swp(2, 4)
     assert f(-2, 4) == swp(-2, 4)
     assert f(4, 100) == swp(4, 100)
@@ -38,12 +42,9 @@ def test_swap_basic_2(language):
     assert f(x, y) == swp(x, y)
 
 
-def test_swap_basic_3(language):
-    def swp(a: int, b: int, c: int):
-        a, b, c = b, c, a
-        return a, b, c
-
-    f = epyccel(swp, language=language)
+def test_swap_basic_3(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.swap_basic_3
+    f = epyc_epyccel_expressions_mod.swap_basic_3
     assert f(2, 4, 8) == swp(2, 4, 8)
     assert f(-2, 4, -6) == swp(-2, 4, -6)
     assert f(4, 100, 234) == swp(4, 100, 234)
@@ -53,12 +54,9 @@ def test_swap_basic_3(language):
     assert f(x, y, z) == swp(x, y, z)
 
 
-def test_swap_basic_4(language):
-    def swp(a: int, b: int, c: int):
-        a, b, c = c, b, a  # pylint: disable=self-assigning-variable
-        return a, b, c
-
-    f = epyccel(swp, language=language)
+def test_swap_basic_4(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.swap_basic_4
+    f = epyc_epyccel_expressions_mod.swap_basic_4
     assert f(2, 4, 8) == swp(2, 4, 8)
     assert f(-2, 4, -6) == swp(-2, 4, -6)
     assert f(4, 100, 234) == swp(4, 100, 234)
@@ -68,13 +66,9 @@ def test_swap_basic_4(language):
     assert f(x, y, z) == swp(x, y, z)
 
 
-def test_swap_index_1(language):
-    def swp(a: int, b: int, c: int):
-        l = [a, b, c]
-        l[0], l[1] = l[1], l[0]
-        return l[0], l[1], l[2]
-
-    f = epyccel(swp, language=language)
+def test_swap_index_1(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.swap_index_1
+    f = epyc_epyccel_expressions_mod.swap_index_1
     assert f(2, 4, 8) == swp(2, 4, 8)
     assert f(-2, 4, -6) == swp(-2, 4, -6)
     assert f(4, 100, 234) == swp(4, 100, 234)
@@ -84,24 +78,17 @@ def test_swap_index_1(language):
     assert f(x, y, z) == swp(x, y, z)
 
 
-def test_swap_index_2(language):
-    def swp(i: int, j: int):
-        l = [1, 2, 3]
-        l[i], l[j] = l[j], l[i]
-        return l[0], l[1], l[2]
-
-    f = epyccel(swp, language=language)
+def test_swap_index_2(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.swap_index_2
+    f = epyc_epyccel_expressions_mod.swap_index_2
     assert f(0, 1) == swp(0, 1)
     assert f(1, 0) == swp(1, 0)
     assert f(2, 1) == swp(2, 1)
 
 
-def test_multi_level_swap(language):
-    def swp(a: int, b: int, c: int):
-        d, (b, c) = a, (c, b)
-        return a, b, c, d
-
-    f = epyccel(swp, language=language)
+def test_multi_level_swap(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.multi_level_swap
+    f = epyc_epyccel_expressions_mod.multi_level_swap
     assert f(2, 4, 8) == swp(2, 4, 8)
     assert f(-2, 4, -6) == swp(-2, 4, -6)
     assert f(4, 100, 234) == swp(4, 100, 234)
@@ -111,27 +98,21 @@ def test_multi_level_swap(language):
     assert f(x, y, z) == swp(x, y, z)
 
 
-def test_multi_type_swap(language):
-    def swp(a: float, b: int, c: float, d: int):
-        a, b, c, d = c, d, a, b
-        return a, b, c, d
-
+def test_multi_type_swap(epyc_epyccel_expressions_mod):
+    swp = epyccel_expressions.multi_type_swap
     b = randint(min_int, max_int)
     d = randint(min_int, max_int)
     a = rand() * 100
     c = rand() * 100
 
-    f = epyccel(swp, language=language)
+    f = epyc_epyccel_expressions_mod.multi_type_swap
     assert f(a, b, c, d) == swp(a, b, c, d)
     assert f(-2.0, 4, -6.0, 10) == swp(-2.0, 4, -6.0, 10)
 
 
-def test_tuple_assign(language):
-    def tup_assign(a: int, b: int):
-        c, d = a, a + b
-        return c, d
-
-    f = epyccel(tup_assign, language=language)
+def test_tuple_assign(epyc_epyccel_expressions_mod):
+    tup_assign = epyccel_expressions.tup_assign
+    f = epyc_epyccel_expressions_mod.tup_assign
     assert f(2, 4) == tup_assign(2, 4)
     assert f(-2, 4) == tup_assign(-2, 4)
     assert f(4, 100) == tup_assign(4, 100)
@@ -140,12 +121,9 @@ def test_tuple_assign(language):
     assert f(x, y) == tup_assign(x, y)
 
 
-def test_tuple_assign2(language):
-    def tup_assign(a: int, b: int):
-        a, d = a, a + b  # pylint: disable=self-assigning-variable
-        return a, b, d
-
-    f = epyccel(tup_assign, language=language)
+def test_tuple_assign2(epyc_epyccel_expressions_mod):
+    tup_assign = epyccel_expressions.tuple_assign2
+    f = epyc_epyccel_expressions_mod.tuple_assign2
     assert f(2, 4) == tup_assign(2, 4)
     assert f(-2, 4) == tup_assign(-2, 4)
     assert f(4, 100) == tup_assign(4, 100)
@@ -154,12 +132,9 @@ def test_tuple_assign2(language):
     assert f(x, y) == tup_assign(x, y)
 
 
-def test_tuple_assign3(language):
-    def tup_assign(a: int):
-        a, a = a + 3, a + 5  # pylint: disable=redeclared-assigned-name
-        return a
-
-    f = epyccel(tup_assign, language=language)
+def test_tuple_assign3(epyc_epyccel_expressions_mod):
+    tup_assign = epyccel_expressions.tuple_assign3
+    f = epyc_epyccel_expressions_mod.tuple_assign3
     assert f(2) == tup_assign(2)
     assert f(-2) == tup_assign(-2)
     assert f(40) == tup_assign(40)
