895ccea4870f24ab17080f74d7bbc690c10336da
EmilyBourne <louise.bourne@gmail.com>
2026-07-09 12:51:17 +0200

    Use epyccel_module_with_fallback for test_epyccel_generators

--- tests/epyccel/test_epyccel_generators.py
+++ tests/epyccel/test_epyccel_generators.py
@@ -6,81 +6,76 @@ import pytest
 from numpy.random import randint
 
 from pyccel import epyccel
+from modules import epyccel_generators
+from utilities import epyccel_module_with_fallback
 
 
-def test_sum_range(language):
-    def f(a0: "int[:]"):
-        return sum(a0[i] for i in range(len(a0)))
+@pytest.fixture(scope="module")
+def epyc_epyccel_generators_mod(language):
+    return epyccel_module_with_fallback(epyccel_generators, language)
 
+
+
+
+def test_sum_range(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_range
     n = randint(1, 50)
     x = np.array(randint(100, size=n), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.sum_range
 
     assert f(x) == f_epyc(x)
 
 
-def test_sum_var(language):
-    def f(a: "int[:]"):
-        return sum(ai for ai in a)
-
+def test_sum_var(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_var
     n = randint(1, 50)
     x = np.array(randint(100, size=n), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.sum_var
 
     assert f(x) == f_epyc(x)
 
 
-def test_sum_var2(language):
-    def f(a: "int[:,:]"):
-        return sum(aii for ai in a for aii in ai)
-
+def test_sum_var2(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_var2
     n1 = randint(1, 10)
     n2 = randint(1, 10)
     x = np.array(randint(10, size=(n1, n2)), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.sum_var2
 
     assert f(x) == f_epyc(x)
 
 
-def test_sum_var3(language):
-    def f(a: "int[:,:,:]"):
-        m, n, p = a.shape
-        return sum(a[i, j, k] for i in range(m) for j in range(n) for k in range(p))
-
+def test_sum_var3(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_var3
     n1 = randint(1, 10)
     n2 = randint(1, 10)
     n3 = randint(1, 10)
     x = np.array(randint(10, size=(n1, n2, n3)), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.sum_var3
 
     assert f(x) == f_epyc(x)
 
 
-def test_sum_var4(language):
-    def f(a: "int[:]"):
-        s = 3
-        return sum(ai for ai in a), s
-
+def test_sum_var4(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_var4
     n = randint(1, 50)
     x = np.array(randint(100, size=n), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.sum_var4
 
     assert f(x) == f_epyc(x)
 
 
-def test_sum_var5(language):
-    def f(a: "bool[:]"):
-        return sum(ai for ai in a)
-
+def test_sum_var5(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_var5
     n = randint(1, 50)
     x = np.ones(n, dtype=bool)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.sum_var5
 
     assert f(x) == f_epyc(x)
 
@@ -133,199 +128,130 @@ def test_min(language):
     assert f() == f_epyc()
 
 
-def test_expression1(language):
-    def f(b: "float[:]"):
-        n = b.shape[0]
-        return (2 * sum(b[i] for i in range(n)) ** 5 + 5) * min(
-            j + 1.0 for j in b
-        ) ** 4 + 9 * max(j + 1.0 for j in b) ** 4
-
+def test_expression1(epyc_epyccel_generators_mod):
+    f = epyccel_generators.expression1
     n = randint(1, 10)
     x = np.array(randint(100, size=n), dtype=float)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.expression1
 
     assert np.isclose(f(x), f_epyc(x), rtol=1e-14, atol=1e-14)
 
 
-def test_expression2(language):
-    def f(b: "int64[:]"):
-        def incr(x: "int64"):
-            y = x + 1
-            return y
-
-        n = b.shape[0]
-        return 5 + incr(2 + incr(6 + sum(b[i] for i in range(n))))
-
+def test_expression2(epyc_epyccel_generators_mod):
+    f = epyccel_generators.expression2
     n = randint(1, 10)
     x = randint(100, size=n).astype(np.int64)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.expression2
 
     assert f(x) == f_epyc(x)
 
 
-def test_nested_generators1(language):
-    def f(a: "float[:,:,:,:]"):
-        return sum(
-            sum(sum(a[i, k, o, 2] for i in range(5)) for k in range(5))
-            for o in range(5)
-        )
-
+def test_nested_generators1(epyc_epyccel_generators_mod):
+    f = epyccel_generators.nested_generators1
     x = randint(0, 50, size=(5, 5, 5, 5)).astype(float)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.nested_generators1
 
     assert f(x) == f_epyc(x)
 
 
-def test_nested_generators2(language):
-    def f(a: "float[:,:,:,:]"):
-        return min(
-            min(
-                sum(
-                    min(max(a[i, k, o, l] * l for i in range(5)) for k in range(5))
-                    for o in range(5)
-                )
-                for l in range(5)
-            ),
-            0.0,
-        )
-
+def test_nested_generators2(epyc_epyccel_generators_mod):
+    f = epyccel_generators.nested_generators2
     x = randint(0, 50, size=(5, 5, 5, 5)).astype(float)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.nested_generators2
 
     assert f(x) == f_epyc(x)
 
 
-def test_nested_generators3(language):
-    def f(a: "float[:,:,:,:]"):
-        return sum(sum(a[i, k, 4, 2] for i in range(5)) for k in range(5)) ** 2
-
+def test_nested_generators3(epyc_epyccel_generators_mod):
+    f = epyccel_generators.nested_generators3
     x = randint(0, 10, size=(5, 5, 5, 5)).astype(float)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.nested_generators3
 
     assert f(x) == f_epyc(x)
 
 
-def test_nested_generators4(language):
-    def f(a: "float[:,:,:,:]"):
-        return min(max(a[i, k, 4, 2] for i in range(5)) for k in range(5)) ** 2
-
+def test_nested_generators4(epyc_epyccel_generators_mod):
+    f = epyccel_generators.nested_generators4
     x = randint(0, 10, size=(5, 5, 5, 5)).astype(float)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.nested_generators4
 
     assert f(x) == f_epyc(x)
 
 
-def test_sum_range_overwrite(language):
-    def f(a0: "int[:]"):
-        v = sum(a0[i] for i in range(len(a0)))
-        v = sum(a0[i] for i in range(len(a0)))
-        return v
-
+def test_sum_range_overwrite(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_range_overwrite
     n = randint(1, 50)
     x = np.array(randint(100, size=n), dtype=int)
 
-    f_epyc = epyccel(f, language=language)
+    f_epyc = epyc_epyccel_generators_mod.sum_range_overwrite
 
     assert f(x) == f_epyc(x)
 
 
-def test_sum_with_condition(language):
-    def f():
-        v = sum(i for i in range(20) if i % 2 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_sum_with_condition(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_with_condition
+    f_epyc = epyc_epyccel_generators_mod.sum_with_condition
     assert f() == f_epyc()
 
 
-def test_sum_with_multiple_conditions(language):
-    def f():
-        v = sum(i - j for i in range(20) if i % 2 == 1 for j in range(30) if j % 3 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_sum_with_multiple_conditions(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_with_multiple_conditions
+    f_epyc = epyc_epyccel_generators_mod.sum_with_multiple_conditions
     assert f() == f_epyc()
 
 
-def test_max_with_condition(language):
-    def f():
-        v = max(i for i in range(20) if i % 2 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_max_with_condition(epyc_epyccel_generators_mod):
+    f = epyccel_generators.max_with_condition
+    f_epyc = epyc_epyccel_generators_mod.max_with_condition
     assert f() == f_epyc()
 
 
-def test_max_with_condition_float(language):
-    def f():
-        v = max(i / 2 for i in range(20) if i % 2 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_max_with_condition_float(epyc_epyccel_generators_mod):
+    f = epyccel_generators.max_with_condition_float
+    f_epyc = epyc_epyccel_generators_mod.max_with_condition_float
     assert f() == f_epyc()
 
 
-def test_max_with_multiple_conditions(language):
-    def f():
-        v = max(i - j for i in range(20) if i % 2 == 1 for j in range(30) if j % 3 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_max_with_multiple_conditions(epyc_epyccel_generators_mod):
+    f = epyccel_generators.max_with_multiple_conditions
+    f_epyc = epyc_epyccel_generators_mod.max_with_multiple_conditions
     assert f() == f_epyc()
 
 
-def test_min_with_condition(language):
-    def f():
-        v = min(i for i in range(20) if i % 2 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_min_with_condition(epyc_epyccel_generators_mod):
+    f = epyccel_generators.min_with_condition
+    f_epyc = epyc_epyccel_generators_mod.min_with_condition
     assert f() == f_epyc()
 
 
-def test_min_with_condition_float(language):
-    def f():
-        v = min(i / 2 for i in range(20) if i % 2 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_min_with_condition_float(epyc_epyccel_generators_mod):
+    f = epyccel_generators.min_with_condition_float
+    f_epyc = epyc_epyccel_generators_mod.min_with_condition_float
     assert f() == f_epyc()
 
 
-def test_min_with_multiple_conditions(language):
-    def f():
-        v = min(i - j for i in range(20) if i % 2 == 1 for j in range(30) if j % 3 == 1)
-        return v
-
-    f_epyc = epyccel(f, language=language)
+def test_min_with_multiple_conditions(epyc_epyccel_generators_mod):
+    f = epyccel_generators.min_with_multiple_conditions
+    f_epyc = epyc_epyccel_generators_mod.min_with_multiple_conditions
     assert f() == f_epyc()
 
 
-def test_sum_with_two_variables(language):
-    def f():
-        x = sum(i - j for i in range(10) for j in range(7))
-        return x
-
-    f_epyc = epyccel(f, language=language)
+def test_sum_with_two_variables(epyc_epyccel_generators_mod):
+    f = epyccel_generators.sum_with_two_variables
+    f_epyc = epyc_epyccel_generators_mod.sum_with_two_variables
 
     assert f() == f_epyc()
 
 
-def test_min_max_values(language):
-    T = TypeVar("T", "int16[:]", "int32[:]", "int64[:]", "float32[:]", "float64[:]")
-
-    def f(a: T):
-        min_val = min(ai for ai in a)
-        max_val = max(ai for ai in a)
-        return min_val, max_val
-
-    f_epyc = epyccel(f, language=language)
+def test_min_max_values(epyc_epyccel_generators_mod):
+    f = epyccel_generators.min_max_values
+    f_epyc = epyc_epyccel_generators_mod.min_max_values
 
     for dtype in (np.int16, np.int32, np.int64, np.float32, np.float64):
         x = randint(0, 100, size=(5,)).astype(dtype)
