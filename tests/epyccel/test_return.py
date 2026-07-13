45636b43defb36885e7107821aa86caf542aa458
EmilyBourne <louise.bourne@gmail.com>
2026-07-13 10:46:45 +0200

    Use epyccel_module_with_fallback for test_epyccel_return

--- tests/epyccel/test_return.py
+++ tests/epyccel/test_return.py
@@ -3,215 +3,134 @@ import numpy as np
 import pytest
 
 from pyccel import epyccel
+from modules import return_module
+from utilities import epyccel_module_with_fallback
+
+
+@pytest.fixture(scope="module")
+def epyc_return_mod(language):
+    return epyccel_module_with_fallback(return_module, language, flags="-Werror  -Wunused-variable")
+
+
 
 # Most of the tests are currently skipped for LLVM because
 # flang-new does not support most -W* flags, except -Werror
 
 
-def test_single_return_var_assign(language):
-    def single_return_var_assign():
-        y = 3
-        return y
-
-    epyc_single_return_var_assign = epyccel(single_return_var_assign, language=language)
+def test_single_return_var_assign(epyc_return_mod):
+    single_return_var_assign = return_module.single_return_var_assign
+    epyc_single_return_var_assign = epyc_return_mod.single_return_var_assign
     assert epyc_single_return_var_assign() == single_return_var_assign()
 
 
 @pytest.mark.skip_llvm
-def test_assign_vars_return(language):
-    def assign_vars_return(a: "int", b: "int"):
-        c = a + b
-        d = a - b
-        return c + d
-
-    epyc_assign_vars_return = epyccel(
-        assign_vars_return, language=language, flags="-Werror  -Wunused-variable"
-    )
+def test_assign_vars_return(epyc_return_mod):
+    assign_vars_return = return_module.assign_vars_return
+    epyc_assign_vars_return = epyc_return_mod.assign_vars_return
     assert epyc_assign_vars_return(3, 4) == assign_vars_return(3, 4)
 
 
 @pytest.mark.skip_llvm
-def test_sum_in_single_return(language):
-    def sum_in_single_return(a: "int", b: "int"):
-        c = a + b
-        return c
-
-    epyc_sum_in_single_return = epyccel(
-        sum_in_single_return, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_sum_in_single_return(epyc_return_mod):
+    sum_in_single_return = return_module.sum_in_single_return
+    epyc_sum_in_single_return = epyc_return_mod.sum_in_single_return
     assert epyc_sum_in_single_return(7, 2) == sum_in_single_return(7, 2)
 
 
 @pytest.mark.skip_llvm
-def test_return_expr(language):
-    def return_expr(x: "int", y: "int"):
-        return x + y
-
-    epyc_return_expr = epyccel(
-        return_expr, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_return_expr(epyc_return_mod):
+    return_expr = return_module.return_expr
+    epyc_return_expr = epyc_return_mod.return_expr
     assert epyc_return_expr(7, 2) == return_expr(7, 2)
 
 
 @pytest.mark.skip_llvm
-def test_return_single_var(language):
-    def return_single_var(x: "int"):
-        return x
-
-    epyc_return_single_var = epyccel(
-        return_single_var, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_return_single_var(epyc_return_mod):
+    return_single_var = return_module.return_single_var
+    epyc_return_single_var = epyc_return_mod.return_single_var
     assert epyc_return_single_var(7) == return_single_var(7)
 
 
 @pytest.mark.skip_llvm
-def test_return_scalare(language):
-    def return_scalare():
-        return 5
-
-    epyc_return_scalare = epyccel(
-        return_scalare, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_return_scalare(epyc_return_mod):
+    return_scalare = return_module.return_scalare
+    epyc_return_scalare = epyc_return_mod.return_scalare
     assert epyc_return_scalare() == return_scalare()
 
 
 @pytest.mark.skip_llvm
-def test_multi_return_scalare(language):
-    def multi_return_scalare():
-        return 5, 7
-
-    epyc_multi_return_scalare = epyccel(
-        multi_return_scalare, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_multi_return_scalare(epyc_return_mod):
+    multi_return_scalare = return_module.multi_return_scalare
+    epyc_multi_return_scalare = epyc_return_mod.multi_return_scalare
     assert epyc_multi_return_scalare() == multi_return_scalare()
 
 
 @pytest.mark.skip_llvm
-def test_multi_return_vars(language):
-    def multi_return_vars(a: "int", b: "int"):
-        return a, b
-
-    epyc_multi_return_vars = epyccel(
-        multi_return_vars, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_multi_return_vars(epyc_return_mod):
+    multi_return_vars = return_module.multi_return_vars
+    epyc_multi_return_vars = epyc_return_mod.multi_return_vars
     assert epyc_multi_return_vars(7, 2) == multi_return_vars(7, 2)
 
 
 @pytest.mark.skip_llvm
-def test_multi_return_vars_expr(language):
-    def multi_return_vars_expr(a: "int", b: "int"):
-        return (a - b), (a + b)
-
-    epyc_multi_return_vars_expr = epyccel(
-        multi_return_vars_expr, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_multi_return_vars_expr(epyc_return_mod):
+    multi_return_vars_expr = return_module.multi_return_vars_expr
+    epyc_multi_return_vars_expr = epyc_return_mod.multi_return_vars_expr
     assert epyc_multi_return_vars_expr(7, 2) == multi_return_vars_expr(7, 2)
 
 
 @pytest.mark.skip_llvm
-def test_scalare_multi_return_stmts(language):
-    def scalare_multi_return_stmts(a: "int"):
-        a = 7
-        if a:
-            return 1
-        else:
-            return 2
-        a = 4
-        return a
-
-    epyc_scalare_multi_return_stmts = epyccel(
-        scalare_multi_return_stmts, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_scalare_multi_return_stmts(epyc_return_mod):
+    scalare_multi_return_stmts = return_module.scalare_multi_return_stmts
+    epyc_scalare_multi_return_stmts = epyc_return_mod.scalare_multi_return_stmts
     assert epyc_scalare_multi_return_stmts(7) == scalare_multi_return_stmts(7)
 
 
 @pytest.mark.skip_llvm
-def test_create_arr(language):
-    def create_arr(i: int):
-        _ = np.ones(i)
-        return True
-
-    epyc_create_arr = epyccel(
-        create_arr, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_create_arr(epyc_return_mod):
+    create_arr = return_module.create_arr
+    epyc_create_arr = epyc_return_mod.create_arr
     assert epyc_create_arr(7) == create_arr(7)
 
 
 @pytest.mark.skip_llvm
-def test_return_arr_element(language):
-    def return_arr_element(i: int):
-        a = np.ones(i)
-        return a[0]
-
-    epyc_return_arr_element = epyccel(
-        return_arr_element, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_return_arr_element(epyc_return_mod):
+    return_arr_element = return_module.return_arr_element
+    epyc_return_arr_element = epyc_return_mod.return_arr_element
     assert epyc_return_arr_element(7) == return_arr_element(7)
 
 
 @pytest.mark.skip_llvm
-def test_create_multi_arrs(language):
-    def create_multi_arrs(i: int):
-        _ = np.ones(i)
-        _ = np.zeros(i)
-        _ = np.zeros(i)
-        return True
-
-    epyc_create_multi_arrs = epyccel(
-        create_multi_arrs, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_create_multi_arrs(epyc_return_mod):
+    create_multi_arrs = return_module.create_multi_arrs
+    epyc_create_multi_arrs = epyc_return_mod.create_multi_arrs
     assert epyc_create_multi_arrs(7) == create_multi_arrs(7)
 
 
 @pytest.mark.skip_llvm
-def test_expr_arrs_elements(language):
-    def expr_arrs_elements(i: int):
-        a = np.ones(i)
-        b = np.zeros(i)
-        return a[i - 1] + b[i - 1]
-
-    epyc_expr_arrs_elements = epyccel(
-        expr_arrs_elements, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_expr_arrs_elements(epyc_return_mod):
+    expr_arrs_elements = return_module.expr_arrs_elements
+    epyc_expr_arrs_elements = epyc_return_mod.expr_arrs_elements
     assert epyc_expr_arrs_elements(7) == expr_arrs_elements(7)
 
 
 @pytest.mark.skip_llvm
-def test_complex_expr(language):
-    def complex_expr(i: int):
-        a = np.ones(i)
-        return ((4 + 5) / (6 - 3) * a[0]) % (9 - a[1])
-
-    epyc_complex_expr = epyccel(
-        complex_expr, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_complex_expr(epyc_return_mod):
+    complex_expr = return_module.complex_expr
+    epyc_complex_expr = epyc_return_mod.complex_expr
     assert epyc_complex_expr(7) == complex_expr(7)
 
 
 @pytest.mark.skip_llvm
-def test_multi_allocs(language):
-    def multi_allocs(i: int):
-        a = np.ones(i)
-        b = np.ones(i)
-        c = np.ones(i)
-        d = np.ones(i)
-        e = np.ones(i)
-        return ((4 + 5) / (d[0] + e[2]) * c[0]) % (b[2] + a[1]) - 4
-
-    epyc_multi_allocs = epyccel(
-        multi_allocs, language=language, flags="-Werror -Wunused-variable"
-    )
+def test_multi_allocs(epyc_return_mod):
+    multi_allocs = return_module.multi_allocs
+    epyc_multi_allocs = epyc_return_mod.multi_allocs
     assert epyc_multi_allocs(7) == multi_allocs(7)
 
 
-def test_return_nothing(language):
-    def divide_by(a: "float[:]", b: "float"):
-        if abs(b) < 0.1:
-            return
-        for i, ai in enumerate(a):
-            a[i] = ai / b
-
-    epyc_divide_by = epyccel(divide_by, language=language)
+def test_return_nothing(epyc_return_mod):
+    divide_by = return_module.divide_by
+    epyc_divide_by = epyc_return_mod.divide_by
     x = np.ones(5)
     x_copy = x.copy()
     b = 0.01
@@ -224,16 +143,9 @@ def test_return_nothing(language):
     assert np.allclose(x, x_copy, rtol=1e-13, atol=1e-14)
 
 
-def test_return_None(language):
-    def divide_by(
-        a: "float[:]", b: "float"
-    ):  # pylint: disable=inconsistent-return-statements
-        if abs(b) < 0.1:
-            return None
-        for i, ai in enumerate(a):
-            a[i] = ai / b
-
-    epyc_divide_by = epyccel(divide_by, language=language)
+def test_return_None(epyc_return_mod):
+    divide_by = return_module.return_None
+    epyc_divide_by = epyc_return_mod.return_None
     x = np.ones(5)
     x_copy = x.copy()
     b = 0.01
@@ -247,33 +159,16 @@ def test_return_None(language):
 
 
 @pytest.mark.skip_llvm
-def test_arg_arr_element_op(language):
-    def return_mult_arr_arg_element(i: "int", arg: "float[:]"):
-        a = np.ones(i)
-        return a[0] * arg[0]
-
-    def return_add_arr_arg_element(i: "int", arg: "float[:]"):
-        a = np.ones(i)
-        return a[0] + arg[0]
-
-    def return_op_arr_arg_element(i: "int", arg: "float[:]"):
-        a = np.ones(i)
-        return ((a[2] + arg[0]) * arg[2] - 2) / 4 * 2
-
+def test_arg_arr_element_op(epyc_return_mod):
     arr = np.array([1, 2, 3, 4], dtype=float)
-    epyc_return_mult_arr_arg_element = epyccel(
-        return_mult_arr_arg_element,
-        language=language,
-        flags="-Werror -Wunused-variable",
-    )
+    return_mult_arr_arg_element = return_module.return_mult_arr_arg_element
+    epyc_return_mult_arr_arg_element = epyc_return_mod.return_mult_arr_arg_element
     assert epyc_return_mult_arr_arg_element(7, arr) == return_mult_arr_arg_element(
         7, arr
     )
-    epyc_return_add_arr_arg_element = epyccel(
-        return_add_arr_arg_element, language=language, flags="-Werror -Wunused-variable"
-    )
+    return_add_arr_arg_element = return_module.return_add_arr_arg_element
+    epyc_return_add_arr_arg_element = epyc_return_mod.return_add_arr_arg_element
     assert epyc_return_add_arr_arg_element(7, arr) == return_add_arr_arg_element(7, arr)
-    epyc_return_op_arr_arg_element = epyccel(
-        return_op_arr_arg_element, language=language, flags="-Werror -Wunused-variable"
-    )
+    return_op_arr_arg_element = return_module.return_op_arr_arg_element
+    epyc_return_op_arr_arg_element = epyc_return_mod.return_op_arr_arg_element
     assert epyc_return_op_arr_arg_element(7, arr) == return_op_arr_arg_element(7, arr)
