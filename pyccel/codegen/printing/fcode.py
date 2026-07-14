cf53b279fc22371ebc478e05c8e52be6c2848c78
Emily Bourne <louise.bourne@gmail.com>
2026-07-14 11:38:54 +0200

    Remove code associated with deprecated Python/NumPy versions (#2626)
    Remove code associated with deprecated Python/NumPy versions. We support
    Python>=3.10, which means we also require NumPy>=v2.1 (the CHANGELOG is
    retroactively modified to indicate when support for NumPy v1 was dropped,
    even if the supported NumPy version is entirely dictated by the Python version).
    There are still multiple places in the code implementing different logic for some
    older versions. This is not necessary so it is removed.

--- pyccel/codegen/printing/fcode.py
+++ pyccel/codegen/printing/fcode.py
@@ -10,7 +10,6 @@ www.fortran90.org as much as possible."""
 import ast
 import re
 import string
-import sys
 from collections import OrderedDict
 from itertools import chain
 
@@ -3455,12 +3454,7 @@ class FCodePrinter(CodePrinter):
 
     def _print_Assert(self, expr):
         if isinstance(expr.test, LiteralTrue):
-            if sys.version_info < (3, 9):
-                return ""
-            else:
-                return (
-                    "!" + ast.unparse(expr.python_ast) + "\n"
-                )  # pylint: disable=no-member
+            return "!" + ast.unparse(expr.python_ast) + "\n"
         test_code = self._print(expr.test)
         return f"if ( .not. ({test_code})) then\n" "stop 1\n" "end if\n"
 
