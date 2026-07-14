4525868e058f50b8391364f620a2f11f2240fc8b
EmilyBourne <louise.bourne@gmail.com>
2026-07-14 10:42:57 +0200

    Allow kwargs to be passed to epyccel_with_fallback

--- tests/test_utils/epyccel_utilities.py
+++ tests/test_utils/epyccel_utilities.py
@@ -35,21 +35,31 @@ class LazyPerFunctionEpyccel:
     test(s) that use it, rather than every test sharing the module.
     """
 
-    def __init__(self, pymod, language):
+    def __init__(self, pymod, language, epyccel_kwargs=None):
         self._pymod = pymod
         self._language = language
+        self._epyccel_kwargs = epyccel_kwargs or {}
         self._cache = {}
 
     def __getattr__(self, name):
         if name not in self._cache:
             self._cache[name] = epyccel(
-                getattr(self._pymod, name), language=self._language
+                getattr(self._pymod, name),
+                language=self._language,
+                **self._epyccel_kwargs,
             )
         return self._cache[name]
 
+    @property
+    def language(self):
+        """
+        The language we are translating to.
+        """
+        return self._language
+
 
 # ==============================================================================
-def epyccel_module_with_fallback(pymod, language):
+def epyccel_module_with_fallback(pymod, language, **kwargs):
     """
     Translate `pymod` as a whole module in one pass.
 
@@ -59,6 +69,8 @@ def epyccel_module_with_fallback(pymod, language):
     functions on first access.
     """
     try:
-        return epyccel(pymod, language=language)
+        mod = epyccel(pymod, language=language, **kwargs)
     except (PyccelError, ImportError):
-        return LazyPerFunctionEpyccel(pymod, language)
+        return LazyPerFunctionEpyccel(pymod, language, kwargs)
+    mod.language = language
+    return mod
