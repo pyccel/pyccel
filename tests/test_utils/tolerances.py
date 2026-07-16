24545b18e7d73f3d04510f31e1050db6369f3452
EmilyBourne <louise.bourne@gmail.com>
2026-07-16 11:23:11 +0200

    Use np.<dtype> format

--- tests/test_utils/tolerances.py
+++ tests/test_utils/tolerances.py
@@ -4,31 +4,31 @@ import sys
 import numpy as np
 from numpy import finfo, iinfo
 
-min_int8 = iinfo("int8").min
-max_int8 = iinfo("int8").max
+min_int8 = iinfo(np.int8).min
+max_int8 = iinfo(np.int8).max
 
-min_int16 = iinfo("int16").min
-max_int16 = iinfo("int16").max
+min_int16 = iinfo(np.int16).min
+max_int16 = iinfo(np.int16).max
 
 # Use int32 for Windows compatibility
 min_int = iinfo(np.int32).min
 max_int = iinfo(np.int32).max
 
-min_int32 = iinfo("int32").min
-max_int32 = iinfo("int32").max
+min_int32 = iinfo(np.int32).min
+max_int32 = iinfo(np.int32).max
 
-min_int64 = iinfo("int64").min
-max_int64 = iinfo("int64").max
+min_int64 = iinfo(np.int64).min
+max_int64 = iinfo(np.int64).max
 
 min_abs_float = sys.float_info.min
-min_float = finfo("float").min
-max_float = finfo("float").max
+min_float = finfo(float).min
+max_float = finfo(float).max
 
-min_float32 = finfo("float32").min / 2
-max_float32 = finfo("float32").max / 2
+min_float32 = finfo(np.float32).min / 2
+max_float32 = finfo(np.float32).max / 2
 
-min_float64 = finfo("float64").min / 2
-max_float64 = finfo("float64").max / 2
+min_float64 = finfo(np.float64).min / 2
+max_float64 = finfo(np.float64).max / 2
 
 # Relative and absolute tolerances for array comparisons in the form
 # numpy.isclose(a, b, rtol, atol). Windows has larger round-off errors.
