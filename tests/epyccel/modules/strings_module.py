1ead5de95ad563481748cad2a2910ff4680cb88b
EmilyBourne <louise.bourne@gmail.com>
2026-07-17 15:58:00 +0200

    Reduce number of modules

--- tests/epyccel/modules/strings_module.py
+++ tests/epyccel/modules/strings_module.py
@@ -73,3 +73,29 @@ def string_function_call_on_literal():
 
 def string_function_return():
     return str("hello")
+
+
+def str_comp():
+    a = "hello"
+    if a == "world":
+        return 1
+    elif a != "boo":
+        return 2
+    elif a == "hello":
+        return 3
+    else:
+        return 4
+
+
+def str_option_test(option: str):
+    if option == "do this":
+        return 1.0
+    else:
+        return 2.0
+
+
+def string_argument_optional_str_option_test(option: str = None):
+    if option is not None and option == "do this":
+        return 1.0
+    else:
+        return 2.0
--- tests/epyccel/test_strings.py
+++ tests/epyccel/test_strings.py
@@ -2,24 +2,14 @@
 import inspect
 
 import pytest
-from modules import strings, strings_module
+from modules import strings_module
 
 from epyccel_utilities import epyccel_module_with_fallback
 
 
 @pytest.fixture(scope="module")
-def epyc_strings_mod(language):
-    return epyccel_module_with_fallback(strings, language)
-
-
-@pytest.fixture(scope="module")
 def epyc_strings_module(language):
     return epyccel_module_with_fallback(strings_module, language)
-
-
-string_funcs = [
-    f for f in strings_module.__all__ if inspect.isfunction(getattr(strings_module, f))
-]
 
 failing_tests = {
     "concatenate": "C does not support string concatenation",
@@ -28,7 +18,18 @@ failing_tests = {
 }
 
 
-@pytest.mark.parametrize("test_func", string_funcs)
+@pytest.mark.parametrize("test_func", [
+    "one_quote",
+    "two_quote",
+    "three_quote",
+    "return_literal",
+    "concatenate",
+    "concatenate_multiple",
+    "concatenate_expr",
+    "string_function_call",
+    "string_function_call_on_literal",
+    "string_function_return",
+])
 def test_strings(test_func, epyc_strings_module):
     if test_func in failing_tests and epyc_strings_module.language == "c":
         pytest.xfail(failing_tests[test_func])
@@ -43,24 +44,24 @@ def test_strings(test_func, epyc_strings_module):
     assert python_out == pyccel_out
 
 
-def test_string_compare(epyc_strings_mod):
-    str_comp = strings.str_comp
-    f = epyc_strings_mod.str_comp
+def test_string_compare(epyc_strings_module):
+    str_comp = strings_module.str_comp
+    f = epyc_strings_module.str_comp
 
     assert str_comp() == f()
 
 
-def test_string_argument(epyc_strings_mod):
-    str_option_test = strings.str_option_test
-    f = epyc_strings_mod.str_option_test
+def test_string_argument(epyc_strings_module):
+    str_option_test = strings_module.str_option_test
+    f = epyc_strings_module.str_option_test
 
     assert str_option_test("do this") == f("do this")
     assert str_option_test("do that") == f("do that")
 
 
-def test_string_argument_optional(epyc_strings_mod):
-    str_option_test = strings.string_argument_optional_str_option_test
-    f = epyc_strings_mod.string_argument_optional_str_option_test
+def test_string_argument_optional(epyc_strings_module):
+    str_option_test = strings_module.string_argument_optional_str_option_test
+    f = epyc_strings_module.string_argument_optional_str_option_test
 
     assert str_option_test("do this") == f("do this")
     assert str_option_test("do that") == f("do that")
