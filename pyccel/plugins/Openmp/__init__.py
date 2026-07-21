038bcfbfeb215f63f4ff903f67ead8bf90db2940
EmilyBourne <louise.bourne@gmail.com>
2026-07-21 14:59:13 +0200

    Connect plugin

---
+++ pyccel/plugins/Openmp/__init__.py
@@ -0,0 +1,10 @@
+"""
+Hook implementation marker for pyccel plugins.
+
+This module exposes the ``hookimpl`` decorator used to mark functions
+in a plugin module as implementations of pyccel's plugin hooks defined
+in `pyccel.plugins.hookspecs`.
+"""
+
+from . import plugin
+
--- pyccel/plugins/Openmp/plugin.py
+++ pyccel/plugins/Openmp/plugin.py
@@ -3,7 +3,35 @@ Hook implementations for the OpenMP pyccel plugin.
 """
 
 from .. import hookimpl
-from .openmp_4_5 import syntactic
+from . import openmp_4_5
+from . import openmp_5_0
+
+class OpenMPConfig:
+    def __init__(self):
+        self._version = None
+
+    @property
+    def version(self):
+        return self._version
+
+    @version.setter
+    def version(self, v):
+        self._version = v
+        if v == 4.5:
+            mod = openmp_4_5
+        elif v == 5.0:
+            mod = openmp_5_0
+        else:
+            raise RuntimeError("Unsupported version")
+
+        self.syntactic = mod.syntactic
+        self.semantic = mod.semantic
+        self.fcode = mod.fcode
+        self.ccode = mod.ccode
+        self.pycode = mod.pycode
+
+openmp_config = OpenMPConfig()
+
 
 @hookimpl
 def get_description():
@@ -58,11 +86,28 @@ def read_cli_arguments(kwargs: dict):
     kwargs : dict
         The keyword arguments passed to the pipeline.
     """
-    # TODO
-    kwargs.pop('omp_version')
+    openmp_config.version = kwargs.pop('omp_version')
+    kwargs.setdefault("accelerators", []).append("openmp")
 
 
 @hookimpl
 def get_updated_syntactic_methods():
-    return [getattr(syntactic, n) for n in syntactic.__all__]
+    return [getattr(openmp_config.syntactic, n) for n in openmp_config.syntactic.__all__]
 
+
+
+@hookimpl
+def get_updated_semantic_methods():
+    return [getattr(openmp_config.semantic, n) for n in openmp_config.semantic.__all__]
+
+
+@hookimpl
+def get_updated_codegen_methods(language : str):
+    if language == 'fortran':
+        return [getattr(openmp_config.fcode, n) for n in openmp_config.fcode.__all__]
+    elif language == 'c':
+        return [getattr(openmp_config.ccode, n) for n in openmp_config.ccode.__all__]
+    elif language == 'python':
+        return [getattr(openmp_config.pycode, n) for n in openmp_config.pycode.__all__]
+    else:
+        return []
