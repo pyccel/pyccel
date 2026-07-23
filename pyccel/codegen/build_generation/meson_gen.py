316ffa617b0176f5c54fb5d0eaa184c42bc5c756
Emily Bourne <louise.bourne@gmail.com>
2026-07-23 21:34:30 +0200

    GNU specific fix

--- pyccel/codegen/build_generation/meson_gen.py
+++ pyccel/codegen/build_generation/meson_gen.py
@@ -290,7 +290,7 @@ class MesonHandler(BuildSystemHandler):
         ]
         if self._verbose > 1:
             print(" ".join(setup_cmd))
-        if sys.platform == "win32":
+        if sys.platform == "win32" and self._compiler.compiler_family == "GNU":
             setup_cmd += ["-Dfortran_link_args=-static-libgfortran"]
 
         env = os.environ.copy()
