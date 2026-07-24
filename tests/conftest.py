24cdedb7545bd4523f94c5d40b0ce6096ea64878
EmilyBourne <louise.bourne@gmail.com>
2026-07-24 17:37:01 +0200

    xdist-thread-specific pyccel clean can always be run

--- tests/conftest.py
+++ tests/conftest.py
@@ -116,21 +116,16 @@ def pytest_runtest_teardown(item, nextitem):
     move_coverage(path_dir)
 
     config = item.config
-    xdist_plugin = config.pluginmanager.getplugin("xdist")
-    if (
-        xdist_plugin is None
-        or "PYTEST_XDIST_WORKER_COUNT" not in os.environ
-        or os.getenv("PYTEST_XDIST_WORKER_COUNT") == 1
-    ):
-        marks = [m.name for m in item.own_markers]
-        if "mpi" not in marks:
+    marks = [m.name for m in item.own_markers]
+    if "mpi" not in marks:
+        pyccel_clean(path_dir, remove_shared_libs=True)
+    else:
+        comm = MPI.COMM_WORLD
+        comm.Barrier()
+        if comm.rank == 0:
             pyccel_clean(path_dir, remove_shared_libs=True)
-        else:
-            comm = MPI.COMM_WORLD
-            comm.Barrier()
-            if comm.rank == 0:
-                pyccel_clean(path_dir, remove_shared_libs=True)
-            comm.Barrier()
+            pass
+        comm.Barrier()
 
 
 def pytest_addoption(parser):
@@ -155,11 +150,10 @@ def pytest_sessionstart(session):
     path_dir = os.path.dirname(os.path.realpath(__file__))
 
     config = session.config
-    xdist_plugin = config.pluginmanager.getplugin("xdist")
-    if xdist_plugin is None:
-        marks = [m.name for m in session.own_markers]
-        if "mpi" not in marks:
-            pyccel_clean(path_dir)
+    marks = [m.name for m in session.own_markers]
+    if "mpi" not in marks:
+        pyccel_clean(path_dir)
+        pass
 
 
 def pytest_runtest_setup(item):
