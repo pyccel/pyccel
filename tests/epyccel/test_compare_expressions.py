daeb606500e9be568d03aed061ccbf0ec15ae5f8
EmilyBourne <louise.bourne@gmail.com>
2026-08-06 17:35:40 +0200

    Fix comparison

--- tests/epyccel/test_compare_expressions.py
+++ tests/epyccel/test_compare_expressions.py
@@ -16,49 +16,49 @@ def test_mod_eq_pow(epyc_compare_expressions_mod):
     f = compare_expressions.mod_eq_pow
     epyc_f = epyc_compare_expressions_mod.mod_eq_pow
     # True
-    np.equal(f(10, 3, 1), epyc_f(10, 3, 1))
-    np.equal(f(19, 10, 3), epyc_f(19, 10, 3))
-    np.equal(f(21, 3, 0), epyc_f(21, 3, 0))
+    assert f(10, 3, 1) == epyc_f(10, 3, 1)
+    assert f(19, 10, 3) == epyc_f(19, 10, 3)
+    assert f(21, 3, 0) == epyc_f(21, 3, 0)
     # False
-    np.equal(f(10, 5, 2), epyc_f(10, 5, 2))
-    np.equal(f(19, 10, 1), epyc_f(19, 10, 1))
-    np.equal(f(21, 3, 1), epyc_f(21, 3, 1))
+    assert f(10, 5, 2) == epyc_f(10, 5, 2)
+    assert f(19, 10, 1) == epyc_f(19, 10, 1)
+    assert f(21, 3, 1) == epyc_f(21, 3, 1)
 
 
 def test_mod_neq_pow(epyc_compare_expressions_mod):
     f = compare_expressions.mod_neq_pow
     epyc_f = epyc_compare_expressions_mod.mod_neq_pow
     # True
-    np.equal(f(10, 5, 2), epyc_f(10, 5, 2))
-    np.equal(f(19, 10, 1), epyc_f(19, 10, 1))
-    np.equal(f(21, 3, 1), epyc_f(21, 3, 1))
+    assert f(10, 5, 2) == epyc_f(10, 5, 2)
+    assert f(19, 10, 1) == epyc_f(19, 10, 1)
+    assert f(21, 3, 1) == epyc_f(21, 3, 1)
     # False
-    np.equal(f(10, 3, 1), epyc_f(10, 3, 1))
-    np.equal(f(19, 10, 3), epyc_f(19, 10, 3))
-    np.equal(f(21, 3, 0), epyc_f(21, 3, 0))
+    assert f(10, 3, 1) == epyc_f(10, 3, 1)
+    assert f(19, 10, 3) == epyc_f(19, 10, 3)
+    assert f(21, 3, 0) == epyc_f(21, 3, 0)
 
 
 def test_idiv_gt_add(epyc_compare_expressions_mod):
     f = compare_expressions.idiv_gt_add
     epyc_f = epyc_compare_expressions_mod.idiv_gt_add
     # True
-    np.equal(f(10, 3, 2), epyc_f(10, 3, 2))
-    np.equal(f(8, 2, 2), epyc_f(8, 2, 2))
-    np.equal(f(16, 3, 4), epyc_f(16, 3, 4))
+    assert f(10, 3, 2) == epyc_f(10, 3, 2)
+    assert f(8, 2, 2) == epyc_f(8, 2, 2)
+    assert f(16, 3, 4) == epyc_f(16, 3, 4)
     # False
-    np.equal(f(10, 3, 2), epyc_f(10, 3, 2))
-    np.equal(f(8, 2, 3), epyc_f(8, 2, 3))
-    np.equal(f(16, 3, 5), epyc_f(16, 3, 5))
+    assert f(10, 3, 2) == epyc_f(10, 3, 2)
+    assert f(8, 2, 3) == epyc_f(8, 2, 3)
+    assert f(16, 3, 5) == epyc_f(16, 3, 5)
 
 
 def test_in_interval(epyc_compare_expressions_mod):
     f = compare_expressions.in_interval
     epyc_f = epyc_compare_expressions_mod.in_interval
     # True
-    np.equal(f(0.0, 1.0, 2.0), epyc_f(0.0, 1.0, 2.0))
-    np.equal(f(-2.0, -1.0, 2.0), epyc_f(-2.0, -1.0, 2.0))
-    np.equal(f(-2.0, -1.3, -1.0), epyc_f(-2.0, -1.3, -1.0))
+    assert f(0.0, 1.0, 2.0) == epyc_f(0.0, 1.0, 2.0)
+    assert f(-2.0, -1.0, 2.0) == epyc_f(-2.0, -1.0, 2.0)
+    assert f(-2.0, -1.3, -1.0) == epyc_f(-2.0, -1.3, -1.0)
     # False
-    np.equal(f(0.0, 10.0, 2.0), epyc_f(0.0, 10.0, 2.0))
-    np.equal(f(-2.0, -10.0, 2.0), epyc_f(-2.0, -10.0, 2.0))
-    np.equal(f(-2.0, -0.3, -1.0), epyc_f(-2.0, -0.3, -1.0))
+    assert f(0.0, 10.0, 2.0) == epyc_f(0.0, 10.0, 2.0)
+    assert f(-2.0, -10.0, 2.0) == epyc_f(-2.0, -10.0, 2.0)
+    assert f(-2.0, -0.3, -1.0) == epyc_f(-2.0, -0.3, -1.0)
